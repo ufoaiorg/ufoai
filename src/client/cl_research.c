bb93694697f058e7807d1e71a2451fb81e502610
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-01-24 21:39:13 +0000

    add some notes about the new flags

--- src/client/cl_research.c
+++ src/client/cl_research.c
@@ -3,6 +3,13 @@
 #include "client.h"
 #include "cl_research.h"
 
+
+/* 
+Note: 
+ObjDef_t in q_shared.h holds the researchNeeded and researchDone flags
+cl_actor.c: We need to check wheter the flags are set before shooting or reloading
+*/
+
 /*======================
 MN_ResearchInit
 ======================*/
