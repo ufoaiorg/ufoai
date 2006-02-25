9849796a634d1b8b07374e13e4390a4f15096a2b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-02-25 14:57:32 +0000

    automatic mapcompiler written in perl under windows grab activeperl and call via perl compile.pl

---
+++ base/maps/compile.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+use strict;
+
+my $extra = "";
+
+sub readDir
+{
+	my $dir = shift || die "No dir given in sub readDir\n";
+	my $status = opendir (DIR, "$dir");
+	my @files = ();
+	unless ( $status )
+	{
+ 		print "Could not read $dir\n";
+ 		return ();
+	}
+
+	@files = readdir ( DIR );
+	closedir (DIR);
+	@files;
+}
+
+sub compile
+{
+	my $dir = shift || die "No dir given in sub compile\n";
+	my $found = 0;
+	print "...entering $dir\n";
+	foreach ( readDir( $dir ) )
+	{
+		next if $_ =~ /^\.|(CVS)/;
+		if ( -d $_ )
+		{
+			print "...found dir $_\n";
+			$found += compile("$dir/$_");
+ 			print "...dir $dir/$_ finished\n";
+			next;
+		}
+		next unless $_ =~ /\.map$/;
+		$_ =~ s/\.map$//;
+		unless ( -e "$_.bsp" )
+		{
+			print "..found $dir/$_\n";
+			system("qbsp3 $dir/$_.map");
+			system("qrad3 $extra $dir/$_.bsp");
+			$found++;
+		}
+		else
+		{
+			print "..already compiled $_\n";
+		}
+	}
+	return $found;
+}
+
+#read the given dir
+my $dir = $ARGV[0] || ".";
+print "=====================================================\n"
+print "Mapcompiler for UFO:AI (http://sf.net/projects/ufoai)\n";
+print "Giving base/maps as parameter or start from base/maps\n";
+print "will compile all maps were no bsp-file exists\n";
+print "Keep in mind that qbsp3 and qrad3 needs to be in path\n"
+print "=====================================================\n"
+
+die "Dir $dir does not exists\n" if ( !-e $dir );
+
+
+print "Found ". compile($dir) ." maps\n";
+
+print "...finished\n"
