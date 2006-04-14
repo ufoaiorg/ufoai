be00519254ef7ba83da42d9f1d1282452eebb567
Werner Höhrer <hoehrer@users.sourceforge.net>
2006-04-14 09:21:27 +0000

    * Some code cleanup to reduce compiler warnings: ** Fixed usage of "ioctlsocket" -> changed "qboolean" and "int" to "u_long" for _true variable .. i hope this will not cause any problems.

--- src/win32/net_wins.c
+++ src/win32/net_wins.c
@@ -486,7 +486,7 @@ int NET_IPSocket (char *net_interface, int port)
 {
 	int					newsocket;
 	struct sockaddr_in	address;
-	qboolean			_true = true;
+	u_long			_true = true;
 	int					i = 1;
 	int					err;
 
@@ -596,7 +596,7 @@ int NET_IPXSocket (int port)
 {
 	int					newsocket;
 	struct sockaddr_ipx	address;
-	int					_true = 1;
+	u_long				_true = 1;
 	int					err;
 
 	if ((newsocket = socket (PF_IPX, SOCK_DGRAM, NSPROTO_IPX)) == -1)
