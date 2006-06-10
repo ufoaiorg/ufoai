2d33ed26c1b5be037e3d2eea171d6eaf3f1b8398
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-06-10 19:29:56 +0000

    * comments

--- src/client/cl_global.h
+++ src/client/cl_global.h
@@ -23,50 +23,48 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "cl_basemanagement.h"
 
 
-/* ============================ */
-/* globalData_t gd */
-/* */
-/* This struct/variable holds the global data for a game. */
-/* */
-/* Content */
-/* * This struct will be save directly to the savegame-file. */
-/* * Everything that should be saved needs to be in here. */
-/* * No pointers are allowed inside this struct. Use an index of an array instead. */
-/* ============================ */
-/* TODO: Implement me */
-/* ============================ */
+/* ============================
+globalData_t gd
+
+This struct/variable holds the global data for a game.
+
+Content
+This struct will be save directly to the savegame-file.
+Everything that should be saved needs to be in here.
+No pointers are allowed inside this struct. Use an index of an array instead.
+*/
 
 typedef struct globalData_s
 {
-	/* == technolgies == */
-	/* A list of all research-topics resp. the research-tree. */
+	/* == technolgies ==
+	A list of all research-topics resp. the research-tree. */
 	technology_t	technologies[MAX_TECHNOLOGIES];
 	/* Total nubmer of technologies. */
 	int	numTechnologies;
 
-	/* == pedia == */
-	/* A list of all Ufopedia chapters. */
+	/* == pedia ==
+	A list of all Ufopedia chapters. */
 	pediaChapter_t	upChapters[MAX_PEDIACHAPTERS];
-	/* Total number uf upchapters, */
+	/* Total number uf upchapters */
 	int numChapters;
 
-	/* == employees == */
-	/* A list of all phalanx employees (soldiers, scies, workers, etc...). */
+	/* == employees ==
+	A list of all phalanx employees (soldiers, scies, workers, etc...) */
 	employee_t	employees[MAX_EMPLOYEES];
 	/* Total number of employees. */
 	int   numEmployees;
 
-	/* == bases == */
-	/* A list of _all_ bases ... even unbuilt ones. */
+	/* == bases ==
+	A list of _all_ bases ... even unbuilt ones. */
 	base_t	bases[MAX_BASES];
-	
+
 	/* Total number of parsed base-names. */
 	int	numBaseNames;
 	/* Total number of built bases (how many are enabled). */
 	int	numBases;
 
-	/* == buildings in bases == */
-	/* TODO: A list of all possible unique buldings. */
+	/* == buildings in bases ==
+	TODO: A list of all possible unique buldings. */
 	building_t buildingTypes[MAX_BUILDINGS];
 	int numBuildingTypes;
 
@@ -74,7 +72,7 @@ typedef struct globalData_s
 	building_t buildings[MAX_BASES][MAX_BUILDINGS];
 	/* TODO: Total number of buildings per base. */
 	int numBuildings[MAX_BASES];
-	
+
 	/* TODO
 	building_t	buildings[MAX_BASES][MAX_BUILDINGS];
 	int numBuildings;
@@ -96,6 +94,3 @@ typedef struct globalData_s
 extern globalData_t	gd;
 
 globalData_t	gd;
-
-/* TODO: Implement the saving of the initial parsed data. */
-/*globalData_t	gd_on_startup; */
