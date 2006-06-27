6e73e054ecee1eff4324b7ec13554b0344ab43fd
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-06-27 12:29:53 +0000

    * campaign_editor code

---
+++ src/tools/campaign_editor/src/callbacks.c
@@ -0,0 +1,34 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_beenden1_activate                   (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_info1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+mission_save                           (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ src/tools/campaign_editor/src/callbacks.h
@@ -0,0 +1,14 @@
+#include <gtk/gtk.h>
+
+
+void
+on_beenden1_activate                   (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+on_info1_activate                      (GtkMenuItem     *menuitem,
+                                        gpointer         user_data);
+
+void
+mission_save                           (GtkButton       *button,
+                                        gpointer         user_data);
