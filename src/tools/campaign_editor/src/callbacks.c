1eb6994906045a03cfd1482ea873c85d018dd36b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-06-29 11:18:45 +0000

    * gpl, doxygen * reformatting * textfields are editable

--- src/tools/campaign_editor/src/callbacks.c
+++ src/tools/campaign_editor/src/callbacks.c
@@ -1,3 +1,28 @@
+/**
+ * @file callbacks.c
+ * @brief
+ */
+
+/*
+Copyright (C) 2002-2006 UFO: Alien Invasion team.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
 #endif
@@ -17,33 +42,34 @@ void on_quit_activate (GtkMenuItem *menuitem, gpointer user_data)
 
 void on_info_activate (GtkMenuItem *menuitem, gpointer user_data)
 {
-  create_about_box();
+	create_about_box();
 }
 
 void mission_save (GtkButton *button, gpointer user_data)
 {
-  gtk_widget_hide( mission_dialog );
-  gtk_widget_show( mis_txt );
+	gtk_widget_hide( mission_dialog );
+	gtk_widget_show( mis_txt );
+	/* TODO: put the mission text into the text field of mis_txt */
 }
 
 void button_press_event (GtkWidget *widget, GdkEventButton *event)
 {
-  float xf, yf;
+	float xf, yf;
 
-  xf = (0.0f-(float)event->x / 2048 + 0.5) * 360.0;
-  yf = (0.0f-(float)event->x / 1024 + 0.5) * 180.0;
+	xf = (0.0f-(float)event->x / 2048 + 0.5) * 360.0;
+	yf = (0.0f-(float)event->x / 1024 + 0.5) * 180.0;
 
-  while (xf > 180.0)
-    xf -= 360.0;
-  while (yf < -180.0)
-    yf += 360.0;
+	while (xf > 180.0)
+		xf -= 360.0;
+	while (yf < -180.0)
+		yf += 360.0;
 
-  x = xf;
-  y = yf;
+	x = xf;
+	y = yf;
 
-  gtk_widget_show (mission_dialog);
+	gtk_widget_show (mission_dialog);
 
-//  g_print("click: long: %i - lat: %i, %i\n", x, y, event->state);
+/*	g_print("click: long: %i - lat: %i, %i\n", x, y, event->state);*/
 }
 
 void motion_notify_event (GtkWidget *widget, GdkEventMotion *event)
@@ -52,10 +78,10 @@ void motion_notify_event (GtkWidget *widget, GdkEventMotion *event)
 
 void button_mission_dialog_cancel (GtkWidget *widget, GdkEventButton *event)
 {
-  gtk_widget_hide( mission_dialog );
+	gtk_widget_hide( mission_dialog );
 }
 
 void button_mis_txt_cancel (GtkWidget *widget, GdkEventButton *event)
 {
-  gtk_widget_hide( mis_txt );
+	gtk_widget_hide( mis_txt );
 }
--- src/tools/campaign_editor/src/main.c
+++ src/tools/campaign_editor/src/main.c
@@ -3,6 +3,26 @@
  * @brief Main file for UFO:AI campaign editor
  */
 
+/*
+Copyright (C) 2002-2006 UFO: Alien Invasion team.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
 #endif
@@ -15,22 +35,29 @@
 int main (int argc, char *argv[])
 {
 #ifdef ENABLE_NLS
-  bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
-  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-  textdomain (GETTEXT_PACKAGE);
+	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
 #endif
 
-  gtk_set_locale ();
-  gtk_init (&argc, &argv);
+	gtk_set_locale ();
+	gtk_init (&argc, &argv);
 
-  add_pixmap_directory ("base/pics/menu");
-  add_pixmap_directory (PACKAGE_DATA_DIR "/" PACKAGE "/pixmaps");
+	/* add ufo base dir to pixmaps searchpath */
+	add_pixmap_directory ("base/pics/menu");
+	/* system pixmaps dir */
+	add_pixmap_directory (PACKAGE_DATA_DIR "/" PACKAGE "/pixmaps");
 
-  campaign_editor = create_campaign_editor ();
-  gtk_widget_show (campaign_editor);
-  mis_txt = create_mis_txt();
-  mission_dialog = create_mission_dialog ();
-  gtk_main ();
-  return 0;
+	/* first visible window */
+	campaign_editor = create_campaign_editor ();
+	gtk_widget_show (campaign_editor);
+
+	/* create two more dialogs, but don't show them */
+	mis_txt = create_mis_txt();
+	mission_dialog = create_mission_dialog ();
+
+	/* enter main loop */
+	gtk_main ();
+	return 0;
 }
 
