552077615cfa6285d5e9dd126bfc47c77beb6e8a
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-07-02 13:59:27 +0000

    * coding guidelines * snprintf => Com_sprintf * strcpy => Q_strncpyz * strcmp => Q_strcmp

--- src/macosx/q_shosx.c
+++ src/macosx/q_shosx.c
@@ -57,15 +57,13 @@ char* strlwr (char *s)
 
 /*_____________________________________________________________________________________________________Hunk_Begin() */
 
-void	*Hunk_Begin (int theMaxSize)
+void* Hunk_Begin (int theMaxSize)
 {
     maxhunksize = theMaxSize + sizeof (int);
     curhunksize = 0;
     membase = malloc (maxhunksize);
     if (membase == NULL)
-    {
         Sys_Error ("unable to virtual allocate %d bytes", theMaxSize);
-    }
     
     *((int *) membase) = curhunksize;
 
@@ -74,15 +72,13 @@ void	*Hunk_Begin (int theMaxSize)
 
 /*_____________________________________________________________________________________________________Hunk_Alloc() */
 
-void *	Hunk_Alloc (int theSize)
+void* Hunk_Alloc (int theSize)
 {
     unsigned char	*myMemory;
 
     theSize = (theSize + 31) & ~31;
     if (curhunksize + theSize > maxhunksize)
-    {
         Sys_Error ("Hunk_Alloc overflow");
-    }
     myMemory = membase + sizeof(int) + curhunksize;
     curhunksize += theSize;
     
@@ -91,12 +87,11 @@ void *	Hunk_Alloc (int theSize)
 
 /*______________________________________________________________________________________________________Hunk_Free() */
 
-void	Hunk_Free (void *theBuffer)
+void Hunk_Free (void *theBuffer)
 {
     unsigned char 	*myMemory;
 
-    if (theBuffer != NULL)
-    {
+    if (theBuffer != NULL) {
         myMemory = ((unsigned char *) theBuffer) - sizeof (int);
         free (myMemory);
     }
@@ -104,14 +99,14 @@ void	Hunk_Free (void *theBuffer)
 
 /*_______________________________________________________________________________________________________Hunk_End() */
 
-int	Hunk_End (void)
+int Hunk_End (void)
 {
     return (curhunksize);
 }
 
 /*_______________________________________________________________________________________________Sys_Milliseconds() */
 
-int	Sys_Milliseconds (void)
+int Sys_Milliseconds (void)
 {
     struct timeval	myTimeValue;
     struct timezone	myTimeZone;
@@ -119,8 +114,7 @@ int	Sys_Milliseconds (void)
 
     gettimeofday (&myTimeValue, &myTimeZone);
     
-    if (!myStartSeconds)
-    {
+    if (!myStartSeconds) {
         myStartSeconds = myTimeValue.tv_sec;
         return (myTimeValue.tv_usec / 1000);
     }
@@ -132,14 +126,13 @@ int	Sys_Milliseconds (void)
 
 /*______________________________________________________________________________________________________Sys_Mkdir() */
 
-void	Sys_Mkdir (char *thePath)
+void Sys_Mkdir (char *thePath)
 {
-    if (mkdir (thePath, 0777) != -1) return;
+    if (mkdir (thePath, 0777) != -1) 
+	return;
     
     if (errno != EEXIST)
-    {
         Sys_Error ("\"mkdir %s\" failed, reason: \"%s\".", thePath, strerror(errno));
-    }
 }
 
 /*__________________________________________________________________________________________Sys_CompareAttributes() */
@@ -150,115 +143,84 @@ static qboolean Sys_CompareAttributes (char *thePath, char *theName, unsigned th
     char 	myFileName[MAX_OSPATH];
 
     /* . and .. never match */
-    if (strcmp (theName, ".") == 0 || strcmp (theName, "..") == 0)
-    {
+    if (Q_strcmp (theName, ".") == 0 || Q_strcmp (theName, "..") == 0)
         return qfalse;
-    }
 
-    snprintf(myFileName, MAX_OSPATH, "%s/%s", thePath, theName);
+    Com_sprintf(myFileName, MAX_OSPATH, "%s/%s", thePath, theName);
     
     if (stat (myFileName, &myStat) == -1)
-    {
         return (qfalse);
-    }
     
     if ((myStat.st_mode & S_IFDIR) && (theCantHave & SFF_SUBDIR))
-    {
         return (qfalse);
-    }
 
     if ((theMustHave & SFF_SUBDIR) && !(myStat.st_mode & S_IFDIR))
-    {
         return (qfalse);
-    }
 
     return (qtrue);
 }
 
 /*__________________________________________________________________________________________________Sys_FindFirst() */
 
-char	*Sys_FindFirst (char *thePath, unsigned theMustHave, unsigned theCantHave)
+char* Sys_FindFirst (char *thePath, unsigned theMustHave, unsigned theCantHave)
 {
     struct dirent	*myDirEnt;
     char		*myPointer;
 
     if (gSysFindDir != NULL)
-    {
         Sys_Error ("Sys_BeginFind without close");
-    }
 
-    strcpy (gSysFindBase, thePath);
+    Q_strncpyz (gSysFindBase, thePath, MAX_OSPATH);
 
-    if ((myPointer = strrchr (gSysFindBase, '/')) != NULL)
-    {
+    if ((myPointer = strrchr (gSysFindBase, '/')) != NULL) {
         *myPointer = 0;
-        strcpy (gSysFindPattern, myPointer + 1);
-    }
-    else
-    {
-        strcpy (gSysFindPattern, "*");
-    }
+        Q_strncpyz (gSysFindPattern, myPointer + 1, MAX_OSPATH);
+    } else
+        Q_strncpyz (gSysFindPattern, "*", MAX_OSPATH);
 
-    if (strcmp (gSysFindPattern, "*.*") == 0)
-    {
-        strcpy (gSysFindPattern, "*");
-    }
+    if (Q_strcmp (gSysFindPattern, "*.*") == 0)
+        Q_strncpyz (gSysFindPattern, "*", MAX_OSPATH);
     
     if ((gSysFindDir = opendir (gSysFindBase)) == NULL)
-    {
         return (NULL);
-    }
+
     while ((myDirEnt = readdir (gSysFindDir)) != NULL)
-    {
         if (!*gSysFindPattern || glob_match(gSysFindPattern, myDirEnt->d_name))
-        {
-            if (Sys_CompareAttributes (gSysFindBase, myDirEnt->d_name, theMustHave, theCantHave))
-            {
-                snprintf (gSysFindPath, MAX_OSPATH, "%s/%s", gSysFindBase, myDirEnt->d_name);
+            if (Sys_CompareAttributes (gSysFindBase, myDirEnt->d_name, theMustHave, theCantHave)) {
+                Com_sprintf (gSysFindPath, MAX_OSPATH, "%s/%s", gSysFindBase, myDirEnt->d_name);
                 return (gSysFindPath);
             }
-        }
-    }
     
     return (NULL);
 }
 
 /*___________________________________________________________________________________________________Sys_FindNext() */
 
-char *	Sys_FindNext (unsigned theMustHave, unsigned theCantHave)
+char* Sys_FindNext (unsigned theMustHave, unsigned theCantHave)
 {
     struct dirent 	*myDirEnt;
 
     /* just security: */
     if (gSysFindDir == NULL)
-    {
         return (NULL);
-    }
     
     /* find next... */
     while ((myDirEnt = readdir (gSysFindDir)) != NULL)
-    {
         if (!*gSysFindPattern || glob_match (gSysFindPattern, myDirEnt->d_name))
-        {
-            if (Sys_CompareAttributes (gSysFindBase, myDirEnt->d_name, theMustHave, theCantHave))
-            {
-                snprintf (gSysFindPath, MAX_OSPATH, "%s/%s", gSysFindBase, myDirEnt->d_name);
+            if (Sys_CompareAttributes (gSysFindBase, myDirEnt->d_name, theMustHave, theCantHave)) {
+                Com_sprintf (gSysFindPath, MAX_OSPATH, "%s/%s", gSysFindBase, myDirEnt->d_name);
                 return (gSysFindPath);
             }
-        }
-    }
     
     return (NULL);
 }
 
 /*__________________________________________________________________________________________________Sys_FindClose() */
 
-void	Sys_FindClose (void)
+void Sys_FindClose (void)
 {
     if (gSysFindDir != NULL)
-    {
         closedir (gSysFindDir);
-    }
     gSysFindDir = NULL;
 }
 
