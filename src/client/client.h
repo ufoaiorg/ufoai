d1eb56b0c8c957b633bec7b23631c8ff072610c3
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-07-17 08:33:23 +0000

    * removed unused function

--- src/client/client.h
+++ src/client/client.h
@@ -950,7 +950,6 @@ extern vec3_t map_dropship_coord;
 void V_Init(void);
 void V_RenderView(float stereo_separation);
 void V_AddEntity(entity_t * ent);
-void V_AddParticle(vec3_t org, int color, float alpha);
 void V_AddLight(vec3_t org, float intensity, float r, float g, float b);
 void V_AddLightStyle(int style, float r, float g, float b);
 entity_t *V_GetEntity(void);
