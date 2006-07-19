cefadbf8f72c53071f7353c6890295064febaa3f
Eddy Cullen <screamingwns@users.sourceforge.net>
2006-07-19 16:02:13 +0000

    * Added common/debug.h

---
+++ src/common/debug.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2002-2006 UFO: Alien Invasion team.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+ 
+#ifndef COMMON_DEBUG_H
+#define COMMON_DEBUG_H
+
+/* If paranoid is enabled, enable debugging - doesn't make sense otherwise */
+#if defined PARANOID && !defined DEBUG
+#define DEBUG
+#endif
+
+#ifdef DEBUG
+#undef NDEBUG
+#endif /* DEBUG */
+
+#ifndef DEBUG
+#define NDEBUG
+#endif
+
+/* NOTE: Assert MUST be included AFTER the macros defined above.
+ * This is to protect from rogue definitions of NDEBUG.
+ */
+#include <assert.h>
+
+#endif /* COMMON_DEBUG_H */
