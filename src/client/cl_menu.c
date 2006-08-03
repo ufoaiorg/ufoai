3516edee47f39da105fa9c523aa1798d43b7e4e7
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-08-03 16:09:44 +0000

    * coding guidelines

--- src/client/cl_menu.c
+++ src/client/cl_menu.c
@@ -1528,8 +1528,8 @@ void MN_DrawMenus(void)
 									x += node->texh[1];
 								cur = tab;
 							} while (1);
-							
-                            /* the conditional expression at the end is a hack to draw "/n/n" as a blank line */
+
+							/* the conditional expression at the end is a hack to draw "/n/n" as a blank line */
 							y += re.FontDrawString(font, node->align, x, y, node->pos[0], node->pos[1], node->size[0], node->size[1], (*cur ? cur : " \0"));
 
 							if (node->mousefx && line == mouseOver)
