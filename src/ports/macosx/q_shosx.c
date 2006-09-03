aff4260c4848fbafcd7ee92bcdbeac1b93140b7f
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-09-03 21:11:52 +0000

    * coding guidelines

--- src/ports/macosx/q_shosx.c
+++ src/ports/macosx/q_shosx.c
@@ -1,14 +1,28 @@
-/*______________________________________________________________________________________________________________nFO */
-/* "q_shosx.c" - shared functions for the plug-ins. */
+/**
+ * @file q_shosx.c
+ * @brief shared functions for the plug-ins
+ * Written by:	awe				[mailto:awe@fruitz-of-dojo.de]
+ * 2001-2002 Fruitz Of Dojo 	[http://www.fruitz-of-dojo.de]
+ */
 
-/* Written by:	awe				[mailto:awe@fruitz-of-dojo.de]. */
-/*		©2001-2002 Fruitz Of Dojo 	[http://www.fruitz-of-dojo.de]. */
+/*
 
-/* Quake IIª is copyrighted by id software	[http://www.idsoftware.com]. */
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-/* Version History: */
-/* v1.0.0:   Initial release. */
-/*_________________________________________________________________________________________________________iNCLUDES */
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
 
 #pragma mark =Includes=
 
@@ -27,24 +41,20 @@
 
 #pragma mark -
 
-/*________________________________________________________________________________________________________vARIABLES */
-
 #pragma mark =Variables=
 
-byte 		*membase;
-int 		maxhunksize,
-                curhunksize,
-                curtime;
+byte *membase;
+int maxhunksize, curhunksize, curtime;
 
-static char	gSysFindBase[MAX_OSPATH],
-                gSysFindPath[MAX_OSPATH],
-                gSysFindPattern[MAX_OSPATH];
+static char	gSysFindBase[MAX_OSPATH], gSysFindPath[MAX_OSPATH], gSysFindPattern[MAX_OSPATH];
 static	DIR	*gSysFindDir;
 
 #pragma mark -
 
-/*_________________________________________________________________________________________________________strlwr() */
 
+/**
+ * @brief
+ */
 char* strlwr (char *s)
 {
 	char* origs = s;
@@ -55,173 +65,181 @@ char* strlwr (char *s)
 	return origs;
 }
 
-/*_____________________________________________________________________________________________________Hunk_Begin() */
-
+/**
+ * @brief
+ */
 void* Hunk_Begin (int theMaxSize)
 {
-    maxhunksize = theMaxSize + sizeof (int);
-    curhunksize = 0;
-    membase = malloc (maxhunksize);
-    if (membase == NULL)
-        Sys_Error ("unable to virtual allocate %d bytes", theMaxSize);
-    
-    *((int *) membase) = curhunksize;
+	maxhunksize = theMaxSize + sizeof (int);
+	curhunksize = 0;
+	membase = malloc (maxhunksize);
+	if (membase == NULL)
+		Sys_Error ("unable to virtual allocate %d bytes", theMaxSize);
 
-    return (membase + sizeof(int));
+	*((int *) membase) = curhunksize;
+
+	return (membase + sizeof(int));
 }
 
-/*_____________________________________________________________________________________________________Hunk_Alloc() */
-
+/**
+ * @brief
+ */
 void* Hunk_Alloc (int theSize)
 {
-    unsigned char	*myMemory;
+	unsigned char	*myMemory;
 
-    theSize = (theSize + 31) & ~31;
-    if (curhunksize + theSize > maxhunksize)
-        Sys_Error ("Hunk_Alloc overflow");
-    myMemory = membase + sizeof(int) + curhunksize;
-    curhunksize += theSize;
-    
-    return (myMemory);
+	theSize = (theSize + 31) & ~31;
+	if (curhunksize + theSize > maxhunksize)
+		Sys_Error ("Hunk_Alloc overflow");
+	myMemory = membase + sizeof(int) + curhunksize;
+	curhunksize += theSize;
+
+	return (myMemory);
 }
 
-/*______________________________________________________________________________________________________Hunk_Free() */
-
+/**
+ * @brief
+ */
 void Hunk_Free (void *theBuffer)
 {
-    unsigned char 	*myMemory;
+	unsigned char 	*myMemory;
 
-    if (theBuffer != NULL) {
-        myMemory = ((unsigned char *) theBuffer) - sizeof (int);
-        free (myMemory);
-    }
+	if (theBuffer != NULL) {
+		myMemory = ((unsigned char *) theBuffer) - sizeof (int);
+		free (myMemory);
+	}
 }
 
-/*_______________________________________________________________________________________________________Hunk_End() */
-
+/**
+ * @brief
+ */
 int Hunk_End (void)
 {
-    return (curhunksize);
+	return (curhunksize);
 }
 
-/*_______________________________________________________________________________________________Sys_Milliseconds() */
-
+/**
+ * @brief
+ */
 int Sys_Milliseconds (void)
 {
-    struct timeval	myTimeValue;
-    struct timezone	myTimeZone;
-    static int		myStartSeconds;
+	struct timeval	myTimeValue;
+	struct timezone	myTimeZone;
+	static int		myStartSeconds;
 
-    gettimeofday (&myTimeValue, &myTimeZone);
-    
-    if (!myStartSeconds) {
-        myStartSeconds = myTimeValue.tv_sec;
-        return (myTimeValue.tv_usec / 1000);
-    }
+	gettimeofday (&myTimeValue, &myTimeZone);
 
-    curtime = (myTimeValue.tv_sec - myStartSeconds) * 1000 + myTimeValue.tv_usec / 1000;
-    
-    return (curtime);
+	if (!myStartSeconds) {
+		myStartSeconds = myTimeValue.tv_sec;
+		return (myTimeValue.tv_usec / 1000);
+	}
+
+	curtime = (myTimeValue.tv_sec - myStartSeconds) * 1000 + myTimeValue.tv_usec / 1000;
+
+	return (curtime);
 }
 
-/*______________________________________________________________________________________________________Sys_Mkdir() */
-
+/**
+ * @brief
+ */
 void Sys_Mkdir (char *thePath)
 {
-    if (mkdir (thePath, 0777) != -1) 
+	if (mkdir (thePath, 0777) != -1)
 	return;
-    
-    if (errno != EEXIST)
-        Sys_Error ("\"mkdir %s\" failed, reason: \"%s\".", thePath, strerror(errno));
+
+	if (errno != EEXIST)
+		Sys_Error ("\"mkdir %s\" failed, reason: \"%s\".", thePath, strerror(errno));
 }
 
-/*__________________________________________________________________________________________Sys_CompareAttributes() */
-
+/**
+ * @brief
+ */
 static qboolean Sys_CompareAttributes (char *thePath, char *theName, unsigned theMustHave, unsigned theCantHave)
 {
-    struct stat myStat;
-    char 	myFileName[MAX_OSPATH];
+	struct stat myStat;
+	char 	myFileName[MAX_OSPATH];
 
-    /* . and .. never match */
-    if (Q_strcmp (theName, ".") == 0 || Q_strcmp (theName, "..") == 0)
-        return qfalse;
+	/* . and .. never match */
+	if (Q_strcmp (theName, ".") == 0 || Q_strcmp (theName, "..") == 0)
+		return qfalse;
 
-    Com_sprintf(myFileName, MAX_OSPATH, "%s/%s", thePath, theName);
-    
-    if (stat (myFileName, &myStat) == -1)
-        return (qfalse);
-    
-    if ((myStat.st_mode & S_IFDIR) && (theCantHave & SFF_SUBDIR))
-        return (qfalse);
+	Com_sprintf(myFileName, MAX_OSPATH, "%s/%s", thePath, theName);
 
-    if ((theMustHave & SFF_SUBDIR) && !(myStat.st_mode & S_IFDIR))
-        return (qfalse);
+	if (stat (myFileName, &myStat) == -1)
+		return (qfalse);
 
-    return (qtrue);
+	if ((myStat.st_mode & S_IFDIR) && (theCantHave & SFF_SUBDIR))
+		return (qfalse);
+
+	if ((theMustHave & SFF_SUBDIR) && !(myStat.st_mode & S_IFDIR))
+		return (qfalse);
+
+	return (qtrue);
 }
 
-/*__________________________________________________________________________________________________Sys_FindFirst() */
-
+/**
+ * @brief
+ */
 char* Sys_FindFirst (char *thePath, unsigned theMustHave, unsigned theCantHave)
 {
-    struct dirent	*myDirEnt;
-    char		*myPointer;
+	struct dirent	*myDirEnt;
+	char		*myPointer;
 
-    if (gSysFindDir != NULL)
-        Sys_Error ("Sys_BeginFind without close");
+	if (gSysFindDir != NULL)
+		Sys_Error ("Sys_BeginFind without close");
 
-    Q_strncpyz (gSysFindBase, thePath, MAX_OSPATH);
+	Q_strncpyz (gSysFindBase, thePath, MAX_OSPATH);
 
-    if ((myPointer = strrchr (gSysFindBase, '/')) != NULL) {
-        *myPointer = 0;
-        Q_strncpyz (gSysFindPattern, myPointer + 1, MAX_OSPATH);
-    } else
-        Q_strncpyz (gSysFindPattern, "*", MAX_OSPATH);
+	if ((myPointer = strrchr (gSysFindBase, '/')) != NULL) {
+		*myPointer = 0;
+		Q_strncpyz (gSysFindPattern, myPointer + 1, MAX_OSPATH);
+	} else
+		Q_strncpyz (gSysFindPattern, "*", MAX_OSPATH);
 
-    if (Q_strcmp (gSysFindPattern, "*.*") == 0)
-        Q_strncpyz (gSysFindPattern, "*", MAX_OSPATH);
-    
-    if ((gSysFindDir = opendir (gSysFindBase)) == NULL)
-        return (NULL);
+	if (Q_strcmp (gSysFindPattern, "*.*") == 0)
+		Q_strncpyz (gSysFindPattern, "*", MAX_OSPATH);
 
-    while ((myDirEnt = readdir (gSysFindDir)) != NULL)
-        if (!*gSysFindPattern || glob_match(gSysFindPattern, myDirEnt->d_name))
-            if (Sys_CompareAttributes (gSysFindBase, myDirEnt->d_name, theMustHave, theCantHave)) {
-                Com_sprintf (gSysFindPath, MAX_OSPATH, "%s/%s", gSysFindBase, myDirEnt->d_name);
-                return (gSysFindPath);
-            }
-    
-    return (NULL);
+	if ((gSysFindDir = opendir (gSysFindBase)) == NULL)
+		return (NULL);
+
+	while ((myDirEnt = readdir (gSysFindDir)) != NULL)
+		if (!*gSysFindPattern || glob_match(gSysFindPattern, myDirEnt->d_name))
+			if (Sys_CompareAttributes (gSysFindBase, myDirEnt->d_name, theMustHave, theCantHave)) {
+				Com_sprintf (gSysFindPath, MAX_OSPATH, "%s/%s", gSysFindBase, myDirEnt->d_name);
+				return (gSysFindPath);
+			}
+
+	return (NULL);
 }
 
-/*___________________________________________________________________________________________________Sys_FindNext() */
-
+/**
+ * @brief
+ */
 char* Sys_FindNext (unsigned theMustHave, unsigned theCantHave)
 {
-    struct dirent 	*myDirEnt;
+	struct dirent 	*myDirEnt;
 
-    /* just security: */
-    if (gSysFindDir == NULL)
-        return (NULL);
-    
-    /* find next... */
-    while ((myDirEnt = readdir (gSysFindDir)) != NULL)
-        if (!*gSysFindPattern || glob_match (gSysFindPattern, myDirEnt->d_name))
-            if (Sys_CompareAttributes (gSysFindBase, myDirEnt->d_name, theMustHave, theCantHave)) {
-                Com_sprintf (gSysFindPath, MAX_OSPATH, "%s/%s", gSysFindBase, myDirEnt->d_name);
-                return (gSysFindPath);
-            }
-    
-    return (NULL);
+	/* just security: */
+	if (gSysFindDir == NULL)
+		return (NULL);
+
+	/* find next... */
+	while ((myDirEnt = readdir (gSysFindDir)) != NULL)
+		if (!*gSysFindPattern || glob_match (gSysFindPattern, myDirEnt->d_name))
+			if (Sys_CompareAttributes (gSysFindBase, myDirEnt->d_name, theMustHave, theCantHave)) {
+				Com_sprintf (gSysFindPath, MAX_OSPATH, "%s/%s", gSysFindBase, myDirEnt->d_name);
+				return (gSysFindPath);
+			}
+
+	return (NULL);
 }
 
-/*__________________________________________________________________________________________________Sys_FindClose() */
-
+/**
+ * @brief
+ */
 void Sys_FindClose (void)
 {
-    if (gSysFindDir != NULL)
-        closedir (gSysFindDir);
-    gSysFindDir = NULL;
+	if (gSysFindDir != NULL)
+		closedir (gSysFindDir);
+	gSysFindDir = NULL;
 }
-
-/*______________________________________________________________________________________________________________eOF */
