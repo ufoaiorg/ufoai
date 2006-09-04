31699e5bb32af76e06397a1624a6232137043f28
Jose Alonso Cardenas Marquez <acardenas@users.sourceforge.net>
2006-09-04 00:28:16 +0000

    * Fix build for FreeBSD

--- src/ports/linux/snd_sdl.h
+++ src/ports/linux/snd_sdl.h
@@ -1,5 +1,10 @@
+#if defined (__FreeBSD__)
+#include <SDL11/SDL.h>
+#include <SDL11/SDL_version.h>
+#else
 #include <SDL/SDL.h>
 #include <SDL/SDL_version.h>
+#endif
 
 #include "../../client/client.h"
 #include "../../client/snd_loc.h"
