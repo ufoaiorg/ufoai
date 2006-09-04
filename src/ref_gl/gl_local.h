ea3bdf9dfbf9f828c2c84fe9183889d2fc5ad540
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-09-04 05:05:36 +0000

    * fixed compile error

--- src/ref_gl/gl_local.h
+++ src/ref_gl/gl_local.h
@@ -52,7 +52,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <SDL_ttf.h>
 #else
 #include <SDL/SDL.h>
-#include <SDL/SDL_ttf.h
+#include <SDL/SDL_ttf.h>
 #endif
 void GL_ShutdownSDLFonts(void);	/* gl_draw.c */
 
