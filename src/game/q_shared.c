eb735085f1e557936323078101b39ebcdab02526
Mikolaj Konarski <bandobraz@users.sourceforge.net>
2006-09-08 04:20:55 +0000

    * src/game/q_shared.c only first part of armor name is now used for model lookup * base/ufos/equipment_missions.ufo human armors changed to alien armors * base/ufos/research.ufo fake alien armor techs added * base/ufos/campaign.ufo increased the delay before the Japan and assorted missions * base/ufos/armor.ufo defined the alien armors and increased laser resistance on human armors to be in accord with wiki descriptions

--- src/game/q_shared.c
+++ src/game/q_shared.c
@@ -2897,6 +2897,9 @@ static char returnModel[MAX_VAR];
  */
 char *Com_CharGetBody(character_t * chr)
 {
+	char kurz[MAX_VAR];
+	char *underline;
+
 	assert(chr);
 #ifdef DEBUG
 	if (!chr)
@@ -2913,7 +2916,14 @@ char *Com_CharGetBody(character_t * chr)
 	if (chr->inv->c[CSI->idArmor] && chr->fieldSize == ACTOR_SIZE_NORMAL) {
 		assert(!Q_strcmp(CSI->ods[chr->inv->c[CSI->idArmor]->item.t].type, "armor"));
 /*		Com_Printf("Com_CharGetBody: Use '%s' as armor\n", CSI->ods[chr->inv->c[CSI->idArmor]->item.t].kurz);*/
-		Com_sprintf(returnModel, MAX_VAR, "%s%s/%s", chr->path, CSI->ods[chr->inv->c[CSI->idArmor]->item.t].kurz, chr->body);
+
+		/* check for the underline */
+		Q_strncpyz(kurz, CSI->ods[chr->inv->c[CSI->idArmor]->item.t].kurz, MAX_VAR);
+		underline = strchr(kurz, '_');
+		if (underline)
+			*underline = 0;
+
+		Com_sprintf(returnModel, MAX_VAR, "%s%s/%s", chr->path, kurz, chr->body);
 	} else
 		Com_sprintf(returnModel, MAX_VAR, "%s/%s", chr->path, chr->body);
 	return returnModel;
@@ -2926,6 +2936,9 @@ char *Com_CharGetBody(character_t * chr)
  */
 char *Com_CharGetHead(character_t * chr)
 {
+	char kurz[MAX_VAR];
+	char *underline;
+
 	assert(chr);
 #ifdef DEBUG
 	if (!chr)
@@ -2942,7 +2955,14 @@ char *Com_CharGetHead(character_t * chr)
 	if (chr->inv->c[CSI->idArmor] && chr->fieldSize == ACTOR_SIZE_NORMAL) {
 		assert(!Q_strcmp(CSI->ods[chr->inv->c[CSI->idArmor]->item.t].type, "armor"));
 /*		Com_Printf("Com_CharGetHead: Use '%s' as armor\n", CSI->ods[chr->inv->c[CSI->idArmor]->item.t].kurz);*/
-		Com_sprintf(returnModel, MAX_VAR, "%s%s/%s", chr->path, CSI->ods[chr->inv->c[CSI->idArmor]->item.t].kurz, chr->head);
+
+		/* check for the underline */
+		Q_strncpyz(kurz, CSI->ods[chr->inv->c[CSI->idArmor]->item.t].kurz, MAX_VAR);
+		underline = strchr(kurz, '_');
+		if (underline)
+			*underline = 0;
+
+		Com_sprintf(returnModel, MAX_VAR, "%s%s/%s", chr->path, kurz, chr->head);
 	} else
 		Com_sprintf(returnModel, MAX_VAR, "%s/%s", chr->path, chr->head);
 	return returnModel;
