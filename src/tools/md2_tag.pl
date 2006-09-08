867a8ec967673da22e644dd01416714f2b178a79
Werner Höhrer <hoehrer@users.sourceforge.net>
2006-09-08 13:15:19 +0000

    * minor changes

--- src/tools/md2_tag.pl
+++ src/tools/md2_tag.pl
@@ -43,33 +43,16 @@ use constant FORMAT => (
 	OffsetExtractEnd	=> 'I',	# offset
 
 	TagName			=> ['a64', '{$NumTags}', 1 ],	# 64chars * NumTags
-	Data			=> ['f12', '{$NumTags*$NumFrames}', 12 ]	# 12 floats * NumTags * NumFrames
+	TagData			=> ['f12', '{$NumTags*$NumFrames}', 12 ]	# 12 floats * NumTags * NumFrames
 );
 
 package TagName;
 use base 'Parse::Binary';
 use constant FORMAT => ('a64');
 
-package Data;
+package TagData;
 use base 'Parse::Binary';
 use constant FORMAT => ('f12');
-
-#package Data;
-#use base 'Parse::Binary';
-#use constant FORMAT => (
-#	Row1_x => 'f',
-#	Row1_y => 'f',
-#	Row1_z => 'f',
-#	Row2_x => 'f',
-#	Row2_y => 'f',
-#	Row2_z => 'f',
-#	Row3_x => 'f',
-#	Row3_y => 'f',
-#	Row3_z => 'f',
-#	Row4_x => 'f',
-#	Row4_y => 'f',
-#	Row4_z => 'f'
-#);
 
 
 #######################################
@@ -109,7 +92,7 @@ for (my $i = 0; $i < $md2tag_file->NumTags; $i++) {
 
 if ($verbose) {
 	print "Coords:\n";
-	#print Dumper($md2tag_file);
+	print Dumper($md2tag_file);
 	for (my $tags = 0; $tags < $md2tag_file->NumTags; $tags++) {
 		my $name = $md2tag_file->TagName->[$tags][0];
 		$name =~ s/\0*//sg; # Remove all NUL from the string.
@@ -118,21 +101,21 @@ if ($verbose) {
 		for (my $j = $tags * $md2tag_file->NumFrames; $j < ($tags+1) * $md2tag_file->NumFrames; $j++) {
 			$frame++;
 			print "Frame ",$frame," --- ",$name," ----------------------\n";
-			print "Row1 ",$md2tag_file->Data->[$j][0];
-			print "  ",$md2tag_file->Data->[$j][1];
-			print "  ",$md2tag_file->Data->[$j][2];
+			print "Row1 ",$md2tag_file->TagData->[$j][0];
+			print "  ",$md2tag_file->TagData->[$j][1];
+			print "  ",$md2tag_file->TagData->[$j][2];
 			print "\n";
-			print "Row2 ",$md2tag_file->Data->[$j][3];
-			print "  ",$md2tag_file->Data->[$j][4];
-			print "  ",$md2tag_file->Data->[$j][5];
+			print "Row2 ",$md2tag_file->TagData->[$j][3];
+			print "  ",$md2tag_file->TagData->[$j][4];
+			print "  ",$md2tag_file->TagData->[$j][5];
 			print "\n";
-			print "Row3 ",$md2tag_file->Data->[$j][6];
-			print "  ",$md2tag_file->Data->[$j][7];
-			print "  ",$md2tag_file->Data->[$j][8];
+			print "Row3 ",$md2tag_file->TagData->[$j][6];
+			print "  ",$md2tag_file->TagData->[$j][7];
+			print "  ",$md2tag_file->TagData->[$j][8];
 			print "\n";
-			print "Row4 ",$md2tag_file->Data->[$j][9];
-			print "  ",$md2tag_file->Data->[$j][10];
-			print "  ",$md2tag_file->Data->[$j][11];
+			print "Row4 ",$md2tag_file->TagData->[$j][9];
+			print "  ",$md2tag_file->TagData->[$j][10];
+			print "  ",$md2tag_file->TagData->[$j][11];
 			print "\n";
 		}
 	}
