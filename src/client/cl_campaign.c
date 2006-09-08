50aad933c88c5d694fcf05070d56e694d18f4f72
Ivor Blockley <cassiterite@users.sourceforge.net>
2006-09-08 02:04:46 +0000

    * src/client/cl_campaign.c filter out null state strings in CL_CampaignEndStage (fixes a bug I encountered during testing)

--- src/client/cl_campaign.c
+++ src/client/cl_campaign.c
@@ -548,12 +548,14 @@ static void CL_CampaignEndStage(char *name)
 {
 	stageState_t *state;
 	int i;
-
-	for (i = 0, state = ccs.stage; i < numStages; i++, state++)
-		if (!Q_strncmp(state->def->name, name, MAX_VAR)) {
-			state->active = qfalse;
-			return;
+	for (i = 0, state = ccs.stage; i < numStages; i++, state++) {
+		if (state->def->name != NULL) {
+			if (!Q_strncmp(state->def->name, name, MAX_VAR)) {
+				state->active = qfalse;
+				return;
+			}
 		}
+	}
 
 	Com_Printf("CL_CampaignEndStage: stage '%s' not found.\n", name);
 }
