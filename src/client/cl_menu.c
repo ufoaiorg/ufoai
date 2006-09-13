7622d23a91c644ef3161c8b978062e35bafd43ca
Mikolaj Konarski <bandobraz@users.sourceforge.net>
2006-09-13 18:02:14 +0000

    * merged 3592 into branch 2.0 (animation crashes fix)

--- src/client/cl_menu.c
+++ src/client/cl_menu.c
@@ -1665,10 +1665,6 @@ void MN_DrawMenus(void)
 					} else
 						menuModel = node->menuModel;
 
-					if (!node->data[2] && Q_strncmp(oldSource, source, MAX_VAR)) {
-						Q_strncpyz(oldSource, source, MAX_VAR);
-						node->data[4] = NULL;
-					}
 					mi.name = source;
 
 					mi.origin = node->origin;
@@ -1726,6 +1722,10 @@ void MN_DrawMenus(void)
 							/* do animations */
 							if (node->data[1] && *(char *) node->data[1]) {
 								ref = MN_GetReferenceString(menu, node->data[1]);
+								if (!node->data[2] && Q_strncmp(oldSource, source, MAX_VAR)) {
+									Q_strncpyz(oldSource, source, MAX_VAR);
+									node->data[4] = NULL;
+								}
 								if (!node->data[4]) {
 									/* new anim state */
 									as = (animState_t *) curadata;
