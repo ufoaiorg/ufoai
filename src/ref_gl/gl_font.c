a2456d28cb2d21ef56d5dc0ba5fbbdbe35ec4de6
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-09-18 06:00:51 +0000

    * try to reduce the "Could not generate font surface" Sys_Errors

--- src/ref_gl/gl_font.c
+++ src/ref_gl/gl_font.c
@@ -499,6 +499,7 @@ int Font_DrawString(char *fontID, int align, int x, int y, int absX, int absY, i
 	int line = 0;
 	float texh0, fh, fy; /* rounding errors break mouse-text corelation */
 	qboolean skipline = qfalse;
+	int secondTime = 0; /* allow a Could not generate font surface once */
 
 	/* transform from 1024x768 coordinates for drawing */
 	absX = (float) absX *vid.rx;
@@ -610,8 +611,14 @@ int Font_DrawString(char *fontID, int align, int x, int y, int absX, int absY, i
 			if (!cache)
 				cache = Font_GenerateCache(buffer, searchString, f);
 
-			if (!cache)
-				ri.Sys_Error(ERR_FATAL, "...could not generate font surface '%s'\n", buffer);
+			if (!cache) {
+				/* maybe we are running out of mem */
+				if (!secondTime) {
+					Font_CleanCache();
+					secondTime = 1;
+				} else
+					ri.Sys_Error(ERR_FATAL, "...could not generate font surface '%s'\n", buffer);
+			}
 
 			Font_GenerateGLSurface(cache, x, fy, absX, absY, maxWidth, maxHeight);
 			fy += fh;
