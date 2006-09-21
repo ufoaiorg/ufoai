4b4b31d7c6b1680ff0f89c6c8d345e3fafea029e
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-09-21 09:27:36 +0000

    * gpled version of md4 calculation

--- src/qcommon/md4.c
+++ src/qcommon/md4.c
@@ -1,210 +1,230 @@
-
-#include "../qcommon/ufotypes.h"
+/**
+ * @file md4.c
+ * @brief An implementation of MD4 designed for use in the samba SMB authentication protocol
+ * @note This code makes no attempt to be fast!
+ * @note It assumes that a int is at least 32 bits long
+ */
 
 /*
-Public Domain C source implementation of RFC 1320:
-The MD4 Message-Digest Algorithm
-http://www.faqs.org/rfcs/rfc1320.html
+ Copyright (C) 1997-1998  Andrew Tridgell
 
-by Steven Fuller
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to:
+
+ Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307, USA
+
+ $Id$
 */
 
-#define ROTATELEFT32(x, s)	(((x)<<(s))|((x)>>(32-(s))))
+#include <string.h>		/* XoXus: needed for memset call */
+#include "md4.h"
 
-#define F(X, Y, Z)		(((X)&(Y)) | ((~X)&(Z)))
-#define G(X, Y, Z)		(((X)&(Y)) | ((X)&(Z)) | ((Y)&(Z)))
-#define H(X, Y, Z)		((X) ^ (Y) ^ (Z))
+static struct mdfour *m;
 
-#define S(a, b, c, d, k, s)	{							\
-					a += (F((b), (c), (d)) + X[(k)]);		\
-					a = ROTATELEFT32(a, s);				\
-				}
-#define T(a, b, c, d, k, s)	{							\
-					a += (G((b), (c), (d)) + X[(k)] + 0x5A827999);	\
-					a = ROTATELEFT32(a, s);				\
-				}
-#define U(a, b, c, d, k, s)	{							\
-					a += (H((b), (c), (d)) + X[(k)] + 0x6ED9EBA1);	\
-					a = ROTATELEFT32(a, s);				\
-				}
-
-static uint32_t X[16];
-static uint32_t A, AA;
-static uint32_t B, BB;
-static uint32_t C, CC;
-static uint32_t D, DD;
-
-static void DoMD4()
-{
-	AA = A;
-	BB = B;
-	CC = C;
-	DD = D;
-
-	S(A, B, C, D, 0, 3);
-	S(D, A, B, C, 1, 7);
-	S(C, D, A, B, 2, 11);
-	S(B, C, D, A, 3, 19);
-	S(A, B, C, D, 4, 3);
-	S(D, A, B, C, 5, 7);
-	S(C, D, A, B, 6, 11);
-	S(B, C, D, A, 7, 19);
-	S(A, B, C, D, 8, 3);
-	S(D, A, B, C, 9, 7);
-	S(C, D, A, B, 10, 11);
-	S(B, C, D, A, 11, 19);
-	S(A, B, C, D, 12, 3);
-	S(D, A, B, C, 13, 7);
-	S(C, D, A, B, 14, 11);
-	S(B, C, D, A, 15, 19);
-
-	T(A, B, C, D, 0, 3);
-	T(D, A, B, C, 4, 5);
-	T(C, D, A, B, 8, 9);
-	T(B, C, D, A, 12, 13);
-	T(A, B, C, D, 1, 3);
-	T(D, A, B, C, 5, 5);
-	T(C, D, A, B, 9, 9);
-	T(B, C, D, A, 13, 13);
-	T(A, B, C, D, 2, 3);
-	T(D, A, B, C, 6, 5);
-	T(C, D, A, B, 10, 9);
-	T(B, C, D, A, 14, 13);
-	T(A, B, C, D, 3, 3);
-	T(D, A, B, C, 7, 5);
-	T(C, D, A, B, 11, 9);
-	T(B, C, D, A, 15, 13);
-
-	U(A, B, C, D, 0, 3);
-	U(D, A, B, C, 8, 9);
-	U(C, D, A, B, 4, 11);
-	U(B, C, D, A, 12, 15);
-	U(A, B, C, D, 2, 3);
-	U(D, A, B, C, 10, 9);
-	U(C, D, A, B, 6, 11);
-	U(B, C, D, A, 14, 15);
-	U(A, B, C, D, 1, 3);
-	U(D, A, B, C, 9, 9);
-	U(C, D, A, B, 5, 11);
-	U(B, C, D, A, 13, 15);
-	U(A, B, C, D, 3, 3);
-	U(D, A, B, C, 11, 9);
-	U(C, D, A, B, 7, 11);
-	U(B, C, D, A, 15, 15);
-
-	A += AA;
-	B += BB;
-	C += CC;
-	D += DD;
-}
-
-#if 1
-static void PerformMD4(const unsigned char *buf, int length, unsigned char *digest)
+#define F(X,Y,Z) (((X)&(Y)) | ((~(X))&(Z)))
+#define G(X,Y,Z) (((X)&(Y)) | ((X)&(Z)) | ((Y)&(Z)))
+#define H(X,Y,Z) ((X)^(Y)^(Z))
+#ifdef LARGE_INT32
+#define lshift(x,s) ((((x)<<(s))&0xFFFFFFFF) | (((x)>>(32-(s)))&0xFFFFFFFF))
 #else
-static void PerformMD4(const unsigned char *buf, int length, uint32_t * digest)
-#endif
-{
-	int len = length / 64;		/* number of full blocks */
-	int rem = length % 64;		/* number of left over bytes */
-
-	int i, j;
-	const unsigned char *ptr = buf;
-
-	/* initialize the MD buffer */
-	A = 0x67452301;
-	B = 0xEFCDAB89;
-	C = 0x98BADCFE;
-	D = 0x10325476;
-
-	for (i = 0; i < len; i++) {
-		for (j = 0; j < 16; j++) {
-			X[j] = ((ptr[0] << 0) | (ptr[1] << 8) | (ptr[2] << 16) | (ptr[3] << 24));
-
-			ptr += 4;
-		}
-
-		DoMD4();
-	}
-
-	i = rem / 4;
-	for (j = 0; j < i; j++) {
-		X[j] = ((ptr[0] << 0) | (ptr[1] << 8) | (ptr[2] << 16) | (ptr[3] << 24));
-
-		ptr += 4;
-	}
-
-	switch (rem % 4) {
-	case 0:
-		X[j] = 0x80U;
-		break;
-	case 1:
-		X[j] = ((ptr[0] << 0) | ((0x80U) << 8));
-		break;
-	case 2:
-		X[j] = ((ptr[0] << 0) | (ptr[1] << 8) | ((0x80U) << 16));
-		break;
-	case 3:
-		X[j] = ((ptr[0] << 0) | (ptr[1] << 8) | (ptr[2] << 16) | ((0x80U) << 24));
-		break;
-	}
-	j++;
-
-	if (j > 14) {
-		for (; j < 16; j++)
-			X[j] = 0;
-		DoMD4();
-
-		j = 0;
-	}
-
-	for (; j < 14; j++)
-		X[j] = 0;
-	X[14] = (length & 0x1FFFFFFF) << 3;
-	X[15] = (length & ~0x1FFFFFFF) >> 29;
-
-	DoMD4();
-
-#if 1
-	digest[0] = (unsigned char) ((A & 0x000000FF) >> 0);
-	digest[1] = (unsigned char) ((A & 0x0000FF00) >> 8);
-	digest[2] = (unsigned char) ((A & 0x00FF0000) >> 16);
-	digest[3] = (unsigned char) ((A & 0xFF000000) >> 24);
-	digest[4] = (unsigned char) ((B & 0x000000FF) >> 0);
-	digest[5] = (unsigned char) ((B & 0x0000FF00) >> 8);
-	digest[6] = (unsigned char) ((B & 0x00FF0000) >> 16);
-	digest[7] = (unsigned char) ((B & 0xFF000000) >> 24);
-	digest[8] = (unsigned char) ((C & 0x000000FF) >> 0);
-	digest[9] = (unsigned char) ((C & 0x0000FF00) >> 8);
-	digest[10] = (unsigned char) ((C & 0x00FF0000) >> 16);
-	digest[11] = (unsigned char) ((C & 0xFF000000) >> 24);
-	digest[12] = (unsigned char) ((D & 0x000000FF) >> 0);
-	digest[13] = (unsigned char) ((D & 0x0000FF00) >> 8);
-	digest[14] = (unsigned char) ((D & 0x00FF0000) >> 16);
-	digest[15] = (unsigned char) ((D & 0xFF000000) >> 24);
-#else
-	digest[0] = SWAPLSB(A);
-	digest[1] = SWAPLSB(B);
-	digest[2] = SWAPLSB(C);
-	digest[3] = SWAPLSB(D);
+#define lshift(x,s) (((x)<<(s)) | ((x)>>(32-(s))))
 #endif
 
-	A = AA = 0;
-	B = BB = 0;
-	C = CC = 0;
-	D = DD = 0;
-	for (j = 0; j < 16; j++)
+#define ROUND1(a,b,c,d,k,s) a = lshift(a + F(b,c,d) + X[k], s)
+#define ROUND2(a,b,c,d,k,s) a = lshift(a + G(b,c,d) + X[k] + 0x5A827999,s)
+#define ROUND3(a,b,c,d,k,s) a = lshift(a + H(b,c,d) + X[k] + 0x6ED9EBA1,s)
+
+/* this applies md4 to 64 byte chunks */
+static void mdfour64(uint32 *M)
+{
+	int j;
+	uint32 AA, BB, CC, DD;
+	uint32 X[16];
+	uint32 A,B,C,D;
+
+	for (j=0;j<16;j++)
+		X[j] = M[j];
+
+	A = m->A; B = m->B; C = m->C; D = m->D;
+	AA = A; BB = B; CC = C; DD = D;
+
+	ROUND1(A,B,C,D,  0,  3);  ROUND1(D,A,B,C,  1,  7);
+	ROUND1(C,D,A,B,  2, 11);  ROUND1(B,C,D,A,  3, 19);
+	ROUND1(A,B,C,D,  4,  3);  ROUND1(D,A,B,C,  5,  7);
+	ROUND1(C,D,A,B,  6, 11);  ROUND1(B,C,D,A,  7, 19);
+	ROUND1(A,B,C,D,  8,  3);  ROUND1(D,A,B,C,  9,  7);
+	ROUND1(C,D,A,B, 10, 11);  ROUND1(B,C,D,A, 11, 19);
+	ROUND1(A,B,C,D, 12,  3);  ROUND1(D,A,B,C, 13,  7);
+	ROUND1(C,D,A,B, 14, 11);  ROUND1(B,C,D,A, 15, 19);
+
+	ROUND2(A,B,C,D,  0,  3);  ROUND2(D,A,B,C,  4,  5);
+	ROUND2(C,D,A,B,  8,  9);  ROUND2(B,C,D,A, 12, 13);
+	ROUND2(A,B,C,D,  1,  3);  ROUND2(D,A,B,C,  5,  5);
+	ROUND2(C,D,A,B,  9,  9);  ROUND2(B,C,D,A, 13, 13);
+	ROUND2(A,B,C,D,  2,  3);  ROUND2(D,A,B,C,  6,  5);
+	ROUND2(C,D,A,B, 10,  9);  ROUND2(B,C,D,A, 14, 13);
+	ROUND2(A,B,C,D,  3,  3);  ROUND2(D,A,B,C,  7,  5);
+	ROUND2(C,D,A,B, 11,  9);  ROUND2(B,C,D,A, 15, 13);
+
+	ROUND3(A,B,C,D,  0,  3);  ROUND3(D,A,B,C,  8,  9);
+	ROUND3(C,D,A,B,  4, 11);  ROUND3(B,C,D,A, 12, 15);
+	ROUND3(A,B,C,D,  2,  3);  ROUND3(D,A,B,C, 10,  9);
+	ROUND3(C,D,A,B,  6, 11);  ROUND3(B,C,D,A, 14, 15);
+	ROUND3(A,B,C,D,  1,  3);  ROUND3(D,A,B,C,  9,  9);
+	ROUND3(C,D,A,B,  5, 11);  ROUND3(B,C,D,A, 13, 15);
+	ROUND3(A,B,C,D,  3,  3);  ROUND3(D,A,B,C, 11,  9);
+	ROUND3(C,D,A,B,  7, 11);  ROUND3(B,C,D,A, 15, 15);
+
+	A += AA; B += BB; C += CC; D += DD;
+
+#ifdef LARGE_INT32
+	A &= 0xFFFFFFFF; B &= 0xFFFFFFFF;
+	C &= 0xFFFFFFFF; D &= 0xFFFFFFFF;
+#endif
+
+	for (j=0;j<16;j++)
 		X[j] = 0;
+
+	m->A = A; m->B = B; m->C = C; m->D = D;
 }
 
-/* Quake2-specific function */
-unsigned Com_BlockChecksum(void *buffer, int length)
+static void copy64(uint32 *M, unsigned char *in)
 {
-	uint32_t digest[4];
+	int i;
+
+	for (i=0;i<16;i++)
+		M[i] = (in[i*4+3]<<24) | (in[i*4+2]<<16) |
+			(in[i*4+1]<<8) | (in[i*4+0]<<0);
+}
+
+static void copy4(unsigned char *out,uint32 x)
+{
+	out[0] = x&0xFF;
+	out[1] = (x>>8)&0xFF;
+	out[2] = (x>>16)&0xFF;
+	out[3] = (x>>24)&0xFF;
+}
+
+void mdfour_begin(struct mdfour *md)
+{
+	md->A = 0x67452301;
+	md->B = 0xefcdab89;
+	md->C = 0x98badcfe;
+	md->D = 0x10325476;
+	md->totalN = 0;
+}
+
+
+static void mdfour_tail(unsigned char *in, int n)
+{
+	unsigned char buf[128];
+	uint32 M[16];
+	uint32 b;
+
+	m->totalN += n;
+
+	b = m->totalN * 8;
+
+	memset(buf, 0, 128);
+	if (n) memcpy(buf, in, n);
+	buf[n] = 0x80;
+
+	if (n <= 55) {
+		copy4(buf+56, b);
+		copy64(M, buf);
+		mdfour64(M);
+	} else {
+		copy4(buf+120, b);
+		copy64(M, buf);
+		mdfour64(M);
+		copy64(M, buf+64);
+		mdfour64(M);
+	}
+}
+
+void mdfour_update(struct mdfour *md, unsigned char *in, int n)
+{
+	uint32 M[16];
+
+	/** start of edit by Forest 'LordHavoc' Hale
+	 * commented out to prevent crashing when length is 0
+	 * if (n == 0) mdfour_tail(in, n);
+	 * end of edit by Forest 'LordHavoc' Hale
+	 */
+
+	m = md;
+
+	while (n >= 64) {
+		copy64(M, in);
+		mdfour64(M);
+		in += 64;
+		n -= 64;
+		m->totalN += 64;
+	}
+
+	mdfour_tail(in, n);
+}
+
+
+void mdfour_result(struct mdfour *md, unsigned char *out)
+{
+	m = md;
+
+	copy4(out, m->A);
+	copy4(out+4, m->B);
+	copy4(out+8, m->C);
+	copy4(out+12, m->D);
+}
+
+
+void mdfour(unsigned char *out, unsigned char *in, int n)
+{
+	struct mdfour md;
+	mdfour_begin(&md);
+	mdfour_update(&md, in, n);
+	mdfour_result(&md, out);
+}
+
+/**
+ * MD4-based checksum utility functions
+ * Copyright (C) 2000       Jeff Teunissen <d2deek@pmail.net>
+ * Author: Jeff Teunissen	<d2deek@pmail.net>
+ * Date: 01 Jan 2000
+ */
+
+/**
+ * @brief
+ */
+unsigned Com_BlockChecksum (void *buffer, int length)
+{
+	int digest[4];
 	unsigned val;
 
-	PerformMD4((unsigned char *) buffer, length, (unsigned char *) digest);
+	mdfour((unsigned char *) digest, (unsigned char *) buffer, length);
 
 	val = digest[0] ^ digest[1] ^ digest[2] ^ digest[3];
 
 	return val;
 }
+
+/**
+ * @brief
+ */
+void Com_BlockFullChecksum (void *buffer, int len, unsigned char *outbuf)
+{
+	mdfour(outbuf, (unsigned char *) buffer, len);
+}
+
