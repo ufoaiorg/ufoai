b84656de3842bfcd5d6f77f82606ff696eaa9c17
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-09-25 12:42:31 +0000

    * fixed some compile errors and added the framework for the worldspawn settings   NOTE: The plugin still does nothing

--- src/tools/gtkradiant/plugin/ufoai/plugin.cpp
+++ src/tools/gtkradiant/plugin/ufoai/plugin.cpp
@@ -21,25 +21,62 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "plugin.h"
 
-#include "debugging/debugging.h"
-
-#include "iplugin.h"
+#if 0
 #include "ifilter.h"
-#include "iundo.h"       // declaration of undo system
-#include "ientity.h"     // declaration of entity system
-
 #include "filters.h"
+#endif
 
+#include "debugging/debugging.h"
+#include "iplugin.h"
 #include "string/string.h"
 #include "modulesystem/singletonmodule.h"
-
 #include <gtk/gtk.h>
-
 #include "gtkutil/messagebox.h"
 #include "gtkutil/filechooser.h"
+#include "iscenegraph.h" // declaration of datastructure of the map
+#include "scenelib.h"    // declaration of datastructure of the map
+#include "qerplugin.h"   // declaration to use other interfaces as a plugin
+#include "iundo.h"       // declaration of undo system
+#include "ientity.h"     // declaration of entity system
+#include "string/string.h"
 
-#if 0
-void about_plugin_window();
+#define VERSION "0.1"
+
+void about_plugin_window(void);
+void WorldSpawnSettings(void);
+
+#ifdef __linux__
+// linux itoa implementation
+char* itoa( int value, char* result, int base )
+{
+	// check that the base if valid
+	if (base < 2 || base > 16)
+	{
+	  *result = 0;
+	  return result;
+	}
+
+	char* out = result;
+	int quotient = value;
+
+	do
+	{
+		*out = "0123456789abcdef"[abs(quotient % base)];
+		++out;
+
+		quotient /= base;
+	} while (quotient);
+
+	// Only apply negative sign for base 10
+	if( value < 0 && base == 10)
+	  *out++ = '-';
+
+	std::reverse(result, out);
+
+	*out = 0;
+	return result;
+}
+#endif
 
 //  **************************
 // ** GTK callback functions **
@@ -68,6 +105,7 @@ static void close_window(GtkWidget *widget, gpointer data)
 	gtk_widget_destroy(gtk_widget_get_toplevel(widget));
 }
 
+#if 0
 class filter_face_level : public UfoAIFilter
 {
 	int m_contents;
@@ -82,23 +120,20 @@ class filter_face_level : public UfoAIFilter
 
 namespace UfoAI
 {
-	GtkWindow* g_mainwnd;
+	GtkWindow* main_window = NULL;
+	Entity *theWorldspawn = NULL;
+	int maxlevel = 8;
 
-	void construct ( void ) {
-/*		filter_face_level g_filter_face_level1(CONTENTS_LEVEL1);
-		filter_brush_all_faces g_filter_brush_level1(&g_filter_face_level1);
-		add_brush_filter(g_filter_brush_level1, EXCLUDE_LEVEL1);*/
-	}
 	const char* init(void* hApp, void* pMainWidget) {
-		g_mainwnd = (GtkWindow*)pMainWidget;
-		ASSERT_NOTNULL(g_mainwnd);
+		main_window = (GtkWindow*)pMainWidget;
+		ASSERT_NOTNULL(main_window);
 		return "UFO:AI Filters";
 	}
 	const char* getName() {
 		return "UFO:AI Filters";
 	}
 	const char* getCommandList() {
-		return "StepOn,-,Level8,Level7,Level6,Level5,Level4,Level3,Level2,Level1,-,Map properties";
+		return "StepOn,-,Level8,Level7,Level6,Level5,Level4,Level3,Level2,Level1,-,Map properties,-,About...";
 	}
 	const char* getCommandTitleList() {
 		return "";
@@ -110,7 +145,7 @@ namespace UfoAI
 		globalOutputStream() << "UFO:Alien Invasion Plugin\n";
 		if(string_equal(command, "Map properties")) {
 			globalOutputStream() << "Worldspawn properties\n";
-/*			WorldSpawnSettings();*/
+			WorldSpawnSettings();
 		} else if(string_equal(command, "Level1")) {
 			globalOutputStream() << "Show only level 1\n";
 		} else if(string_equal(command, "Level2")) {
@@ -129,16 +164,14 @@ namespace UfoAI
 			globalOutputStream() << "Show only level 8\n";
 		} else if(string_equal(command, "StepOn")) {
 			globalOutputStream() << "Hide all stepons\n";
-#if 0
 		} else if(string_equal(command, "About...")) {
 			about_plugin_window();
-#endif
 		}
     }
 } // namespace
 
 
-class UfoAIPluginModule : public TypeSystemRef
+class UfoAIPluginModule
 {
 	_QERPluginTable m_plugin;
 	public:
@@ -169,7 +202,7 @@ extern "C" void RADIANT_DLLEXPORT Radiant_RegisterModules(ModuleServer& server)
 	g_UfoAIPluginModule.selfRegister();
 }
 
-#if 0
+
 //  **************************
 // ** find entities by class **  from radiant/map.cpp
 //  **************************
@@ -199,15 +232,7 @@ Entity* Scene_FindEntityByClass(const char* name)
 	return entity;
 }
 
-#endif
 
-/*
-Entity *theWorldspawn = NULL;
-theWorldspawn = Scene_FindEntityByClass("worldspawn"); // find the entity worldspawn
-*/
-
-
-#if 0
 // About dialog
 void about_plugin_window()
 {
@@ -225,7 +250,7 @@ void about_plugin_window()
 	vbox = gtk_vbox_new(FALSE, 10); // create a box to arrange new objects vertically
 	gtk_container_add(GTK_CONTAINER(window), vbox); // add the box to the window
 
-	label = gtk_label_new("UFO:AI v0.1 for GtkRadiant 1.5\nby Martin Gerhardy"); // create a label
+	label = gtk_label_new("UFO:AI Plugin v"VERSION" for GtkRadiant 1.5\nby Martin Gerhardy"); // create a label
 	gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_LEFT); // text align left
 	gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 2); // insert the label in the box
 
@@ -238,4 +263,72 @@ void about_plugin_window()
 	gtk_widget_show_all(window); // show the window and all subelements
 }
 
-#endif
+// write the values of the Spinner-Boxes to the worldspawn
+static void set_maxlevel(GtkWidget *widget, gpointer data)
+{
+	//Str str_min, str_max;
+	char buffer[10];
+
+	strcat(buffer, itoa(UfoAI::maxlevel, buffer, 10));
+	UndoableCommand undo("UfoAI.entitySetMaxlevel");
+	UfoAI::theWorldspawn->setKeyValue("maxlevel", buffer);
+
+	close_window(widget, NULL);
+}
+
+// callback function to assign the optimal maxlevel value
+static void set_optimal_maxlevel(GtkWidget *widget, gpointer data)
+{
+}
+
+// MapCoordinator dialog window
+void WorldSpawnSettings(void)
+{
+	GtkWidget *window, *vbox, *label, *button;
+	const char *buffer;
+
+	// in any case we need a window to show the user what to do
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL); // create the window
+	gtk_window_set_transient_for(GTK_WINDOW(window), UfoAI::main_window); // make the window to stay in front of the main window
+	g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(delete_event), NULL); // connect the delete event for the window
+	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL); // connect the destroy event for the window
+	gtk_window_set_title(GTK_WINDOW(window), "UFO:AI Worlspawn settings"); // set the title of the window for the window
+	gtk_window_set_resizable(GTK_WINDOW(window), FALSE); // don't let the user resize the window
+	gtk_window_set_modal(GTK_WINDOW(window), TRUE); // force the user not to do something with the other windows
+	gtk_container_set_border_width(GTK_CONTAINER(window), 10); // set the border of the window
+
+	vbox = gtk_vbox_new(FALSE, 10); // create a box to arrange new objects vertically
+	gtk_container_add(GTK_CONTAINER(window), vbox); // add the box to the window
+
+	UfoAI::theWorldspawn = Scene_FindEntityByClass("worldspawn"); // find the entity worldspawn
+	if (UfoAI::theWorldspawn != NULL) { // need to have a worldspawn otherwise setting a value crashes the radiant
+		globalOutputStream() << "UfoAI: worldspawn found!\n"; // output error to console
+
+		buffer = UfoAI::theWorldspawn->getKeyValue("maxlevel"); // upper left corner
+		if (strlen(buffer) == 1) {
+			UfoAI::maxlevel = atoi(buffer);
+		}
+
+		button = gtk_button_new_with_label("Set optimal maxlevel"); // create button with text
+		g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(set_optimal_maxlevel), NULL); // connect button with callback function
+		gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 2); // insert button into vbox
+		gtk_box_pack_start(GTK_BOX(vbox), gtk_hseparator_new(), FALSE, FALSE, 2); // insert separator into vbox
+
+		button = gtk_button_new_with_label("Cancel"); // create a button with text
+		g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(close_window), NULL); // connect the click event to close the window
+		gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 2); // insert the button in the box
+	} else {
+		globalOutputStream() << "UfoAI: no worldspawn found!\n"; // output error to console
+
+		label = gtk_label_new("ERROR: No worldspawn was found in the map!\nIn order to use this tool the map must have at least one brush in the worldspawn. "); // create a label
+		gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_LEFT); // text align left
+		gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 2); // insert the label in the box
+
+		button = gtk_button_new_with_label("OK"); // create a button with text
+		g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(close_window), NULL); // connect the click event to close the window
+		gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 2); // insert the button in the box
+	}
+
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); // center the window
+	gtk_widget_show_all(window); // show the window and all subelements
+}
