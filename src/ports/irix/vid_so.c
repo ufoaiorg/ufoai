71e43245bd26a2d9206af225f26eda9106654803
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-09-30 19:20:13 +0000

    * SO_FILE isn't using anywhere

--- src/ports/irix/vid_so.c
+++ src/ports/irix/vid_so.c
@@ -21,8 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /* is used for both the software and OpenGL rendering versions of the */
 /* Quake refresh engine. */
 
-#define SO_FILE "/etc/ufo.conf"
-
 #include <errno.h>
 #include <assert.h>
 #include <dlfcn.h> /* ELF dl loader */
