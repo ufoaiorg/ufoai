8c5fb07ec129fad946fa9d58350e24c29959af13
Tom Shackell <tomshackell@users.sourceforge.net>
2006-10-03 14:51:41 +0000

    * Fixed dependency analysis bug -MQ introduces unfortunate escaping, so use -MT instead.

--- build/defs.mk
+++ build/defs.mk
@@ -9,7 +9,7 @@ _BUILDDIR=$(strip $(BUILDDIR))
 _MODULE=$(strip $(MODULE))
 _SRCDIR=$(strip $(SRCDIR))
 
-DEP=$(CC) $(CFLAGS) -c $< -MM -MQ "$(@:%.d=%.o) $@" -MF $@
+DEP=$(CC) $(CFLAGS) -c $< -MM -MT "$(@:%.d=%.o) $@" -MF $@
 
 LIBTOOL_LD=$(LIBTOOL) --silent --mode=link $(CC) -module -rpath / $(LDFLAGS)
 LIBTOOL_CC=$(LIBTOOL) --silent --mode=compile $(CC) -prefer-pic $(CFLAGS)
