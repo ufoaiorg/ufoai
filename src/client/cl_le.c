307d3c959ff4f947acbb3c1b4c1b48ac1dfc2f6d
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-11-10 17:11:20 +0000

    * this should make the frame parameter for misc_model work   NOTE: This is untested

--- src/client/cl_le.c
+++ src/client/cl_le.c
@@ -77,6 +77,7 @@ void LM_AddToScene(void)
 		VectorCopy(lm->angles, ent.angles);
 		ent.model = lm->model;
 		ent.skinnum = lm->skin;
+		ent.as.frame = lm->frame;
 		if (lm->animname[0]) {
 			ent.as = lm->as;
 			/* do animation */
