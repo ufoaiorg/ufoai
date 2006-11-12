450b0d12a232a05beb067d3a654fc60519dbdf7b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-11-12 16:14:15 +0000

    * fixed winquake.h path

--- src/client/qal.h
+++ src/client/qal.h
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "client.h"
 
 #ifdef _WIN32
-	#include "../win32/winquake.h"
+	#include "../ports/win32/winquake.h"
 	#include "../ports/win32/qal_win.h"
 #endif
 
