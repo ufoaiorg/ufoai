f19a8295c697064dc2e757d6c524fce2a57cc657
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-12-02 16:36:08 +0000

    * some changes to ufo:ai plugin - doesn't compile nor work atm

--- src/tools/gtkradiant/plugin/ufoai/ufoai_filters.cpp
+++ src/tools/gtkradiant/plugin/ufoai/ufoai_filters.cpp
@@ -23,14 +23,16 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "../../radiant/brush.h"
 
+#include "generic/callback.h"
+
 #include <list>
 
 
-class filter_face_shader : public FaceFilter
+class ufoai_filter_face_shader : public FaceFilter
 {
   const char* m_shader;
 public:
-  filter_face_shader(const char* shader) : m_shader(shader)
+  ufoai_filter_face_shader(const char* shader) : m_shader(shader)
   {
   }
   bool filter(const Face& face) const
@@ -39,11 +41,11 @@ public:
   }
 };
 
-class filter_face_shader_prefix : public FaceFilter
+class ufoai_filter_face_shader_prefix : public FaceFilter
 {
   const char* m_prefix;
 public:
-  filter_face_shader_prefix(const char* prefix) : m_prefix(prefix)
+  ufoai_filter_face_shader_prefix(const char* prefix) : m_prefix(prefix)
   {
   }
   bool filter(const Face& face) const
@@ -52,11 +54,11 @@ public:
   }
 };
 
-class filter_face_flags : public FaceFilter
+class ufoai_filter_face_flags : public FaceFilter
 {
   int m_flags;
 public:
-  filter_face_flags(int flags) : m_flags(flags)
+  ufoai_filter_face_flags(int flags) : m_flags(flags)
   {
   }
   bool filter(const Face& face) const
@@ -65,11 +67,11 @@ public:
   }
 };
 
-class filter_face_contents : public FaceFilter
+class ufoai_filter_face_contents : public FaceFilter
 {
   int m_contents;
 public:
-  filter_face_contents(int contents) : m_contents(contents)
+  ufoai_filter_face_contents(int contents) : m_contents(contents)
   {
   }
   bool filter(const Face& face) const
@@ -173,54 +175,66 @@ public:
 typedef std::list<BrushFilterWrapper> BrushFilters;
 BrushFilters g_brushFilters;
 
-void add_brush_filter(BrushFilter& filter, int mask, bool invert)
+BrushFilterWrapper* add_ufoaibrush_filter(BrushFilter& filter, int mask, bool invert)
 {
   g_brushFilters.push_back(BrushFilterWrapper(filter, invert));
   GlobalFilterSystem().addFilter(g_brushFilters.back(), mask);
+  return &g_brushFilters.back();
 }
 
-filter_face_flags g_filter_face_clip(CONTENTS_ACTORCLIP);
-filter_brush_all_faces g_filter_brush_clip(&g_filter_face_clip);
+ufoai_filter_face_flags g_ufoai_filter_face_clip(CONTENTS_ACTORCLIP);
+filter_brush_all_faces g_filter_brush_clip(&g_ufoai_filter_face_clip);
 
-filter_face_flags g_filter_face_stepon(CONTENTS_STEPON);
-filter_brush_all_faces g_filter_brush_stepon(&g_filter_face_stepon);
+ufoai_filter_face_flags g_ufoai_filter_face_stepon(CONTENTS_STEPON);
+filter_brush_all_faces g_filter_brush_stepon(&g_ufoai_filter_face_stepon);
 
-filter_face_flags g_filter_face_level1(CONTENTS_LEVEL1);
-filter_brush_all_faces g_filter_brush_level1(&g_filter_face_level1);
+ufoai_filter_face_flags g_ufoai_filter_face_level1(CONTENTS_LEVEL1);
+filter_brush_all_faces g_filter_brush_level1(&g_ufoai_filter_face_level1);
 
-filter_face_flags g_filter_face_level2(CONTENTS_LEVEL2);
-filter_brush_all_faces g_filter_brush_level2(&g_filter_face_level2);
+ufoai_filter_face_flags g_ufoai_filter_face_level2(CONTENTS_LEVEL2);
+filter_brush_all_faces g_filter_brush_level2(&g_ufoai_filter_face_level2);
 
-filter_face_flags g_filter_face_level3(CONTENTS_LEVEL3);
-filter_brush_all_faces g_filter_brush_level3(&g_filter_face_level3);
+ufoai_filter_face_flags g_ufoai_filter_face_level3(CONTENTS_LEVEL3);
+filter_brush_all_faces g_filter_brush_level3(&g_ufoai_filter_face_level3);
 
-filter_face_flags g_filter_face_level4(CONTENTS_LEVEL4);
-filter_brush_all_faces g_filter_brush_level4(&g_filter_face_level4);
+ufoai_filter_face_flags g_ufoai_filter_face_level4(CONTENTS_LEVEL4);
+filter_brush_all_faces g_filter_brush_level4(&g_ufoai_filter_face_level4);
 
-filter_face_flags g_filter_face_level5(CONTENTS_LEVEL5);
-filter_brush_all_faces g_filter_brush_level5(&g_filter_face_level5);
+ufoai_filter_face_flags g_ufoai_filter_face_level5(CONTENTS_LEVEL5);
+filter_brush_all_faces g_filter_brush_level5(&g_ufoai_filter_face_level5);
 
-filter_face_flags g_filter_face_level6(CONTENTS_LEVEL6);
-filter_brush_all_faces g_filter_brush_level6(&g_filter_face_level6);
+ufoai_filter_face_flags g_ufoai_filter_face_level6(CONTENTS_LEVEL6);
+filter_brush_all_faces g_filter_brush_level6(&g_ufoai_filter_face_level6);
 
-filter_face_flags g_filter_face_level7(CONTENTS_LEVEL7);
-filter_brush_all_faces g_filter_brush_level7(&g_filter_face_level7);
+ufoai_filter_face_flags g_ufoai_filter_face_level7(CONTENTS_LEVEL7);
+filter_brush_all_faces g_filter_brush_level7(&g_ufoai_filter_face_level7);
 
-filter_face_flags g_filter_face_level8(CONTENTS_LEVEL8);
-filter_brush_all_faces g_filter_brush_level8(&g_filter_face_level8);
+ufoai_filter_face_flags g_ufoai_filter_face_level8(CONTENTS_LEVEL8);
+filter_brush_all_faces g_filter_brush_level8(&g_ufoai_filter_face_level8);
+
+BrushFilterWrapper* f_stepon;
+BrushFilterWrapper* f_actorclip;
+BrushFilterWrapper* f_level1;
+BrushFilterWrapper* f_level2;
+BrushFilterWrapper* f_level3;
+BrushFilterWrapper* f_level4;
+BrushFilterWrapper* f_level5;
+BrushFilterWrapper* f_level6;
+BrushFilterWrapper* f_level7;
+BrushFilterWrapper* f_level8;
 
 void init_filters(void)
 {
-	add_brush_filter(g_filter_brush_clip, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_stepon, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_level1, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_level2, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_level3, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_level4, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_level5, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_level6, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_level7, EXCLUDE_CLIP);
-	add_brush_filter(g_filter_brush_level8, EXCLUDE_CLIP);
+	f_stepon = add_ufoaibrush_filter(g_filter_brush_stepon, EXCLUDE_CLIP);
+	f_level1 = add_ufoaibrush_filter(g_filter_brush_level1, EXCLUDE_CLIP);
+	f_level2 = add_ufoaibrush_filter(g_filter_brush_level2, EXCLUDE_CLIP);
+	f_level3 = add_ufoaibrush_filter(g_filter_brush_level3, EXCLUDE_CLIP);
+	f_level4 = add_ufoaibrush_filter(g_filter_brush_level4, EXCLUDE_CLIP);
+	f_level5 = add_ufoaibrush_filter(g_filter_brush_level5, EXCLUDE_CLIP);
+	f_level6 = add_ufoaibrush_filter(g_filter_brush_level6, EXCLUDE_CLIP);
+	f_level7 = add_ufoaibrush_filter(g_filter_brush_level7, EXCLUDE_CLIP);
+	f_level8 = add_ufoaibrush_filter(g_filter_brush_level8, EXCLUDE_CLIP);
+	f_actorclip = add_ufoaibrush_filter(g_filter_brush_clip, EXCLUDE_CLIP);
 	globalOutputStream() << "initialized UFO:AI fiters\n";
 }
 
@@ -235,7 +249,32 @@ void deactive_filter_level(int level)
 {
 	globalOutputStream() << "deactive_filter_level: " << level << "\n";
 	activeLevelFilter = 0;
-	globalOutputStream() << "TODO: Implement me\n";
+	switch (level) {
+	case 1:
+		f_level1->setActive(0);
+		break;
+	case 2:
+		f_level2->setActive(0);
+		break;
+	case 3:
+		f_level3->setActive(0);
+		break;
+	case 4:
+		f_level4->setActive(0);
+		break;
+	case 5:
+		f_level5->setActive(0);
+		break;
+	case 6:
+		f_level6->setActive(0);
+		break;
+	case 7:
+		f_level7->setActive(0);
+		break;
+	case 8:
+		f_level8->setActive(0);
+		break;
+	};
 }
 
 /**
@@ -244,13 +283,40 @@ void deactive_filter_level(int level)
  */
 void filter_level(int level)
 {
+	int flag = 1;
 	if (activeLevelFilter > 0)
 		deactive_filter_level(activeLevelFilter);
 	if (activeLevelFilter != level)
 	{
 		activeLevelFilter = level;
-		globalOutputStream() << "filter_level: " << activeLevelFilter << "\n";
-		globalOutputStream() << "TODO: Implement me\n";
+		flag <<= (level - 1);
+		switch (level) {
+		case 0:
+			f_level1->setActive(1);
+			break;
+		case 1:
+			f_level2->setActive(1);
+			break;
+		case 2:
+			f_level3->setActive(1);
+			break;
+		case 3:
+			f_level4->setActive(1);
+			break;
+		case 4:
+			f_level5->setActive(1);
+			break;
+		case 5:
+			f_level6->setActive(1);
+			break;
+		case 6:
+			f_level7->setActive(1);
+			break;
+		case 7:
+			f_level8->setActive(1);
+			break;
+		};
+		globalOutputStream() << "filter_level: " << activeLevelFilter << " flag: " << flag << "\n";
 	}
 	else
 	{
@@ -266,7 +332,7 @@ void deactive_filter_stepon(void)
 {
 	globalOutputStream() << "deactive_filter_stepon\n";
 	activeSteponFilter = 0;
-	globalOutputStream() << "TODO: Implement me\n";
+	f_stepon->setActive(activeSteponFilter);
 }
 
 /**
@@ -280,8 +346,8 @@ void filter_stepon(void)
 	else
 	{
 		activeSteponFilter = 1;
+		f_stepon->setActive(activeSteponFilter);
 		globalOutputStream() << "filter_stepon\n";
-		globalOutputStream() << "TODO: Implement me\n";
 	}
 }
 
@@ -294,7 +360,7 @@ void deactive_filter_actorclip(void)
 {
 	globalOutputStream() << "deactive_filter_actorclip\n";
 	activeActorClipFilter = 0;
-	globalOutputStream() << "TODO: Implement me\n";
+	f_actorclip->setActive(activeSteponFilter);
 }
 
 /**
@@ -308,8 +374,60 @@ void filter_actorclip(void)
 	else
 	{
 		activeActorClipFilter = 1;
+		f_actorclip->setActive(activeSteponFilter);
 		globalOutputStream() << "filter_actorclip\n";
-		globalOutputStream() << "TODO: Implement me\n";
 	}
 }
 
+bool brush_filtered(Brush& brush)
+{
+  for(BrushFilters::iterator i = g_brushFilters.begin(); i != g_brushFilters.end(); ++i)
+  {
+    if((*i).active() && (*i).filter(brush))
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+class ClassnameFilter : public Filterable
+{
+  scene::Node& m_node;
+public:
+  Brush& m_brush;
+
+  ClassnameFilter(Brush& brush, scene::Node& node) : m_node(node), m_brush(brush)
+  {
+  }
+  ~ClassnameFilter()
+  {
+  }
+
+  void instanceAttach()
+  {
+    GlobalFilterSystem().registerFilterable(*this);
+  }
+  void instanceDetach()
+  {
+    GlobalFilterSystem().unregisterFilterable(*this);
+  }
+
+  void updateFiltered()
+  {
+    if(brush_filtered(m_brush))
+    {
+      m_node.enable(scene::Node::eFiltered);
+    }
+    else
+    {
+      m_node.disable(scene::Node::eFiltered);
+    }
+  }
+
+  void classnameChanged(const char* value)
+  {
+    updateFiltered();
+  }
+  typedef MemberCaller1<ClassnameFilter, const char*, &ClassnameFilter::classnameChanged> ClassnameChangedCaller;
+};
