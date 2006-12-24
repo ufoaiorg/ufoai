3a0da4021fddfd63619f37c88876ed404ff62449
Martin Gerhardy <tlh2000@users.sourceforge.net>
2006-12-24 15:21:02 +0000

    * moved comment to file header comment

--- src/client/cl_input.c
+++ src/client/cl_input.c
@@ -1,6 +1,23 @@
 /**
  * @file cl_input.c
  * @brief Client input handling - bindable commands.
+ * @note Continuous button event tracking is complicated by the fact that two different
+ * input sources (say, mouse button 1 and the control key) can both press the
+ * same button, but the button should only be released when both of the
+ * pressing key have been released.
+ * 
+ * When a key event issues a button command (+forward, +attack, etc), it appends
+ * its key number as a parameter to the command so it can be matched up with
+ * the release.
+ * 
+ * state bit 0 is the current state of the key
+ * state bit 1 is edge triggered on the up to down transition
+ * state bit 2 is edge triggered on the down to up transition
+ *
+ *
+ * Key_Event (int key, qboolean down, unsigned time);
+ *
+ *  +mlook src time
  */
 
 /*
@@ -54,27 +71,7 @@ static int routeLevelStart, routeLevelEnd;
 
 /*
 ===============================================================================
-
 KEY BUTTONS
-
-Continuous button event tracking is complicated by the fact that two different
-input sources (say, mouse button 1 and the control key) can both press the
-same button, but the button should only be released when both of the
-pressing key have been released.
-
-When a key event issues a button command (+forward, +attack, etc), it appends
-its key number as a parameter to the command so it can be matched up with
-the release.
-
-state bit 0 is the current state of the key
-state bit 1 is edge triggered on the up to down transition
-state bit 2 is edge triggered on the down to up transition
-
-
-Key_Event (int key, qboolean down, unsigned time);
-
-  +mlook src time
-
 ===============================================================================
 */
 
