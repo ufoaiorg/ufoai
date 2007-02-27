4a2cff72aceb1ffbe5b10c2d67f55fd0b65f7138
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-02-27 17:06:42 +0000

    * fixed alien shooting (maybe/untested)

--- src/game/g_ai.c
+++ src/game/g_ai.c
@@ -176,7 +176,7 @@ static float AI_FighterCalcGuete (edict_t * ent, pos3_t to, ai_action_t * aia)
 		weap_fds_idx = INV_FiredefsIDXForWeapon(od, weap_idx);
 		/* if od was not null and weap_idx was not NONE - then we have a problem here
 		 * maybe this is only a maptest and thus no scripts parsed */
-		if (weap_fds_idx != -1)
+		if (weap_fds_idx == -1)
 			continue;
 		/* TODO: is this how it should work? i just added this additional loop but don't know anything about the function */
 		for (fd_idx = 0; fd_idx < od->numFiredefs[weap_fds_idx]; fd_idx++) {
