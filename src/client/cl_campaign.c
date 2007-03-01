0cd9de146af93092bc74ad47d6e5dd831d3a3d6d
Maciej Wierzbicki <voovoos@users.sourceforge.net>
2007-03-01 12:01:11 +0000

    * cl_campaign.c: doxygen and TODO comments

--- src/client/cl_campaign.c
+++ src/client/cl_campaign.c
@@ -2997,7 +2997,9 @@ void CL_SellOrAddItems (void)
 
 
 /**
- * @brief
+ * @brief Update employeer stats after mission.
+ * @param[in] won Determines whether we won the mission or not.
+ * @sa CL_GameResults_f
  *
  * FIXME: See TODO and FIXME included
  */
@@ -3028,10 +3030,17 @@ static void CL_UpdateCharacterStats (int won)
 				if (chr->skills[j] < MAX_SKILL)
 					chr->skills[j]++;
 
-			/* Check if the soldier meets the requirements for a higher rank -> Promotion; also, perhaps use "won" in some way */
+			/* TODO: improve weapon, power and accuracy skills here.
+			   Add to character_t some killed_by_heavy, killed_by_sniper,
+			   etc, count proper kills and calculate skills to improve here. */
+
+			/* Check if the soldier meets the requirements for a higher rank
+			   and do a promotion. */
+			/* TODO: use param[in] in some way. */
 			if (gd.numRanks >= 2) {
 				for (j = gd.numRanks - 1; j > chr->rank; j--) {
 					rank = &gd.ranks[j];
+					/* FIXME: (Zenerka 20080301) extend ranks and change calculations here. */
 					if (rank->type == EMPL_SOLDIER && (chr->skills[ABILITY_MIND] >= rank->mind)
 						&& (chr->kills[KILLED_ALIENS] >= rank->killed_enemies)
 						&& ((chr->kills[KILLED_CIVILIANS] + chr->kills[KILLED_TEAM]) <= rank->killed_others)) {
