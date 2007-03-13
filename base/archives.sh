0e5e68da0ccc3c16e4e1a119da51fd3fa84035d8
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-03-13 16:33:10 +0000

    * remove the user settings (don't add them to the game archives) * zip command is no longer quiet

--- base/archives.sh
+++ base/archives.sh
@@ -3,18 +3,20 @@
 echo "generating archives"
 echo
 echo "pics and textures..."
-zip -ruq9 0pics.zip pics textures -i \*.jpg \*.tga \*.png \*.bmp \*.pcx
+zip -ru9 0pics.zip pics textures -i \*.jpg \*.tga \*.png \*.bmp \*.pcx
 echo "models..."
-zip -ruq9 0models.zip models -i \*.md2 \*.jpg \*.tga \*.png \*.txt \*.anm \*.tag
+zip -ru9 0models.zip models -i \*.md2 \*.jpg \*.tga \*.png \*.txt \*.anm \*.tag
 echo "sound and music..."
-zip -ruq9 0snd.zip music sound -i \*.txt \*.ogg \*.wav
+zip -ru9 0snd.zip music sound -i \*.txt \*.ogg \*.wav
 echo "maps..."
-zip -ruq9 0maps.zip maps -i \*.bsp
+zip -ru9 0maps.zip maps -i \*.bsp
 echo "media..."
-zip -ruq9 0media.zip media shaders -i \*.fp \*.vp \*.ttf COPYING
+zip -ru9 0media.zip media shaders -i \*.fp \*.vp \*.ttf COPYING
 echo "i18n..."
 echo "don't include this in the zipfiles - gettext doesn't have support for it"
 echo "ufos..."
-zip -ruq9 0ufos.zip ufos -i \*.ufo
+zip -ru9 0ufos.zip ufos -i \*.ufo
 echo "base..."
-zip -uq9 0base.zip *.cfg
+#don't add the user settings
+rm config.cfg
+zip -u9 0base.zip *.cfg
