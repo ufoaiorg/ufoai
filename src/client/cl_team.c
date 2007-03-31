7d456decda6745676dc2c70aa8651b4a022ab4a8
Maciej Wierzbicki <voovoos@users.sourceforge.net>
2007-03-31 14:39:30 +0000

    * fix for bug introduced in r7160

--- src/client/cl_team.c
+++ src/client/cl_team.c
@@ -706,7 +706,7 @@ static void CL_GenerateEquipment_f (void)
 		if ((Q_strncmp(csi.ods[i].type, "armor", MAX_VAR) == 0) && (csi.ods[i].useable != team))
 			continue;
 		/* Don't allow to show unresearched items. */
-		if (RS_IsResearched_ptr(csi.ods[i].tech))
+		if (!RS_IsResearched_ptr(csi.ods[i].tech))
 			continue;
 		while (unused.num[i]) {
 			item.t = i;
