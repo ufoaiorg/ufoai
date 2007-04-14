c9a9cafb014053f73417f19f2cc832c7c869dabb
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-04-14 09:14:11 +0000

    * include winquake.h here

--- src/ports/win32/cd_win.c
+++ src/ports/win32/cd_win.c
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <windows.h>
 #include "../../client/client.h"
+#include "winquake.h"
 
 extern	HWND	cl_hwnd;
 
