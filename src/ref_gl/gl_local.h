9a03b8d020e7b05683632fa98573c707d459b0f0
tom555 <tom555@users.sourceforge.net>
2007-04-16 07:41:43 +0000

    * fixed a missing "

--- src/ref_gl/gl_local.h
+++ src/ref_gl/gl_local.h
@@ -7,10 +7,10 @@
 All original materal Copyright (C) 2002-2007 UFO: Alien Invasion team.
 
 12/04/07, Michael Ploujnikov (Plouj):
-	Documented viddef_t.
-	Added doxygen file comment.
-	Updated copyright notice.
-	Changed some image_s struct comments.
+    Documented viddef_t.
+    Added doxygen file comment.
+    Updated copyright notice.
+    Changed some image_s struct comments.
 
 Original file from Quake 2 v3.21: quake2-2.31/ref_gl/gl_local.h
 Copyright (C) 1997-2001 Id Software, Inc.
@@ -57,42 +57,42 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "gl_arb_shader.h"
 
 #if !defined _WIN32 || ( defined(__MINGW32__) && !defined(DEVCPP ) )
-#	ifdef USE_SDL_FRAMEWORK
-#		include <SDL/SDL.h>
-#	else
+#   ifdef USE_SDL_FRAMEWORK
+#       include <SDL/SDL.h>
+#   else
 #       ifndef __APPLE__
-#		include <SDL.h>
+#       include <SDL.h>
 #       endif
-#	endif
+#   endif
 
-#	ifdef USE_SDL_TTF_FRAMEWORK
-#		include <SDL_ttf/SDL_ttf.h>
-#	else
-#		include <SDL_ttf.h>
-#	endif
+#   ifdef USE_SDL_TTF_FRAMEWORK
+#       include <SDL_ttf/SDL_ttf.h>
+#   else
+#       include <SDL_ttf.h>
+#   endif
 #else
 #   ifdef __APPLE__
 #   include <SDL.h>
-#   include <SDL/SDL_ttf.h"
+#   include <SDL/SDL_ttf.h>
 #   endif
-#	include <SDL/SDL.h>
-#	include <SDL/SDL_ttf.h>
+#   include <SDL/SDL.h>
+#   include <SDL/SDL_ttf.h>
 #endif
 
-void GL_ShutdownSDLFonts(void);	/* gl_draw.c */
+void GL_ShutdownSDLFonts(void); /* gl_draw.c */
 
 #include "qgl.h"
 
-#define	REF_VERSION	"GL 0.12"
+#define REF_VERSION "GL 0.12"
 
 /* up / down */
-#define	PITCH	0
+#define PITCH   0
 
 /* left / right */
-#define	YAW		1
+#define YAW     1
 
 /* fall over */
-#define	ROLL	2
+#define ROLL    2
 
 
 #ifndef __VIDDEF_T
@@ -111,10 +111,10 @@ void GL_ShutdownSDLFonts(void);	/* gl_draw.c */
  *
 */
 typedef struct {
-	unsigned width;		/**< game screen/window width */
-	unsigned height;	/**< game screen/window height */
-	float rx;		/**< horizontal screen scale factor */
-	float ry;		/**< vertical screen scale factor */
+    unsigned width;     /**< game screen/window width */
+    unsigned height;    /**< game screen/window height */
+    float rx;       /**< horizontal screen scale factor */
+    float ry;       /**< vertical screen scale factor */
 } viddef_t;
 #endif
 
@@ -123,21 +123,21 @@ extern viddef_t vid;
 #ifdef DEBUG
 # ifdef _MSC_VER
 #  define GL_CHECK_ERROR \
-	do { \
-		GLenum error = qglGetError(); \
-		if (error != GL_NO_ERROR) \
-			ri.Con_Printf(PRINT_ALL, "OpenGL err: %s (%d) 0x%X\n", \
-					__FILE__, __LINE__, error); \
-	} while(0);
+    do { \
+        GLenum error = qglGetError(); \
+        if (error != GL_NO_ERROR) \
+            ri.Con_Printf(PRINT_ALL, "OpenGL err: %s (%d) 0x%X\n", \
+                    __FILE__, __LINE__, error); \
+    } while(0);
 # else
 #  define GL_CHECK_ERROR \
-	do { \
-		GLenum error = qglGetError(); \
-		if (error != GL_NO_ERROR) \
-			ri.Con_Printf(PRINT_ALL, "OpenGL err: %s (%d): %s 0x%X\n", \
-					__FILE__, __LINE__, \
-					__PRETTY_FUNCTION__, error); \
-	} while(0);
+    do { \
+        GLenum error = qglGetError(); \
+        if (error != GL_NO_ERROR) \
+            ri.Con_Printf(PRINT_ALL, "OpenGL err: %s (%d): %s 0x%X\n", \
+                    __FILE__, __LINE__, \
+                    __PRETTY_FUNCTION__, error); \
+    } while(0);
 # endif
 #else
 # define GL_CHECK_ERROR
@@ -155,46 +155,46 @@ pic
 */
 
 typedef enum {
-	it_skin,
-	it_sprite,
-	it_wall,
-	it_pic,
-	it_wrappic
+    it_skin,
+    it_sprite,
+    it_wall,
+    it_pic,
+    it_wrappic
 } imagetype_t;
 
 typedef struct image_s {
-	char name[MAX_QPATH];		/* game path, including extension, must be first */
-	imagetype_t type;
-	int width, height;			/* source image dimensions */
-	int upload_width, upload_height;	/* dimensions after power of two and picmip */
-	int registration_sequence;	/* 0 = free */
-	struct msurface_s *texturechain;	/* for sort-by-texture world drawing */
-	int texnum;					/* gl texture binding */
-	float sl, tl, sh, th;		/* 0,0 - 1,1 unless part of the scrap */
-	qboolean scrap;
-	qboolean has_alpha;
-	shader_t *shader;			/* pointer to shader from refdef_t */
-	qboolean paletted;
+    char name[MAX_QPATH];       /* game path, including extension, must be first */
+    imagetype_t type;
+    int width, height;          /* source image dimensions */
+    int upload_width, upload_height;    /* dimensions after power of two and picmip */
+    int registration_sequence;  /* 0 = free */
+    struct msurface_s *texturechain;    /* for sort-by-texture world drawing */
+    int texnum;                 /* gl texture binding */
+    float sl, tl, sh, th;       /* 0,0 - 1,1 unless part of the scrap */
+    qboolean scrap;
+    qboolean has_alpha;
+    shader_t *shader;           /* pointer to shader from refdef_t */
+    qboolean paletted;
 } image_t;
 
-#define	TEXNUM_LIGHTMAPS	1024
-#define	TEXNUM_SCRAPS		1280
-#define	TEXNUM_IMAGES		1281
+#define TEXNUM_LIGHTMAPS    1024
+#define TEXNUM_SCRAPS       1280
+#define TEXNUM_IMAGES       1281
 
-#define		MAX_GLERRORTEX	4096
-#define		MAX_GLTEXTURES	1024
+#define     MAX_GLERRORTEX  4096
+#define     MAX_GLTEXTURES  1024
 
 /* starting offset for font texture numbers (used in font-cache) */
-#define	TEXNUM_FONTS		TEXNUM_IMAGES + MAX_GLTEXTURES
+#define TEXNUM_FONTS        TEXNUM_IMAGES + MAX_GLTEXTURES
 
 /*=================================================================== */
 
 typedef struct {
-	int width;
-	int height;
-	byte *captureBuffer;
-	byte *encodeBuffer;
-	qboolean motionJpeg;
+    int width;
+    int height;
+    byte *captureBuffer;
+    byte *encodeBuffer;
+    qboolean motionJpeg;
 } videoFrameCommand_t;
 
 size_t SaveJPGToBuffer(byte * buffer, int quality, int image_width, int image_height, byte * image_buffer);
@@ -210,12 +210,12 @@ void WriteTGA(FILE *f, byte *buffer, int width, int height);
 /*=================================================================== */
 
 typedef enum {
-	rserr_ok,
+    rserr_ok,
 
-	rserr_invalid_fullscreen,
-	rserr_invalid_mode,
+    rserr_invalid_fullscreen,
+    rserr_invalid_mode,
 
-	rserr_unknown
+    rserr_unknown
 } rserr_t;
 
 #include "gl_model.h"
@@ -232,23 +232,23 @@ void GL_UpdateSwapInterval(void);
 extern float gldepthmin, gldepthmax;
 
 typedef struct {
-	float x, y, z;
-	float s, t;
-	float r, g, b;
+    float x, y, z;
+    float s, t;
+    float r, g, b;
 } glvert_t;
 
 
-#define	MAX_LBM_HEIGHT		1024	/* Was 480 (some standard?), but we used some higher textures now. */
+#define MAX_LBM_HEIGHT      1024    /* Was 480 (some standard?), but we used some higher textures now. */
 
-#define BACKFACE_EPSILON	0.01
+#define BACKFACE_EPSILON    0.01
 
-#define	MAX_MOD_KNOWN	512
+#define MAX_MOD_KNOWN   512
 
 /* entity transform */
 typedef struct {
-	qboolean done;
-	qboolean processing;
-	float matrix[16];
+    qboolean done;
+    qboolean processing;
+    float matrix[16];
 } transform_t;
 
 extern transform_t trafo[MAX_ENTITIES];
@@ -286,7 +286,7 @@ extern cvar_t *r_lerpmodels;
 
 extern cvar_t *r_anisotropic;
 extern cvar_t *r_ext_max_anisotropy;
-extern cvar_t *r_texture_lod;	/* lod_bias */
+extern cvar_t *r_texture_lod;   /* lod_bias */
 extern cvar_t *r_displayrefresh;
 
 extern cvar_t *gl_vertex_arrays;
@@ -486,102 +486,102 @@ void R_DrawPtls(void);
 /*
 ** GL config stuff
 */
-#define GL_RENDERER_VOODOO		0x00000001
-#define GL_RENDERER_VOODOO2   	0x00000002
-#define GL_RENDERER_VOODOO_RUSH	0x00000004
-#define GL_RENDERER_BANSHEE		0x00000008
-#define		GL_RENDERER_3DFX		0x0000000F
+#define GL_RENDERER_VOODOO      0x00000001
+#define GL_RENDERER_VOODOO2     0x00000002
+#define GL_RENDERER_VOODOO_RUSH 0x00000004
+#define GL_RENDERER_BANSHEE     0x00000008
+#define     GL_RENDERER_3DFX        0x0000000F
 
-#define GL_RENDERER_PCX1		0x00000010
-#define GL_RENDERER_PCX2		0x00000020
-#define GL_RENDERER_PMX			0x00000040
-#define		GL_RENDERER_POWERVR		0x00000070
+#define GL_RENDERER_PCX1        0x00000010
+#define GL_RENDERER_PCX2        0x00000020
+#define GL_RENDERER_PMX         0x00000040
+#define     GL_RENDERER_POWERVR     0x00000070
 
-#define GL_RENDERER_PERMEDIA2	0x00000100
-#define GL_RENDERER_GLINT_MX	0x00000200
-#define GL_RENDERER_GLINT_TX	0x00000400
-#define GL_RENDERER_3DLABS_MISC	0x00000800
-#define		GL_RENDERER_3DLABS	0x00000F00
+#define GL_RENDERER_PERMEDIA2   0x00000100
+#define GL_RENDERER_GLINT_MX    0x00000200
+#define GL_RENDERER_GLINT_TX    0x00000400
+#define GL_RENDERER_3DLABS_MISC 0x00000800
+#define     GL_RENDERER_3DLABS  0x00000F00
 
-#define GL_RENDERER_REALIZM		0x00001000
-#define GL_RENDERER_REALIZM2	0x00002000
-#define		GL_RENDERER_INTERGRAPH	0x00003000
+#define GL_RENDERER_REALIZM     0x00001000
+#define GL_RENDERER_REALIZM2    0x00002000
+#define     GL_RENDERER_INTERGRAPH  0x00003000
 
-#define GL_RENDERER_3DPRO		0x00004000
-#define GL_RENDERER_REAL3D		0x00008000
-#define GL_RENDERER_RIVA128		0x00010000
-#define GL_RENDERER_DYPIC		0x00020000
+#define GL_RENDERER_3DPRO       0x00004000
+#define GL_RENDERER_REAL3D      0x00008000
+#define GL_RENDERER_RIVA128     0x00010000
+#define GL_RENDERER_DYPIC       0x00020000
 
-#define GL_RENDERER_V1000		0x00040000
-#define GL_RENDERER_V2100		0x00080000
-#define GL_RENDERER_V2200		0x00100000
-#define		GL_RENDERER_RENDITION	0x001C0000
+#define GL_RENDERER_V1000       0x00040000
+#define GL_RENDERER_V2100       0x00080000
+#define GL_RENDERER_V2200       0x00100000
+#define     GL_RENDERER_RENDITION   0x001C0000
 
 #define GL_RENDERER_O2          0x00100000
 #define GL_RENDERER_IMPACT      0x00200000
-#define GL_RENDERER_RE			0x00400000
-#define GL_RENDERER_IR			0x00800000
-#define		GL_RENDERER_SGI			0x00F00000
+#define GL_RENDERER_RE          0x00400000
+#define GL_RENDERER_IR          0x00800000
+#define     GL_RENDERER_SGI         0x00F00000
 
-#define GL_RENDERER_MCD			0x01000000
-#define GL_RENDERER_OTHER		0x80000000
+#define GL_RENDERER_MCD         0x01000000
+#define GL_RENDERER_OTHER       0x80000000
 
 typedef struct {
-	int renderer;
-	const char *renderer_string;
-	const char *vendor_string;
-	const char *version_string;
-	const char *extensions_string;
-	int maxTextureSize;
+    int renderer;
+    const char *renderer_string;
+    const char *vendor_string;
+    const char *version_string;
+    const char *extensions_string;
+    int maxTextureSize;
 } glconfig_t;
 
 typedef struct {
-	float inverse_intensity;
-	int displayrefresh;
-	qboolean fullscreen;
+    float inverse_intensity;
+    int displayrefresh;
+    qboolean fullscreen;
 
-	int prev_mode;
+    int prev_mode;
 
-	unsigned char *d_16to8table;
+    unsigned char *d_16to8table;
 
-	int lightmap_textures;
+    int lightmap_textures;
 
-	int currenttextures[2];
-	int currenttmu;
+    int currenttextures[2];
+    int currenttmu;
 
-	qboolean hwgamma;
+    qboolean hwgamma;
 
-	float camera_separation;
-	qboolean stereo_enabled;
+    float camera_separation;
+    qboolean stereo_enabled;
 
-	qboolean stencil_two_side;
-	qboolean ati_separate_stencil;
+    qboolean stencil_two_side;
+    qboolean ati_separate_stencil;
 
-	int max_lod;
+    int max_lod;
 
-	qboolean blend;
-	qboolean alpha_test;
-	qboolean fog_coord;
+    qboolean blend;
+    qboolean alpha_test;
+    qboolean fog_coord;
 
-	qboolean stencil_wrap;
-	qboolean anisotropic;
-	qboolean lod_bias;
-	qboolean arb_fragment_program;
-	qboolean glsl_program;
+    qboolean stencil_wrap;
+    qboolean anisotropic;
+    qboolean lod_bias;
+    qboolean arb_fragment_program;
+    qboolean glsl_program;
 
-	unsigned char originalRedGammaTable[256];
-	unsigned char originalGreenGammaTable[256];
-	unsigned char originalBlueGammaTable[256];
+    unsigned char originalRedGammaTable[256];
+    unsigned char originalGreenGammaTable[256];
+    unsigned char originalBlueGammaTable[256];
 } glstate_t;
 
 extern glconfig_t gl_config;
 extern glstate_t gl_state;
 
-#define GLSTATE_DISABLE_ALPHATEST	if (gl_state.alpha_test) { qglDisable(GL_ALPHA_TEST); gl_state.alpha_test=qfalse; }
-#define GLSTATE_ENABLE_ALPHATEST	if (!gl_state.alpha_test) { qglEnable(GL_ALPHA_TEST); gl_state.alpha_test=qtrue; }
+#define GLSTATE_DISABLE_ALPHATEST   if (gl_state.alpha_test) { qglDisable(GL_ALPHA_TEST); gl_state.alpha_test=qfalse; }
+#define GLSTATE_ENABLE_ALPHATEST    if (!gl_state.alpha_test) { qglEnable(GL_ALPHA_TEST); gl_state.alpha_test=qtrue; }
 
-#define GLSTATE_DISABLE_BLEND		if (gl_state.blend) { qglDisable(GL_BLEND); gl_state.blend=qfalse; }
-#define GLSTATE_ENABLE_BLEND		if (!gl_state.blend) { qglEnable(GL_BLEND); gl_state.blend=qtrue; }
+#define GLSTATE_DISABLE_BLEND       if (gl_state.blend) { qglDisable(GL_BLEND); gl_state.blend=qfalse; }
+#define GLSTATE_ENABLE_BLEND        if (!gl_state.blend) { qglEnable(GL_BLEND); gl_state.blend=qtrue; }
 
 /*
 ====================================================================
@@ -610,10 +610,10 @@ void GLimp_SetGamma(void);
 
 /* 3d globe */
 
-#define PHYSICAL_EARTH_RADIUS 6378137	/* Earth's radius, in meters. (From RFC1876) */
-#define SPEED_OF_LIGHT 299792458		/* The big C, in meters/second. */
+#define PHYSICAL_EARTH_RADIUS 6378137   /* Earth's radius, in meters. (From RFC1876) */
+#define SPEED_OF_LIGHT 299792458        /* The big C, in meters/second. */
 #define PHYSICAL_EARTH_CIRC (2.0 * M_PI * PHYSICAL_EARTH_RADIUS)
-#define NOT_SELECTABLE ~0U				/* For picking reasons, see which_site() */
+#define NOT_SELECTABLE ~0U              /* For picking reasons, see which_site() */
 
 void Draw_3DGlobe(int x, int y, int w, int h, float p, float q, vec3_t rotate, float zoom, const char *map);
 void Draw_3DMapLine(vec3_t angles, float zoom, int n, float dist, vec2_t * path);
@@ -622,4 +622,4 @@ void Draw_3DMapMarkers(vec3_t angles, float zoom, float latitude, float longitud
 /* end of 3d globe */
 
 
-#endif							/* GL_LOCAL_H */
+#endif                          /* GL_LOCAL_H */
