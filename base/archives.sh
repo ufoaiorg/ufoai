c3fa87e70cc4df0f152e4eddf88f550fea77e386
Yann Dirson <ydirson@users.sourceforge.net>
2007-04-23 21:52:21 +0000

    Split music into its own .pk3, since it's an optional component.

--- base/archives.sh
+++ base/archives.sh
@@ -5,24 +5,26 @@ EXT=pk3
 echo "generating archives"
 echo
 echo "pics and textures..."
-(find pics -type f -print; find textures -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.jpg$/||/\.tga$/||/\.pcx$/||/\.bmp$/||/\.png/ {print}' | xargs zip -u9@ 0pics.${EXT}
+(find pics -type f -print; find textures -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.jpg$/||/\.tga$/||/\.pcx$/||/\.bmp$/||/\.png$/ {print}' | xargs zip -u9@ 0pics.${EXT}
 #zip -ru9 0pics.zip pics textures -i \*.jpg \*.tga \*.png \*.bmp \*.pcx
 echo "models..."
-find models -type f -print | awk '{if ($0 !~ /\.svn/) {if ($0 !~ /\/misc_source_files\//) {print}}}' | awk '/\.md2$/||/\.jpg$/||/\.png$/||/\.tga$/||/\.anm$/||/\.txt$/||/\.tag/ {print}' | xargs zip -u9@ 0models.${EXT}
+find models -type f -print | awk '{if ($0 !~ /\.svn/) {if ($0 !~ /\/misc_source_files\//) {print}}}' | awk '/\.md2$/||/\.jpg$/||/\.png$/||/\.tga$/||/\.anm$/||/\.txt$/||/\.tag$/ {print}' | xargs zip -u9@ 0models.${EXT}
 #zip -ru9 0models.zip models -i \*.md2 \*.jpg \*.tga \*.png \*.txt \*.anm \*.tag
-echo "sound and music..."
-(find music -type f -print; find sound -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.ogg$/||/\.txt$/||/\.wav/ {print}' | xargs zip -u9@ 0snd.${EXT}
+echo "sound..."
+(find sound -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.txt$/||/\.wav$/ {print}' | xargs zip -u9@ 0snd.${EXT}
+echo "music..."
+(find music -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.ogg$/||/\.txt$/ {print}' | xargs zip -u9@ 0music.${EXT}
 #zip -ru9 0snd.zip music sound -i \*.txt \*.ogg \*.wav
 echo "maps..."
-find maps -type f -print | egrep -v '(\/\.svn\/)' | awk '/\.bsp$/||/\.ump/ {print}' | xargs zip -u9@ 0maps.${EXT}
+find maps -type f -print | egrep -v '(\/\.svn\/)' | awk '/\.bsp$/||/\.ump$/ {print}' | xargs zip -u9@ 0maps.${EXT}
 #zip -ru9 0maps.zip maps -i \*.bsp
 echo "media..."
-(find media -type f -print; find shaders -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.fp$/||/\.vp$/||/\.ttf/ {print}' | xargs zip -u9@ 0media.${EXT}
+(find media -type f -print; find shaders -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.fp$/||/\.vp$/||/\.ttf$/ {print}' | xargs zip -u9@ 0media.${EXT}
 #zip -ru9 0media.zip media shaders -i \*.fp \*.vp \*.ttf COPYING
 echo "i18n..."
 echo "don't include this in the zipfiles - gettext doesn't have support for it"
 echo "ufos..."
-find ufos -type f -print | egrep -v '(\/\.svn\/)' | awk '/\.ufo/ {print}' | xargs zip -u9@ 0ufos.${EXT}
+find ufos -type f -print | egrep -v '(\/\.svn\/)' | awk '/\.ufo$/ {print}' | xargs zip -u9@ 0ufos.${EXT}
 #zip -ru9 0ufos.zip ufos -i \*.ufo
 echo "base..."
 #don't add the user settings
