2460e1d5996112a60c2e05b76ae9303d6a71e5c0
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-04-24 17:24:15 +0000

    * merged 7876 and 7881 from trunk to 2.1 branch

--- src/client/cl_basemanagement.c
+++ src/client/cl_basemanagement.c
@@ -2645,6 +2645,8 @@ extern qboolean B_Load (sizebuf_t* sb, void* data)
 		b->numAircraftInBase = MSG_ReadByte(sb);
 		for (k = 0; k < b->numAircraftInBase; k++) {
 			aircraft = CL_GetAircraft(MSG_ReadString(sb));
+			if (!aircraft)
+				return qfalse;
 			memcpy(&b->aircraft[k], aircraft, sizeof(aircraft_t));
 			aircraft = &b->aircraft[k];
 			aircraft->idx = MSG_ReadShort(sb);
