c411c28016db82b9ff093dd100cfc828ee67dc77
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-04-30 05:49:02 +0000

    * savegames: fixed range error if no aircraftCurrent set in the base (break savegames again)

--- src/client/cl_basemanagement.c
+++ src/client/cl_basemanagement.c
@@ -2700,7 +2700,7 @@ extern qboolean B_Save (sizebuf_t* sb, void* data)
 		MSG_WriteByte(sb, b->baseStatus);
 
 		MSG_WriteByte(sb, b->aircraftCurrent);
-		MSG_WriteByte(sb, b->numAircraftInBase);
+		MSG_WriteShort(sb, b->aircraftCurrent); /* might be -1 */
 		for (k = 0; k < b->numAircraftInBase; k++) {
 			aircraft = &b->aircraft[k];
 			MSG_WriteString(sb, aircraft->id);
@@ -2836,7 +2836,7 @@ extern qboolean B_Load (sizebuf_t* sb, void* data)
 		gd.numBuildings[i] = MSG_ReadShort(sb);
 		b->condition = MSG_ReadByte(sb);
 		b->baseStatus = MSG_ReadByte(sb);
-		b->aircraftCurrent = MSG_ReadByte(sb);
+		b->aircraftCurrent = MSG_ReadShort(sb);
 		b->numAircraftInBase = MSG_ReadByte(sb);
 		for (k = 0; k < b->numAircraftInBase; k++) {
 			aircraft = AIR_GetAircraft(MSG_ReadString(sb));
