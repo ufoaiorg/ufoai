856b0d213cbe8ade6bb1a389fb8e33903cd498e8
tritol <tritol@users.sourceforge.net>
2007-05-01 09:37:58 +0000

    * install.mk, tar.ex_src: fixed sourcearchive target (no copying at all), please verify

--- build/install.mk
+++ build/install.mk
@@ -1,6 +1,7 @@
 VERSION=$(shell grep UFO_VERSION src/qcommon/qcommon.h | sed -e 's/.*UFO_VERSION \"\(.*\)\"/\1/')
 
 installer: win32installer linuxinstaller sourcearchive
+
 mappack:
 	tar -cvjp --exclude-from=src/ports/linux/tar.ex -f ufoai-$(VERSION)-mappack.tar.bz2 ./base/maps
 
@@ -17,22 +18,24 @@ linuxinstaller:
 	$(MAKE) -f src/ports/linux/installer/Makefile
 
 sourcearchive:
-	mkdir -p ./ufoai-$(VERSION)-source
-	mkdir -p tarsrc
-	cp -rf * ./ufoai-$(VERSION)-source
-	mv ./ufoai-$(VERSION)-source  ./tarsrc
-	tar -cj \
-		-X src/ports/linux/tar.ex \
+	if [ -x ./ufo ] ; then $(MAKE) clean ; fi
+	mkdir -p ./tarsrc
+	ln -s ../ tarsrc/ufoai-$(VERSION)-source
+	tar -cvjh \
+		-X src/ports/linux/tar.ex_src \
+		--exclude ufoai-$(VERSION)-source/Makefile \
+		--exclude ufoai-$(VERSION)-source/config.h \
 		-f ufoai-$(VERSION)-source.tar.bz2 \
-		-C tarsrc ufoai-$(VERSION)-source
+		-C ./tarsrc ufoai-$(VERSION)-source
 	rm -rf ./tarsrc
 
-packfiles:
-	cd base; zip -r 0textures.zip textures -x \*.svn\*
-	cd base; zip -r 0sounds.zip sound -x \*.svn\*
-	cd base; zip -r 0pics.zip pics -x \*.svn\*
-	cd base; zip -r 0music.zip music -x \*.svn\*
-	cd base; zip -r 0maps.zip maps -x \*.svn\*
-	cd base; zip -r 0models.zip models -x \*.svn\*
-	cd base; zip -r 0ufos.zip ufos -x \*.svn\*
+# this done by base/archives.sh
+#packfiles:
+#	cd base; zip -r 0textures.zip textures -x \*.svn\*
+#	cd base; zip -r 0sounds.zip sound -x \*.svn\*
+#	cd base; zip -r 0pics.zip pics -x \*.svn\*
+#	cd base; zip -r 0music.zip music -x \*.svn\*
+#	cd base; zip -r 0maps.zip maps -x \*.svn\*
+#	cd base; zip -r 0models.zip models -x \*.svn\*
+#	cd base; zip -r 0ufos.zip ufos -x \*.svn\*
 
