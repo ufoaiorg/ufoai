9d8a2ed713e7a0a0e1f2e2037c2a5f54fb654b49
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-05-22 18:07:41 +0000

    * redruced MAX_ALIENCONT_CAP from 512 to 32

--- src/client/cl_aliencont.h
+++ src/client/cl_aliencont.h
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define CLIENT_CL_ALIENCONT_H
 
 #define MAX_CARGO		32
-#define MAX_ALIENCONT_CAP	512
+#define MAX_ALIENCONT_CAP	32
 
 /** types of aliens */
 typedef enum {
