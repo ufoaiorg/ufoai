ed9dd21d8a4793a8ca775583cc011b68a71cf695
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-05-24 19:13:06 +0000

    * fixed sync-po target (thanks Zenerka)

--- build/lang.mk
+++ build/lang.mk
@@ -16,9 +16,13 @@ update-po:
 	$(MAKE) -C src/po update-po
 
 po-sync:
-	@cd src/po
+	@echo "This will sync all po files with the wiki articles - run update-po before this step"
+	@echo "Gamers don't to do this - translators should use ./src/po/update_po_from_wiki <lang> directly"
+	@echo "Hit any key if you are sure you really want to start the sync"
 	@pofiles='$(POFILES)'; \
+	read enter; cd src/po; \
 	for po in $$pofiles; do \
-	  po=`basename $$po|awk -F\. '{print $1}'`; \
-	  ./update_po_from_wiki $$po;
+	  po=`basename $$po .po`; \
+	  echo $$po; \
+	  ./update_po_from_wiki.sh $$po; \
 	done
