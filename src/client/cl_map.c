f8509473c19fa53bf082d6cc5106f2bed428b568
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-06-13 07:08:33 +0000

    * added textures for different base terrains

--- src/client/cl_map.c
+++ src/client/cl_map.c
@@ -1505,6 +1505,7 @@ extern nation_t* MAP_GetNation (const vec2_t pos)
  * @param[in] byte the color value to the the zone type from
  * @return returns the zone name - the first char is used for base assemble themes
  * @note never may return a null pointer or an empty string
+ * @note Make sure, that there are textures with the same name in base/textures/tex_bases
  */
 extern const char* MAP_GetZoneType (byte* color)
 {
