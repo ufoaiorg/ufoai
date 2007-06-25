9625b95d6464f53fe583c13515f88701a46fdee1
Werner Höhrer <hoehrer@users.sourceforge.net>
2007-06-25 10:03:41 +0000

    * Cleanup of whitespaces.

--- src/ref_gl/gl_md3.h
+++ src/ref_gl/gl_md3.h
@@ -4,56 +4,56 @@
  * -Harven
  */
 
-typedef struct maliascoord_s {
-	vec2_t			st;
+typedef	struct	maliascoord_s {
+	vec2_t	st;
 } maliascoord_t;
 
-typedef struct maliasvertex_s {
-	vec3_t			point;
-	vec3_t			normal;
+typedef	struct	maliasvertex_s {
+	vec3_t	point;
+	vec3_t	normal;
 } maliasvertex_t;
 
-typedef struct {
-    vec3_t			mins, maxs;
-    vec3_t			translate;
-    float			radius;
+typedef	struct {
+	vec3_t	mins, maxs;
+	vec3_t	translate;
+	float	radius;
 } maliasframe_t;
 
-typedef struct {
-	char			name[MD3_MAX_PATH];
+typedef	struct {
+	char	name[MD3_MAX_PATH];
 	dorientation_t	orient;
 } maliastag_t;
 
-typedef struct {
-	char			name[MD3_MAX_PATH];
-	int				shader;
+typedef	struct {
+	char	name[MD3_MAX_PATH];
+	int	shader;
 } maliasskin_t;
 
-typedef struct {
-    int				num_verts;
-	char			name[MD3_MAX_PATH];
+typedef	struct {
+	int	num_verts;
+	char	name[MD3_MAX_PATH];
 	maliasvertex_t	*vertexes;
 	maliascoord_t	*stcoords;
 
-    int				num_tris;
-    index_t			*indexes;
-	int				*trneighbors;
+	int	num_tris;
+	index_t	*indexes;
+	int	*trneighbors;
 
-    int				num_skins;
+	int		num_skins;
 	maliasskin_t	*skins;
 } maliasmesh_t;
 
-typedef struct maliasmodel_s {
-    int				num_frames;
+typedef	struct	maliasmodel_s {
+	int		num_frames;
 	maliasframe_t	*frames;
 
-    int				num_tags;
-	maliastag_t		*tags;
+	int		num_tags;
+	maliastag_t	*tags;
 
-    int				num_meshes;
+	int		num_meshes;
 	maliasmesh_t	*meshes;
 
-	int				num_skins;
+	int		num_skins;
 	maliasskin_t	*skins;
 } maliasmodel_t;
 
