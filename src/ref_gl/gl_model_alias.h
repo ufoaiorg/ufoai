fe1d0ccf1d7be2464a584403575b1ee4a8b13bf9
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-06-25 16:20:12 +0000

    * moved generic alias struct into gl_model_alias.h

--- src/ref_gl/gl_model_alias.h
+++ src/ref_gl/gl_model_alias.h
@@ -23,6 +23,66 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
+#define MODEL_MAX_PATH 64
+
+typedef	struct	maliascoord_s {
+	vec2_t	st;
+} maliascoord_t;
+
+typedef	struct	maliasvertex_s {
+	vec3_t	point;
+	vec3_t	normal;
+} maliasvertex_t;
+
+typedef	struct {
+	vec3_t	mins, maxs;
+	vec3_t	translate;
+	float	radius;
+} maliasframe_t;
+
+typedef struct {
+	vec3_t origin;
+	float axis[3][3];
+} maliastagorientation_t;
+
+typedef	struct {
+	char	name[MODEL_MAX_PATH];
+	maliastagorientation_t	orient;
+} maliastag_t;
+
+typedef	struct {
+	char	name[MODEL_MAX_PATH];
+	int	shader;
+} maliasskin_t;
+
+typedef	struct {
+	int	num_verts;
+	char	name[MODEL_MAX_PATH];
+	maliasvertex_t	*vertexes;
+	maliascoord_t	*stcoords;
+
+	int	num_tris;
+	index_t	*indexes;
+	int	*trneighbors;
+
+	int		num_skins;
+	maliasskin_t	*skins;
+} maliasmesh_t;
+
+typedef	struct	maliasmodel_s {
+	int		num_frames;
+	maliasframe_t	*frames;
+
+	int		num_tags;
+	maliastag_t	*tags;
+
+	int		num_meshes;
+	maliasmesh_t	*meshes;
+
+	int		num_skins;
+	maliasskin_t	*skins;
+} maliasmodel_t;
+
 void Mod_BuildTriangleNeighbors(neighbors_t *neighbors, dtriangle_t *tris, int numtris);
 int Mod_FindTriangleWithEdge(neighbors_t *neighbors, dtriangle_t *tris, int numtris, int triIndex, int edgeIndex);
 void Mod_LoadAnims(model_t * mod, void *buffer);
--- src/ref_gl/gl_model_md3.h
+++ src/ref_gl/gl_model_md3.h
@@ -23,58 +23,5 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-typedef	struct	maliascoord_s {
-	vec2_t	st;
-} maliascoord_t;
-
-typedef	struct	maliasvertex_s {
-	vec3_t	point;
-	vec3_t	normal;
-} maliasvertex_t;
-
-typedef	struct {
-	vec3_t	mins, maxs;
-	vec3_t	translate;
-	float	radius;
-} maliasframe_t;
-
-typedef	struct {
-	char	name[MD3_MAX_PATH];
-	dorientation_t	orient;
-} maliastag_t;
-
-typedef	struct {
-	char	name[MD3_MAX_PATH];
-	int	shader;
-} maliasskin_t;
-
-typedef	struct {
-	int	num_verts;
-	char	name[MD3_MAX_PATH];
-	maliasvertex_t	*vertexes;
-	maliascoord_t	*stcoords;
-
-	int	num_tris;
-	index_t	*indexes;
-	int	*trneighbors;
-
-	int		num_skins;
-	maliasskin_t	*skins;
-} maliasmesh_t;
-
-typedef	struct	maliasmodel_s {
-	int		num_frames;
-	maliasframe_t	*frames;
-
-	int		num_tags;
-	maliastag_t	*tags;
-
-	int		num_meshes;
-	maliasmesh_t	*meshes;
-
-	int		num_skins;
-	maliasskin_t	*skins;
-} maliasmodel_t;
-
 void R_DrawAliasMD3Model(entity_t *e);
 void Mod_LoadAliasMD3Model(model_t *mod, void *buffer, int bufSize);
