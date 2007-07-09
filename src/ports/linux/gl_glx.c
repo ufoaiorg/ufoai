61c0173d3140f9ab9e463f4b1b65766276c2539a
Maciej Wierzbicki <voovoos@users.sourceforge.net>
2007-07-09 21:34:56 +0000

    * fixed typo found by asuffield

--- src/ports/linux/gl_glx.c
+++ src/ports/linux/gl_glx.c
@@ -28,7 +28,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
-#if !(defined__FreeBSD__ || defined __NetBSD__ || defined __sun)
+#if !(defined __FreeBSD__ || defined __NetBSD__ || defined __sun)
 #	include <sys/vt.h>
 #endif
 #include <signal.h>
