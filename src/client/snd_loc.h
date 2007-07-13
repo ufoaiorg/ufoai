d67fed9136a63672bc68be202b850ac57c4e42a7
Maciej Wierzbicki <voovoos@users.sourceforge.net>
2007-07-13 08:39:50 +0000

    * #include "cl_sound.h" (fixes broken devcpp compilation)

--- src/client/snd_loc.h
+++ src/client/snd_loc.h
@@ -29,6 +29,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef CLIENT_SND_LOC_H
 #define CLIENT_SND_LOC_H
 
+#include "cl_sound.h"
+
 /* !!! if this is changed, the asm code must change !!! */
 typedef struct {
 	int left;
