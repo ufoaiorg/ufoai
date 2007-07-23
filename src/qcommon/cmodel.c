d6076d4132c74d4a994e533a744fbca6b825c950
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-07-23 12:10:34 +0000

    * seams like merging is broken these days ;-)

--- src/qcommon/cmodel.c
+++ src/qcommon/cmodel.c
@@ -30,7 +30,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define GRENADE_ALPHAFAC	0.7
 #define GRENADE_MINALPHA	M_PI/6
 #define GRENADE_MAXALPHA	M_PI*7/16
-#define QUANT		4
 
 /* 1/32 epsilon to keep floating point happy */
 #define	DIST_EPSILON	(0.03125)
