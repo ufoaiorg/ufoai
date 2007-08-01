145547224f1230ba8718b8e6b1248929422edcf8
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-08-01 07:28:13 +0000

    * removed unused sound modifier flags

--- src/game/q_shared.h
+++ src/game/q_shared.h
@@ -625,9 +625,6 @@ typedef struct {
 #define	CHAN_VOICE              2
 #define	CHAN_ITEM               3
 #define	CHAN_BODY               4
-/* modifier flags */
-#define	CHAN_NO_PHS_ADD			8	/* send to all clients, not just ones in PHS (ATTN 0 will also do this) */
-#define	CHAN_RELIABLE			16	/* send by reliable message, not datagram */
 
 /* sound attenuation values */
 #define ATTN_NONE               0   /* full volume the entire level */
