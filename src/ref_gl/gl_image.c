4b49927c62630fd34c2b0742e22f9e2d0968747c
Christoph Mallon <dertron@users.sourceforge.net>
2007-08-20 08:02:44 +0000

    * Suppress a warning when both jpeglib and SDL declare the macro HAVE_STDLIB_H.

--- src/ref_gl/gl_image.c
+++ src/ref_gl/gl_image.c
@@ -25,6 +25,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "gl_local.h"
 
+/* Workaround for a warning about redeclaring the macro. jpeglib sets this macro
+ * and SDL, too. */
+#undef HAVE_STDLIB_H
+
 #include <jpeglib.h>
 #include <png.h>
 
