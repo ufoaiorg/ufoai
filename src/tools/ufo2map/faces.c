843672f892c60b3e27c67dfb92ce7d2354008060
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-08-31 11:56:14 +0000

    * fixed light emitting surfaces not emitting downwards

--- src/tools/ufo2map/faces.c
+++ src/tools/ufo2map/faces.c
@@ -843,7 +843,10 @@ static face_t *FaceFromPortal (portal_t *p, int pside)
 
 	/* do back-clipping */
 	if (!config.nobackclip && mapplanes[f->planenum].normal[2] < -0.9)
-		return NULL;
+		/* this face is not visible from birds view - optimize away
+		 * but only if it's not light emitting surface */
+		if (!texinfo[f->texinfo].flags & SURF_LIGHT)
+			return NULL;
 
 	if (texinfo[f->texinfo].flags & SURF_NODRAW)
 		return NULL;
