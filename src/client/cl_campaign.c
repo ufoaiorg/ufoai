6907bb0e458086c1ea4270939bc1b4f395ef1d2b
Bas Fournier <btaxis@users.sourceforge.net>
2007-09-04 12:11:50 +0000

    * Updated string

--- src/client/cl_campaign.c
+++ src/client/cl_campaign.c
@@ -923,7 +923,7 @@ static void CP_CheckLostCondition (qboolean lost, mission_t* mission, int civili
 		curCampaign->civiliansKilledUntilLost -= civiliansKilled;
 		if (curCampaign->civiliansKilledUntilLost <= 0) {
 			/* lost the game */
-			menuText[TEXT_STANDARD] = _("Under your command, PHALANX operations have caused the deaths of too many innocent civilians.");
+			menuText[TEXT_STANDARD] = _("Under your command, PHALANX operations have consistently failed to protect innocent civilians. Dozens have lost their lives through your inability to handle the alien threat. The UN, highly unsatisfied with your performance, has decided to remove you from command and subsequently disbands the PHALANX project as an effective task force. No further attempts at global cooperation are made. Earth's nations each try to stand alone against the aliens, and eventually fall one by one.");
 			endCampaign = qtrue;
 		}
 	}
