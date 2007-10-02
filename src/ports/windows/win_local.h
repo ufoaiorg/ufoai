b083d9053989a76650eb2cdad11d348150e653f1
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-10-02 21:14:07 +0000

    * removed cl_sound.h from win_local.h

--- src/ports/windows/win_local.h
+++ src/ports/windows/win_local.h
@@ -28,7 +28,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <windows.h>
 #include <shlobj.h>
-#include "cl_sound.h"
 
 #ifndef DWORD_PTR
 # ifndef _WIN64
