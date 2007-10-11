7c70c2df4900984d29b30865cc640a755a682de6
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-10-11 08:27:27 +0000

    * merged r12356 (actor positions hack) and r12357 (selActor=>le) from trunk to 2.2 branch

--- src/client/cl_le.c
+++ src/client/cl_le.c
@@ -588,8 +588,18 @@ static void LET_PathMove (le_t * le)
 		} else {
 			/* end of move */
 			le_t *floor;
+			pos3_t gridPos;
 
-			CL_ConditionalMoveCalc(&clMap, selActor, MAX_ROUTE);
+			/* HACK: Attempt to correct actor positions */
+			VecToPos(le->origin, gridPos);
+			if (!VectorCompare(gridPos, le->pos)) {
+				Com_Printf("LET_PathMove: Warning: Actor positions doesn't match "
+					"(origin grid: %i:%i:%i, le->pos: %i:%i:%i)\n",
+					gridPos[0], gridPos[1], gridPos[2], le->pos[0], le->pos[1], le->pos[2]);
+				VectorCopy(gridPos, le->pos);
+			}
+
+			CL_ConditionalMoveCalc(&clMap, le, MAX_ROUTE);
 
 			/* link any floor container into the actor temp floor container */
 			floor = LE_Find(ET_ITEM, le->pos);
