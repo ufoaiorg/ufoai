2f84bbb7c8c065f61a21c7f9d94abbcaa8b01ff4
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-10-17 06:06:55 +0000

    * reverted parts of r12489 (Changed the difficulty base value) - the Com_Printf is now a Com_DPrintf again

--- src/game/g_combat.c
+++ src/game/g_combat.c
@@ -356,15 +356,15 @@ static void G_Damage (edict_t * ent, fireDef_t *fd, int damage, edict_t * attack
 	if (damage > 0) {
 		if (ent->i.c[gi.csi->idArmour]) {
 			objDef_t *ad = &gi.csi->ods[ent->i.c[gi.csi->idArmour]->item.t];
-			Com_Printf("G_Damage: damage for '%s': %i, dmgweight (%i) protection: %i",
+			Com_DPrintf(DEBUG_GAME, "G_Damage: damage for '%s': %i, dmgweight (%i) protection: %i",
 				ent->chr.name, damage, fd->dmgweight, ad->protection[fd->dmgweight]);
 			damage = max(1, damage - ad->protection[fd->dmgweight]);
 		} else {
-			Com_Printf("G_Damage: damage for '%s': %i, dmgweight (%i) protection: 0",
+			Com_DPrintf(DEBUG_GAME, "G_Damage: damage for '%s': %i, dmgweight (%i) protection: 0",
 				ent->chr.name, damage, fd->dmgweight);
 		}
 	}
-	Com_Printf(" Total damage: %d\n", damage);
+	Com_DPrintf(DEBUG_GAME, " Total damage: %d\n", damage);
 
 	assert((attacker->team >= 0) && (attacker->team < MAX_TEAMS));
 	assert((ent->team >= 0) && (ent->team < MAX_TEAMS));
