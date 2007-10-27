bf4976ea39601e1477257080e223e483266b6969
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-10-27 09:12:56 +0000

    * added ROOT_DIR and INST_DIR * changed UFO.app to UFOAI.app

--- macinstall.sh
+++ macinstall.sh
@@ -1,32 +1,35 @@
 #!/bin/sh
 
+ROOT_DIR="../../../"
+INST_DIR="./"
+
 echo "Deleting old app bundle (if any...)"
-rm -rf UFO.app
+rm -rf $(INST_DIR)UFOAI.app
 
 echo "Creating app bundle..."
-mkdir -p UFO.app/Contents/MacOS
-mkdir -p UFO.app/Contents/Frameworks
-mkdir -p UFO.app/Contents/Libraries
+mkdir -p $(INST_DIR)UFOAI.app/Contents/MacOS
+mkdir -p $(INST_DIR)UFOAI.app/Contents/Frameworks
+mkdir -p $(INST_DIR)UFOAI.app/Contents/Libraries
 
 echo "Copying content to it..."
-cp ufo UFO.app/Contents/MacOS
+cp ufo $(INST_DIR)UFOAI.app/Contents/MacOS
 
-mkdir UFO.app/base
-cp -v base/*.pk3 UFO.app/base/
-cp -r base/i18n UFO.app/base/i18n
-cp -v base/game.dylib UFO.app/base/
-cp -v base/*.cfg UFO.app/base/
+mkdir -p $(INST_DIR)UFOAI.app/base
+cp -v $(ROOT_DIR)base/*.pk3 $(INST_DIR)UFOAI.app/base/
+cp -r $(ROOT_DIR)base/i18n $(INST_DIR)UFOAI.app/base/i18n
+cp -v $(ROOT_DIR)base/game.dylib $(INST_DIR)UFOAI.app/base/
+cp -v $(ROOT_DIR)base/*.cfg $(INST_DIR)UFOAI.app/base/
 
 echo "Fixing libraries..."
 
 perl macfixlibs.pl
 
 echo "Creating dmg..."
-IMGNAME='ufo-mac-distrib'
+IMGNAME='ufoai-mac-distrib'
 rm -rf $IMGNAME
 rm -f $IMGNAME.dmg
 mkdir $IMGNAME
-mv UFO.app $IMGNAME
+mv UFOAI.app $IMGNAME
 cp README $IMGNAME
 cp CONTRIBUTORS $IMGNAME
 cp COPYING $IMGNAME
