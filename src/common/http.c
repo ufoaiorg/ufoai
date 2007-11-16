c423a804e3574986fb9bcce552d559de2fc38481
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-11-16 07:59:15 +0000

    * new cvar http_timeout * http_proxy is now an archive cvar * cvar description

--- src/common/http.c
+++ src/common/http.c
@@ -99,7 +99,7 @@ char* HTTP_GetURL (const char *url)
 	dl.curl = curl_easy_init();
 
 	Com_sprintf(dl.URL, sizeof(dl.URL), url);
-
+	curl_easy_setopt(dl.curl, CURLOPT_CONNECTTIMEOUT, http_timeout->integer);
 	curl_easy_setopt(dl.curl, CURLOPT_ENCODING, "");
 	curl_easy_setopt(dl.curl, CURLOPT_NOPROGRESS, 1);
 	curl_easy_setopt(dl.curl, CURLOPT_WRITEDATA, &dl);
