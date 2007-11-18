5ac41ae5509063cc9bff997c9dc71034c0b71279
Olivier Alloschery <kracken@users.sourceforge.net>
2007-11-18 08:08:41 +0000

    * sorry, this shouldn't have been included in r12933

--- src/client/cl_team.c
+++ src/client/cl_team.c
@@ -944,8 +944,6 @@ static void CL_Select_f (void)
 	selectSoldierModes_t mode;
 	employeeType_t employeeType;
 
-	Com_Printf("Enter function\n");
-
 	/* check syntax */
 	if (Cmd_Argc() < 2) {
 		Com_Printf("Usage: %s <num>\n", Cmd_Argv(0));
@@ -972,8 +970,6 @@ static void CL_Select_f (void)
 	else
 		return;
 
-	Com_Printf("mode: %i\n", mode);
-
 	switch (mode) {
 	case SELECT_MODE_SOLDIER:
 		/* check whether we are connected (tactical mission) */
@@ -981,7 +977,6 @@ static void CL_Select_f (void)
 			CL_ActorSelectList(num);
 			return;
 		}
-		Com_Printf("this is menu to choose soldier\n");
 		/* we are still in the menu - so fall through */
 	case SELECT_MODE_EQUIP:
 		/* no base or no aircraft selected */
