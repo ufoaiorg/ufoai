c2e87260c725b1506a425add4c080b0a53a851de
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-11-20 18:33:09 +0000

    * check whether ufo recovery is still active

--- src/client/cl_ufo.c
+++ src/client/cl_ufo.c
@@ -555,7 +555,7 @@ void UFO_Recovery (void)
 
 	for (i = 0; i < MAX_RECOVERIES; i++) {
 		recovery = &gd.recoveries[i];
-		if (recovery->event.day == ccs.date.day) {
+		if (recovery->active && recovery->event.day == ccs.date.day) {
 			base = &gd.bases[recovery->baseID];
 			if (!base->founded) {
 				/* Destination base was destroyed meanwhile. */
