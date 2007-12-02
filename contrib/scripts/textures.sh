4d089cfd295f4ad564d5594b989d971cae945c1c
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-12-02 13:25:42 +0000

    * put the texture check into functions * NOTE: this is work-in-progress

--- contrib/scripts/textures.sh
+++ contrib/scripts/textures.sh
@@ -5,30 +5,73 @@
 
 LANG=C;
 
-for i in `find base/textures/ -name '*.jpg' -print`;
-	do svn proplist -R -v $i | awk '{
-		k++;
-		{
-			if (k == 1 && $1 ~ /Properties/) {
-				printf $NF" ";
-				count=1
-			}
-		};
-		{
-			if (k > 1 && $1 ~ /svn:license/) {
-				if ($0 ~ /GNU/ || $0 ~ /GPL/) {
-					print "license OK!";
-					count=0
-				} else {
-					print "probably wrong " $0;
-					count=0
+function searchFiles {
+	directory=$1;
+	extension=$2;
+
+	for i in `find ${directory} -name '*.${extension}' -print`;
+		do svn proplist -R -v $i | awk '{
+			k++;
+			{
+				if (k == 1 && $1 ~ /Properties/) {
+					printf $NF" ";
+					count=1
+				}
+			};
+			{
+				if (k > 1 && $1 ~ /svn:license/) {
+					if ($0 ~ /GNU/ || $0 ~ /GPL/) {
+						print "license OK!";
+						count=0
+					} else {
+						print "probably wrong " $0;
+						count=0
+					}
 				}
 			}
-		}
-		{
-			if (k > 1 && count && $0 !~ /svn:source/) {
-				print "NO LICENSE!"
+			{
+				if (k > 1 && count && $0 !~ /svn:source/) {
+					print "NO LICENSE!"
+				}
 			}
-		}
-	}';
-done
+		}';
+	done
+}
+
+# check whether the files that are printed as NO LICENSE are
+# used in the map source files
+function checkmaps {
+	echo "TODO"
+}
+
+function usage {
+	echo "Usage $(0) [-checkmaps]"
+	echo " checkmaps      - checks whether the textures that don't"
+	echo "                  have a license set are used in the maps"
+	echo " extension      - the files to check"
+}
+
+if [ $# -eq 0 ]
+then
+	searchFiles "base/textures/" "jpg"
+	searchFiles "base/textures/" "tga"
+	searchFiles "base/textures/" "png"
+else
+	case "$1" in
+		"-checkmaps")
+			checkmaps
+			;;
+		"-extension")
+			if [ $# -eq 2 ]
+			then
+				searchFiles "base/textures/" $2
+			else
+				usage
+			fi
+			;;
+		 *)
+			usage
+			;;
+	esac
+fi
+
