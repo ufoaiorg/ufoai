65ebdf913bc9861240e618461f5f581ca560f769
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-12-09 16:50:48 +0000

    * new cvar r_acceleratedvisuals

--- src/renderer/r_main.c
+++ src/renderer/r_main.c
@@ -97,6 +97,7 @@ cvar_t *r_maxtexres;
 cvar_t *r_showtris;
 cvar_t *r_flashblend;
 cvar_t *r_swapinterval;
+cvar_t *r_acceleratedvisuals;
 cvar_t *r_texturemode;
 cvar_t *r_texturealphamode;
 cvar_t *r_texturesolidmode;
@@ -713,6 +714,7 @@ static void R_Register (void)
 
 	r_drawbuffer = Cvar_Get("r_drawbuffer", "GL_BACK", 0, NULL);
 	r_swapinterval = Cvar_Get("r_swapinterval", "1", CVAR_ARCHIVE, NULL);
+	r_acceleratedvisuals = Cvar_Get("r_acceleratedvisuals", "1", CVAR_ARCHIVE, NULL);
 
 	for (commands = r_commands; commands->name; commands++)
 		Cmd_AddCommand(commands->name, commands->function, commands->description);
