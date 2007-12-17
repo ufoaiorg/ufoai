ebb4d697c19da8d26f0faecbef2667dcf4c0b576
Martin Gerhardy <tlh2000@users.sourceforge.net>
2007-12-17 19:48:30 +0000

    * use egrep

--- base/archives.sh
+++ base/archives.sh
@@ -8,7 +8,7 @@ echo "pics and textures..."
 (find pics -type f -print; find textures -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.jpg$/||/\.tga$/||/\.png$/ {print}' | xargs zip -u9@ 0pics.${EXT}
 #zip -ru9 0pics.zip pics textures -i \*.jpg \*.tga \*.png \*.bmp \*.pcx
 echo "models..."
-find models -type f -print | awk '{if ($0 !~ /\.svn/) {print}' | awk '/\.md2$/||/\.md3$/||/\.jpg$/||/\.png$/||/\.tga$/||/\.anm$/||/\.txt$/||/\.tag$/ {print}' | xargs zip -u9@ 0models.${EXT}
+(find models -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.md2$/||/\.md3$/||/\.jpg$/||/\.png$/||/\.tga$/||/\.anm$/||/\.txt$/||/\.tag$/ {print}' | xargs zip -u9@ 0models.${EXT}
 #zip -ru9 0models.zip models -i \*.md2 \*.jpg \*.tga \*.png \*.txt \*.anm \*.tag
 echo "sound..."
 (find sound -type f -print) | egrep -v '(\/\.svn\/)' | awk '/\.txt$/||/\.wav$/ {print}' | xargs zip -u9@ 0snd.${EXT}
