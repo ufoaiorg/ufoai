345a9afa20c4135d598816e577e76fb4b6975884
Olivier Alloschery <kracken@users.sourceforge.net>
2008-01-13 18:19:59 +0000

    * made halo around sun smaller

--- src/renderer/r_draw.c
+++ src/renderer/r_draw.c
@@ -692,7 +692,7 @@ void R_Draw3DGlobe (int x, int y, int w, int h, int day, int second, vec3_t rota
 	if (sun != r_notexture && (v[2] < 0)) {
 		const float sunZoom = 1000.0f;
 		qglEnable(GL_BLEND);
-		R_DrawTexture(sun->texnum, centerx - 128 * viddef.rx + 1000 * v[1] * viddef.rx , centery - 128 * viddef.ry + sunZoom * v[0] * viddef.ry, 256 * viddef.rx, 256 * viddef.ry);
+		R_DrawTexture(sun->texnum, centerx - 64 * viddef.rx + sunZoom * v[1] * viddef.rx , centery - 64 * viddef.ry + sunZoom * v[0] * viddef.ry, 128 * viddef.rx, 128 * viddef.ry);
 		qglDisable(GL_BLEND);
 	}
 
