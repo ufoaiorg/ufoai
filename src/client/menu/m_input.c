d73209ed053e5aa1ed8212d83a8b53604537b6c2
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-01-21 08:17:21 +0000

    * removed unused header inclusion

--- src/client/menu/m_input.c
+++ src/client/menu/m_input.c
@@ -23,8 +23,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "../client.h"
-#include "m_menu.h"
-
 #include "m_main.h"
 #include "m_parse.h"
 #include "m_input.h"
