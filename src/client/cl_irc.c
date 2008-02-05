a4333b5289584cf4c96c629fcedf754a99234f25
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-02-05 15:51:01 +0000

    * merged r14455 (personal message stuff) from trunk to 2.2 branch

--- src/client/cl_irc.c
+++ src/client/cl_irc.c
@@ -742,7 +742,14 @@ static void Irc_Client_CmdPrivmsg (const char *prefix, const char *params, const
 			/* check whether this is no message to the channel - but to the user */
 			if (params && Q_strcmp(params, irc_defaultChannel->string)) {
 				S_StartLocalSound("misc/lobbyprivmsg");
-				MN_AddChatMessage(va("<%s> %s", nick, trailing));
+				MN_AddChatMessage(va("<%s> %s\n", nick, trailing));
+			} else if (strstr(trailing, irc_nick->string)) {
+				S_StartLocalSound("misc/lobbyprivmsg");
+				MN_AddChatMessage(va("<%s> %s\n", nick, trailing));
+				if (Q_strcmp(menu->name, mn_hud->string)) {
+					/* we are not in hud mode, use a popup */
+					MN_PushMenu("chat_popup");
+				}
 			}
 		}
 
