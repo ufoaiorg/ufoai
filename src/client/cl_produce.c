753c258cd20900f0ae97a8bf3438cc2b13206c56
Olivier Alloschery <kracken@users.sourceforge.net>
2008-02-08 21:12:30 +0000

    * merged r14592 (should fix first part of bug #1888562: don't stay in disassembling production menu if you remove an item from queue) from trunk to branch

--- src/client/cl_produce.c
+++ src/client/cl_produce.c
@@ -1250,6 +1250,7 @@ static void PR_ProductionStop_f (void)
 	if (queue->numItems == 0) {
 		selectedQueueItem = qfalse;
 		selectedIndex = NONE;
+		/* empty selected item description */
 		PR_ProductionInfo(base, qfalse);
 	} else if (selectedIndex >= queue->numItems) {
 		selectedIndex = queue->numItems - 1;
@@ -1259,8 +1260,13 @@ static void PR_ProductionStop_f (void)
 			PR_ProductionInfo(base, qfalse);
 	}
 
-	PR_UpdateDisassemblingList_f();
-	PR_UpdateProductionList(base);
+	if (production_disassembling) {
+		PR_ProductionInfo(base, qtrue);
+		PR_UpdateDisassemblingList_f();
+	} else {
+		PR_ProductionInfo(base, qfalse);
+		PR_UpdateProductionList(base);
+	}
 }
 
 /**
