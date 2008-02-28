3818dd725ec3bd0be4b345c80d1c9183d8460d29
Dima Nemchenko <nemchenk@users.sourceforge.net>
2008-02-28 14:47:17 +0000

    * Patch #1903128 -- 1.5 second fade-out when playing music

--- src/client/cl_sound.c
+++ src/client/cl_sound.c
@@ -117,7 +117,7 @@ static void S_Music_Start (const char *file)
 
 	/* we are still playing some background track - fade it out @sa S_Frame */
 	if (Mix_PlayingMusic()) {
-		if (!Mix_FadeOutMusic(3000))
+		if (!Mix_FadeOutMusic(1500))
 			S_Music_Stop();
 		if (music.nextMusicTrack)
 			Mem_Free(music.nextMusicTrack);
