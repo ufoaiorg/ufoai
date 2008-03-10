7f9cefa717d747d8186cda62cf098034352ff5d2
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-03-10 18:10:07 +0000

    * added HACK comment for cvar snd_music_crackleWorkaround

--- src/client/cl_sound.c
+++ src/client/cl_sound.c
@@ -59,6 +59,7 @@ static cvar_t *snd_channels;
 static cvar_t *snd_rate;
 static cvar_t *snd_music;
 static cvar_t *snd_music_volume;
+/* HACK: Remove this as soon as the reason was found */
 static cvar_t *snd_music_crackleWorkaround;
 
 static int audio_rate, audio_channels;
