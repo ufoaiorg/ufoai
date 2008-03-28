e937c2b480b2b689d8c0e81f282ec3d2a898bee9
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-03-28 08:32:10 +0000

    * cleanup

--- src/renderer/r_misc.c
+++ src/renderer/r_misc.c
@@ -74,14 +74,6 @@ SCREEN SHOTS
 ==============================================================================
 */
 
-typedef struct _TargaHeader {
-	unsigned char id_length, colormap_type, image_type;
-	unsigned short colormap_index, colormap_length;
-	unsigned char colormap_size;
-	unsigned short x_origin, y_origin, width, height;
-	unsigned char pixel_size, attributes;
-} TargaHeader;
-
 enum {
 	SSHOTTYPE_JPG,
 	SSHOTTYPE_PNG,
