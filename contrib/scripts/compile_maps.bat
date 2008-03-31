5127197ca428d9809c86894a4e2ab5b9385194a7
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-03-31 05:48:44 +0000

    * patch #1929537 (W2K compatibility of contrib/scripts/compile_*.bat)

--- contrib/scripts/compile_maps.bat
+++ contrib/scripts/compile_maps.bat
@@ -1,14 +1,22 @@
 echo off
 
-cd ../..
+cd ..\..
 
 if NOT EXIST ufo2map.exe (
 	echo Missing ufo2map
 	goto end
 )
 
-for %%i in (base/maps/*.map) DO (
-	ufo2map.exe -extra base/maps/%%i
+for /D %%i in (base\maps\*) DO (
+	echo "...found dir %%i";
+	for %%j in (%%i\*.map) DO (
+		ufo2map.exe -extra %%j
+	)
+	echo "...dir %%i finished";
+)
+
+for %%i in (base\maps\*.map) DO (
+	ufo2map.exe -extra base\maps\%%i
 )
 
 :end
--- contrib/scripts/compile_po.bat
+++ contrib/scripts/compile_po.bat
@@ -2,24 +2,24 @@ echo off
 echo =====================================================
 echo po compiler for UFO:AI (http://sf.net/projects/ufoai)
 echo 'msgfmt' utility must be in your path
-echo compile all .po files and in src/po and place them
-echo into base/i18n/
+echo compile all .po files and in src\po and place them
+echo into base\i18n\
 echo =====================================================
 
 set msgfmtBin=msgfmt.exe
-set i18ndir=../../base/i18n
-set scriptdir=../../contrib/scripts
+set i18ndir=..\..\base\i18n
+set scriptdir=..\..\contrib\scripts
 
 if NOT EXIST %i18ndir% (
 	echo %i18ndir% does not exists;
 )
 
-cd ../../src/po
-for %%i in (./*.po) DO (
-if NOT EXIST "%i18ndir%/%%~ni/LC_MESSAGES" (
-mkdir "%i18ndir%/%%~ni/LC_MESSAGES"
-)
-echo compiling %%i to "%i18ndir%/%%~ni/LC_MESSAGES/ufoai.mo"
-%msgfmtBin% -o "%i18ndir%/%%~ni/LC_MESSAGES/ufoai.mo" %%i
+cd ..\..\src\po
+for %%i in (.\*.po) DO (
+	if NOT EXIST "%i18ndir%\%%~ni\LC_MESSAGES" (
+		mkdir "%i18ndir%\%%~ni\LC_MESSAGES"
+	)
+	echo compiling %%i to "%i18ndir%\%%~ni\LC_MESSAGES\ufoai.mo"
+	%msgfmtBin% -o "%i18ndir%\%%~ni\LC_MESSAGES\ufoai.mo" %%i
 )
 cd %scriptdir%
