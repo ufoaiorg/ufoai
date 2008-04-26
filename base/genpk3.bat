09c53525f2cecc0d657596c027ff3ce4a735c745
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-04-26 05:59:19 +0000

    * missing eol * added some extensions

--- base/genpk3.bat
+++ base/genpk3.bat
@@ -2,11 +2,11 @@ rem NOTE: This batch file requires you to have the 7-zip command line utility in
 rem You can get this utility here: http://www.7-zip.org/download.html
 
 7za a -tzip 0base.pk3 *.cfg -x!".svn"
-7za a -r -tzip 0maps.pk3 materials maps\*.bsp maps\*.ump -x!".svn"
-7za a -r -tzip 0media.pk3 shaders media -x!".svn"
+7za a -r -tzip 0maps.pk3 materials\*.mat maps\*.bsp maps\*.ump -x!".svn"
+7za a -r -tzip 0media.pk3 shaders\*.fp shaders\*.vp media\*.ttf -x!".svn"
 7za a -r -tzip 0models.pk3 models -x!".svn"
-7za a -r -tzip 0music.pk3 music -x!".svn"
+7za a -r -tzip 0music.pk3 music\*.ogg -x!".svn"
 7za a -r -tzip 0pics.pk3 pics textures -x!".svn"
 7za a -r -tzip 0snd.pk3 sound -x!".svn"
-7za a -r -tzip 0ufos.pk3 ufos -x!".svn"
-7za a -r -tzip 0vids.pk3 videos -x!".svn"
+7za a -r -tzip 0ufos.pk3 ufos\*.ufo -x!".svn"
+7za a -r -tzip 0vids.pk3 videos\*.roq -x!".svn"
