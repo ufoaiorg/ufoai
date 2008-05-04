1a81d5daded775a0e888eb413cee9b7986f3d710
Andy Buckle <blondandy@users.sourceforge.net>
2008-05-04 10:20:11 +0000

    * improved usage * displays svn revision number with usage.

--- net/ufoai/sf/maputils/MapUtils.java
+++ net/ufoai/sf/maputils/MapUtils.java
@@ -1,11 +1,18 @@
 package net.ufoai.sf.maputils;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.jar.JarFile;
 
 /*
+
+copyright 2008 UFO:Alien Invasion team
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
@@ -33,10 +40,33 @@ public class MapUtils {
     private static Pattern naturalNumberEndPattern=Pattern.compile("\\d*$");
     private static boolean verbose=true, fix=false;
     private static PrintStream out=System.out;
+    private static final String jarfilename="maputils.jar";
+    private static final String svn_info_filename="svn_info.txt";
+    private static String svnInfoText=null;
+    private static Pattern urlLinePattern=Pattern.compile("^Repository Root.*?$",Pattern.MULTILINE);
+    private static Pattern revisionLinePattern=Pattern.compile("^Revision.*?$",Pattern.MULTILINE);
+    private static final String subDir="branches/maputils_java/";
     
     public static void main(String[] args) {
         try {
-            for(int i=0;i<args.length;i++){//parse mode-seeting args first
+            //parse svn info from jar
+            try{
+                JarFile jar=new JarFile(jarfilename);
+                InputStream jarIn=jar.getInputStream(jar.getEntry(svn_info_filename));
+                InputStreamReader svnInfoReader=new InputStreamReader(jarIn);
+                svnInfoText=load(svnInfoReader);
+                jarIn.close();
+                //System.out.println(""+svnInfoText);
+                Matcher urlMatcher= urlLinePattern.matcher(svnInfoText); urlMatcher.find();
+                out.println(urlMatcher.group());
+                out.println("Subdirectory"+subDir);
+                Matcher revMatcher= revisionLinePattern.matcher(svnInfoText); revMatcher.find();
+                out.println(revMatcher.group());
+            }catch (Exception e){
+                out.printf("***%s:%s%n***%s%n",e.getClass(),e.getMessage(),"while reading svn info from jar: svn_info.txt may be missing from jar.");
+            }
+            
+            for(int i=0;i<args.length;i++){//parse mode-setting args first
                 if(args[i].equalsIgnoreCase("-fix")) fix=true;
                 if(args[i].equalsIgnoreCase("-quiet")) verbose=false;
                 if(args[i].equalsIgnoreCase("-h") || args[i].equalsIgnoreCase("-help")) {
@@ -89,6 +119,14 @@ public class MapUtils {
         }
     }
     
+    private static String load(InputStreamReader reader) throws IOException {
+        String txt="";
+        final int bs=131072; int numRead=0;
+        char[] buff=new char[bs];
+        while( (numRead=reader.read(buff,0,bs))!=-1 ) txt+=(new String(buff)).substring(0,numRead);
+        return txt;
+    }
+    
     public static void printf(String format, Object... args){
         if(verbose){
             out.printf(format, args);
@@ -107,9 +145,16 @@ public class MapUtils {
         out.println("options");
         out.println("  -help, -h Print (this) advice and exit.");
         out.println("  -quiet    Do not write notes to standard out. Default is verbose.");
+        out.println("  -nodraws  Set nodraw flags and textures to faces which are always");
+        out.println("            hidden by being abbutted to another face");
         out.println("  -fix      Make changes to the map. save overwrites foo.map");
         out.println("            also writes safety foo.map.original_0");
         out.println("            if foo.map.original_0 exists then saves foo.map.original_1, etc");
         out.println("  -lvlflags Calculate levelflags based on brush vertex coordinates.");
+        out.println();
+        out.println("will probably need to set extra memory. to allow java ~1Meg.");
+        out.println("java -Xmx1000M -jar maputils.jar [options] <map>");
+        out.println();
+        
     }
 }
