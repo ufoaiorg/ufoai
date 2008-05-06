5978cf6b2f2bb9e34005855efd950646c374c127
richlv <richlv@users.sourceforge.net>
2008-05-06 20:58:57 +0000

    * use variable for separator; recompile only changed .java files; recreate jarfile only of at least one .java file was changed

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@
 # you are probably better off using netbeans (the sun IDE)
 # javac and jar are included in java sdk
 
+SEPARATOR="_______________________________"
+
 fail() {
     echo -e "\n$1"
     exit 1
@@ -14,6 +16,9 @@ SVN=$(which svn 2> /dev/null)
 FIND=$(which find 2> /dev/null)
 [[ $FIND ]] || fail "no find in path"
 
+AWK=$(which awk 2> /dev/null)
+[[ $AWK ]] || fail "no awk in path"
+
 JAVAC=$(which javac 2> /dev/null)
 [[ $JAVAC ]] || fail "no javac in path"
 
@@ -22,27 +27,40 @@ JAR=$(which jar 2> /dev/null)
 
 JAVA=$(which java 2> /dev/null)
 
-# change to directory  with java source in
+# change to directory with java source
 cd ../net/ufoai/sf/maputils
 for FILE in *.java; do
-	# classpath is the directory containing the net directory
-	echo "_______________________________"
-	echo "compiling $FILE"
-	$JAVAC -classpath ../../../.. $FILE
+    # classpath is the directory containing the net directory
+    echo $SEPARATOR
+    CLASSSECONDS=$(ls -l --time-style="+%s" ${FILE%\.java}.class 2>/dev/null | $AWK '{print $6}')
+    [[ $CLASSSECONDS ]] && {
+	JAVASECONDS=$(ls -l --time-style="+%s" $FILE 2>/dev/null | awk '{print $6}')
+	if [ "$JAVASECONDS" -lt "$CLASSSECONDS" ]; then
+	    echo "skipping $FILE, classfile more recent"
+	    continue
+	fi
+    }
+    echo "compiling $FILE"
+    COMPILED=yes
+    $JAVAC -classpath ../../../.. $FILE
 done
 
 # change to the directory containing the net directory
 cd ../../../..
 
-echo "_______________________________"
-echo "using svn to create file with revision number in"
-$SVN info . > svn_info.txt
+[[ $COMPILED ]] && {
 
-echo "_______________________________"
-echo "using jar to create archive"
-jar cvfm maputils.jar MANIFEST.MF svn_info.txt $($FIND net -type f ! -wholename '*.svn*' ! -name '*.java')
+    echo $SEPARATOR
+    echo "using svn to create file with revision number"
+    $SVN info . > svn_info.txt
 
+    echo $SEPARATOR
+    echo "using jar to create archive"
+    $JAR cvfm maputils.jar MANIFEST.MF svn_info.txt $($FIND net -type f ! -wholename '*.svn*' ! -name '*.java')
+} || {
+    echo $SEPARATOR
+    echo "skipping jar creation, there have been no changes to .java files"
+}
 [[ $JAVA ]] || fail "no java in path, skipping the example run"
-echo "_______________________________"
-echo "example run"
+echo -e "$SEPARATOR\nexample run\n$SEPARATOR"
 $JAVA -jar maputils.jar -h
