a361b1cfd13b6aa7702839421d1c0986bcc60fd0
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-07-21 14:25:46 +0000

    * doxygen

--- src/common/common.c
+++ src/common/common.c
@@ -921,7 +921,7 @@ void Qcommon_Init (int argc, const char **argv)
 
 	Schedule_Timer(Cvar_Get("sv_freq", "10", CVAR_NOSET, NULL), &SV_Frame, NULL);
 
-	/* XXX: This line wants to be removed */
+	/** @todo This line wants to be removed */
 	Schedule_Timer(Cvar_Get("cbuf_freq", "10", 0, NULL), &Cbuf_Execute_timer, NULL);
 
 	Com_Printf("====== UFO Initialized ======\n\n");
