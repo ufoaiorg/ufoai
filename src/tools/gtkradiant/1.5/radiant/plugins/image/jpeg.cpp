8e3491b6adaad9b8752aeb1a555eda924debd54d
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-08-08 20:28:14 +0000

    * cleanup

--- src/tools/gtkradiant/1.5/radiant/plugins/image/jpeg.cpp
+++ src/tools/gtkradiant/1.5/radiant/plugins/image/jpeg.cpp
@@ -41,15 +41,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdio.h>
 #include <string.h>
 
-#ifdef __cplusplus
 extern "C"
 {
-#endif
 #include <jpeglib.h>
 #include <jerror.h>
-#ifdef __cplusplus
 }
-#endif
 
 #include "ifilesystem.h"
 
