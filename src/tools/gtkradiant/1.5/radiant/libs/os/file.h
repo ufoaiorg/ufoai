012691a0536bd0fed689ee65fee7137dba1b99c6
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-08-12 10:29:03 +0000

    * cleanup

--- src/tools/gtkradiant/1.5/radiant/libs/os/file.h
+++ src/tools/gtkradiant/1.5/radiant/libs/os/file.h
@@ -25,12 +25,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 /// \file
 /// \brief OS file-system querying and manipulation.
 
-#if defined( WIN32 )
+#if defined(WIN32)
 #define S_ISDIR(mode) (mode & _S_IFDIR)
 #include <io.h> // _access()
-#define F_OK 0x00
-#define W_OK 0x02
-#define R_OK 0x04
 #define access(path, mode) _access(path, mode)
 #else
 #include <unistd.h> // access()
