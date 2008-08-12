9ad803e6543abc12ffaa08ef4616b848d260a152
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-08-12 17:14:25 +0000

    * changed RADIANT_DLLEXPORT to cdecl

--- src/tools/radiant/include/modulesystem.h
+++ src/tools/radiant/include/modulesystem.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "debugging/debugging.h"
 
 #if defined(WIN32)
-#define RADIANT_DLLEXPORT __stdcall
+#define RADIANT_DLLEXPORT cdecl
 #else
 #define RADIANT_DLLEXPORT
 #endif
