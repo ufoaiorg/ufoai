b38acc771bf6d8e29a9df12b8258b283cea32ac2
Andy Buckle <blondandy@users.sourceforge.net>
2008-08-18 17:19:03 +0000

    * /clean do not use -onlynewer arg to ufo2map * /fast do not use -extra * simplify nesting in if ... else if ... else * popd not required - automatic on batch exit when setlocal is used * fix reference to removed label :End (which was not necessary) * echo ufo2map command line

--- contrib/scripts/compile_maps.bat
+++ contrib/scripts/compile_maps.bat
@@ -1,5 +1,8 @@
 @echo off
 setlocal
+rem setlocal ensures implicit endlocal call howevert batch exits.
+rem so we can goto :EOF  or ctrl-c an at point and env variables
+rem and directory will be reset
 
 pushd ..\..
 
@@ -12,29 +15,35 @@ rem defaults
 set curpath=base\maps
 set usecores=%NUMBER_OF_PROCESSORS%
 set starttime=%TIME%
+set onlynewer=-onlynewer
+set extrasamples=-extra
 
 REM loop through args. SHIFT turns %2 into %1 etc
 :Loop
 IF "%1"=="" GOTO Continue
 if EXIST base\maps\%1 (
 	set curpath=base\maps\%1
+) else if "%1"=="/-t" (
+	set usecores=1
+) else if "%1"=="/help" (
+	echo compile_maps [/-t] [/help] [path]
+	echo   /-t     always use -t 1 in call to ufo2map, do not use extra processors
+	rem parentheses need to be escaped with caret
+	echo   /help   print ^(this^) help and exit
+	echo   /clean  recompile all maps. default is to only compile if .map is newer than.bsp
+	echo   /fast   only do one pass on light samples. default is five.
+	echo   path    relative to base\maps. eg foo processes *.map in base\maps\foo
+	goto :EOF
+) else if "%1"=="/clean" (
+	set onlynewer=
+) else if "%1"=="/fast" (
+	set extrasamples=
 ) else (
-	if "%1"=="/-t" (
-		set usecores=1
-	) else (
-		if "%1"=="/help" (
-			echo compile_maps [/-t] [/help] [path]
-			echo   /-t    always use -t 1 in call to ufo2map, do not use extra processors
-			rem parentheses need to be escaped with caret
-			echo   /help  print ^(this^) help and exit
-			echo   path   relative to base\maps. eg foo processes *.map in base\maps\foo
-			GOTO End
-		) else (
-			echo path "base\maps\%1" not found, %1 argument not understood, try /help
-			GOTO End
-		)
-	)
+	echo path "base\maps\%1" not found, %1 argument not understood, try /help
+	goto :EOF
 )
+
+
 SHIFT
 GOTO Loop
 :Continue
@@ -43,7 +52,7 @@ echo found %NUMBER_OF_PROCESSORS% processors
 echo using %usecores% processors
 echo compiling maps in %curpath%
 
-set ufo2mapparameters=-extra -onlynewer -t %usecores%
+set ufo2mapparameters=%extrasamples% %onlynewer% -t %usecores%
 
 for /D %%i in (%curpath%\*) DO (
 	call :compilemap %%i
@@ -54,13 +63,12 @@ echo.
 echo  started at %starttime%
 echo finished at %TIME%
 
-popd
-	
 goto :EOF
 
 :compilemap
 	echo ...found dir "%1"
 	for %%j in (%1\*.map) DO (
+		echo ufo2map.exe %ufo2mapparameters% %%j
 		ufo2map.exe %ufo2mapparameters% %%j
 	)
 	echo ...dir "%1" finished
