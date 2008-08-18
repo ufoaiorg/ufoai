d38fb1a2a8d2c9c513f781ed7435e9b2993734b5
Andy Buckle <blondandy@users.sourceforge.net>
2008-08-18 20:06:15 +0000

    * delete bsp if ufo2map returns nonzero, or is interrupted (thanks wilminator). * /shutdown (when compilation is complete). sleep, save electricity, awake to freshly compiled maps :)

--- contrib/scripts/compile_maps.bat
+++ contrib/scripts/compile_maps.bat
@@ -17,6 +17,7 @@ set usecores=%NUMBER_OF_PROCESSORS%
 set starttime=%TIME%
 set onlynewer=-onlynewer
 set extrasamples=-extra
+set shutdownonfinish=false
 
 REM loop through args. SHIFT turns %2 into %1 etc
 :Loop
@@ -26,18 +27,23 @@ if EXIST base\maps\%1 (
 ) else if "%1"=="/-t" (
 	set usecores=1
 ) else if "%1"=="/help" (
-	echo compile_maps [/-t] [/help] [path]
-	echo   /-t     always use -t 1 in call to ufo2map, do not use extra processors
+	echo compile_maps <options>
+	echo   /-t        always use -t 1 in call to ufo2map, do not use extra processors
 	rem parentheses need to be escaped with caret
-	echo   /help   print ^(this^) help and exit
-	echo   /clean  recompile all maps. default is to only compile if .map is newer than.bsp
-	echo   /fast   only do one pass on light samples. default is five.
-	echo   path    relative to base\maps. eg foo processes *.map in base\maps\foo
+	echo   /help      print ^(this^) help and exit
+	echo   /clean     recompile all maps. default is to only compile 
+	echo              if .map is newer than.bsp
+	echo   /fast      only do one pass on light samples. default is five.
+	echo   /shutdown  shutdown computer when compilation complete
+	echo   path       relative to base\maps. 
+	echo              eg foo processes *.map in base\maps\foo
 	goto :EOF
 ) else if "%1"=="/clean" (
 	set onlynewer=
 ) else if "%1"=="/fast" (
 	set extrasamples=
+) else if "%1"=="/shutdown" (
+	set shutdownonfinish=true
 ) else (
 	echo path "base\maps\%1" not found, %1 argument not understood, try /help
 	goto :EOF
@@ -63,15 +69,21 @@ echo.
 echo  started at %starttime%
 echo finished at %TIME%
 
+if "%shutdownonfinish%"=="true" (
+	shutdown /s
+)
+
 goto :EOF
 
 :compilemap
 	echo ...found dir "%1"
 	for %%j in (%1\*.map) DO (
 		echo ufo2map.exe %ufo2mapparameters% %%j
-		ufo2map.exe %ufo2mapparameters% %%j
+		ufo2map.exe %ufo2mapparameters% %%j || (
+			echo.
+			echo interrupt or ufo2map returned nonzero, deleting .bsp
+			Del %%~dpnj.bsp
+		)
 	)
 	echo ...dir "%1" finished
-rem CHECK ERRORLEVEL AND REMOVE MAP IF != 0
-rem		if errorlevel 1 echo "TODO"
 exit /b
