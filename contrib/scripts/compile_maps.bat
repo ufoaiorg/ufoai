690c1205f252b6e49cbaa0d814e200e8d5a747b0
Kostia Romanov <kildor@users.sourceforge.net>
2008-08-19 11:17:23 +0000

    * fix bat file.

--- contrib/scripts/compile_maps.bat
+++ contrib/scripts/compile_maps.bat
@@ -20,22 +20,24 @@ set extrasamples=-extra
 set shutdownonfinish=false
 
 REM loop through args. SHIFT turns %2 into %1 etc
+
 :Loop
-IF "%1"=="" GOTO Continue
+
+IF "%1"=="" ( GOTO Continue)
 if EXIST base\maps\%1 (
 	set curpath=base\maps\%1
 ) else if "%1"=="/-t" (
 	set usecores=1
 ) else if "%1"=="/help" (
-	echo compile_maps <options>
+	echo compile_maps ^<options^>
 	echo   /-t        always use -t 1 in call to ufo2map, do not use extra processors
 	rem parentheses need to be escaped with caret
 	echo   /help      print ^(this^) help and exit
-	echo   /clean     recompile all maps. default is to only compile 
+	echo   /clean     recompile all maps. default is to only compile
 	echo              if .map is newer than.bsp
 	echo   /fast      only do one pass on light samples. default is five.
 	echo   /shutdown  shutdown computer when compilation complete
-	echo   path       relative to base\maps. 
+	echo   path       relative to base\maps.
 	echo              eg foo processes *.map in base\maps\foo
 	goto :EOF
 ) else if "%1"=="/clean" (
