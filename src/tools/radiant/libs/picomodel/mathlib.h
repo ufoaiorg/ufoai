13cd10f4ab0ddc3fc852f9b41469ea5798b9e07e
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-08-27 16:13:35 +0000

    * cleanup

--- src/tools/radiant/libs/picomodel/mathlib.h
+++ src/tools/radiant/libs/picomodel/mathlib.h
@@ -24,18 +24,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 /* mathlib.h */
 #include <math.h>
-
-#ifdef __cplusplus
-
-/* start declarations of functions defined in C library. */
-extern "C" {
-
-#endif
-
-	typedef float vec_t;
-	typedef vec_t vec3_t[3];
-	typedef vec_t vec5_t[5];
-	typedef vec_t vec4_t[4];
+typedef float vec_t;
+typedef vec_t vec3_t[3];
+typedef vec_t vec5_t[5];
+typedef vec_t vec4_t[4];
 
 #define DotProduct(x,y) ((x)[0]*(y)[0]+(x)[1]*(y)[1]+(x)[2]*(y)[2])
 #define VectorSubtract(a,b,c) ((c)[0]=(a)[0]-(b)[0],(c)[1]=(a)[1]-(b)[1],(c)[2]=(a)[2]-(b)[2])
@@ -49,30 +41,26 @@ extern "C" {
 #define CrossProduct(a,b,c) ((c)[0]=(a)[1]*(b)[2]-(a)[2]*(b)[1],(c)[1]=(a)[2]*(b)[0]-(a)[0]*(b)[2],(c)[2]=(a)[0]*(b)[1]-(a)[1]*(b)[0])
 #define VectorClear(x) ((x)[0]=(x)[1]=(x)[2]=0)
 
-	vec_t VectorLength(const vec3_t v);
+vec_t VectorLength(const vec3_t v);
 
-	void VectorMA(const vec3_t va, vec_t scale, const vec3_t vb, vec3_t vc);
+void VectorMA(const vec3_t va, vec_t scale, const vec3_t vb, vec3_t vc);
 
-	/*!
-	aabb_t -  "axis-aligned" bounding box...
-	  origin: centre of bounding box...
-	  extents: +/- extents of box from origin...
-	*/
-	typedef struct aabb_s {
-		vec3_t origin;
-		vec3_t extents;
-	} aabb_t;
+/**
+ * @brief "axis-aligned" bounding box...
+ * origin: centre of bounding box...
+ * extents: +/- extents of box from origin...
+ */
+typedef struct aabb_s {
+	vec3_t origin;
+	vec3_t extents;
+} aabb_t;
 
-	/*! Extend AABB to include point. */
-	void aabb_extend_by_point(aabb_t* aabb, const vec3_t point);
-	/*! Extend AABB to include aabb_src. */
-	void aabb_extend_by_aabb(aabb_t* aabb, const aabb_t* aabb_src);
+/** @brief Extend AABB to include point. */
+void aabb_extend_by_point(aabb_t* aabb, const vec3_t point);
+/** @brief Extend AABB to include aabb_src. */
+void aabb_extend_by_aabb(aabb_t* aabb, const aabb_t* aabb_src);
 
-	/*! Calculate the corners of the aabb. */
-	void aabb_corners(const aabb_t* aabb, vec3_t corners[8]);
+/*! Calculate the corners of the aabb. */
+void aabb_corners(const aabb_t* aabb, vec3_t corners[8]);
 
-#ifdef __cplusplus
-}
 #endif
-
-#endif /* __MATHLIB__ */
