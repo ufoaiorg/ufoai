4d2fb2d22dc37d498897048560cb361a8ce1c0db
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-08-31 09:21:44 +0000

    * added ati hack

--- base/shaders/light.fs
+++ base/shaders/light.fs
@@ -8,17 +8,16 @@ varying vec3 normal;
 LightFragment
 */
 void LightFragment(vec4 diffuse, vec4 lightmap){
-	vec4 light, l;
+	vec3 light, l;
 	vec3 delta, dir;
 	float dist, d;
 	float atten;
 	int i;
 
-	light = vec4(0.0, 0.0, 0.0, 0.0);
+	light = vec3(0.0, 0.0, 0.0);
 
-	// accumulate diffuse lighting for this fragment
-
-	// if the light is off, we're done
+#if defined(ATI)
+	// ATI can't handle for loops and array access in fragment shaders - use at least two lights
 	if (gl_LightSource[0].constantAttenuation > 0.0) {
 		delta = gl_LightSource[0].position.xyz - vertex;
 		dist = length(delta);
@@ -38,7 +37,58 @@ void LightFragment(vec4 diffuse, vec4 lightmap){
 			}
 		}
 	}
+	if (gl_LightSource[1].constantAttenuation > 0.0) {
+		delta = gl_LightSource[1].position.xyz - vertex;
+		dist = length(delta);
+
+		if (dist <= gl_LightSource[1].constantAttenuation) {
+			dir = normalize(delta);
+			d = dot(normal, dir);
+
+			if (d > 0.0) {
+				atten = 0.5 / (dist / gl_LightSource[1].constantAttenuation);
+
+				l = gl_LightSource[1].diffuse * d;
+				l *= atten;
+				l *= atten * atten;
+
+				light += clamp(l, 0.0, 2.5);
+			}
+		}
+	}
+#else
+	// accumulate diffuse lighting for this fragment
+	for(i = 0; i < 8; i++){
+
+		// if the light is off, we're done
+		if(gl_LightSource[i].constantAttenuation == 0.0)
+			break;
+
+		delta = gl_LightSource[i].position.xyz - vertex;
+		dist = length(delta);
+
+		if(dist > gl_LightSource[i].constantAttenuation)
+			continue;
+
+		dir = normalize(delta);
+		d = dot(normal, dir);
+
+		if(d <= 0.0)
+			continue;
+
+		atten = 0.5 / (dist / gl_LightSource[i].constantAttenuation);
+
+		l = gl_LightSource[i].diffuse.rgb * d;
+		l *= atten;
+		l *= atten * atten;
+
+		light += clamp(l, 0.0, 2.5);
+	}
+#endif
 
 	// now modulate the diffuse sample with the modified lightmap
-	gl_FragColor = diffuse * (lightmap + light);
+	gl_FragColor.rgb = diffuse.rgb * (lightmap.rgb + light);
+
+	// lastly pass the alpha value through, unaffected
+	gl_FragColor.a = diffuse.a * lightmap.a;
 }
