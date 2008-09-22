4a1a6c349762e26b3b8d3984a4dcb132e42d1c7d
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-09-22 05:28:02 +0000

    * merged q2w changes: Re-normalize eyedir per fragment.

--- base/shaders/bump.fs
+++ base/shaders/bump.fs
@@ -12,9 +12,9 @@ void BumpFragment(in vec3 deluxemap, in vec3 normalmap){
 
 	float diffuse = dot(deluxemap, normalmap * vec3(BUMP));
 
-	float spec = max(-dot(eyedir, reflect(deluxemap, normalmap * vec3(SPECULAR))), 0.0);
+	float spec = max(-dot(normalize(eyedir), reflect(deluxemap, normalmap * vec3(SPECULAR))), 0.0);
 
-	float bump = diffuse + pow(spec, 8.0);
+	float bump = diffuse + pow(spec, 4.0);
 
 	gl_FragColor.rgb = gl_FragColor.rgb * vec3(bump);
 }
