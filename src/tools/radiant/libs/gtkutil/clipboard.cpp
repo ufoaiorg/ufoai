872e4eb5f7b6a9c4014f2352df29861fe5f26aab
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-09-28 07:37:15 +0000

    * removed win32 hack for clipboard support (please test with current gtk versions)

--- src/tools/radiant/libs/gtkutil/clipboard.cpp
+++ src/tools/radiant/libs/gtkutil/clipboard.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file clipboard.cpp
+ * @brief Platform-independent GTK clipboard support.
+ * @todo Using GDK_SELECTION_CLIPBOARD fails on win32, so we use the win32 API directly for now.
+ */
+
 /*
 Copyright (C) 2001-2006, William Joseph.
 All Rights Reserved.
@@ -24,74 +30,16 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "stream/memstream.h"
 #include "stream/textstream.h"
 
-
-/// \file
-/// \brief Platform-independent GTK clipboard support.
-/// \todo Using GDK_SELECTION_CLIPBOARD fails on win32, so we use the win32 API directly for now.
-#if defined(WIN32)
-
-const char* c_clipboard_format = "RadiantClippings";
-
-#include <windows.h>
-
-void clipboard_copy(ClipboardCopyFunc copy) {
-	BufferOutputStream ostream;
-	copy(ostream);
-
-	bool bClipped = false;
-	UINT nClipboard = ::RegisterClipboardFormat(c_clipboard_format);
-	if (nClipboard > 0) {
-		if (::OpenClipboard(0)) {
-			EmptyClipboard();
-			std::size_t length = ostream.size();
-			HANDLE h = ::GlobalAlloc(GMEM_ZEROINIT | GMEM_MOVEABLE | GMEM_DDESHARE, length + sizeof(std::size_t));
-			if (h != 0) {
-				char *buffer = reinterpret_cast<char*>(::GlobalLock(h));
-				*reinterpret_cast<std::size_t*>(buffer) = length;
-				buffer += sizeof(std::size_t);
-				memcpy(buffer, ostream.data(), length);
-				::GlobalUnlock(h);
-				::SetClipboardData(nClipboard, h);
-				::CloseClipboard();
-				bClipped = true;
-			}
-		}
-	}
-
-	if (!bClipped) {
-		globalWarningStream() << "Unable to register Windows clipboard formats, copy/paste between editors will not be possible\n";
-	}
-}
-
-void clipboard_paste(ClipboardPasteFunc paste) {
-	UINT nClipboard = ::RegisterClipboardFormat(c_clipboard_format);
-	if (nClipboard > 0 && ::OpenClipboard(0)) {
-		if (IsClipboardFormatAvailable(nClipboard)) {
-			HANDLE h = ::GetClipboardData(nClipboard);
-			if (h) {
-				const char *buffer = reinterpret_cast<const char*>(::GlobalLock(h));
-				std::size_t length = *reinterpret_cast<const std::size_t*>(buffer);
-				buffer += sizeof(std::size_t);
-				BufferInputStream istream(buffer, length);
-				paste(istream);
-				::GlobalUnlock(h);
-			}
-		}
-		::CloseClipboard();
-	}
-}
-
-#else
-
 #include <gtk/gtk.h>
 
-enum {
-	RADIANT_CLIPPINGS = 23,
-};
+#define RADIANT_CLIPPINGS 23
 
-static const GtkTargetEntry clipboard_targets = {
-	"RADIANT_CLIPPINGS", 0, RADIANT_CLIPPINGS
-};
+static const GtkTargetEntry clipboard_targets = {"RADIANT_CLIPPINGS", 0, RADIANT_CLIPPINGS};
+/**
+ * @sa Map_ExportSelected
+ * @sa Map_ImportSelected
+ */
+static ClipboardPasteFunc g_clipboardPasteFunc = 0;
 
 static void clipboard_get (GtkClipboard *clipboard, GtkSelectionData *selection_data, guint info, gpointer data) {
 	std::size_t len = *reinterpret_cast<std::size_t*>(data);
@@ -118,6 +66,9 @@ static void clipboard_received (GtkClipboard *clipboard, GtkSelectionData *data,
 	}
 }
 
+/**
+ * @sa clipboard_paste
+ */
 void clipboard_copy(ClipboardCopyFunc copy) {
 	GtkClipboard* clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
 
@@ -131,13 +82,12 @@ void clipboard_copy(ClipboardCopyFunc copy) {
 	gtk_clipboard_set_with_data(clipboard, &clipboard_targets, 1, clipboard_get, clipboard_clear, data);
 }
 
-static ClipboardPasteFunc g_clipboardPasteFunc = 0;
+/**
+ * @sa clipboard_copy
+ */
 void clipboard_paste(ClipboardPasteFunc paste) {
 	GtkClipboard* clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
 
 	g_clipboardPasteFunc = paste;
 	gtk_clipboard_request_contents(clipboard, gdk_atom_intern(clipboard_targets.target, FALSE), clipboard_received, &g_clipboardPasteFunc);
 }
-
-
-#endif
