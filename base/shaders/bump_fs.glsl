7ecaa665215be9330cca4bdc15460fd146c45b7c
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-10-04 06:56:16 +0000

    * merged fixes from q2w

--- base/shaders/bump_fs.glsl
+++ base/shaders/bump_fs.glsl
@@ -1,20 +1,24 @@
 // bumpmap fragment shader
 
-vec3 eyedir;
+varying vec3 eyedir;
 
 uniform float BUMP;
 uniform float PARALLAX;
 uniform float SPECULAR;
+
+vec3 eye;
+
 
 /*
 BumpTexcoord
 */
 vec2 BumpTexcoord(in float height){
 
-	eyedir = normalize(eyedir);
+	eye = normalize(eyedir);
 
-	return vec2(height * PARALLAX * 0.04 - 0.02) * eyedir.xy;
+	return vec2(height * PARALLAX * 0.04 - 0.02) * eye.xy;
 }
+
 
 /*
 BumpFragment
@@ -22,10 +26,10 @@ BumpFragment
 void BumpFragment(in vec3 deluxemap, in vec3 normalmap){
 
 	float diffuse = dot(deluxemap,
-					vec3(normalmap.x * BUMP, normalmap.y * BUMP, normalmap.z));
+			vec3(normalmap.x * BUMP, normalmap.y * BUMP, normalmap.z));
 
-	float specular = pow(max(-dot(eyedir,
-					reflect(deluxemap, normalmap)), 0.0), 8.0 * SPECULAR);
+	float specular = pow(max(-dot(eye,
+			reflect(deluxemap, normalmap)), 0.0), 8.0 * SPECULAR);
 
 	gl_FragColor.rgb *= vec3(diffuse + specular);
 }
