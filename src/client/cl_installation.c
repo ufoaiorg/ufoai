b1744440da00ec47302db19fdd42c29fa2afd9c8
Tamás Fehérvári <geever@users.sourceforge.net>
2008-10-08 21:33:09 +0000

    * recount numInstallations on load * [ 2153109 ] can't use installation (SAM), see: https://sourceforge.net/tracker/index.php?func=detail&aid=2153109&group_id=157793&atid=805242

--- src/client/cl_installation.c
+++ src/client/cl_installation.c
@@ -691,6 +691,7 @@ qboolean INS_Load (sizebuf_t* sb, void* data)
 			Com_Printf("Could not find installation template\n");
 			return qfalse;
 		}
+		gd.numInstallations++;
 		Q_strncpyz(inst->name, MSG_ReadStringRaw(sb), sizeof(inst->name));
 		MSG_ReadPos(sb, inst->pos);
 		inst->installationStatus = MSG_ReadByte(sb);
@@ -717,5 +718,6 @@ qboolean INS_Load (sizebuf_t* sb, void* data)
 		/** @todo aircraft */
 		/** @todo don't forget to recalc the capacities like we do for bases */
 	}
+	Cvar_SetValue("mn_installation_count", gd.numInstallations);
 	return qtrue;
 }
