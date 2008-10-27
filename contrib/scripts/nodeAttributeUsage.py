2da8693caede945d2eb3655301e118517b53aaff
bayo <bayo@users.sourceforge.net>
2008-10-27 19:06:06 +0000

    * add script to check note attribute usage

---
+++ contrib/scripts/nodeAttributeUsage.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+#
+# @brief check the usage of menuNode_s attributes into all menu file (call it from the root of the trunk)
+# @license Public domain,e
+# @return an XHTML page into stdout
+# @todo reading "attributes" from the nodes.h file
+#
+
+import os
+
+dir = 'src/client/menu'
+
+attributes = [
+	"name",
+	"key",
+	"type",
+	"origin",
+	"scale",
+	"angles",
+	"pos",
+	"size",
+	"texh",
+	"texl",
+	"state",
+	"align",
+	"border",
+	"padding",
+	"invis",
+	"blend",
+	"mousefx",
+	"container",
+	"horizontalScroll",
+	"textScroll",
+	"textLines",
+	"textLineSelected",
+	"timeOut",
+	"timePushed",
+	"timeOutOnce",
+	"num",
+	"height",
+	"baseid",
+	"options",
+	"color",
+	"bgcolor",
+	"bordercolor",
+	"selectedColor",
+	"click",
+	"rclick",
+	"mclick",
+	"wheel",
+	"mouseIn",
+	"mouseOut",
+	"wheelUp",
+	"wheelDown",
+	"repeat",
+	"clickDelay",
+	"scrollbar",
+	"scrollbarLeft",
+	"exclude",
+	"excludeNum",
+	"depends",
+	"menu",
+	"next",
+	"linestrips",
+	"pointWidth",
+	"gapWidth",
+	"scriptValues",
+	"dataStringOrImageOrModel",
+	"dataAnimOrFont",
+	"dataModelSkinOrCVar",
+	"dataNodeTooltip",
+	"abstractvalue",
+	"model",
+]
+
+files = []
+
+for f in os.listdir(dir):
+	if ".c" not in f:
+		continue
+
+	file = open(dir + '/' + f, "rt")
+	data = file.read()
+	file.close()
+
+	fd = f, data
+	files.append(fd)
+
+print "<html>"
+print "<body>"
+print "<table>"
+
+print "<tr><td></td>\n"
+for a in attributes:
+	print "<th>" + a + "</th>\n"
+print "</tr>"
+
+for fd in files:
+	f, data = fd
+
+	print "<tr><th>" + f + "</th>"
+	for a in attributes:
+		c = data.count("node->" + a)
+		style = ""
+		if c > 0:
+			style = " style=\"background-color:#00FF00\""
+		else:
+			a = ""
+		print "<td" + style + ">" + a + "</td>"
+	print "</tr>\n"
+
+print "</table>"
+
+
+
+print "<table>"
+for a in attributes:
+
+	print "<tr><th>" + a + "</th>"
+	print "<td>"
+	for fd in files:
+		f, data = fd
+		
+		c = data.count("node->" + a)
+		style = ""
+		if c > 0:
+			print f + " "
+	print "</td>"
+	print "</tr>"
+print "</table>"
+
+
+print "</body>"
+print "</html>"
+
+
