73f59f74f182942b8ae4c59e1ee26ea4eb8f52e3
bayo <bayo@users.sourceforge.net>
2008-11-01 08:02:59 +0000

    * tool to check menu

---
+++ contrib/scripts/UFOMenuAPI.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+
+#
+# @brief functions to check and update menu_*.ufo files
+# @license Public domaine
+#
+
+import os
+import re
+
+ufobase = 'base/ufos'
+
+
+removeComment = re.compile('//.*\\n')
+removeEvents = re.compile('(\s)(click|rclick|mclick|wheel|in|out|whup|whdown)\s*\{.*?\}', re.DOTALL)
+removeFunctions = re.compile('(\s)(confunc)(\s*)([a-zA-Z0-9_-]*)(\s*)\{.*?\}', re.DOTALL)
+
+mouseevent = ["click", "rclick", "mclick", "wheel", "in", "out", "whup", "whdown"]
+
+parseNodes = re.compile('\s*([a-zA-Z0-9_-]+)\s+([a-zA-Z0-9_-]+)\s*\{(.*?)\}', re.DOTALL)
+matchParam = re.compile("([^\"\s]\S*|\".*?\")")
+
+renameMenuParam = '(\smenu\s+%s\s*\{\s*\{.*?\s)(%s)(\s)'
+updateMenuParam = '(\smenu\s+%s\s*\{\s*\{.*?\s%s\s*)([^\"\s]\S*|\".*?\")(\s)'
+updateNodeParam = '(\smenu\s+%s\s*\{.*?\s%s\s+%s\s*\{.*?%s\s+)([^\"\s]\S*|\".*?\")(\s)'
+
+
+class Root:
+
+	def __init__(self):
+		self.nodes = NodeObject("", "")
+		self.files = {}
+		self.fileBases = {}
+
+	def parseUFOParam(self, object, data):
+		data = "           " + data.strip()
+		result = matchParam.findall(data)
+		while len(result) >= 2:
+			name, value = result.pop(0), result.pop(0)
+			object.addParam(name, value)
+		if len(result) > 0:
+			print 'unused value (' + object.name + '):' + result[0]
+
+	def parseUFOMenu(self, data, filename):
+		data = removeComment.sub('\n', data)
+		data = removeEvents.sub('\\1\\2 ...', data)
+		data = removeFunctions.sub('\\1\\2\\3\\4\\5{}', data)
+		data = '\n\n' + data
+	
+		menus = data.split('\nmenu')
+		menus.pop(0)
+		for m in menus:
+		
+			m = m.split('{', 1)
+			name = m[0].strip()
+			menu = NodeObject("menu", name)
+			menu.filename = filename
+			menu.root = self
+			m = m[1].strip()
+			m = m[0:len(m) - 1]
+	
+			# menu param
+			if m[0] == '{':
+				m = m.split('}', 1)
+				params = m[0][1:]
+				m = m[1].strip()
+				self.parseUFOParam(menu, params)
+			
+			# nodes
+			nodes = parseNodes.findall(m)
+			for node in nodes:
+				behaviour, name, param = node
+				node = NodeObject(behaviour, name)
+				node.filename = filename
+				node.root = self
+				node.parent = menu
+				menu.addChild(node)
+				self.parseUFOParam(node, param)
+			
+			
+			self.nodes.addChild(menu)
+
+	def loadFile(self, filename):
+		print 'load ' + filename
+		file = open(filename, "rt")
+		data = file.read()
+		file.close()
+		
+		self.files[filename] = data
+		self.fileBases[filename] = data
+		
+		self.parseUFOMenu(data, filename)
+	
+	
+	def loadAll(self):
+		for f in os.listdir(ufobase):
+			if "menu_" not in f:
+				continue
+			self.loadFile(ufobase + '/' + f)
+
+	def save(self):
+		for f, data in self.files.iteritems():
+			if data == self.fileBases[f]:
+				continue
+
+			print 'save ' + f
+			file = open(f, "wt")
+			file.write(data)
+			file.close()
+
+	def renameMenuParam(self, node, param, newname):
+		menu = node.name
+		filename = node.filename
+	
+		data = self.files[filename]
+		match = re.compile(renameMenuParam % (menu, param), re.DOTALL)
+		data = match.sub('\\1' + newname + '\\3', data, 1)
+		self.files[filename] = data
+
+	def updateMenuParam(self, node, param, value):
+		menu = node.name
+		filename = node.filename
+	
+		data = self.files[filename]
+		match = re.compile(updateMenuParam % (menu, param), re.DOTALL)
+		data = match.sub('\\1' + value + '\\3', data, 1)
+		self.files[filename] = data
+
+	def updateNodeParam(self, node, param, value):
+		menu = node.parent.name
+		filename = node.filename
+	
+		data = self.files[filename]
+		match = re.compile(updateNodeParam % (menu, node.behaviour, node.name, param), re.DOTALL)
+		data = match.sub('\\1' + value + '\\3', data, 1)
+		self.files[filename] = data
+
+		
+class NodeObject:
+	def __init__(self, behaviour, name):
+		self.behaviour = behaviour
+		self.name = name
+		self.param = {}
+		self.child = []
+		self.childname = {}
+		self.mouseevent = None
+		
+	def addChild(self, node):
+		self.child.append(node)
+		self.childname[node.name] = node
+		
+	def addParam(self, name, value):
+		if name == 'pos':
+			v = value[1:len(value)-1].split(" ")
+			self.pos = (int(v[0]), int(v[len(v)-1]))
+		elif name == 'size':
+			v = value[1:len(value)-1].split(" ")
+			self.size = (int(v[0]), int(v[len(v)-1]))
+		
+		self.param[name] = value
+		
+	def __print__(self, level = 0):
+		tab = '\t' * level
+		print tab + self.behaviour + " " + self.name
+		for name, value in self.param.iteritems():
+			print tab + '\t' + name + ' ' + str(value)
+		for child in self.child:
+			child.__print__(level + 1)
+
+	def existsParam(self, param):
+		return param in self.param
+	
+	def renameParam(self, old, new):
+		if self.behaviour == "menu":
+			self.root.renameMenuParam(self, old, new)
+
+	def updateParam(self, old, new):
+		if self.behaviour == "menu":
+			self.root.updateMenuParam(self, old, new)
+		else:
+			self.root.updateNodeParam(self, old, new)
+
+	def useMouseEvent(self):
+		if self.mouseevent == None:
+			self.mouseevent = False
+			for e in mouseevent:
+				if e in self.param:
+					self.mouseevent = True
+					break
+		return self.mouseevent
+
+
+
+def renameMenuOriginToPos():
+	root = Root()
+	root.loadAll()
+	#root.loadFile('base/ufos/menu_options.ufo')
+	
+	print '-----'
+	
+	for menu in root.nodes.child:
+		if not menu.existsParam("origin"):
+			continue
+		print menu.name
+		menu.renameMenuParam("origin", "pos")
+	
+	root.save()
+
+
+def searchWindowpanelWithoutParentPosSize():
+	root = Root()
+	root.loadAll()
+	#root.loadFile('base/ufos/menu_options.ufo')
+	
+	print '-----'
+	
+	for menu in root.nodes.child:
+		if menu.existsParam("pos") and menu.existsParam("size"):
+			continue
+		for node in menu.child:
+			if node.behaviour != "windowpanel":
+				continue
+				
+			print menu.filename + ' > menu ' + menu.name + ' have no pos or size'
+
+
+def fixeWindowpanelUnderWindow():
+	root = Root()
+	#root.loadAll()
+	#root.loadFile('base/ufos/menu_singleplayer.ufo')
+	#root.loadFile('base/ufos/menu_aircraft.ufo')
+	#root.loadFile('base/ufos/menu_geoscape.ufo')
+	#root.loadFile('base/ufos/menu_irc.ufo')
+	#root.loadFile('base/ufos/menu_lostwon.ufo')
+	#root.loadFile('base/ufos/menu_main.ufo')
+	root.loadFile('base/ufos/menu_multiplayer.ufo')
+	#root.loadFile('base/ufos/menu_options.ufo')
+	#root.loadFile('base/ufos/menu_singleplayer.ufo')
+
+	print '-----'
+	
+	for menu in root.nodes.child:
+		if not menu.existsParam("pos") or not menu.existsParam("size"):
+			continue
+		print 'menu ' + menu.name
+		
+		# find 	windowpanel
+		windowpanel = None
+		for node in menu.child:
+			if node.behaviour != "windowpanel":
+				continue
+			windowpanel = node
+			break
+		
+		# menu without background
+		if windowpanel == None:
+			break
+
+		# all right
+		if windowpanel.pos == (0, 0):
+			print 'menu ' + menu.name + ' (' + menu.filename + ') at right place'
+			break
+		
+		dx, dy = windowpanel.pos
+		x, y = menu.pos[0] + dx, menu.pos[1] + dy
+		menu.updateParam("pos", '"' + str(x) + " " + str(y) + '"')
+		
+		for node in menu.child:
+			if not node.existsParam("pos"):
+				continue
+				
+			x, y = node.pos[0] - dx, node.pos[1] - dy
+			node.updateParam("pos", '"' + str(x) + " " + str(y) + '"')
+
+	root.save()
+
+def replaceOptionContent():
+	root = Root()
+	root.loadFile('base/ufos/menu_options.ufo')
+
+	print '-----'
+	
+	for menu in root.nodes.child:
+		if menu.name not in ["options_video", "options_sound", "options_keys", "options_pause", "options_game", "options_input"]:
+			continue
+		print 'menu ' + menu.name
+		
+		dx, dy = 415, 0
+		for node in menu.child:
+			if not node.existsParam("pos"):
+				continue
+				
+			x, y = node.pos[0] - dx, node.pos[1] - dy
+			node.updateParam("pos", '"' + str(x) + " " + str(y) + '"')
+
+	root.save()
+
+replaceOptionContent()
