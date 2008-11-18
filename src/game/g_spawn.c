4ed5231c941114a2f9840f94c2bbdf93b771fa0b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-11-18 20:52:07 +0000

    * patch #2309302 (hurt crashed aliens)

--- src/game/g_spawn.c
+++ src/game/g_spawn.c
@@ -603,6 +603,19 @@ static void SP_alien_start (edict_t *ent)
 	/* set stats */
 	ent->STUN = 0;
 	ent->HP = MAX_HP;
+	/* hurt aliens in ufo crash missions (5%: almost dead, 15%: wounded, 30%: stunned)  */
+	if (gi.csi->currentMD && Q_strcmp(gi.csi->currentMD->id, "ufocrash") == 0) {
+		const float random = frand();
+		if (random <= .05f) {
+			ent->STUN = 50;
+			ent->HP = 5;
+		} else if (random <= .15f) {
+			ent->STUN = 30;
+			ent->HP /= 2;
+		} else if (random <= .3f) {
+			ent->STUN = 75;
+		}
+	}
 
 	G_ActorSpawn(ent);
 }
