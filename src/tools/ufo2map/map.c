f628da288167b0ca154ed72df666e4e9dd9dcbb6
Andy Buckle <blondandy@users.sourceforge.net>
2008-11-23 07:30:10 +0000

    prefix texture name length warning with two spaces to emulate Check_Printf output when in check or fix mode

--- src/tools/ufo2map/map.c
+++ src/tools/ufo2map/map.c
@@ -640,9 +640,11 @@ static void ParseBrush (entity_t *mapent, const char *filename)
 		/* read the texturedef */
 		GetToken(qfalse);
 		if (strlen(parsedToken) >= MAX_TEXPATH) {
+			if (config.performMapCheck || config.fixMap)
+				Com_Printf("  ");/* hack to make this look like output from Check_Printf() */
 			Com_Printf("ParseBrush: texture name too long (limit %i): %s\n", MAX_TEXPATH, parsedToken);
 			if (config.fixMap)
-				Sys_Error("Exiting, as -fix is active and saving might corrupt *.map by truncating texture name");
+				Sys_Error("Aborting, as -fix is active and saving might corrupt *.map by truncating texture name");
 		}
 		Q_strncpyz(td.name, parsedToken, sizeof(td.name));
 
