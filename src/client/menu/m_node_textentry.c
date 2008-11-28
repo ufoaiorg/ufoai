c0f46aaa13528534b2168575ba3619172f8cefb0
bayo <bayo@users.sourceforge.net>
2008-11-28 11:17:06 +0000

    * implement textentry

--- src/client/menu/m_node_textentry.c
+++ src/client/menu/m_node_textentry.c
@@ -1,5 +1,7 @@
 /**
  * @file m_node_textentry.c
+ * @todo must we need to use command to interact with keyboard?
+ * @todo add a command to force the text edition from script
  */
 
 /*
@@ -24,19 +26,171 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "m_nodes.h"
 #include "m_node_textentry.h"
+#include "m_parse.h"
+#include "m_drawutil.h"
+#include "m_font.h"
+#include "m_input.h"
+#include "m_actions.h"
+#include "../client.h"
 
-static void MN_TextEntryClick (menuNode_t *node, int x, int y)
+static const int TILE_SIZE = 64;
+static const int CORNER_SIZE = 17;
+static const int MID_SIZE = 1;
+static const int MARGE = 3;
+
+static void MN_TextEntryNodeRemoveFocus(menuNode_t *node);
+static void MN_TextEntryNodeSetFocus(menuNode_t *node);
+
+static char cmdChanged[64];
+static char cmdAborted[64];
+
+/**
+ * @brief fire the change event
+ */
+static inline void MN_TextEntryNodeFireChange (menuNode_t *node)
 {
+	/* fire change event */
+	if (node->change) {
+		MN_ExecuteEventActions(node, node->change);
+	}
+}
+
+/**
+ * @brief callback from the keyboard
+ */
+static void MN_TextEntryNodeKeyboardChanged_f ()
+{
+	menuNode_t *node = (menuNode_t *) Cmd_Userdata();
+	MN_TextEntryNodeFireChange(node);
+	MN_TextEntryNodeRemoveFocus(node);
+}
+
+/**
+ * @brief callback from the keyboard
+ */
+static void MN_TextEntryNodeKeyboardAborted_f ()
+{
+	menuNode_t *node = (menuNode_t *) Cmd_Userdata();
+	MN_TextEntryNodeRemoveFocus(node);
+}
+
+/**
+ * @todo save last existing commands, to restitute it
+ * @todo log warning for buffer overflow
+ */
+static void MN_TextEntryNodeSetFocus (menuNode_t *node)
+{
+	MN_SetMouseCapture(node);
+
+	/* register keyboard callback */
+	snprintf(cmdChanged, sizeof(cmdChanged), "%s_changed", &((char*)node->text)[6]);
+	if (Cmd_Exists(cmdChanged)) {
+		Sys_Error("MN_TextEntryNodeSetFocus: '%s' already used, code down yet allow context restitution. Plese clean up your script.\n", cmdChanged);
+	}
+	Cmd_AddCommand(cmdChanged, MN_TextEntryNodeKeyboardChanged_f, "Text entry callback");
+	Cmd_AddUserdata(cmdChanged, node);
+	snprintf(cmdAborted, sizeof(cmdAborted), "%s_aborted", &((char*)node->text)[6]);
+	if (Cmd_Exists(cmdAborted)) {
+		Sys_Error("MN_TextEntryNodeSetFocus: '%s' already used, code down yet allow context restitution. Plese clean up your script.\n", cmdAborted);
+	}
+	Cmd_AddCommand(cmdAborted, MN_TextEntryNodeKeyboardAborted_f, "Text entry callback");
+	Cmd_AddUserdata(cmdAborted, node);
+
+	/* start typing */
+	Cbuf_AddText(va("msgmenu ?%s\n", &((char*)node->text)[6]));
+}
+
+static void MN_TextEntryNodeRemoveFocus (menuNode_t *node)
+{
+	Cmd_RemoveCommand(cmdChanged);
+	Cmd_RemoveCommand(cmdAborted);
+	MN_MouseRelease();
+}
+
+/**
+ * @todo remove the "mouse capture" for a "focus", maybe better
+ */
+static void MN_TextEntryNodeClick (menuNode_t *node, int x, int y)
+{
+	if (node->disabled)
+		return;
+
+	/* no cvar */
+	if (!node->text)
+		return;
+	if (Q_strncmp(node->text, "*cvar", 5))
+		return;
+
+	if (!MN_GetMouseCapture()) {
+		MN_TextEntryNodeSetFocus(node);
+	} else {
+		int x = mousePosX;
+		int y = mousePosY;
+		MN_NodeAbsoluteToRelativePos(node, &x, &y);
+		if (x < 0 || y < 0 || x > node->pos[0] || y > node->pos[1]) {
+			/* keyboard, please stop */
+			Cbuf_AddText("msgmenu .\n");
+		}
+	}
 }
 
 static void MN_TextEntryNodeDraw (menuNode_t *node)
 {
+	const char *text;
+	const char *font;
+	int texX, texY;
+	const float *textColor;
+	const char *image;
+	vec2_t pos;
+	static vec4_t disabledColor = {0.5, 0.5, 0.5, 1.0};
+
+	if (node->disabled) {
+		/** @todo need custom color when button is disabled */
+		textColor = disabledColor;
+		texX = TILE_SIZE;
+		texY = TILE_SIZE;
+	} else if (node->state || MN_GetMouseCapture() == node) {
+		textColor = node->selectedColor;
+		texX = TILE_SIZE;
+		texY = 0;
+	} else {
+		textColor = node->color;
+		texX = 0;
+		texY = 0;
+	}
+
+	MN_GetNodeAbsPos(node, pos);
+
+	image = MN_GetReferenceString(node->menu, node->dataImageOrModel);
+	if (image) {
+		MN_DrawPanel(pos, node->size, image, node->blend, texX, texY, CORNER_SIZE, MID_SIZE, MARGE);
+	}
+
+	text = MN_GetReferenceString(node->menu, node->text);
+	if (text != NULL && *text != '\0') {
+		/** @todo we dont need to edit the text to draw the cursor */
+		if (MN_GetMouseCapture() == node) {
+			if (cl.time % 1000 < 500) {
+				text = va("%s|", text);
+			}
+		}
+
+		font = MN_GetFont(node->menu, node);
+		R_ColorBlend(textColor);
+		R_FontDrawStringInBox(font, node->textalign,
+			pos[0] + node->padding, pos[1] + node->padding,
+			node->size[0] - node->padding - node->padding, node->size[1] - node->padding - node->padding,
+			text, LONGLINES_PRETTYCHOP);
+		R_ColorBlend(NULL);
+	}
+
 }
 
 static void MN_TextEntryNodeLoaded (menuNode_t *node)
 {
 	node->padding = 8;
-	node->textalign = ALIGN_LC;
+	node->textalign = ALIGN_CL;
+	Vector4Set(node->color, 1, 1, 1, 1);
 	Vector4Set(node->selectedColor, 1, 1, 1, 1);
 }
 
@@ -44,7 +198,7 @@ void MN_RegisterTextEntryNode (nodeBehaviour_t *behaviour)
 {
 	behaviour->name = "textentry";
 	behaviour->id = MN_TEXTENTRY;
-	behaviour->leftClick = MN_TextEntryClick;
+	behaviour->leftClick = MN_TextEntryNodeClick;
 	behaviour->draw = MN_TextEntryNodeDraw;
 	behaviour->loaded = MN_TextEntryNodeLoaded;
 }
