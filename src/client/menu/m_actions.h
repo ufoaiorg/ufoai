93b3595b6277755348ca2233f2a673c16a38f91e
bayo <bayo@users.sourceforge.net>
2008-12-16 09:37:35 +0000

    * prevent problem with ming (if it exists something better?)

--- src/client/menu/m_actions.h
+++ src/client/menu/m_actions.h
@@ -54,7 +54,18 @@ typedef struct menuAction_s {
 	const value_t *scriptValues;
 } menuAction_t;
 
+/* prevent preprocessing of 'printf' into __attribute__ */
+#define _SAVE_PRINTF_ printf
+#undef printf
+
+#ifdef __GNUC__
 void MN_ExecuteConfunc(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+#else
+void MN_ExecuteConfunc(const char *fmt, ...);
+#endif
+
+#define printf _SAVE_PRINTF_
+
 
 void MN_ExecuteActions(const menu_t* const menu, menuAction_t* const first);
 void MN_ConfuncCommand_f(void);
