9b3669e87a4f6093b0bdbbc40021ec2a48c2ceab
Martin Gerhardy <tlh2000@users.sourceforge.net>
2008-12-17 15:55:37 +0000

    * missing include

--- src/tools/radiant/radiant/sidebar/sidebar.h
+++ src/tools/radiant/radiant/sidebar/sidebar.h
@@ -19,6 +19,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "entitylist.h"
 #include "entityinspector.h"
+#include "../surfaceinspector/surfaceinspector.h"
 #include <gtk/gtk.h>
 
 void Sidebar_constructEntities(GtkWidget *notebook);
