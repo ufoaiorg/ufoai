1a4bb37b3cd739e524302ddc8cf64b1db5b1946f
Kostia Romanov <kildor@users.sourceforge.net>
2009-01-03 19:58:21 +0000

    * fixed compile_po.bat to reflect last naming changes. ! It need to be run with /ufoai or|and /uforadiant keys to compile accordinaly languages * add changes to ufo|radiant projects * add russian draft of radiant translation (done less then 10%) * some changes in ufoai-ru.po

--- contrib/scripts/compile_po.bat
+++ contrib/scripts/compile_po.bat
@@ -8,18 +8,76 @@ echo =====================================================
 
 set msgfmtBin=msgfmt.exe
 set i18ndir=..\..\base\i18n
+set i18nradiantdir=..\..\radiant\i18n
 set scriptdir=..\..\contrib\scripts
 
 if NOT EXIST %i18ndir% (
 	echo %i18ndir% does not exists;
 )
 
+IF "%1"=="" (
+echo Missed argument.
+echo 	/ufoai or /uforadiant are acceptable
+GOTO Continue)
+
+
 cd ..\..\src\po
-for %%i in (.\*.po) DO (
-	if NOT EXIST "%i18ndir%\%%~ni\LC_MESSAGES" (
-		mkdir "%i18ndir%\%%~ni\LC_MESSAGES"
+
+
+:Loop
+IF "%1"=="" ( GOTO Continue)
+if "%1" == "/ufoai" (
+	echo Compiling UFO:AI language files.
+	for %%i in (.\ufoai-*.po) DO (
+		call :getlocaleforgame %%i
+		)
+) else if "%1" == "/uforadiant" (
+	echo Compiling UFORadiant language files.
+	for %%i in (.\uforadiant-*.po) DO (
+		call :getlocaleforradiant %%i
+		)
+) else (
+	echo Argument "%1" doesn`t understood.
+	echo 	/ufoai or /uforadiant are acceptable
+	goto :EOF
 	)
-	echo compiling %%i to "%i18ndir%\%%~ni\LC_MESSAGES\ufoai.mo"
-	%msgfmtBin% -o "%i18ndir%\%%~ni\LC_MESSAGES\ufoai.mo" %%i
-)
+
+SHIFT
+goto :Loop
+
+:Continue
+
 cd %scriptdir%
+
+goto :EOF
+
+
+:getlocaleforgame
+
+set dirname=%~n1
+set shortname=%dirname:~6%
+
+	if NOT EXIST "%i18ndir%\%shortname%\LC_MESSAGES" (
+		mkdir "%i18ndir%\%shortname%\LC_MESSAGES"
+	)
+	echo compiling %1 to "%i18ndir%\%shortname%\LC_MESSAGES\ufoai.mo"
+	%msgfmtBin% -o "%i18ndir%\%shortname%\LC_MESSAGES\ufoai.mo" %1
+
+exit /b
+
+:getlocaleforradiant
+
+set dirname=%~n1
+set shortname=%dirname:~11%
+
+	if NOT EXIST "%i18nradiantdir%\%shortname%\LC_MESSAGES" (
+		mkdir "%i18nradiantdir%\%shortname%\LC_MESSAGES"
+	)
+	echo compiling %1 to "%i18nradiantdir%\%shortname%\LC_MESSAGES\uforadiant.mo"
+	%msgfmtBin% -o "%i18nradiantdir%\%shortname%\LC_MESSAGES\uforadiant.mo" %1
+
+exit /b
+
+
+
+:EOF
