eedbabf122e77b19eab01ce0273fa75b53acc524
Kostia Romanov <kildor@users.sourceforge.net>
2009-01-10 17:44:18 +0000

    * update script (now old version is backuped as POTFILES.old)

--- contrib/scripts/update_potfiles_in.bat
+++ contrib/scripts/update_potfiles_in.bat
@@ -13,6 +13,7 @@ pushd ..\..\src
 set poTMP=po\POTFILES.tmp
 set poTMP1=po\POTFILES1.tmp
 set poIN=po\POTFILES.in
+set poOLD=po\POTFILES.old
 
 if exist %poTMP% (del %poTMP%)
 if exist %poTMP1% (del %poTMP1%)
@@ -26,6 +27,9 @@ echo ./po/OTHER_STRINGS >> %poTMP%
 
 for /f "tokens=*" %%a in ('more^<%poTMP%') do call :fixslashes %%a
 
+echo Backup old version to %poOLD%
+
+move %poIN% %poOLD%
 move %poTMP1% %poIN%
 del %poTMP%
 
