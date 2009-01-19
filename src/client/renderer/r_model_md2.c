17c3dc279a27a245c029693de5092e40cb510aeb
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-01-19 06:02:33 +0000

    * removed MAX_LBM_HEIGHT

--- src/client/renderer/r_model_md2.c
+++ src/client/renderer/r_model_md2.c
@@ -25,8 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "r_local.h"
 
-#define MAX_LBM_HEIGHT 1024
-
 /*
 ==============================================================================
 MD2 ALIAS MODELS
