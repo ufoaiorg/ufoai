610299c719046531bb389838601e5812ce19b5b3
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-01-24 17:28:54 +0000

    * modified the valgrind script a little bit

--- contrib/scripts/valgrind.sh
+++ contrib/scripts/valgrind.sh
@@ -1,22 +1,55 @@
 #!/bin/sh
 
 LANG=C
-DIR=.
 
-if [ $# -eq 1 ]
+if [ $# -ne 1 ]
 then
-	echo "Use $1"
-	DIR="$1"
+	echo "No binary given"
+	exit 1
 fi
 
+APP_PATH=$1
 
-APP=${DIR}/ufo
-LOG=${DIR}/valgrind.log
+APP=$(readlink -f "${APP_PATH}")
+
+if [ ! -x "${APP}" ]
+then
+	echo "${APP} is no executable"
+	exit 1
+fi
+
+PATH_ONLY=$(dirname "${APP}")
+BIN_ONLY=$(basename "${APP}")
+
+# path to the valgrind suppression file
+SUP=$(readlink -f "${0}")
+SUP=$(dirname "${SUP}")
+SUP="${SUP}/valgrind.sup"
+
+cd ${PATH_ONLY}
+LOG=${PATH_ONLY}/valgrind.log
+
+if [ -f "${SUP}" ] 
+then
+	echo "Using valgrind suppression file from '${SUP}'"
+	VALGRIND_OPTIONS="${VALGRIND_OPTIONS} --suppressions=${SUP}"
+else
+	echo "No valgrind suppression file at '${SUP}'"
+fi
+
+echo "Debugging ${APP_PATH}"
+echo "Log file will be created at ${LOG}"
+
+#--gen-suppressions=yes \
+#--verbose \
+#--demangle=yes \
+
 valgrind \
-	--verbose \
 	--show-reachable=yes \
-	--log-file=$LOG \
 	--leak-check=full \
 	--track-fds=yes \
 	--run-libc-freeres=no \
+	--log-file=$LOG \
+	$VALGRIND_OPTIONS \
 	$APP
+
