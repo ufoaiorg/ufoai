f0ca72a007ca8ddb42ec7d46969f46b3016ed8d3
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-01-25 18:50:40 +0000

    * gettext markers

--- src/tools/radiant/radiant/toolbars.cpp
+++ src/tools/radiant/radiant/toolbars.cpp
@@ -24,77 +24,78 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include "radiant_i18n.h"
 #include "toolbars.h"
 #include "gtkmisc.h"
 #include "camwindow.h"
 
 static void File_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_button(toolbar, "Open an existing map (CTRL + O)", "file_open.bmp", "OpenMap");
-	toolbar_append_button(toolbar, "Save the active map (CTRL + S)", "file_save.bmp", "SaveMap");
+	toolbar_append_button(toolbar, _("Open an existing map (CTRL + O)"), "file_open.bmp", "OpenMap");
+	toolbar_append_button(toolbar, _("Save the active map (CTRL + S)"), "file_save.bmp", "SaveMap");
 }
 
 static void UndoRedo_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_button(toolbar, "Undo (CTRL + Z)", "undo.bmp", "Undo");
-	toolbar_append_button(toolbar, "Redo (CTRL + Y)", "redo.bmp", "Redo");
+	toolbar_append_button(toolbar, _("Undo (CTRL + Z)"), "undo.bmp", "Undo");
+	toolbar_append_button(toolbar, _("Redo (CTRL + Y)"), "redo.bmp", "Redo");
 }
 
 static void RotateFlip_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_button(toolbar, "x-axis Flip", "brush_flipx.bmp", "MirrorSelectionX");
-	toolbar_append_button(toolbar, "x-axis Rotate", "brush_rotatex.bmp", "RotateSelectionX");
-	toolbar_append_button(toolbar, "y-axis Flip", "brush_flipy.bmp", "MirrorSelectionY");
-	toolbar_append_button(toolbar, "y-axis Rotate", "brush_rotatey.bmp", "RotateSelectionY");
-	toolbar_append_button(toolbar, "z-axis Flip", "brush_flipz.bmp", "MirrorSelectionZ");
-	toolbar_append_button(toolbar, "z-axis Rotate", "brush_rotatez.bmp", "RotateSelectionZ");
+	toolbar_append_button(toolbar, _("x-axis Flip"), "brush_flipx.bmp", "MirrorSelectionX");
+	toolbar_append_button(toolbar, _("x-axis Rotate"), "brush_rotatex.bmp", "RotateSelectionX");
+	toolbar_append_button(toolbar, _("y-axis Flip"), "brush_flipy.bmp", "MirrorSelectionY");
+	toolbar_append_button(toolbar, _("y-axis Rotate"), "brush_rotatey.bmp", "RotateSelectionY");
+	toolbar_append_button(toolbar, _("z-axis Flip"), "brush_flipz.bmp", "MirrorSelectionZ");
+	toolbar_append_button(toolbar, _("z-axis Rotate"), "brush_rotatez.bmp", "RotateSelectionZ");
 }
 
 static void Select_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_button(toolbar, "Select touching", "selection_selecttouching.bmp", "SelectTouching");
-	toolbar_append_button(toolbar, "Select inside", "selection_selectinside.bmp", "SelectInside");
-	toolbar_append_button(toolbar, "Select whole entity", "selection_selectentities.bmp", "ExpandSelectionToEntities");
-	toolbar_append_button(toolbar, "Select all of type", "selection_selectcompletetall.bmp", "SelectAllOfType");
+	toolbar_append_button(toolbar, _("Select touching"), "selection_selecttouching.bmp", "SelectTouching");
+	toolbar_append_button(toolbar, _("Select inside"), "selection_selectinside.bmp", "SelectInside");
+	toolbar_append_button(toolbar, _("Select whole entity"), "selection_selectentities.bmp", "ExpandSelectionToEntities");
+	toolbar_append_button(toolbar, _("Select all of type"), "selection_selectcompletetall.bmp", "SelectAllOfType");
 }
 
 static void CSG_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_button(toolbar, "CSG Subtract (SHIFT + U)", "selection_csgsubtract.bmp", "CSGSubtract");
-	toolbar_append_button(toolbar, "CSG Merge (CTRL + U)", "selection_csgmerge.bmp", "CSGMerge");
-	toolbar_append_button(toolbar, "Hollow", "selection_makehollow.bmp", "CSGHollow");
+	toolbar_append_button(toolbar, _("CSG Subtract (SHIFT + U)"), "selection_csgsubtract.bmp", "CSGSubtract");
+	toolbar_append_button(toolbar, _("CSG Merge (CTRL + U)"), "selection_csgmerge.bmp", "CSGMerge");
+	toolbar_append_button(toolbar, _("Hollow"), "selection_makehollow.bmp", "CSGHollow");
 }
 
 static void ComponentModes_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_toggle_button(toolbar, "Select Vertices (V)", "modify_vertices.bmp", "DragVertices");
-	toolbar_append_toggle_button(toolbar, "Select Edges (E)", "modify_edges.bmp", "DragEdges");
-	toolbar_append_toggle_button(toolbar, "Select Faces (F)", "modify_faces.bmp", "DragFaces");
+	toolbar_append_toggle_button(toolbar, _("Select Vertices (V)"), "modify_vertices.bmp", "DragVertices");
+	toolbar_append_toggle_button(toolbar, _("Select Edges (E)"), "modify_edges.bmp", "DragEdges");
+	toolbar_append_toggle_button(toolbar, _("Select Faces (F)"), "modify_faces.bmp", "DragFaces");
 }
 
 static void Clipper_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_toggle_button(toolbar, "Clipper (X)", "view_clipper.bmp", "ToggleClipper");
+	toolbar_append_toggle_button(toolbar, _("Clipper (X)"), "view_clipper.bmp", "ToggleClipper");
 }
 
 static void Filters_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_toggle_button(toolbar, "Filter nodraw", "filter_nodraw.bmp", "FilterNodraw");
-	toolbar_append_toggle_button(toolbar, "Filter actorclip", "filter_actorclip.bmp", "FilterActorClips");
-	toolbar_append_toggle_button(toolbar, "Filter weaponclip", "filter_weaponclip.bmp", "FilterWeaponClips");
+	toolbar_append_toggle_button(toolbar, _("Filter nodraw"), "filter_nodraw.bmp", "FilterNodraw");
+	toolbar_append_toggle_button(toolbar, _("Filter actorclip"), "filter_actorclip.bmp", "FilterActorClips");
+	toolbar_append_toggle_button(toolbar, _("Filter weaponclip"), "filter_weaponclip.bmp", "FilterWeaponClips");
 }
 
 static void XYWnd_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_button(toolbar, "Change views", "view_change.bmp", "NextView");
+	toolbar_append_button(toolbar, _("Change views"), "view_change.bmp", "NextView");
 }
 
 static void Manipulators_constructToolbar (GtkToolbar* toolbar)
 {
-	toolbar_append_toggle_button(toolbar, "Translate (W)", "select_mousetranslate.bmp", "MouseTranslate");
-	toolbar_append_toggle_button(toolbar, "Rotate (R)", "select_mouserotate.bmp", "MouseRotate");
-	toolbar_append_toggle_button(toolbar, "Scale", "select_mousescale.bmp", "MouseScale");
-	toolbar_append_toggle_button(toolbar, "Resize (Q)", "select_mouseresize.bmp", "MouseDrag");
+	toolbar_append_toggle_button(toolbar, _("Translate (W)"), "select_mousetranslate.bmp", "MouseTranslate");
+	toolbar_append_toggle_button(toolbar, _("Rotate (R)"), "select_mouserotate.bmp", "MouseRotate");
+	toolbar_append_toggle_button(toolbar, _("Scale"), "select_mousescale.bmp", "MouseScale");
+	toolbar_append_toggle_button(toolbar, _("Resize (Q)"), "select_mouseresize.bmp", "MouseDrag");
 
 	Clipper_constructToolbar(toolbar);
 }
@@ -137,11 +138,12 @@ GtkToolbar* create_main_toolbar_horizontal (MainFrame::EViewStyle style)
 	Filters_constructToolbar(toolbar);
 
 	gtk_toolbar_append_space(GTK_TOOLBAR (toolbar));
-	toolbar_append_button(toolbar, "Refresh Models", "refresh_models.bmp", "RefreshReferences");
-	toolbar_append_button(toolbar, "Background image", "background.bmp", "ToggleBackground");
+	toolbar_append_button(toolbar, _("Refresh Models"), "refresh_models.bmp", "RefreshReferences");
+	toolbar_append_button(toolbar, _("Background image"), "background.bmp", "ToggleBackground");
 
 	gtk_toolbar_append_space(GTK_TOOLBAR (toolbar));
-	toolbar_append_button(toolbar, "Create material entries for selected faces (M)", "material_generate.bmp", "GenerateMaterialFromTexture");
+	toolbar_append_button(toolbar, _("Create material entries for selected faces (M)"), "material_generate.bmp",
+			"GenerateMaterialFromTexture");
 
 	return toolbar;
 }
@@ -167,10 +169,10 @@ GtkToolbar* create_main_toolbar_vertical (MainFrame::EViewStyle style)
 
 	gtk_toolbar_append_space(GTK_TOOLBAR(toolbar));
 
-	toolbar_append_toggle_button(toolbar, "Texture Lock (SHIFT +T)", "texture_lock.bmp", "TogTexLock");
+	toolbar_append_toggle_button(toolbar, _("Texture Lock (SHIFT + T)"), "texture_lock.bmp", "TogTexLock");
 
 	if (style != MainFrame::eRegular) {
-		toolbar_append_button(toolbar, "Texture Browser (T)", "texture_browser.bmp", "ToggleTextures");
+		toolbar_append_button(toolbar, _("Texture Browser (T)"), "texture_browser.bmp", "ToggleTextures");
 	}
 
 	return toolbar;
