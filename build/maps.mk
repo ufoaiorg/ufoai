f401c193cc7a21bdae25eef4acf9aca258d9c7b7
zerm <zerm@users.sourceforge.net>
2009-02-03 14:28:02 +0000

    * no need for xargs (does not work this way on macosx) to delete. find happily deletes on your command

--- build/maps.mk
+++ build/maps.mk
@@ -35,7 +35,9 @@ maps-ents:
 	$(MAKE) maps UFO2MAPFLAGS="$(ENTS_UFO2MAPFLAGS)"
 
 clean-maps:
-	find $(MAPSDIR) -name '*.bsp' | xargs --no-run-if-empty rm
+	@echo "Deleting maps..."
+	@find $(MAPSDIR) -name '*.bsp' -delete
+	@echo "done"
 
 $(BSPS): %.bsp: %.map
 	$(UFO2MAP) $(UFO2MAPFLAGS) $<
