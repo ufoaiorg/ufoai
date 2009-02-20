b05b0b50634671af6f95bfc3e2ee6d8dce4bdf75
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-02-20 16:49:55 +0000

    * added gcc attribute malloc to some _Mem_* functions

--- src/common/mem.h
+++ src/common/mem.h
@@ -53,18 +53,18 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define Mem_TouchGlobal()								_Mem_TouchGlobal(__FILE__,__LINE__)
 
 /* functions */
-struct memPool_s *_Mem_CreatePool(const char *name, const char *fileName, const int fileLine);
+struct memPool_s *_Mem_CreatePool(const char *name, const char *fileName, const int fileLine) __attribute__ ((malloc));
 uint32_t _Mem_DeletePool(struct memPool_s *pool, const char *fileName, const int fileLine);
 
 uint32_t _Mem_Free(void *ptr, const char *fileName, const int fileLine);
 uint32_t _Mem_FreeTag(struct memPool_s *pool, const int tagNum, const char *fileName, const int fileLine);
 uint32_t _Mem_FreePool(struct memPool_s *pool, const char *fileName, const int fileLine);
-void* _Mem_Alloc(size_t size, qboolean zeroFill, struct memPool_s *pool, const int tagNum, const char *fileName, const int fileLine);
+void* _Mem_Alloc(size_t size, qboolean zeroFill, struct memPool_s *pool, const int tagNum, const char *fileName, const int fileLine) __attribute__ ((malloc));
 void* _Mem_ReAlloc(void *ptr, size_t size, const char *fileName, const int fileLine);
 
 size_t Mem_Size(const void *ptr);
 char* _Mem_PoolStrDupTo(const char *in, char **out, struct memPool_s *pool, const int tagNum, const char *fileName, const int fileLine);
-char* _Mem_PoolStrDup(const char *in, struct memPool_s *pool, const int tagNum, const char *fileName, const int fileLine);
+char* _Mem_PoolStrDup(const char *in, struct memPool_s *pool, const int tagNum, const char *fileName, const int fileLine) __attribute__ ((malloc));
 uint32_t _Mem_PoolSize(struct memPool_s *pool);
 uint32_t _Mem_TagSize(struct memPool_s *pool, const int tagNum);
 uint32_t _Mem_ChangeTag(struct memPool_s *pool, const int tagFrom, const int tagTo);
