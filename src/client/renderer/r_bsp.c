6cbd15a8dd3dcbce6295a89784f2fa14f07fa906
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-02-20 16:49:29 +0000

    * fixed warning

--- src/client/renderer/r_bsp.c
+++ src/client/renderer/r_bsp.c
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "r_local.h"
 #include "r_lightmap.h"
 #include "r_material.h"
+#include "r_light.h"
 
 /*
 =============================================================
