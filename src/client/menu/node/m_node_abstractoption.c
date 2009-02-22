b88a624f8fc1949b825e1fd233b1e66220859ad8
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-02-22 18:31:58 +0000

    * removed header inclusion

--- src/client/menu/node/m_node_abstractoption.c
+++ src/client/menu/node/m_node_abstractoption.c
@@ -24,8 +24,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#include "../../client.h"
-#include "../../renderer/r_draw.h"
 #include "../m_main.h"
 #include "../m_internal.h"
 #include "../m_parse.h"
