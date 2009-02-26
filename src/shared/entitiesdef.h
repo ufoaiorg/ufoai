828de5562dab570f65a56e1ffdf30cd9114bfecd
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-02-26 20:50:29 +0000

    * whitespaces

--- src/shared/entitiesdef.h
+++ src/shared/entitiesdef.h
@@ -76,12 +76,12 @@ typedef struct entityDef_s {
 	int 			numKeyDefs;
 } entityDef_t;
 
-int ED_GetIntVector (const entityKeyDef_t *kd, int v[], const int n);
-int ED_Check (const char *classname, const char *key, const char *value);
-int ED_CheckKey (const entityKeyDef_t *kd, const char *value);
-const entityDef_t *ED_GetEntityDef (const char *classname);
-const entityKeyDef_t *ED_GetKeyDef (const char *classname, const char *keyname, const int abstract);
-const entityKeyDef_t *ED_GetKeyDefEntity (const entityDef_t *ed, const char *keyname, const int abstract);
+int ED_GetIntVector(const entityKeyDef_t *kd, int v[], const int n);
+int ED_Check(const char *classname, const char *key, const char *value);
+int ED_CheckKey(const entityKeyDef_t *kd, const char *value);
+const entityDef_t *ED_GetEntityDef(const char *classname);
+const entityKeyDef_t *ED_GetKeyDef(const char *classname, const char *keyname, const int abstract);
+const entityKeyDef_t *ED_GetKeyDefEntity(const entityDef_t *ed, const char *keyname, const int abstract);
 int ED_Parse(const char *data_p);
 const char *ED_GetLastError(void);
 void ED_Free(void);
