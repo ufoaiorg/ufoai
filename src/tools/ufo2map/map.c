33d4a30521a402773b1bbaa43ab9a44828e751d9
Andy Buckle <blondandy@users.sourceforge.net>
2009-03-01 09:54:29 +0000

    * include checkenties.h, wich declares the recently moved Check_ExtraBrushesForWorldspawn

--- src/tools/ufo2map/map.c
+++ src/tools/ufo2map/map.c
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "bsp.h"
 #include "check/check.h"
+#include "check/checkentities.h"
 #include "common/aselib.h"
 
 mapbrush_t mapbrushes[MAX_MAP_BRUSHES];
