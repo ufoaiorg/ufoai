aa75d2843d7e28f3968efd506f7c499bf139f07f
bayo <bayo@users.sourceforge.net>
2009-04-13 15:51:03 +0000

    * fix wrong copy-paste (see previous commit)

--- src/client/cl_main.c
+++ src/client/cl_main.c
@@ -674,7 +674,7 @@ void CL_ParseClientData (const char *type, const char *name, const char **text)
 	else if (!strcmp(type, "ugv"))
 		CL_ParseUGVs(name, text);
 	else if (!strcmp(type, "window"))
-		MN_ParseMenu(type, name, &text);
+		MN_ParseMenu(type, name, text);
 }
 
 /** @brief Cvars for initial check (popup at first start) */
