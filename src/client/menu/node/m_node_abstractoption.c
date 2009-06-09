2fa27b40932f44b4bd462e0d01836041fcccba86
bayo <bayo@users.sourceforge.net>
2009-06-09 17:06:55 +0000

    * add mn_updateoption to update some option status * add a demo into contrib

--- src/client/menu/node/m_node_abstractoption.c
+++ src/client/menu/node/m_node_abstractoption.c
@@ -72,6 +72,52 @@ menuOption_t* MN_NodeAppendOption (menuNode_t *node, menuOption_t *newOption)
 	return newOption;
 }
 
+static void MN_UpdateOption_f (void)
+{
+	menuNode_t *node;
+	menuOption_t *option;
+
+	if (Cmd_Argc() != 4) {
+		Com_Printf("Usage: %s <nodepath> <optionname> <hide|display|enable|disable>\n", Cmd_Argv(0));
+		return;
+	}
+
+	node = MN_GetNodeByPath(Cmd_Argv(1));
+	if (node == NULL) {
+		Com_Printf("MN_UpdateOption_f: '%s' node not found.\n", Cmd_Argv(1));
+		return;
+	}
+
+	if (!MN_NodeInstanceOf(node, "abstractoption")) {
+		Com_Printf("MN_UpdateOption_f: '%s' node is not an 'abstractoption'.\n", Cmd_Argv(1));
+		return;
+	}
+
+	option = node->u.option.first;
+	while (option) {
+		if (!strcmp(option->id, Cmd_Argv(2)))
+			break;
+		option = option->next;
+	}
+	if (option == NULL) {
+		Com_Printf("MN_UpdateOption_f: option '%s' from '%s' node not found.\n", Cmd_Argv(2), Cmd_Argv(1));
+		return;
+	}
+
+	if (!strcmp("disable", Cmd_Argv(3))) {
+		option->disabled = qtrue;
+	} else if (!strcmp("enable", Cmd_Argv(3))) {
+		option->disabled = qfalse;
+	} else if (!strcmp("hide", Cmd_Argv(3))) {
+		option->invis = qtrue;
+	} else if (!strcmp("display", Cmd_Argv(3))) {
+		option->invis = qfalse;
+	} else {
+		Com_Printf("MN_AddListener_f: '%s' command do not exists.\n", Cmd_Argv(3));
+		return;
+	}
+}
+
 static const value_t properties[] = {
 	{"dataid", V_UI_DATAID, offsetof(menuNode_t, u.option.dataId), MEMBER_SIZEOF(menuNode_t, u.option.dataId)},
 	{"option", V_UI_OPTIONNODE, offsetof(menuNode_t, u.option.first), 0},
@@ -87,4 +133,5 @@ void MN_RegisterAbstractOptionNode (nodeBehaviour_t *behaviour)
 	behaviour->name = "abstractoption";
 	behaviour->isAbstract = qtrue;
 	behaviour->properties = properties;
+	Cmd_AddCommand("mn_updateoption", MN_UpdateOption_f, "Update some option status");
 }
