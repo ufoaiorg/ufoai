0900dd02fe6a04835daa6c79643d2ef0fc1b8874
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-06-21 17:17:38 +0000

    * doxygen

--- src/client/events/event/actor/e_event_actordie.c
+++ src/client/events/event/actor/e_event_actordie.c
@@ -30,7 +30,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "e_event_actordie.h"
 
 /**
- * @brief Kills actor.
+ * @brief Kills an actor (all that is needed is the local entity state set to STATE_DEAD).
+ * @note Also changes the animation to a random death sequence and appends the dead animation
  * @param[in] msg The netchannel message
  */
 void CL_ActorDie (const eventRegister_t *self, struct dbuffer *msg)
