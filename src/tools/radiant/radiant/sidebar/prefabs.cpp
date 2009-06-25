2e1f2094125d13b8ea6933ebd708d373a856a110
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-06-25 21:45:45 +0000

    * fixed image in prefab view

--- src/tools/radiant/radiant/sidebar/prefabs.cpp
+++ src/tools/radiant/radiant/sidebar/prefabs.cpp
@@ -39,6 +39,11 @@ static const int TABLE_COLUMS = 3;
 static GtkListStore* store;
 static GtkTreeView* view;
 
+static const char *Prefab_GetPath(StringOutputStream &fullpath, const char *file) {
+	fullpath << AppPath_get() << "prefabs/" << PathCleaned(file);
+	return fullpath.c_str();
+}
+
 static gint PrefabList_button_press(GtkWidget *widget, GdkEventButton *event,
 		gpointer data) {
 	if (event->type == GDK_2BUTTON_PRESS) {
@@ -53,9 +58,8 @@ static gint PrefabList_button_press(GtkWidget *widget, GdkEventButton *event,
 			UndoableCommand undo("mapImport");
 
 			gtk_tree_model_get(model, &iter, 0, &text, -1);
-			fullpath << AppPath_get() << "prefabs/" << PathCleaned(text);
 
-			Map_ImportFile(fullpath.c_str());
+			Map_ImportFile(Prefab_GetPath(fullpath, text));
 			g_free(text);
 
 			return TRUE;
@@ -66,14 +70,16 @@ static gint PrefabList_button_press(GtkWidget *widget, GdkEventButton *event,
 
 static void PrefabAdd(const char *name, const char *description,
 		const char *image) {
+	StringOutputStream fullpath(256);
 	GtkTreeIter iter;
-	GtkImage *img = image_new_from_file_with_mask(image);
+	GdkPixbuf *img = pixbuf_new_from_file_with_mask(Prefab_GetPath(fullpath, image));
+	if (!img)
+		g_warning("Could not find image (%s) for prefab %s\n", image, name);
 	gtk_list_store_append(store, &iter);
 	gtk_list_store_set(store, &iter, 0, name, 1, description, 2, img, -1);
 }
 
 GtkWidget* Prefabs_constructNotebookTab(void) {
-
 	StringOutputStream fullpath(256);
 	fullpath << AppPath_get() << "prefabs/prefabs.list";
 
@@ -103,7 +109,7 @@ GtkWidget* Prefabs_constructNotebookTab(void) {
 
 			{
 				store = gtk_list_store_new(3, G_TYPE_STRING, G_TYPE_STRING,
-						G_TYPE_POINTER);
+						GDK_TYPE_PIXBUF);
 				view
 						= GTK_TREE_VIEW(gtk_tree_view_new_with_model(GTK_TREE_MODEL(store)));
 				gtk_tree_view_set_enable_search(GTK_TREE_VIEW(view), FALSE);
