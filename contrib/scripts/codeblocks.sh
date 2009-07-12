20454c8fac19d533c465b1462d65487f5cb22b9a
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-07-12 16:57:13 +0000

    * updated libogg, libvorbis and libtheora

--- contrib/scripts/codeblocks.sh
+++ contrib/scripts/codeblocks.sh
@@ -128,10 +128,10 @@ function start_downloads()
 	download_archive http://www.libsdl.org/projects/SDL_mixer/release/ SDL_mixer-devel-${SDL_MIXER_VERSION}-VC8.zip sdl_mixer.zip
 	download_archive http://www.libsdl.org/projects/SDL_image/release/ SDL_image-devel-${SDL_IMAGE_VERSION}-VC8.zip sdl_image.zip
 
-	download_archive http://oss.netfarm.it/mplayer/pkgs/ libvorbis-mingw32-1.2.0-gcc45.tar.bz2 libvorbis.tar.bz2
-	download_archive http://oss.netfarm.it/mplayer/pkgs/ libtheora-mingw32-1.1alpha1-gcc45.tar.bz2 libtheora.tar.bz2
+	download_archive http://oss.netfarm.it/mplayer/pkgs/ libvorbis-mingw32-1.2.2rc1-gcc45.tar.bz2 libvorbis.tar.bz2
+	download_archive http://oss.netfarm.it/mplayer/pkgs/ libtheora-mingw32-1.1alpha2-gcc45.tar.bz2 libtheora.tar.bz2
 	download_archive http://oss.netfarm.it/mplayer/pkgs/ xvidcore-mingw32-1.2.1-gcc45.tar.bz2 xvidcore.tar.bz2
-	download_archive http://oss.netfarm.it/mplayer/pkgs/ libogg-mingw32-1.1.3-gcc45.tar.bz2 libogg.tar.bz2
+	download_archive http://oss.netfarm.it/mplayer/pkgs/ libogg-mingw32-1.1.4rc1-gcc45.tar.bz2 libogg.tar.bz2
 	
 #	download_archive http://downloads.xiph.org/releases/ogg/ libogg-1.1.3.tar.gz libogg.tar.gz
 #	download_archive http://downloads.xiph.org/releases/ogg/ libvorbis-1.2.0.tar.gz libvorbis.tar.gz
