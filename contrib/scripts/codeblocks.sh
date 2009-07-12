6dcf555e7ff1a991607d359a90b32d522cb34f2b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-07-12 15:51:03 +0000

    * updated gtk and glib versions * added glib binaries

--- contrib/scripts/codeblocks.sh
+++ contrib/scripts/codeblocks.sh
@@ -134,10 +134,11 @@ start_downloads()
 	download_archive http://download.berlios.de/codeblocks/ mingwm10_gcc421.7z codeblocks_mingw.7z
 	download_archive http://download.berlios.de/codeblocks/ CB_20090601_rev5616_win32.7z codeblocks.7z
 
-	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/glib/2.18/ glib-dev_2.18.4-1_win32.zip glib-dev.zip
-	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.14/ gtk+-dev_2.14.7-1_win32.zip gtk+-dev.zip
-	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/pango/1.22/ pango-dev_1.22.4-1_win32.zip pango-dev.zip
-	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/atk/1.24/ atk-dev_1.24.0-1_win32.zip atk-dev.zip
+	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/glib/2.20/ glib_2.20.3-1_win32.zip glib.zip
+	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/glib/2.20/ glib-dev_2.20.3-1_win32.zip glib-dev.zip
+	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.16/ gtk+-dev_2.16.2-1_win32.zip gtk+-dev.zip
+	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/pango/1.24/ pango-dev_1.24.2-1_win32.zip pango-dev.zip
+	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/atk/1.26/ atk-dev_1.26.0-1_win32.zip atk-dev.zip
 	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/ cairo-dev_1.8.6-1_win32.zip cairo-dev.zip
 	download_archive http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/ pkg-config-0.23-2.zip pkg-config.zip
 
@@ -251,6 +252,7 @@ extract_tools()
 
 extract_gtk()
 {
+	extract_archive_zip glib.zip "${MINGW_DIR}"
 	extract_archive_zip glib-dev.zip "${MINGW_DIR}"
 	extract_archive_zip gtk+-dev.zip "${MINGW_DIR}"
 	extract_archive_zip pango-dev.zip "${MINGW_DIR}"
