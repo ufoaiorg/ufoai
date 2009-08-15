b7c12a6de32a2f88a5c7095c1862e2fc4c27b6dc
Christoph Hartwig <rudolfowood@users.sourceforge.net>
2009-08-15 08:19:29 +0000

    * for Windows we need GLExt for OpenGL 1.2 & 1.3

--- src/tools/radiant/include/igl.h
+++ src/tools/radiant/include/igl.h
@@ -29,6 +29,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #if defined(WIN32)
 #define QGL_DLLEXPORT __stdcall
+#include <GL/glext.h>
 #else
 #define QGL_DLLEXPORT
 #endif
