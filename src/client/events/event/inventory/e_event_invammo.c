0e8534c2add9b5b7d2a49794497785a2eb142926
Maciej Wierzbicki <voovoos@users.sourceforge.net>
2009-09-10 20:57:12 +0000

    * welcome in 2009 (part4)

--- src/client/events/event/inventory/e_event_invammo.c
+++ src/client/events/event/inventory/e_event_invammo.c
@@ -3,7 +3,7 @@
  */
 
 /*
-Copyright (C) 1997-2008 UFO:AI Team
+Copyright (C) 2002-2009 UFO: Alien Invasion.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
