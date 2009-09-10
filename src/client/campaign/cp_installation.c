c85d32294507cdd296fa9b8833e1df907914f204
Maciej Wierzbicki <voovoos@users.sourceforge.net>
2009-09-10 20:21:17 +0000

    * this commit is dedicated for geever: welcome in 2009 (part1)

--- src/client/campaign/cp_installation.c
+++ src/client/campaign/cp_installation.c
@@ -5,7 +5,7 @@
  */
 
 /*
-Copyright (C) 2002-2009 UFO: Alien Invasion team.
+Copyright (C) 2002-2009 UFO: Alien Invasion.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
