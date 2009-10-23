d6fbe7f2c7ab98a1b6ab9e0a95fcd99e58a60579
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-10-23 15:06:59 +0000

    * updated gdb

--- contrib/scripts/codeblocks.sh
+++ contrib/scripts/codeblocks.sh
@@ -82,7 +82,7 @@ function start_downloads()
 	download_archive http://downloads.sourceforge.net/mingw/ binutils-2.19.1-mingw32-bin.tar.gz binutils.tar.gz
 	download_archive http://downloads.sourceforge.net/mingw/ mingwrt-3.15.2-mingw32-dev.tar.gz mingwrt.tar.gz
 	download_archive http://downloads.sourceforge.net/mingw/ w32api-3.13-mingw32-dev.tar.gz w32api.tar.gz
-	download_archive http://downloads.sourceforge.net/mingw/ gdb-7.0-mingw-3.tar.bz2 gdb.tar.bz2
+	download_archive http://downloads.sourceforge.net/mingw/ gdb-7.0-2-mingw32-bin.tar.gz gdb.tar.gz
 	download_archive http://downloads.sourceforge.net/mingw/ mingw32-make-3.81-20080326-3.tar.gz make.tar.gz
 
 	download_archive http://downloads.sourceforge.net/mingw/ bash-3.1-MSYS-1.0.11-1.tar.bz2 msys-bash.tar.bz2
@@ -178,7 +178,7 @@ function extract_mingw()
 	extract_archive_gz binutils.tar.gz "${MINGW_DIR}"
 	extract_archive_gz mingwrt.tar.gz "${MINGW_DIR}"
 	extract_archive_gz w32api.tar.gz "${MINGW_DIR}"
-	extract_archive_bz2 gdb.tar.bz2 "${MINGW_DIR}"
+	extract_archive_gz gdb.tar.gz "${MINGW_DIR}"
 	extract_archive_gz gcc.tar.gz "${MINGW_DIR}"
 	extract_archive_gz g++.tar.gz "${MINGW_DIR}"
 	extract_archive_gz make.tar.gz "${MINGW_DIR}"
