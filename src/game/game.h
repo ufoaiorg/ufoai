267e5f12bdb7a407002da6cb11c011c37b34c21b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-11-05 06:49:42 +0000

    * increase the game api version from 4 to 5 (to ensure that nobody loads an old game lib)

--- src/game/game.h
+++ src/game/game.h
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../shared/typedefs.h"
 #include "../common/tracing.h"
 
-#define	GAME_API_VERSION	4
+#define	GAME_API_VERSION	5
 
 /** @brief edict->solid values */
 typedef enum {
