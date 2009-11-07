0d72ca640d925dfcc09b562d6e9d91143ef02f91
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-11-07 15:26:28 +0000

    * extracted classes

---
+++ src/tools/radiant/radiant/camera/CamWnd.h
@@ -0,0 +1,84 @@
+#ifndef CAMWND_H_
+#define CAMWND_H_
+
+class CamWnd
+{
+	private:
+		View m_view;
+		camera_t m_Camera;
+		CameraView m_cameraview;
+
+		guint m_freemove_handle_focusout;
+
+		static Shader* m_state_select1;
+		static Shader* m_state_select2;
+
+		FreezePointer m_freezePointer;
+
+	public:
+		GtkWidget* m_gl_widget;
+		GtkWindow* m_parent;
+
+		SelectionSystemWindowObserver* m_window_observer;
+		XORRectangle m_XORRectangle;
+
+		DeferredDraw m_deferredDraw;
+		DeferredMotion m_deferred_motion;
+
+		guint m_selection_button_press_handler;
+		guint m_selection_button_release_handler;
+		guint m_selection_motion_handler;
+
+		guint m_freelook_button_press_handler;
+
+		guint m_sizeHandler;
+		guint m_exposeHandler;
+
+		CamWnd ();
+		~CamWnd ();
+
+		bool m_drawing;
+		void queue_draw ()
+		{
+			if (m_drawing) {
+				return;
+			}
+			m_deferredDraw.draw();
+		}
+		void draw ();
+
+		static void captureStates ()
+		{
+			m_state_select1 = GlobalShaderCache().capture("$CAM_HIGHLIGHT");
+			m_state_select2 = GlobalShaderCache().capture("$CAM_OVERLAY");
+		}
+		static void releaseStates ()
+		{
+			GlobalShaderCache().release("$CAM_HIGHLIGHT");
+			GlobalShaderCache().release("$CAM_OVERLAY");
+		}
+
+		camera_t& getCamera ()
+		{
+			return m_Camera;
+		}
+
+		void BenchMark ();
+		void Cam_ChangeFloor (bool up);
+
+		void DisableFreeMove ();
+		void EnableFreeMove ();
+		bool m_bFreeMove;
+
+		CameraView& getCameraView ()
+		{
+			return m_cameraview;
+		}
+
+	private:
+		void Cam_Draw ();
+};
+
+typedef MemberCaller<CamWnd, &CamWnd::queue_draw> CamWndQueueDraw;
+
+#endif /* CAMWND_H_ */
---
+++ src/tools/radiant/radiant/camera/Camera.h
@@ -0,0 +1,55 @@
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+enum camera_draw_mode
+{
+	cd_wire, cd_solid, cd_texture, cd_lighting
+};
+
+struct camera_t
+{
+		int width, height;
+
+		bool timing;
+
+		Vector3 origin;
+		Vector3 angles;
+
+		Vector3 color; // background
+
+		Vector3 forward, right; // move matrix (TTimo: used to have up but it was not updated)
+		Vector3 vup, vpn, vright; // view matrix (taken from the modelview matrix)
+
+		Matrix4 projection;
+		Matrix4 modelview;
+
+		bool m_strafe; // true when in strafemode toggled by the ctrl-key
+		bool m_strafe_forward; // true when in strafemode by ctrl-key and shift is pressed for forward strafing
+
+		unsigned int movementflags; // movement flags
+		Timer m_keycontrol_timer;
+		guint m_keymove_handler;
+
+		float fieldOfView;
+
+		DeferredMotionDelta m_mouseMove;
+
+		static void motionDelta (int x, int y, void* data)
+		{
+			Camera_mouseMove(*reinterpret_cast<camera_t*> (data), x, y);
+		}
+
+		View* m_view;
+		Callback m_update;
+
+		static camera_draw_mode draw_mode;
+
+		camera_t (View* view, const Callback& update) :
+			width(0), height(0), timing(false), origin(0, 0, 0), angles(0, 0, 0), color(0, 0, 0), projection(
+					g_matrix4_identity), modelview(g_matrix4_identity), movementflags(0), m_keymove_handler(0),
+					fieldOfView(90.0f), m_mouseMove(motionDelta, this), m_view(view), m_update(update)
+		{
+		}
+};
+
+#endif /* CAMERA_H_ */
