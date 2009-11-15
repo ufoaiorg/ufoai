4ccab110f0b0c4c38fa4b34c67e57876e28bc583
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-11-15 18:33:19 +0000

    * bumped the ufo2map version

--- src/tools/ufo2map/ufo2map.c
+++ src/tools/ufo2map/ufo2map.c
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#define VERSION "1.2.4"
+#define VERSION "1.2.5"
 
 /* valid -nolighting parameters */
 #define	LIGHTING_NONE			1
