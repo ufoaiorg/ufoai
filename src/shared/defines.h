030156a8c02998a6b5656c57a080d22d4b71bd64
aduke1 <aduke1@users.sourceforge.net>
2009-11-24 15:08:03 +0000

    * doxygen

--- src/shared/defines.h
+++ src/shared/defines.h
@@ -30,7 +30,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 /**
  * @note The comment line specifying files has the following syntax:
- *   <parent dir>/<filename>   OR
+ *   [parent dir/filename]     OR
  *   [parent dir]              OR
  *   {project name}
  */
