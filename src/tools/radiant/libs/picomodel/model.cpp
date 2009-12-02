728e70b0761d0192f481d7cd057fa3f61351fcb8
Martin Gerhardy <tlh2000@users.sourceforge.net>
2009-12-02 18:59:28 +0000

    * extracted VectorLightList class

--- src/tools/radiant/libs/picomodel/model.cpp
+++ src/tools/radiant/libs/picomodel/model.cpp
@@ -29,6 +29,7 @@
 
 #include "picomodel.h"
 #include "RenderablePicoModel.h"
+#include "VectorLightList.h"
 
 #include "iarchive.h"
 #include "idatastream.h"
@@ -48,36 +49,6 @@
 #include "transformlib.h"
 #include "traverselib.h"
 #include "render.h"
-
-class VectorLightList: public LightList
-{
-		typedef std::vector<const RendererLight*> Lights;
-		Lights m_lights;
-	public:
-		~VectorLightList ()
-		{
-		}
-		void addLight (const RendererLight& light)
-		{
-			m_lights.push_back(&light);
-		}
-		void clear ()
-		{
-			m_lights.clear();
-		}
-		void evaluateLights () const
-		{
-		}
-		void lightsChanged () const
-		{
-		}
-		void forEachLight (const RendererLightCallback& callback) const
-		{
-			for (Lights::const_iterator i = m_lights.begin(); i != m_lights.end(); ++i) {
-				callback(*(*i));
-			}
-		}
-};
 
 class PicoSurface: public OpenGLRenderable
 {
