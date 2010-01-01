c372f159392251b94dfd75b73e93b85b04e42544
Tamás Fehérvári <geever@users.sourceforge.net>
2010-01-01 22:26:32 +0000

    * prevent segfaulting in MN_DrawTextNode on NULL reference: call MN_ResetData in MN_RegisterText instead

--- src/client/menu/m_data.c
+++ src/client/menu/m_data.c
@@ -129,6 +129,11 @@ int MN_GetDataIDByName (const char* name)
  */
 void MN_RegisterText (int textId, const char *text)
 {
+	if (!text) {
+		MN_ResetData(textId);
+		return;
+	}
+
 	MN_ResetData(textId);
 	mn.sharedData[textId].type = MN_SHARED_TEXT;
 	mn.sharedData[textId].data.text = text;
