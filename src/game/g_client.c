1de37d8fe0e75071d85d0ca9e38694ddc711db8f
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-01-12 19:13:31 +0000

    * fixed warning

--- src/game/g_client.c
+++ src/game/g_client.c
@@ -987,7 +987,7 @@ static void G_ClientReadInventory (edict_t *ent)
 
 		if (container) {
 			if (Com_AddToInventory(&ent->i, item, container, x, y, 1) == NULL)
-				gi.error("G_ClientTeamInfo failed, could not add item to container %i", container);
+				gi.error("G_ClientTeamInfo failed, could not add item to container %i", container->id);
 			Com_DPrintf(DEBUG_GAME, "G_ClientTeamInfo: (container: %i - idArmour: %i) <- Added %s.\n",
 					container->id, gi.csi->idArmour, ent->i.c[container->id]->item.t->id);
 		}
