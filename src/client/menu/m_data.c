1cee99c7c7b655d4eff96acec80fb97a959b56a1
bayo <bayo@users.sourceforge.net>
2010-01-13 21:21:24 +0000

    * fix "update only" of a linked list

--- src/client/menu/m_data.c
+++ src/client/menu/m_data.c
@@ -145,6 +145,11 @@ void MN_RegisterText (int textId, const char *text)
  */
 void MN_RegisterLinkedListText (int textId, linkedList_t *text)
 {
+	/** Hack to disable release memory, if we only want to update the same list */
+	if (mn.sharedData[dataId].type == MN_SHARED_LINKEDLISTTEXT && mn.sharedData[dataId].data.linkedListText == text) {
+		mn.sharedData[dataId].versionId++;
+		return;
+	}
 	MN_ResetData(textId);
 	mn.sharedData[textId].type = MN_SHARED_LINKEDLISTTEXT;
 	mn.sharedData[textId].data.linkedListText = text;
