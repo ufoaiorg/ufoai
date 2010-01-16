5ad5317d14badc6d4d5ea4510aab18b6ed9fba31
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-01-16 07:23:06 +0000

    * added the missing file to the build chain (r_matrix.c)

--- build/client.mk
+++ build/client.mk
@@ -260,6 +260,7 @@ CLIENT_SRCS = \
 	client/renderer/r_lightmap.c \
 	client/renderer/r_main.c \
 	client/renderer/r_material.c \
+	client/renderer/r_matrix.c \
 	client/renderer/r_misc.c \
 	client/renderer/r_mesh.c \
 	client/renderer/r_mesh_anim.c \
