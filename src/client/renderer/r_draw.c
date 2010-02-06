ddef9bb273cd727be83d2cb26ef9ab35c970a560
Olivier Alloschery <kracken@users.sourceforge.net>
2010-02-06 14:31:29 +0000

    * increased a bit more the distance between model and earth center

--- src/client/renderer/r_draw.c
+++ src/client/renderer/r_draw.c
@@ -802,8 +802,8 @@ void R_Draw3DMapMarkers (int x, int y, int w, int h, const vec3_t rotate, const 
 	glRotatef(rotate[0] - pos[0], 0, 0, 1);
 	glRotatef(90.0f - pos[1] , 1, 0, 0);
 	/* Make model a little bit higher than earth surface, or you can see
-	part of the model through earth (only half of earth is drawn) */
-	glTranslatef(0, 0, 1.001f * earthRadius);
+	bottom part of the model through earth (only half of earth is drawn) */
+	glTranslatef(0, 0, 1.002f * earthRadius);
 	glRotatef(-90.0f + direction, 0, 0, 1);
 
 	R_DrawModelDirect(&mi, NULL, NULL);
