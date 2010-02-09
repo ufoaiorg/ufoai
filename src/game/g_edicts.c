81213802824a61bdaa2070863d7f025cb7d67de9
aduke1 <aduke1@users.sourceforge.net>
2010-02-09 22:10:31 +0000

    * removed unused include

--- src/game/g_edicts.c
+++ src/game/g_edicts.c
@@ -27,7 +27,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "g_local.h"
-#include <time.h>
 
 /** This is where we store the edicts */
 static edict_t *g_edicts;
