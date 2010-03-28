82e7f91a9d658d94a81124cf433321b339fe95d7
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-03-28 16:14:29 +0000

    * two targets for the windows installers

--- build/install_windows.mk
+++ build/install_windows.mk
@@ -1,7 +1,11 @@
 WINDOWS_INST_DIR=contrib/installer/windows
 
-wininstaller: installer-pre
+wininstaller: installer-pre wininstaller-ufoai wininstaller-uforadiant
+
+wininstaller-ufoai: installer-pre
 	makensis $(WINDOWS_INST_DIR)/ufoai.nsi
-	makensis $(WINDOWS_INST_DIR)/uforadiant.nsi
 	md5sum $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.md5
+
+wininstaller-uforadiant: installer-pre
+	makensis $(WINDOWS_INST_DIR)/uforadiant.nsi
 	md5sum $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.md5
