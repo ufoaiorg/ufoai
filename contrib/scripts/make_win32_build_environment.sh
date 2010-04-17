54c9fe5f4e4daf159f4d8ad8b3d43f54bc7c5b79
mutonufoai <mutonufoai@users.sourceforge.net>
2010-04-17 18:57:33 +0000

    * fixed a broken download ** libvorbis ** xvidcore ** libogg

--- contrib/scripts/make_win32_build_environment.sh
+++ contrib/scripts/make_win32_build_environment.sh
@@ -507,10 +507,10 @@ function start_downloads()
 	download_archive http://www.libsdl.org/projects/SDL_mixer/release/ SDL_mixer-devel-${SDL_MIXER_VERSION}-VC8.zip sdl_mixer.zip
 	download_archive http://www.libsdl.org/projects/SDL_image/release/ SDL_image-devel-${SDL_IMAGE_VERSION}-VC8.zip sdl_image.zip
 
-	download_archive http://oss.netfarm.it/mplayer/pkgs/ libvorbis-mingw32-1.2.3-gcc42.tar.bz2 libvorbis.tar.bz2
+	download_archive http://oss.netfarm.it/mplayer/pkgs/ libvorbis-mingw32-1.3.1-gcc42.tar.bz2 libvorbis.tar.bz2
 	download_archive http://oss.netfarm.it/mplayer/pkgs/ libtheora-mingw32-1.1.1-gcc42.tar.bz2 libtheora.tar.bz2
-	download_archive http://oss.netfarm.it/mplayer/pkgs/ xvidcore-mingw32-1.2.2-gcc42.tar.bz2 xvidcore.tar.bz2
-	download_archive http://oss.netfarm.it/mplayer/pkgs/ libogg-mingw32-1.1.4-gcc42.tar.bz2 libogg.tar.bz2
+	download_archive http://oss.netfarm.it/mplayer/pkgs/ xvidcore-mingw32-1.2.2-vaq-gcc42.tar.bz2 xvidcore.tar.bz2
+	download_archive http://oss.netfarm.it/mplayer/pkgs/ libogg-mingw32-1.2.0-gcc42.tar.bz2 libogg.tar.bz2
 
 #	download_archive http://downloads.xiph.org/releases/ogg/ libogg-1.1.3.tar.gz libogg.tar.gz
 #	download_archive http://downloads.xiph.org/releases/ogg/ libvorbis-1.2.0.tar.gz libvorbis.tar.gz
@@ -662,3 +662,8 @@ fi
 #			bzip2
 #			bash
 #			make
+#	      Muton (2010.04.17)
+#		update package
+#			libvorbis
+#			xvidcore
+#			libogg
