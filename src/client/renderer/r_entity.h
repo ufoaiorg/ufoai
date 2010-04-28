17c60c6f5cfd5a3b6e734e181b9e7a4e767872fc
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-04-28 05:32:22 +0000

    * fixed array out of bound access

--- src/client/renderer/r_entity.h
+++ src/client/renderer/r_entity.h
@@ -84,7 +84,7 @@ typedef struct entity_s {
 
 	transform_t transform;
 
-	vec3_t shell;					/**< shell color */
+	vec4_t shell;					/**< shell color */
 
 	static_lighting_t *lighting;	/**< cached static lighting info */
 
