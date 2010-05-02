657f0ae9c9e6912f6acac80088f622e6af701a8f
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-05-02 11:54:14 +0000

    * added models to installer-pre target

--- build/install.mk
+++ build/install.mk
@@ -5,7 +5,7 @@ include build/install_linux.mk
 include build/install_mac.mk
 include build/install_windows.mk
 
-installer-pre: lang maps-sync pk3
+installer-pre: lang maps-sync models pk3
 
 installer: wininstaller linuxinstaller sourcearchive mappack
 
