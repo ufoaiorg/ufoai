8569e30e5bc956725f30089ed722c8dc97dc557d
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-05-06 18:06:10 +0000

    * small helper script to cross-compile ufo

---
+++ contrib/scripts/crosscompile.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+JOBS=1
+MINGW=i686-pc-mingw32
+MINGW_CROSS_ENV=/opt/mingw-env
+DIR=$(dirname $(readlink -f ${0}))
+cd "${DIR}/../.."
+
+# http://www.nongnu.org/mingw-cross-env/
+export PATH=${MINGW_CROSS_ENV}/usr/bin:${MINGW_CROSS_ENV}/usr/${MINGW}/bin/:${PATH}
+
+./configure --host=${MINGW} --disable-uforadiant
+
+make -j ${JOBS}
+
