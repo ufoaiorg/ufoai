b2bc0d8f7af907b605fb770eda281ae4c7485585
Ben Mitchell <arisian@users.sourceforge.net>
2010-05-09 13:15:10 +0000

    * forgot to add this file before committing rendering code last night

---
+++ base/shaders/lerp_vs.glsl
@@ -0,0 +1,17 @@
+
+attribute vec3 NEXT_FRAME_VERTS;
+attribute vec3 NEXT_FRAME_NORMALS;
+attribute vec4 TANGENTS;
+attribute vec4 NEXT_FRAME_TANGENTS;
+uniform float TIME;
+
+varying vec4 Vertex;
+varying vec3 Normal;
+varying vec4 Tangent;
+
+void lerpVertex (void)
+{
+	Vertex = mix(vec4(NEXT_FRAME_VERTS, 1.0), gl_Vertex, TIME);
+	Normal = mix(NEXT_FRAME_NORMALS, gl_Normal, TIME);
+	Tangent = mix(NEXT_FRAME_TANGENTS, TANGENTS, TIME);
+}
