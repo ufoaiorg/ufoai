56495cda05312572e497e93aa0ece6c5203fd8e1
aduke1 <aduke1@users.sourceforge.net>
2010-05-12 20:34:53 +0000

    * yxz.h should always be the 1st #include in xyz.c

--- src/common/tracing.c
+++ src/common/tracing.c
@@ -26,9 +26,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
+#include "tracing.h"
 #include "common.h"
 #include "mem.h"
-#include "tracing.h"
 
 /** @note used for statistics
  * @sa CM_LoadMap (cmodel.c) */
