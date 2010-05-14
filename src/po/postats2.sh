965b41527ad4877296ae00674ce2d647515747c7
bayo <bayo@users.sourceforge.net>
2010-05-14 20:48:14 +0000

    * script to generate json/wiki translation status from po files

---
+++ src/po/postats2.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+## Generate 2 files postats.wiki and postats.json from translation status
+# JSON file is not used
+# WIKI data must be pasted into http://ufoai.ninex.info/wiki/index.php/Template:Translation_status/Data
+#
+# Usage
+# move to ./src/po 
+# execute ./postats2.sh . ufoai.pot 
+#
+
+function usage() {
+	echo "Usage: $0 <dir with .po files> .pot <path to images>"
+	exit 1
+}
+
+PO_DIR="${1:-./src/po}"
+POT_FILE="${2:-./src/po/ufoai.pot}"
+IMAGES_DIR="${3:-./src/po/stats}"
+MSGSTATS=$(dirname "${0}")/pomsgstats.sh
+
+echo "using po files from ${PO_DIR}"
+echo "against pot file ${POT_FILE}"
+echo "writing images into ${IMAGES_DIR}"
+echo "msgstats: ${MSGSTATS}"
+
+test -d "${PO_DIR}" || usage
+test -e "${POT_FILE}" || usage
+
+echo -n "" > postats.json
+echo -n "" > postats.wiki
+
+echo "{" >> postats.json
+echo "{{#switch:{{{lang}}}_{{{type}}}" > postats.wiki
+
+for po in $(ls ${PO_DIR}/ufoai-*.po); do
+	echo -n "Generating stats for locale "$(basename "${po}" .po)"..."
+
+	lang=$(echo "${po}" | sed -e 's/^.*ufoai-\(.*\)\.po.*/\1/')
+
+	total=$(${MSGSTATS} "${POT_FILE}" "${po}" | \
+	awk '/total:/ {total = $2}
+			END {print int(total)}')
+	missing=$(${MSGSTATS} "${POT_FILE}" "${po}" | \
+	awk '/untranslated:/ {missing = $2}
+			END {print int(missing)}')
+	fuzzy=$(${MSGSTATS} "${POT_FILE}" "${po}" | \
+	awk '/fuzzy:/ {fuzzy = $2}
+			END {print int(fuzzy)}')
+
+	echo "\"${lang}\": {\"total\": ${total}, \"fuzzy\": ${fuzzy}, \"missing\": ${missing}}," >> postats.json
+	echo "|${lang}_total= ${total}" >> postats.wiki
+	echo "|${lang}_missing= ${missing}" >> postats.wiki
+	echo "|${lang}_fuzzy= ${fuzzy}" >> postats.wiki
+	echo " done"
+done
+
+echo "}" >> postats.json
+echo "}}" >> postats.wiki
