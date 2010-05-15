22684c19c7c747106431f9f76ae26f18c22a7fd9
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-05-15 07:29:56 +0000

    a quick and dirty perl script to find unused textures that can be removed before we make a release

---
+++ contrib/scripts/find_unused_textures.pl
@@ -0,0 +1,103 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Basename;
+
+my $SEARCH = $ARGV[1];
+
+sub read_dir2 {
+	my $DIR = shift(@_);
+	my $EXT = shift(@_);
+	opendir(FILES, $DIR) || die "Could not read dir ($DIR)";
+	my @files = readdir(FILES);
+	my @filter = ();
+	foreach (@files) {
+		next if ($_ =~ m/^\./);
+		if (-d $DIR."/".$_) {
+			push (@filter, read_dir2($DIR."/".$_));
+			next;
+		}
+		next unless $_ =~ /$EXT$/;
+		push(@filter, $DIR."/".$_);
+	}
+	closedir(MAPS);
+
+	return @filter;
+}
+
+sub check_content {
+	my $file = shift(@_);
+	my $content = shift(@_);
+	my $found = 0;
+	open(FILE, $file) || die "Could not read $file";
+	foreach (<FILE>) {
+		if ($_ =~ /$content/) {
+			$found = 1;
+			last;
+		}
+	}
+	close(FILE);
+	return $found;
+}
+
+sub get_maps {
+	return read_dir2("base/maps", ".map");
+}
+
+
+sub get_materials {
+	return read_dir2("base/materials", ".mat");
+}
+
+sub get_textures {
+	my @textures = read_dir2("base/textures", ".png");
+	push (@textures, read_dir2("base/textures", ".jpg"));
+	push (@textures, read_dir2("base/textures", ".tga"));
+	return @textures;
+}
+
+my @maps = get_maps();
+my @textures = get_textures();
+my @materials = get_materials();
+
+my @suffixlist = (".png", ".tga", ".jpg");
+
+foreach my $texture (@textures) {
+	my $used = 0;
+	#$texture = basename($texture, @suffixlist);
+	$texture =~ s/^base\/textures\///;
+	$texture =~ s/\..*$//;
+
+	if ($texture =~ /_gm$/ || $texture =~ /_nm$/) {
+		my $base = $texture;
+		$base =~ s/_.*$//;
+		unless (grep(/$base/, @textures)) {
+			print STDERR "$texture has no base texture\n";
+		}
+		next;
+	}
+
+	foreach my $material (@materials) {
+		if (check_content($material, $texture)) {
+			$used = 1;
+			last;
+		}
+		if ($texture =~ /\d$/) {
+			my $anim =~ s/\d+$//;
+			if (check_content($material, $anim)) {
+				print "$texture is used as animation\n";
+				$used = 1;
+				last;
+			}
+		}
+	}
+	foreach my $map (@maps) {
+		if (check_content($map, $texture)) {
+			print "$texture is used in a map\n";
+			$used = 1;
+			last;
+		}
+	}
+	print STDERR "$texture is not used in materials or maps\n" unless $used;
+}
+
