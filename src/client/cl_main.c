224fb2383026773940b58183e73330a302ae452c
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-06-08 16:13:42 +0000

    * fixed warnings (missing header includes)

--- src/client/cl_main.c
+++ src/client/cl_main.c
@@ -49,6 +49,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "cl_http.h"
 #include "cl_ugv.h"
 #include "input/cl_joystick.h"
+#include "cinematic/cl_cinematic.h"
 #include "sound/s_music.h"
 #include "sound/s_sample.h"
 #include "renderer/r_main.h"
--- src/client/menu/node/m_node_cinematic.c
+++ src/client/menu/node/m_node_cinematic.c
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "../m_nodes.h"
 #include "../m_parse.h"
+#include "../m_draw.h"
 #include "m_node_cinematic.h"
 #include "m_node_abstractnode.h"
 
