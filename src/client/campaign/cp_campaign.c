3f778744a0d4291e35b2de530aa54ef0f1d29290
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-07-04 10:58:29 +0000

    * fixed debug mode warnings

--- src/client/campaign/cp_campaign.c
+++ src/client/campaign/cp_campaign.c
@@ -24,6 +24,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "../client.h"
+#ifdef DEBUG
+#include "../cl_ugv.h"
+#endif
 #include "../cl_game.h"
 #include "../cl_team.h"
 #include "../menu/m_main.h"
