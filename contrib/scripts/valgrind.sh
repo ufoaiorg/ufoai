7a7f4dac1adf45a0ad4aa5ebce630e266e39545e
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-07-06 21:10:58 +0000

    * support for other valgrind tools

--- contrib/scripts/valgrind.sh
+++ contrib/scripts/valgrind.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 LANG=C
+TOOL=memcheck
 
 usage() {
-	echo "Usage: $0 [--help|-h] [-suppression|-s <file>] [-gensup|-g] [--nolog] [-binary|-b <binary>]"
+	echo "Usage: $0 [--help|-h] [--suppression|-s <file>] [--gensup|-g] [--nolog] [--tool|-t <tool>] --binary|-b <binary>"
 	echo " --help -h         - Shows this help message"
 	echo " --suppression -s  - Use the given suppression file - otherwise the standard"
 	echo "                     file will be used (if found)"
 	echo " --gensup -g       - Generate suppression statements"
+	echo " --tool -t         - Valgrind tool (default is memcheck)"
 	echo " --binary -b       - Execute valgrind for the given binary"
 	echo " --nolog           - Don't log into a file but to stdout"
 	exit 1
@@ -24,12 +26,17 @@ while [ $# -gt 0 ]; do
 			APP_PATH=$2
 			shift
 			shift
-			;;				
+			;;
+		--tool|-t)
+			TOOL=$2
+			shift
+			shift
+			;;
 		--suppresion|-s)
 			SUP=$2
 			shift
 			shift
-			;;				
+			;;
 		--gensup|-g)
 			GENERATE_SUPPRESSION=X
 			shift
@@ -38,14 +45,14 @@ while [ $# -gt 0 ]; do
 			NO_LOG=X
 			shift
 			;;
- 	 	--help|-h|*)
+		--help|-h|*)
 			usage
 			;;
- 		-*)
+		-*)
 			error "invalid option $1"
 			;;
 			
- 	esac
+	esac
 done
 
 [ -z "$APP_PATH" ] && error "No binary given"
@@ -68,36 +75,38 @@ then
 		$VALGRIND_OPTIONS \
 		$APP
 else
-	if [ -z "$SUP" ]
+	if [ $TOOL == "memcheck" ]
 	then
-		# path to the valgrind suppression file
-		SUP=$(readlink -f "${0}")
-		SUP=$(dirname "${SUP}")
-		SUP="${SUP}/valgrind.sup"
+		if [ -z "$SUP" ]
+		then
+			# path to the valgrind suppression file
+			SUP=$(readlink -f "${0}")
+			SUP=$(dirname "${SUP}")
+			SUP="${SUP}/valgrind.sup"
+		fi
+
+		if [ -f "${SUP}" ]
+		then
+			echo "Using valgrind suppression file from '${SUP}'"
+			VALGRIND_OPTIONS="${VALGRIND_OPTIONS} --suppressions=${SUP}"
+		else
+			echo "No valgrind suppression file at '${SUP}'"
+		fi
+
+		if [ -z "$NO_LOG" ]
+		then
+			LOG=${PATH_ONLY}/valgrind.log
+			VALGRIND_OPTIONS="--log-file=$LOG $VALGRIND_OPTIONS"
+			echo "Log file will be created at ${LOG}"
+		fi
+		VALGRIND_OPTIONS="--show-reachable=yes $VALGRIND_OPTIONS"
+		VALGRIND_OPTIONS="--leak-check=full $VALGRIND_OPTIONS"
+		VALGRIND_OPTIONS="--track-fds=yes $VALGRIND_OPTIONS"
+		VALGRIND_OPTIONS="--run-libc-freeres=no $VALGRIND_OPTIONS"
+	else
+		VALGRIND_OPTIONS="--tool=$TOOL $VALGRIND_OPTIONS"
 	fi
 
 	cd ${PATH_ONLY}
-
-	if [ -f "${SUP}" ]
-	then
-		echo "Using valgrind suppression file from '${SUP}'"
-		VALGRIND_OPTIONS="${VALGRIND_OPTIONS} --suppressions=${SUP}"
-	else
-		echo "No valgrind suppression file at '${SUP}'"
-	fi
-
-	if [ -z "$NO_LOG" ]
-	then
-		LOG=${PATH_ONLY}/valgrind.log
-		VALGRIND_OPTIONS="--log-file=$LOG $VALGRIND_OPTIONS"
-		echo "Log file will be created at ${LOG}"
-	fi
-
-	valgrind \
-		--show-reachable=yes \
-		--leak-check=full \
-		--track-fds=yes \
-		--run-libc-freeres=no \
-		$VALGRIND_OPTIONS \
-		$APP
+	valgrind $VALGRIND_OPTIONS $APP
 fi
