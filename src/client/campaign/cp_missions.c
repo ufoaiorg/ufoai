0637765c47b2a8fc4bc09ae2457e5878b0ecd35c
Tamás Fehérvári <geever@users.sourceforge.net>
2010-08-12 21:19:20 +0000

    * remove superfluous braces

--- src/client/campaign/cp_missions.c
+++ src/client/campaign/cp_missions.c
@@ -1253,7 +1253,7 @@ void CP_MissionEnd (mission_t* mission, qboolean won)
 	Com_DPrintf(DEBUG_CLIENT, "CP_MissionEnd - num %i\n", numberOfSoldiers);
 
 	/* Check for alien containment in aircraft homebase. */
-	if (AL_GetAircraftAlienCargoTypes(aircraft) && (B_FreeCapacity(base, CAP_ALIENS) <= 0)) {
+	if (AL_GetAircraftAlienCargoTypes(aircraft) && B_FreeCapacity(base, CAP_ALIENS) <= 0) {
 		/* We have captured/killed aliens, but the homebase of this aircraft does not have free alien containment space.
 		 * Popup aircraft transfer dialog to choose a better base. */
 		Cmd_ExecuteString(va("trans_aliens %i", aircraft->idx));
