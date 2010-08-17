d6645b081aea5107ceca8a5ebf61326a3f163801
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-08-17 11:07:39 +0000

    * windows.h isn't needed here

--- src/tools/ufomodel/ufomodel.c
+++ src/tools/ufomodel/ufomodel.c
@@ -23,10 +23,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#if defined _WIN32
-#	include	<windows.h>
-#endif
-
 #include "../../shared/ufotypes.h"
 #include "../../common/mem.h"
 #include "../../shared/shared.h"
