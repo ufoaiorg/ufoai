73acb5963d91a5e597ca976a927590d604221119
Christoph Mallon <dertron@users.sourceforge.net>
2010-09-03 09:28:10 +0200

    * #include <unistd.h> is needed for STDIN_FILENO.

--- src/ports/linux/linux_main.c
+++ src/ports/linux/linux_main.c
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include <fcntl.h>
+#include <unistd.h>
 
 #include "../../common/common.h"
 #include "../system.h"
