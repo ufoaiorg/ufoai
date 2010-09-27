f0b1a47d97ab40fb970e4db2ee0396f239b1c8df
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-09-27 07:37:53 +0200

    * define the version of ufoai and uforadiant when calling makensis

--- build/install_windows.mk
+++ build/install_windows.mk
@@ -3,9 +3,9 @@ WINDOWS_INST_DIR=contrib/installer/windows
 wininstaller: installer-pre wininstaller-ufoai wininstaller-uforadiant
 
 wininstaller-ufoai: installer-pre
-	makensis $(WINDOWS_INST_DIR)/ufoai.nsi
-	md5sum $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.md5
+	$(Q)$(CROSS)makensis -DPRODUCT_VERSION=$(UFOAI_VERSION)  $(WINDOWS_INST_DIR)/ufoai.nsi
+	$(Q)md5sum $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.md5
 
 wininstaller-uforadiant: installer-pre
-	makensis $(WINDOWS_INST_DIR)/uforadiant.nsi
-	md5sum $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.md5
+	$(Q)$(CROSS)makensis -DPRODUCT_VERSION=$(UFORADIANT_VERSION) $(WINDOWS_INST_DIR)/uforadiant.nsi
+	$(Q)md5sum $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.md5
