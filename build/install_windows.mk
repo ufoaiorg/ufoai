f1e8de0acbaed2c26261479050b8adbfc4617f0b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-10-01 16:50:14 +0200

    * uforadiant installer does not compiled maps, models or pk3s

--- build/install_windows.mk
+++ build/install_windows.mk
@@ -6,6 +6,6 @@ wininstaller-ufoai: installer-pre
 	$(Q)$(CROSS)makensis -DPRODUCT_VERSION=$(UFOAI_VERSION)  $(WINDOWS_INST_DIR)/ufoai.nsi
 	$(Q)md5sum $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.md5
 
-wininstaller-uforadiant: installer-pre
+wininstaller-uforadiant: lang
 	$(Q)$(CROSS)makensis -DPRODUCT_VERSION=$(UFORADIANT_VERSION) $(WINDOWS_INST_DIR)/uforadiant.nsi
 	$(Q)md5sum $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.md5
