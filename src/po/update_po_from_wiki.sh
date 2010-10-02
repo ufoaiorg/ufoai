5955eb9669cccf1b4b644f54c5a66e97e0ab7080
bayo <bayo.fr@gmail.com>
2010-10-02 19:53:37 +0200

    * fix download of campaign message

--- src/po/update_po_from_wiki.sh
+++ src/po/update_po_from_wiki.sh
@@ -590,22 +590,6 @@ then
 	fi
 fi
 
-english="standard_campaign_txt"
-download_description $english
-if [[ $? -eq 0 ]]
-then
-	clean_html
-
-	for english in "standard_campaign_txt" "hard_campaign_txt" "veryhard_campaign_txt" "easy_campaign_txt" "veryeasy_campaign_txt" "alien_campaign_txt"
-	do
-		update_one_sentence "1"
-	done
-	if [[ "$debug" = "1" ]]
-	then
-		echo "Checking campaign descriptions : done." >> $log_file
-	fi
-fi
-
 english="mail_aircraft_landed"
 download_description $english
 if [[ $? -eq 0 ]]
