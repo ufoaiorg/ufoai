f634cdd7877286b320f2b8f43da8581904a4b1d6
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-10-12 22:14:57 +0200

    * the strip target would also build all other targets, just to be able to strip them

--- build/install_windows.mk
+++ build/install_windows.mk
@@ -6,6 +6,6 @@ wininstaller-ufoai: ufo ufoded installer-pre
 	$(Q)$(CROSS)makensis -DPRODUCT_VERSION=$(UFOAI_VERSION)  $(WINDOWS_INST_DIR)/ufoai.nsi
 	$(Q)md5sum $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/ufoai-$(UFOAI_VERSION)-win32.md5
 
-wininstaller-uforadiant: uforadiant uforadiant-brushexport lang strip
+wininstaller-uforadiant: uforadiant uforadiant-brushexport lang strip-uforadiant strip-uforadiant-brushexport
 	$(Q)$(CROSS)makensis -DPRODUCT_VERSION=$(UFORADIANT_VERSION) $(WINDOWS_INST_DIR)/uforadiant.nsi
 	$(Q)md5sum $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.exe > $(WINDOWS_INST_DIR)/uforadiant-$(UFORADIANT_VERSION)-win32.md5
