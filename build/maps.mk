42c87e409bb33c996b0ea2fd7a59ae3392cf2ce4
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-10-23 18:07:56 +0200

    * increased quant value again until maps are fixed

--- build/maps.mk
+++ build/maps.mk
@@ -3,7 +3,7 @@ UFO2MAP            = ./ufo2map$(EXE_EXT)
 MAPSRCS           := $(shell find $(MAPSDIR) -name '*.map' \! -name 'tutorial*' \! -name '*autosave*' \! -name 'test*' )
 BSPS              := $(MAPSRCS:.map=.bsp)
 NICE              ?= 19
-UFO2MAPFLAGS      ?= -v 4 -nice $(NICE) -quant 3 -extra
+UFO2MAPFLAGS      ?= -v 4 -nice $(NICE) -quant 4 -extra
 FAST_UFO2MAPFLAGS ?= -v 2 -quant 6 -nice $(NICE)
 ENTS_UFO2MAPFLAGS ?= -v 2 -nice $(NICE) -onlyents
 
