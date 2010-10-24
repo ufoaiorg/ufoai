829f712d97a96e26a6442d1b53c30bec93bfa269
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-10-24 16:39:10 +0200

    * extracted XYRenderer class into an own file

--- src/tools/radiant/radiant/xyview/GlobalXYWnd.cpp
+++ src/tools/radiant/radiant/xyview/GlobalXYWnd.cpp
@@ -70,6 +70,7 @@
 #include "../sidebar/sidebar.h"
 #include "../windowobservers.h"
 #include "../ui/ortho/OrthoContextMenu.h"
+#include "XYRenderer.h"
 
 void LoadTextureRGBA (qtexture_t* q, unsigned char* pPixels, int nWidth, int nHeight);
 
@@ -1834,67 +1835,6 @@ void XYWnd::PaintSizeInfo (int nDim1, int nDim2, Vector3& vMinBounds, Vector3& v
 		GlobalOpenGL().drawString(dimensions.toString());
 	}
 }
-
-class XYRenderer: public Renderer
-{
-		struct state_type
-		{
-				state_type () :
-					m_highlight(0), m_state(0)
-				{
-				}
-				unsigned int m_highlight;
-				Shader* m_state;
-		};
-	public:
-		XYRenderer (RenderStateFlags globalstate, Shader* selected) :
-			m_globalstate(globalstate), m_state_selected(selected)
-		{
-			ASSERT_NOTNULL(selected);
-			m_state_stack.push_back(state_type());
-		}
-
-		void SetState (Shader* state, EStyle style)
-		{
-			ASSERT_NOTNULL(state);
-			if (style == eWireframeOnly)
-				m_state_stack.back().m_state = state;
-		}
-		EStyle getStyle () const
-		{
-			return eWireframeOnly;
-		}
-		void PushState ()
-		{
-			m_state_stack.push_back(m_state_stack.back());
-		}
-		void PopState ()
-		{
-			ASSERT_MESSAGE(!m_state_stack.empty(), "popping empty stack");
-			m_state_stack.pop_back();
-		}
-		void Highlight (EHighlightMode mode, bool bEnable = true)
-		{
-			(bEnable) ? m_state_stack.back().m_highlight |= mode : m_state_stack.back().m_highlight &= ~mode;
-		}
-		void addRenderable (const OpenGLRenderable& renderable, const Matrix4& localToWorld)
-		{
-			if (m_state_stack.back().m_highlight & ePrimitive) {
-				m_state_selected->addRenderable(renderable, localToWorld);
-			} else {
-				m_state_stack.back().m_state->addRenderable(renderable, localToWorld);
-			}
-		}
-
-		void render (const Matrix4& modelview, const Matrix4& projection)
-		{
-			GlobalShaderCache().render(m_globalstate, modelview, projection);
-		}
-	private:
-		std::vector<state_type> m_state_stack;
-		RenderStateFlags m_globalstate;
-		Shader* m_state_selected;
-};
 
 void XYWnd::updateProjection ()
 {
