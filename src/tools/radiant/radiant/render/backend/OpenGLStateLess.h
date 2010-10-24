ufoaiorg/ufoai

commit 0429763e4ae7e7d789513fcab98f9fdc0eaf5752

* extracted classes into separate header files

src/tools/radiant/radiant/render/backend/OpenGLStateLess.h
@@ -1,0 +1,49 @@
+#ifndef OPENGLSTATELESS_H_
+#define OPENGLSTATELESS_H_
+
+#include "iglrender.h"
+
+/**
+ * Comparison class for OpenGLState objects.
+ */
+struct OpenGLStateLess {
+		bool operator()(const OpenGLState* self, const OpenGLState* other) const {
+			// Sort by sort-order override.
+			if (self->m_sort != other->m_sort) {
+				return self->m_sort < other->m_sort;
+			}
+			// Sort by texture handle.
+			if (self->m_texture != other->m_texture) {
+				return self->m_texture < other->m_texture;
+			}
+			if (self->m_texture1 != other->m_texture1) {
+				return self->m_texture1 < other->m_texture1;
+			}
+			if (self->m_texture2 != other->m_texture2) {
+				return self->m_texture2 < other->m_texture2;
+			}
+			if (self->m_texture3 != other->m_texture3) {
+				return self->m_texture3 < other->m_texture3;
+			}
+			if (self->m_texture4 != other->m_texture4) {
+				return self->m_texture4 < other->m_texture4;
+			}
+			if (self->m_texture5 != other->m_texture5) {
+				return self->m_texture5 < other->m_texture5;
+			}
+			if (self->m_texture6 != other->m_texture6) {
+				return self->m_texture6 < other->m_texture6;
+			}
+			if (self->m_texture7 != other->m_texture7) {
+				return self->m_texture7 < other->m_texture7;
+			}
+			// Sort by state bit-vector.
+			if (self->m_state != other->m_state) {
+				return self->m_state < other->m_state;
+			}
+			// Comparing address makes sure states are never equal.
+			return &self < &other;
+		}
+};
+
+#endif /*OPENGLSTATELESS_H_*/

src/tools/radiant/radiant/render/backend/OpenGLStateManager.h
@@ -1,0 +1,43 @@
+#pragma once
+
+#include "OpenGLStateLess.h"
+
+#include <map>
+
+class OpenGLState;
+class OpenGLShaderPass;
+
+namespace render {
+
+/**
+ * A sorted map of OpenGL states, maintained by the OpenGLStateManager. The sort
+ * function intelligently sorts OpenGL states in an order which will allow them
+ * to rendered as efficiently as possible, reducing the number of unnecessary
+ * context switches.
+ */
+//typedef std::map<OpenGLState*, OpenGLShaderPass*, OpenGLStateLess> OpenGLStates;
+typedef std::map<OpenGLState*, OpenGLShaderPass*, OpenGLStateLess> OpenGLStates;
+
+/**
+ * \brief
+ * Interface for an object which can manage sorted GL states. This is
+ * implemented by the OpenGLRenderSystem.
+ */
+class OpenGLStateManager {
+	public:
+
+		/**
+		 * \brief
+		 * Insert a new OpenGL state into the map.
+		 */
+		virtual void insertSortedState(const OpenGLStates::value_type& val) = 0;
+
+		/**
+		 * \brief
+		 * Remove a given OpenGL state from the map.
+		 */
+		virtual void eraseSortedState(const OpenGLStates::key_type& key) = 0;
+
+};
+
+}

src/tools/radiant/include/iglrender.h
@@ -1,23 +1,23 @@
 /*
-Copyright (C) 2001-2006, William Joseph.
-All Rights Reserved.
+ Copyright (C) 2001-2006, William Joseph.
+ All Rights Reserved.
 
-This file is part of GtkRadiant.
+ This file is part of GtkRadiant.
 
-GtkRadiant is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+ GtkRadiant is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-GtkRadiant is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ GtkRadiant is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GtkRadiant; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with GtkRadiant; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #if !defined(INCLUDED_IGLRENDER_H)
 #define INCLUDED_IGLRENDER_H
@@ -28,64 +28,274 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 //! A collection of opengl state information.
 class OpenGLState {
-public:
-	enum ESort {
-		eSortFirst = 0,
-		eSortOpaque = 1,
-		eSortMultiFirst = 2,
-		eSortMultiLast = 1023,
-		eSortOverbrighten = 1024,
-		eSortFullbright = 1025,
-		eSortHighlight = 1026,
-		eSortTranslucent = 1027,
-		eSortOverlayFirst = 1028,
-		eSortOverlayLast = 2047,
-		eSortControlFirst = 2048,
-		eSortControlLast = 3071,
-		eSortGUI0 = 3072,
-		eSortGUI1 = 3073,
-		eSortLast = 4096
-	};
+	public:
+		enum ESort {
+			eSortFirst = 0,
+			eSortOpaque = 1,
+			eSortMultiFirst = 2,
+			eSortMultiLast = 1023,
+			eSortOverbrighten = 1024,
+			eSortFullbright = 1025,
+			eSortHighlight = 1026,
+			eSortTranslucent = 1027,
+			eSortOverlayFirst = 1028,
+			eSortOverlayLast = 2047,
+			eSortControlFirst = 2048,
+			eSortControlLast = 3071,
+			eSortGUI0 = 3072,
+			eSortGUI1 = 3073,
+			eSortLast = 4096
+		};
 
-	unsigned int m_state;
-	std::size_t m_sort;
-	GLint m_texture;
-	GLint m_texture1;
-	GLint m_texture2;
-	GLint m_texture3;
-	GLint m_texture4;
-	GLint m_texture5;
-	GLint m_texture6;
-	GLint m_texture7;
-	Vector4 m_colour;
-	GLenum m_blend_src, m_blend_dst;
-	GLenum m_depthfunc;
-	GLenum m_alphafunc;
-	GLfloat m_alpharef;
-	GLfloat m_linewidth;
-	GLfloat m_pointsize;
-	GLint m_linestipple_factor;
-	GLushort m_linestipple_pattern;
+		unsigned int m_state;
+		std::size_t m_sort;
+		GLint m_texture;
+		GLint m_texture1;
+		GLint m_texture2;
+		GLint m_texture3;
+		GLint m_texture4;
+		GLint m_texture5;
+		GLint m_texture6;
+		GLint m_texture7;
+		Vector4 m_colour;
+		GLenum m_blend_src, m_blend_dst;
+		GLenum m_depthfunc;
+		GLenum m_alphafunc;
+		GLfloat m_alpharef;
+		GLfloat m_linewidth;
+		GLfloat m_pointsize;
+		GLint m_linestipple_factor;
+		GLushort m_linestipple_pattern;
 
-	OpenGLState()
-	{
-	}
+		OpenGLState() {
+		}
 
-	void apply (OpenGLState& current, unsigned int globalstate);
-	void constructDefault ();
+		inline
+		void setTextureState(GLint& current, const GLint& texture, GLenum textureUnit) {
+			if (texture != current) {
+				glActiveTexture(textureUnit);
+				glClientActiveTexture(textureUnit);
+				glBindTexture(GL_TEXTURE_2D, texture);
+
+				current = texture;
+			}
+		}
+
+		inline
+		void setTextureState(GLint& current, const GLint& texture) {
+			if (texture != current) {
+				glBindTexture(GL_TEXTURE_2D, texture);
+				current = texture;
+			}
+		}
+
+		inline
+		void setState(unsigned int state, unsigned int delta, unsigned int flag, GLenum glflag) {
+			if (delta & state & flag) {
+				glEnable(glflag);
+			} else if (delta & ~state & flag) {
+				glDisable(glflag);
+			}
+		}
+
+		void constructDefault() {
+			m_state = RENDER_DEFAULT;
+
+			m_texture = 0;
+			m_texture1 = 0;
+			m_texture2 = 0;
+			m_texture3 = 0;
+			m_texture4 = 0;
+			m_texture5 = 0;
+			m_texture6 = 0;
+			m_texture7 = 0;
+
+			m_colour.set(1, 1, 1, 1);
+
+			m_depthfunc = GL_LESS;
+
+			m_blend_src = GL_SRC_ALPHA;
+			m_blend_dst = GL_ONE_MINUS_SRC_ALPHA;
+
+			m_alphafunc = GL_ALWAYS;
+			m_alpharef = 0;
+
+			m_linewidth = 1;
+			m_pointsize = 1;
+
+			m_linestipple_factor = 1;
+			m_linestipple_pattern = 0xaaaa;
+		}
+
+		void apply(OpenGLState& current, unsigned int globalstate) {
+			if (m_state & RENDER_OVERRIDE) {
+				globalstate |= RENDER_FILL | RENDER_DEPTHWRITE;
+			}
+
+			const unsigned int state = m_state & globalstate;
+			const unsigned int delta = state ^ current.m_state;
+
+			if (delta & state & RENDER_FILL) {
+				glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			} else if (delta & ~state & RENDER_FILL) {
+				glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			}
+
+			if (delta & state & RENDER_LIGHTING) {
+				glEnable(GL_LIGHTING);
+				glEnable(GL_COLOR_MATERIAL);
+				glEnableClientState(GL_NORMAL_ARRAY);
+			} else if (delta & ~state & RENDER_LIGHTING) {
+				glDisable(GL_LIGHTING);
+				glDisable(GL_COLOR_MATERIAL);
+				glDisableClientState(GL_NORMAL_ARRAY);
+			}
+
+			if (delta & state & RENDER_TEXTURE) {
+				if (GlobalOpenGL().GL_1_3()) {
+					glActiveTexture(GL_TEXTURE0);
+					glClientActiveTexture(GL_TEXTURE0);
+				}
+
+				glEnable(GL_TEXTURE_2D);
+				glColor4f(1, 1, 1, m_colour[3]);
+				glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+			} else if (delta & ~state & RENDER_TEXTURE) {
+				if (GlobalOpenGL().GL_1_3()) {
+					glActiveTexture(GL_TEXTURE0);
+					glClientActiveTexture(GL_TEXTURE0);
+				}
+
+				glDisable(GL_TEXTURE_2D);
+				glBindTexture(GL_TEXTURE_2D, 0);
+				glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+			}
+
+			if (delta & state & RENDER_BLEND) {
+				glEnable(GL_BLEND);
+			} else if (delta & ~state & RENDER_BLEND) {
+				glDisable(GL_BLEND);
+			}
+
+			setState(state, delta, RENDER_CULLFACE, GL_CULL_FACE);
+
+			if (delta & state & RENDER_SMOOTH) {
+				glShadeModel(GL_SMOOTH);
+			} else if (delta & ~state & RENDER_SMOOTH) {
+				glShadeModel(GL_FLAT);
+			}
+
+			setState(state, delta, RENDER_SCALED, GL_NORMALIZE); // not GL_RESCALE_NORMAL
+			setState(state, delta, RENDER_DEPTHTEST, GL_DEPTH_TEST);
+
+			if (delta & state & RENDER_DEPTHWRITE) {
+				glDepthMask(GL_TRUE);
+			} else if (delta & ~state & RENDER_DEPTHWRITE) {
+				glDepthMask(GL_FALSE);
+			}
+
+			if (delta & state & RENDER_COLOURWRITE) {
+				glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+			} else if (delta & ~state & RENDER_COLOURWRITE) {
+				glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+			}
+
+			setState(state, delta, RENDER_ALPHATEST, GL_ALPHA_TEST);
+
+			if (delta & state & RENDER_COLOURARRAY) {
+				glEnableClientState(GL_COLOR_ARRAY);
+			} else if (delta & ~state & RENDER_COLOURARRAY) {
+				glDisableClientState(GL_COLOR_ARRAY);
+				glColor4fv(m_colour);
+			}
+
+			if (delta & ~state & RENDER_COLOURCHANGE) {
+				glColor4fv(m_colour);
+			}
+
+			setState(state, delta, RENDER_LINESTIPPLE, GL_LINE_STIPPLE);
+
+			if (state & RENDER_DEPTHTEST && m_depthfunc != current.m_depthfunc) {
+				glDepthFunc(m_depthfunc);
+
+				current.m_depthfunc = m_depthfunc;
+			}
+
+			if (state & RENDER_LINESTIPPLE && (m_linestipple_factor != current.m_linestipple_factor
+					|| m_linestipple_pattern != current.m_linestipple_pattern)) {
+				glLineStipple(m_linestipple_factor, m_linestipple_pattern);
+
+				current.m_linestipple_factor = m_linestipple_factor;
+				current.m_linestipple_pattern = m_linestipple_pattern;
+			}
+
+			if (state & RENDER_ALPHATEST && (m_alphafunc != current.m_alphafunc || m_alpharef
+					!= current.m_alpharef)) {
+				glAlphaFunc(m_alphafunc, m_alpharef);
+
+				current.m_alphafunc = m_alphafunc;
+				current.m_alpharef = m_alpharef;
+			}
+
+			{
+				if (GlobalOpenGL().GL_1_3()) {
+					setTextureState(current.m_texture, m_texture, GL_TEXTURE0);
+					setTextureState(current.m_texture1, m_texture1, GL_TEXTURE1);
+					setTextureState(current.m_texture2, m_texture2, GL_TEXTURE2);
+					setTextureState(current.m_texture3, m_texture3, GL_TEXTURE3);
+					setTextureState(current.m_texture4, m_texture4, GL_TEXTURE4);
+					setTextureState(current.m_texture5, m_texture5, GL_TEXTURE5);
+					setTextureState(current.m_texture6, m_texture6, GL_TEXTURE6);
+					setTextureState(current.m_texture7, m_texture7, GL_TEXTURE7);
+				} else {
+					setTextureState(current.m_texture, m_texture);
+				}
+			}
+
+			if (state & RENDER_TEXTURE && m_colour[3] != current.m_colour[3]) {
+				glColor4f(1, 1, 1, m_colour[3]);
+			}
+
+			if (!(state & RENDER_TEXTURE) && (m_colour[0] != current.m_colour[0] || m_colour[1]
+					!= current.m_colour[1] || m_colour[2] != current.m_colour[2] || m_colour[3]
+					!= current.m_colour[3])) {
+				glColor4fv(m_colour);
+			}
+			current.m_colour = m_colour;
+
+			if (state & RENDER_BLEND && (m_blend_src != current.m_blend_src || m_blend_dst
+					!= current.m_blend_dst)) {
+				glBlendFunc(m_blend_src, m_blend_dst);
+				current.m_blend_src = m_blend_src;
+				current.m_blend_dst = m_blend_dst;
+			}
+
+			if (!(state & RENDER_FILL) && m_linewidth != current.m_linewidth) {
+				glLineWidth(m_linewidth);
+				current.m_linewidth = m_linewidth;
+			}
+
+			if (!(state & RENDER_FILL) && m_pointsize != current.m_pointsize) {
+				glPointSize(m_pointsize);
+				current.m_pointsize = m_pointsize;
+			}
+
+			current.m_state = state;
+		}
 };
 
 class OpenGLStateLibrary {
-public:
-	virtual ~OpenGLStateLibrary(){}
+	public:
+		virtual ~OpenGLStateLibrary() {
+		}
 
-	INTEGER_CONSTANT(Version, 1);
-	STRING_CONSTANT(Name, "openglshaderlibrary");
+		INTEGER_CONSTANT(Version, 1);
+		STRING_CONSTANT(Name, "openglshaderlibrary");
 
-	virtual void getDefaultState(OpenGLState& state) const = 0;
+		virtual void getDefaultState(OpenGLState& state) const = 0;
 
-	virtual void insert(const std::string& name, const OpenGLState& state) = 0;
-	virtual void erase(const std::string& name) = 0;
+		virtual void insert(const std::string& name, const OpenGLState& state) = 0;
+		virtual void erase(const std::string& name) = 0;
 };
 
 #include "modulesystem.h"

src/tools/radiant/radiant/render/LinearLightList.h
@@ -1,0 +1,52 @@
+#ifndef LINEARLIGHTLIST_H_
+#define LINEARLIGHTLIST_H_
+
+#include "irender.h"
+#include <list>
+
+namespace render {
+
+typedef std::set<RendererLight*> RendererLights;
+
+class LinearLightList: public LightList {
+		LightCullable& m_cullable;
+		RendererLights& m_allLights;
+		Callback m_evaluateChanged;
+
+		typedef std::list<RendererLight*> Lights;
+		mutable Lights m_lights;
+		mutable bool m_lightsChanged;
+	public:
+		LinearLightList(LightCullable& cullable, RendererLights& lights,
+				const Callback& evaluateChanged) :
+			m_cullable(cullable), m_allLights(lights), m_evaluateChanged(evaluateChanged) {
+			m_lightsChanged = true;
+		}
+		void evaluateLights() const {
+			m_evaluateChanged();
+			if (m_lightsChanged) {
+				m_lightsChanged = false;
+
+				m_lights.clear();
+				m_cullable.clearLights();
+				for (RendererLights::const_iterator i = m_allLights.begin(); i != m_allLights.end(); ++i) {
+					m_lights.push_back(*i);
+					m_cullable.insertLight(*(*i));
+				}
+			}
+		}
+		void forEachLight(const RendererLightCallback& callback) const {
+			evaluateLights();
+
+			for (Lights::const_iterator i = m_lights.begin(); i != m_lights.end(); ++i) {
+				callback(*(*i));
+			}
+		}
+		void lightsChanged() const {
+			m_lightsChanged = true;
+		}
+};
+
+} // namespace render
+
+#endif /*LINEARLIGHTLIST_H_*/

commit ca81037d378a226825124d12904c03984acc8228

* fixed OpenGLStateLess bug introduced in 0429763e4ae7e7d789513fcab98f9fdc0eaf5752

src/tools/radiant/radiant/render/backend/OpenGLStateLess.h
@@ -42,7 +42,7 @@ struct OpenGLStateLess {
 				return self->m_state < other->m_state;
 			}
 			// Comparing address makes sure states are never equal.
-			return &self < &other;
+			return self < other;
 		}
 };
 

src/tools/radiant/radiant/render/backend/OpenGLStateManager.h
@@ -15,7 +15,6 @@ namespace render {
  * to rendered as efficiently as possible, reducing the number of unnecessary
  * context switches.
  */
-//typedef std::map<OpenGLState*, OpenGLShaderPass*, OpenGLStateLess> OpenGLStates;
 typedef std::map<OpenGLState*, OpenGLShaderPass*, OpenGLStateLess> OpenGLStates;
 
 /**