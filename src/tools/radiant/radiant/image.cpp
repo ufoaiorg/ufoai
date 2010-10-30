d0cf525d85e56579164918067008491dff388de4
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-10-30 14:36:23 +0200

    * cleanup for LoadImageVisitor

--- src/tools/radiant/radiant/image.cpp
+++ src/tools/radiant/radiant/image.cpp
@@ -1,27 +1,26 @@
 /*
-Copyright (C) 2001-2006, William Joseph.
-All Rights Reserved.
+ Copyright (C) 2001-2006, William Joseph.
+ All Rights Reserved.
 
-This file is part of GtkRadiant.
+ This file is part of GtkRadiant.
 
-GtkRadiant is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+ GtkRadiant is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-GtkRadiant is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ GtkRadiant is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GtkRadiant; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with GtkRadiant; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include "image.h"
 
-#include "AutoPtr.h"
 #include "modulesystem.h"
 #include "iimage.h"
 #include "ifilesystem.h"
@@ -31,32 +30,55 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "os/path.h"
 #include "stream/stringstream.h"
 
-
 typedef Modules<_QERPlugImageTable> ImageModules;
-ImageModules& Textures_getImageModules();
+ImageModules& Textures_getImageModules ();
 
-/// \brief Returns a new image for the first file matching \p name in one of the available texture formats, or 0 if no file is found.
-Image* QERApp_LoadImage(void* environment, const std::string& name) {
-	Image* image = 0;
-	class LoadImageVisitor : public ImageModules::Visitor {
-		const std::string m_name;
-		Image*& m_image;
+class LoadImageVisitor: public ImageModules::Visitor
+{
+		// The filename to load
+		const std::string _name;
+
+		// The reference to the pointer in the parent function
+		Image*& _image;
+
 	public:
-		LoadImageVisitor(const std::string& name, Image*& image)
-				: m_name(name), m_image(image) {
+		// Constructor
+		LoadImageVisitor (const std::string& name, Image*& image) :
+			_name(name), _image(image)
+		{
 		}
-		void visit(const std::string& name, const _QERPlugImageTable& table) const {
-			if (m_image == 0) {
-				std::string fullname = m_name + "." + name;
+
+		// Visit function called for each image module. Provides the file extension and
+		// a table with the loadImage function.
+		void visit (const std::string& extension, const _QERPlugImageTable& table)
+		{
+			// Only do anything, if the image pointer is still NULL (i.e. the image load has not succeeded yet)
+			if (_image == NULL) {
+				// Construct the full name of the image to load, including the prefix (e.g. "dds/")
+				// and the file extension.
+				std::string fullName = _name + "." + extension;
+
+				// Try to open the file (will fail if the extension does not fit)
 				AutoPtr<ArchiveFile> file(GlobalFileSystem().openFile(fullname));
+				// Has the file been loaded?
 				if (file) {
-					m_image = table.loadImage(*file);
+					// Try to invoke the imageloader with a reference to the ArchiveFile
+					_image = table.loadImage(*file);
 				}
 			}
 		}
-	};
+}; // class LoadImageVisitor
 
-	Textures_getImageModules().foreachModule(LoadImageVisitor(name, image));
+/// \brief Returns a new image for the first file matching \p name in one of the available texture formats, or 0 if no file is found.
+Image* QERApp_LoadImage (void* environment, const std::string& name)
+{
+	Image* image = NULL;
+
+	// Instantiate a visitor class
+	LoadImageVisitor visitor(name, image);
+
+	// Cycle through all modules and tell them to visit the local class
+	Textures_getImageModules().foreachModule(visitor);
 
 	return image;
 }
