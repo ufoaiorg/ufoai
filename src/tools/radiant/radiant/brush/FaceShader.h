c2c49c0b318a00ee6fa9098cd544365064ee1d26
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-10-30 09:19:20 +0200

    * moved FaceShader and FaceTexDef into own files

---
+++ src/tools/radiant/radiant/brush/FaceShader.h
@@ -0,0 +1,203 @@
+#ifndef FACESHADER_H_
+#define FACESHADER_H_
+
+#include <string>
+#include "debugging/debugging.h"
+#include "container/container.h"
+#include "moduleobserver.h"
+#include "irender.h"
+
+#include "ContentsFlagsValue.h"
+
+class FaceShaderObserver
+{
+	public:
+		virtual ~FaceShaderObserver ()
+		{
+		}
+		virtual void realiseShader () = 0;
+		virtual void unrealiseShader () = 0;
+};
+
+// ----------------------------------------------------------------------------
+
+// greebo: Two Visitor classes to realise/unrealise a FaceShader
+
+class FaceShaderObserverRealise
+{
+	public:
+		void operator() (FaceShaderObserver& observer) const
+		{
+			observer.realiseShader();
+		}
+};
+
+class FaceShaderObserverUnrealise
+{
+	public:
+		void operator() (FaceShaderObserver& observer) const
+		{
+			observer.unrealiseShader();
+		}
+};
+
+// ----------------------------------------------------------------------------
+
+typedef ReferencePair<FaceShaderObserver> FaceShaderObserverPair;
+
+class FaceShader: public ModuleObserver
+{
+	public:
+		class SavedState
+		{
+			public:
+				std::string m_shader;
+				ContentsFlagsValue m_flags;
+
+				SavedState (const FaceShader& faceShader)
+				{
+					m_shader = faceShader.getShader();
+					m_flags = faceShader.m_flags;
+				}
+
+				void exportState (FaceShader& faceShader) const
+				{
+					faceShader.setShader(m_shader.c_str());
+					faceShader.setFlags(m_flags);
+				}
+		};
+
+		std::string m_shader;
+		Shader* m_state;
+		ContentsFlagsValue m_flags;
+		FaceShaderObserverPair m_observers;
+		bool m_instanced;
+		bool m_realised;
+
+		FaceShader (const std::string& shader, const ContentsFlagsValue& flags = ContentsFlagsValue(0, 0, 0, false)) :
+			m_shader(shader), m_state(0), m_flags(flags), m_instanced(false), m_realised(false)
+		{
+			captureShader();
+		}
+		~FaceShader ()
+		{
+			releaseShader();
+		}
+		// copy-construction not supported
+		FaceShader (const FaceShader& other);
+
+		void instanceAttach ()
+		{
+			m_instanced = true;
+			m_state->incrementUsed();
+		}
+		void instanceDetach ()
+		{
+			m_state->decrementUsed();
+			m_instanced = false;
+		}
+
+		void captureShader ()
+		{
+			ASSERT_MESSAGE(m_state == 0, "shader cannot be captured");
+			brush_check_shader(m_shader.c_str());
+			m_state = GlobalShaderCache().capture(m_shader);
+			m_state->attach(*this);
+		}
+		void releaseShader ()
+		{
+			ASSERT_MESSAGE(m_state != 0, "shader cannot be released");
+			m_state->detach(*this);
+			GlobalShaderCache().release(m_shader);
+			m_state = 0;
+		}
+
+		void realise ()
+		{
+			ASSERT_MESSAGE(!m_realised, "FaceTexdef::realise: already realised");
+			m_realised = true;
+			m_observers.forEach(FaceShaderObserverRealise());
+		}
+		void unrealise ()
+		{
+			ASSERT_MESSAGE(m_realised, "FaceTexdef::unrealise: already unrealised");
+			m_observers.forEach(FaceShaderObserverUnrealise());
+			m_realised = false;
+		}
+
+		void attach (FaceShaderObserver& observer)
+		{
+			m_observers.attach(observer);
+			if (m_realised) {
+				observer.realiseShader();
+			}
+		}
+
+		void detach (FaceShaderObserver& observer)
+		{
+			if (m_realised) {
+				observer.unrealiseShader();
+			}
+			m_observers.detach(observer);
+		}
+
+		const std::string& getShader () const
+		{
+			return m_shader;
+		}
+		void setShader (const std::string& name)
+		{
+			if (m_instanced) {
+				m_state->decrementUsed();
+			}
+			releaseShader();
+			m_shader = name;
+			captureShader();
+			if (m_instanced) {
+				m_state->incrementUsed();
+			}
+		}
+		ContentsFlagsValue getFlags () const
+		{
+			ASSERT_MESSAGE(m_realised, "FaceShader::getFlags: flags not valid when unrealised");
+			if (!m_flags.m_specified) {
+				return ContentsFlagsValue(m_state->getTexture().surfaceFlags, m_state->getTexture().contentFlags,
+						m_state->getTexture().value, true);
+			}
+			return m_flags;
+		}
+		void setFlags (const ContentsFlagsValue& flags)
+		{
+			ASSERT_MESSAGE(m_realised, "FaceShader::setFlags: flags not valid when unrealised");
+			m_flags.assignMasked(flags);
+		}
+
+		Shader* state () const
+		{
+			return m_state;
+		}
+
+		std::size_t width () const
+		{
+			if (m_realised) {
+				return m_state->getTexture().width;
+			}
+			return 1;
+		}
+		std::size_t height () const
+		{
+			if (m_realised) {
+				return m_state->getTexture().height;
+			}
+			return 1;
+		}
+		unsigned int shaderFlags () const
+		{
+			if (m_realised) {
+				return m_state->getFlags();
+			}
+			return 0;
+		}
+}; // class FaceShader
+
+#endif /*FACESHADER_H_*/
---
+++ src/tools/radiant/radiant/brush/FaceTexDef.h
@@ -0,0 +1,101 @@
+#ifndef FACETEXDEF_H_
+#define FACETEXDEF_H_
+
+#include "winding.h"
+#include "math/matrix.h"
+#include "math/Vector3.h"
+#include "math/Plane3.h"
+
+#include "FaceShader.h"
+#include "TextureProjection.h"
+
+class FaceTexdef: public FaceShaderObserver
+{
+		// not copyable
+		FaceTexdef (const FaceTexdef& other);
+		// not assignable
+		FaceTexdef& operator= (const FaceTexdef& other);
+	public:
+		class SavedState
+		{
+			public:
+				TextureProjection m_projection;
+
+				SavedState (const FaceTexdef& faceTexdef)
+				{
+					m_projection = faceTexdef.m_projection;
+				}
+
+				void exportState (FaceTexdef& faceTexdef) const
+				{
+					faceTexdef.m_projection.m_texdef = m_projection.m_texdef;
+				}
+		};
+
+		FaceShader& m_shader;
+		TextureProjection m_projection;
+		bool m_projectionInitialised;
+		bool m_scaleApplied;
+
+		FaceTexdef (FaceShader& shader, const TextureProjection& projection, bool projectionInitialised = true) :
+			m_shader(shader), m_projection(projection), m_projectionInitialised(projectionInitialised), m_scaleApplied(
+					false)
+		{
+			m_shader.attach(*this);
+		}
+		~FaceTexdef ()
+		{
+			m_shader.detach(*this);
+		}
+
+		void realiseShader ()
+		{
+		}
+		void unrealiseShader ()
+		{
+		}
+
+		void setTexdef (const TextureProjection& projection)
+		{
+			m_projection.m_texdef = projection.m_texdef;
+		}
+
+		void shift (float s, float t)
+		{
+			ASSERT_MESSAGE(m_projection.m_texdef.isSane(), "FaceTexdef::shift: bad texdef");
+			m_projection.m_texdef.shift(s, t);
+		}
+
+		void scale (float s, float t)
+		{
+			m_projection.m_texdef.scale(s, t);
+		}
+
+		void rotate (float angle)
+		{
+			m_projection.m_texdef.rotate(angle);
+		}
+
+		void fit (const Vector3& normal, const Winding& winding, float s_repeat, float t_repeat)
+		{
+			m_projection.fitTexture(m_shader.width(), m_shader.height(), normal, winding, s_repeat, t_repeat);
+		}
+
+		void emitTextureCoordinates (Winding& winding, const Vector3& normal, const Matrix4& localToWorld)
+		{
+			m_projection.emitTextureCoordinates(m_shader.width(), m_shader.height(), winding, normal,
+					localToWorld);
+		}
+
+		void transform (const Plane3& plane, const Matrix4& matrix)
+		{
+			m_projection.transformLocked(m_shader.width(), m_shader.height(), plane, matrix);
+		}
+
+		TextureProjection normalised () const
+		{
+			return TextureProjection(m_projection.m_texdef);
+		}
+}; // class FaceTexDef
+
+#endif /*FACETEXDEF_H_*/
