fc93b153d579333f06a1a409dc20de01f2edded9
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-11-02 17:35:58 +0100

    * fixed missing text entry support in RegistryConnector

--- src/tools/radiant/libs/gtkutil/RegistryConnector.cpp
+++ src/tools/radiant/libs/gtkutil/RegistryConnector.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 #include "gtk/gtktogglebutton.h"
 #include "gtk/gtkcombobox.h"
+#include "gtk/gtkentry.h"
 
 namespace gtkutil {
 
 // Connects the given GtkObject to the passed registryKey
 void RegistryConnector::connectGtkObject (GtkObject* object, const std::string& registryKey)
 {
-
 	// Add the GtkObject to the internal list
 	_objectKeyMap[object] = registryKey;
 
@@ -20,7 +20,6 @@ void RegistryConnector::connectGtkObject (GtkObject* object, const std::string& 
 
 void RegistryConnector::importKey (GtkObject* obj, const std::string& registryKey)
 {
-
 	if (GTK_IS_TOGGLE_BUTTON(obj)) {
 		// Set the "active" state of the toggle button according to the registry value
 		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(obj), GlobalRegistry().get(registryKey) == "1");
@@ -30,15 +29,17 @@ void RegistryConnector::importKey (GtkObject* obj, const std::string& registryKe
 	} else if (GTK_IS_COMBO_BOX(obj)) {
 		// Set the "active" state of the combo box according to the registry value
 		gtk_combo_box_set_active(GTK_COMBO_BOX(obj), GlobalRegistry().getInt(registryKey));
+	} else if (GTK_IS_ENTRY(obj)) {
+		// Set the content of the input field to the registryKey
+		gtk_entry_set_text(GTK_ENTRY(obj), GlobalRegistry().get(registryKey).c_str());
 	} else {
-		std::cout << "RegistryConnector::importKey failed to identify GTKObject\n";
+		std::cout << "RegistryConnector::importKey failed to identify GTKObject for key " << registryKey << "\n";
 	}
 }
 
 // Retrieve the value from the GtkObject and save it into the registry
 void RegistryConnector::exportKey (GtkObject* obj, const std::string& registryKey)
 {
-
 	if (GTK_IS_TOGGLE_BUTTON(obj)) {
 		// Set the registry key to "1" or "0", according on the toggle button state
 		GlobalRegistry().set(registryKey, gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(obj)) ? "1" : "0");
@@ -48,8 +49,11 @@ void RegistryConnector::exportKey (GtkObject* obj, const std::string& registryKe
 	} else if (GTK_IS_COMBO_BOX(obj)) {
 		// Store the value into the registry
 		GlobalRegistry().setInt(registryKey, gtk_combo_box_get_active(GTK_COMBO_BOX(obj)));
+	} else if (GTK_IS_ENTRY(obj)) {
+		// Get the content of the input field and write it to the registry
+		GlobalRegistry().set(registryKey, gtk_entry_get_text(GTK_ENTRY(obj)));
 	} else {
-		std::cout << "RegistryConnector::exportKey failed to identify GTKObject\n";
+		std::cout << "RegistryConnector::exportKey failed to identify GTKObject for key " << registryKey << "\n";
 	}
 }
 
