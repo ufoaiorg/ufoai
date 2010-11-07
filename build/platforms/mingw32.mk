c903e7d36431d6acfa3a65fc5a53b5c2039be34a
mutonufoai <mutonufoai@users.sourceforge.net>
2010-11-07 11:45:12 +0100

    * add mingw32.mk -mwindows LDFLAG

--- build/platforms/mingw32.mk
+++ build/platforms/mingw32.mk
@@ -21,8 +21,8 @@ SDL_TTF_LIBS             ?= $(call PKG_LIBS,SDL_ttf) $(call PKG_LIBS,freetype2)
 SDL_TTF_CFLAGS           ?= $(call PKG_CFLAGS,SDL_ttf) $(call PKG_CFLAGS,freetype2)
 INTL_LIBS                ?= -lintl
 
-ufo_LDFLAGS              += -lws2_32 -lwinmm -lgdi32 -lfreetype
-ufoded_LDFLAGS           += -lws2_32 -lwinmm -lgdi32
+ufo_LDFLAGS              += -lws2_32 -lwinmm -lgdi32 -lfreetype -mwindows
+ufoded_LDFLAGS           += -lws2_32 -lwinmm -lgdi32 -mwindows
 testall_LDFLAGS          += -lws2_32 -lwinmm -lgdi32 -lfreetype
 ifdef HAVE_BFD_H
     BFD_LIBS             := -lbfd -liberty -limagehlp
@@ -32,4 +32,4 @@ ifdef HAVE_BFD_H
 endif
 ufo2map_LDFLAGS          += -lwinmm
 ufomodel_LDFLAGS         += -lwinmm
-uforadiant_LDFLAGS       += -lglib-2.0 -lgtk-win32-2.0 -lgobject-2.0
+uforadiant_LDFLAGS       += -lglib-2.0 -lgtk-win32-2.0 -lgobject-2.0 -mwindows
