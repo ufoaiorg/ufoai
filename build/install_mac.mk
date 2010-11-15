7935102a7c6f8257af21ed1405d16483435fad2b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-11-15 08:09:20 +0100

    * fixed syntax error in mac install target

--- build/install_mac.mk
+++ build/install_mac.mk
@@ -63,7 +63,7 @@ copydata-uforadiant:
 # =======================
 
 copynotes-ufoai: package-dir-ufoai
-	(Q)@cp README $(MAC_INST_DIR)/$(UFOAI_MAC_PACKAGE_NAME)
+	$(Q)cp README $(MAC_INST_DIR)/$(UFOAI_MAC_PACKAGE_NAME)
 	$(Q)cp COPYING $(MAC_INST_DIR)/$(UFOAI_MAC_PACKAGE_NAME)
 
 copynotes-uforadiant: package-dir-uforadiant
