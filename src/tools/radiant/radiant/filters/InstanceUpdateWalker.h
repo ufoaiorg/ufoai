2de8a777cb122fa308a50303aa03bdee9eaea92d
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-11-21 10:41:04 +0100

    * merged further selection test changes from darkradiant (filter related)
    * this is not yet fixing the 'still-selectable-when-filtered' bug

--- src/tools/radiant/radiant/filters/InstanceUpdateWalker.h
+++ src/tools/radiant/radiant/filters/InstanceUpdateWalker.h
@@ -10,51 +10,87 @@
 
 namespace filters {
 
+// Walker: de-selects a complete subgraph
+class Deselector: public scene::Traversable::Walker
+{
+	public:
+		bool pre (scene::Node& node) const
+		{
+			scene::Instance* instance = GlobalSceneGraph().find(node);
+			Instance_getSelectable(*instance)->setSelected(false);
+			return true;
+		}
+};
+
+// Walker: Shows or hides a complete subgraph
+class NodeVisibilityUpdater: public scene::Traversable::Walker
+{
+	private:
+		bool _filtered;
+
+	public:
+		NodeVisibilityUpdater (bool setFiltered) :
+			_filtered(setFiltered)
+		{
+		}
+
+		bool pre (scene::Node& node) const
+		{
+			scene::Instance* instance = GlobalSceneGraph().find(node);
+			instance->setFiltered(_filtered);
+			return true;
+		}
+};
+
 /**
  * Scenegraph walker to update filtered status of Instances based on the
  * status of their parent entity class.
  */
 class InstanceUpdateWalker: public scene::Graph::Walker
 {
+		// Helper visitors to update subgraphs
+		NodeVisibilityUpdater _hideWalker;
+		NodeVisibilityUpdater _showWalker;
+
 		// Cached boolean to avoid GlobalFilterSystem() queries for each node
 		bool _brushesAreVisible;
 
 	public:
 
 		InstanceUpdateWalker () :
-			_brushesAreVisible(GlobalFilterSystem().isVisible("object", "brush"))
+			_hideWalker(true), _showWalker(false),
+					_brushesAreVisible(GlobalFilterSystem().isVisible("object", "brush"))
 		{
 		}
 
 		// Pre-descent walker function
 		bool pre (const scene::Path& path, scene::Instance& instance) const
 		{
-
+			scene::Node& node = path.top();
 			// Retrieve the parent entity and check its entity class.
-			Entity* entity = Node_getEntity(path.top());
+			Entity* entity = Node_getEntity(node);
 			if (entity) {
 				const EntityClass& eclass = entity->getEntityClass();
 				const std::string& name = eclass.name();
-				if (!GlobalFilterSystem().isVisible("entityclass", name)) {
-					instance.setFiltered(true);
-				} else {
-					instance.setFiltered(false);
-				}
+				bool entityClassVisible = GlobalFilterSystem().isVisible("entityclass", name);
+				Node_traverseSubgraph(node, entityClassVisible ? _showWalker : _hideWalker);
 			}
 
 			// greebo: Update visibility of BrushInstances
-			if (Node_isBrush(path.top())) {
-				instance.setFiltered(!_brushesAreVisible);
+			if (Node_isBrush(node)) {
+				bool isVisible = _brushesAreVisible;// && brush->hasVisibleMaterial();
+				Node_traverseSubgraph(node, isVisible ? _showWalker : _hideWalker);
+			}
+
+			if (!node.visible())
+			{
+				// de-select this node and all children
+				Deselector deselector;
+				Node_traverseSubgraph(node, deselector);
 			}
 
 			// Continue the traversal
 			return true;
-		}
-
-		// Post descent function
-		void post (const scene::Path& path, scene::Instance& instance) const
-		{
-
 		}
 };
 
