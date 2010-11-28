41c5eb78623c212a769ac400a530d25a418ed3b1
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-11-28 19:50:50 +0100

    * fixed configure call

--- contrib/scripts/crosscompile.sh
+++ contrib/scripts/crosscompile.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 JOBS=1
 MINGW=i686-pc-mingw32
 MINGW_CROSS_ENV=/opt/mingw-cross-env
@@ -10,6 +9,6 @@ cd "${DIR}/../.."
 # http://www.nongnu.org/mingw-cross-env/
 export PATH=${MINGW_CROSS_ENV}/usr/bin:${MINGW_CROSS_ENV}/usr/${MINGW}/bin/:${PATH}
 
-./configure --host=${MINGW} --disable-uforadiant
+./configure --target-os=mingw32
 
 make -j ${JOBS}
