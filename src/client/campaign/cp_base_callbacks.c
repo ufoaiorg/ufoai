e4daba2a16b1e8a2f9bf6f5a48ead2f847fdfb14
Tamás Fehérvári <geever@users.sourceforge.net>
2010-12-03 00:30:13 +0100

    * disable transfer button and show tooltip if MAX_TRANSFERS reached

--- src/client/campaign/cp_base_callbacks.c
+++ src/client/campaign/cp_base_callbacks.c
@@ -302,8 +302,10 @@ static void B_BaseInit_f (void)
 	else
 		UI_ExecuteConfunc("update_basebutton buysell true \"%s\"", va(_("No %s functional in base."), _("Storage")));
 
-	if (B_GetFoundedBaseCount() > 1)
-		UI_ExecuteConfunc("update_basebutton transfer false \"%s\"", _("Transfer equipment, aircraft, UGV, aliens and employees to other bases"));
+	if (B_GetFoundedBaseCount() > 1 && ccs.numTransfers < MAX_TRANSFERS)
+		MN_ExecuteConfunc("update_basebutton transfer false \"%s\"", _("Transfer equipment, vehicles, aliens and employees to other bases"));
+	else if (ccs.numTransfers >= MAX_TRANSFERS)
+		MN_ExecuteConfunc("update_basebutton transfer true \"%s\"", _("You have too many active transfers"));
 	else
 		UI_ExecuteConfunc("update_basebutton transfer true \"%s\"", _("Build at least a second base to transfer equipment or personnel"));
 
