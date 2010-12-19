6c9c9341ba3cd29c7f57c9838bce3003437f85e2
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-12-19 19:36:09 +0100

    * add #extension GL_ARB_draw_buffers to the simple_glow fragment shader

--- base/shaders/simple_glow_fs.glsl
+++ base/shaders/simple_glow_fs.glsl
@@ -3,19 +3,21 @@
  * @brief This is the glow shader that is applied if no other shader is
  * active but the glow map should get rendered.
  */
+#extension GL_ARB_draw_buffers : enable
 
-uniform sampler2D SAMPLER0;
-uniform sampler2D SAMPLER1;
+uniform sampler2D SAMPLER0; /* diffuse */
+uniform sampler2D SAMPLER1; /* glowmap */
 
 uniform float GLOWSCALE;
 
 void main(void){
 
-	vec4 color = texture2D(SAMPLER0, gl_TexCoord[0].st);
+	vec4 color;
+	color.rgba = texture2D(SAMPLER0, gl_TexCoord[0].st);
 	gl_FragData[0].rgb = color.rgb * color.a;
 	gl_FragData[0].a = 1.0;
 
-	color = texture2D(SAMPLER1, gl_TexCoord[0].st);
+	color.rgba = texture2D(SAMPLER1, gl_TexCoord[0].st);
 	gl_FragData[1].rgb = color.rgb * color.a * GLOWSCALE;
 	gl_FragData[1].a = 1.0;
 }
