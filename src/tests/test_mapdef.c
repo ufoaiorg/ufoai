6ca1d6b6ce4fafb3761efbf92e3d4290f6891855
Martin Gerhardy <tlh2000@users.sourceforge.net>
2010-12-27 19:25:54 +0100

    * load the mapdef maps in the unittest

--- src/tests/test_mapdef.c
+++ src/tests/test_mapdef.c
@@ -27,6 +27,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "test_mapdef.h"
 #include "../client/client.h"
 #include "../client/cl_shared.h"
+#include "../client/renderer/r_state.h"
+#include "../client/ui/ui_main.h"
+#include "../server/server.h"
 
 /**
  * The suite initialization function.
@@ -35,6 +38,20 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 static int UFO_InitSuiteMapDef (void)
 {
 	TEST_Init();
+
+	sv_genericPool = Mem_CreatePool("mapdef-test");
+	vid_imagePool = Mem_CreatePool("Vid: Image system");
+	sv_dumpmapassembly = Cvar_Get("sv_dumpassembly", "0", 0, NULL);
+
+	cl_genericPool = Mem_CreatePool("Client: Generic");
+
+	r_state.active_texunit = &r_state.texunits[0];
+	R_FontInit();
+	UI_Init();
+
+	memset(&cls, 0, sizeof(cls));
+	Com_ParseScripts(qfalse);
+
 	return 0;
 }
 
@@ -48,13 +65,31 @@ static int UFO_CleanSuiteMapDef (void)
 	return 0;
 }
 
-static void testMapDefs (void)
+static void testMapDefsSingleplayer (void)
 {
 	int i;
+
+	CU_ASSERT_TRUE(cls.numMDs > 0);
 
 	for (i = 0; i < cls.numMDs; i++) {
 		const mapDef_t* md = &cls.mds[i];
 
+		SV_Map(qtrue, md->map, md->param);
+	}
+}
+
+static void testMapDefsMultiplayer (void)
+{
+	int i;
+
+	CU_ASSERT_TRUE(cls.numMDs > 0);
+
+	Cvar_Set("sv_maxclients", "2");
+
+	for (i = 0; i < cls.numMDs; i++) {
+		const mapDef_t* md = &cls.mds[i];
+
+		SV_Map(qtrue, md->map, md->param);
 	}
 }
 
@@ -67,7 +102,10 @@ int UFO_AddMapDefTests (void)
 		return CU_get_error();
 
 	/* add the tests to the suite */
-	if (CU_ADD_TEST(mapDefSuite, testMapDefs) == NULL)
+	if (CU_ADD_TEST(mapDefSuite, testMapDefsSingleplayer) == NULL)
+		return CU_get_error();
+
+	if (CU_ADD_TEST(mapDefSuite, testMapDefsMultiplayer) == NULL)
 		return CU_get_error();
 
 	return CUE_SUCCESS;
