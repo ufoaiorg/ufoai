ufoaiorg/ufoai

commit 4eb47e08f84d3bf9d2cc9790d5859c77466435a4

* introduced more helper macros to set and get the geoscape global pointers and fixed a segfault in CL_TeamListDebug_f

src/client/cl_game_campaign.c
@@ -43,7 +43,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 static void GAME_CP_MissionAutoGo_f (void)
 {
-	mission_t *mission = ccs.geoscape.selectedMission;
+	mission_t *mission = MAP_GetSelectedMission();
 	missionResults_t *results = &ccs.missionResults;
 	battleParam_t *battleParam = &ccs.battleParameters;
 
@@ -52,12 +52,12 @@ static void GAME_CP_MissionAutoGo_f (void)
 		return;
 	}
 
-	if (!ccs.geoscape.missionAircraft) {
+	if (MAP_GetMissionAircraft() != NULL) {
 		Com_Printf("GAME_CP_MissionAutoGo_f: No aircraft at target pos\n");
 		return;
 	}
 
-	if (!ccs.geoscape.interceptAircraft) {
+	if (MAP_GetInterceptorAircraft() != NULL) {
 		Com_Printf("GAME_CP_MissionAutoGo_f: No intercept aircraft given\n");
 		return;
 	}
@@ -77,10 +77,10 @@ static void GAME_CP_MissionAutoGo_f (void)
 	}
 
 	/* start the map */
-	CP_CreateBattleParameters(mission, battleParam, ccs.geoscape.missionAircraft);
+	CP_CreateBattleParameters(mission, battleParam, MAP_GetMissionAircraft());
 
 	results->won = qfalse;
-	CL_GameAutoGo(mission, ccs.geoscape.interceptAircraft, ccs.curCampaign, battleParam, results);
+	CL_GameAutoGo(mission, MAP_GetInterceptorAircraft(), ccs.curCampaign, battleParam, results);
 
 	if (results->won) {
 		Cvar_SetValue("mn_autogo", 1);
@@ -103,9 +103,9 @@ static void GAME_CP_MissionAutoGo_f (void)
  */
 static void GAME_CP_MissionAutoCheck_f (void)
 {
-	const mission_t *mission = ccs.geoscape.selectedMission;
+	const mission_t *mission = MAP_GetSelectedMission();
 
-	if (!mission || !ccs.geoscape.interceptAircraft) {
+	if (!mission || MAP_GetInterceptorAircraft() == NULL) {
 		Com_DPrintf(DEBUG_CLIENT, "GAME_CP_MissionAutoCheck_f: No update after automission\n");
 		return;
 	}
@@ -126,7 +126,7 @@ static void GAME_CP_MissionAutoCheck_f (void)
  */
 static void GAME_CP_Results_f (void)
 {
-	mission_t *mission = ccs.geoscape.selectedMission;
+	mission_t *mission = MAP_GetSelectedMission();
 
 	if (!mission)
 		return;
@@ -295,8 +295,8 @@ void GAME_CP_Results (struct dbuffer *msg, int winner, int *numSpawned, int *num
 	int civiliansSurvived, civiliansKilled, civiliansStunned;
 	const qboolean won = (winner == cls.team);
 	missionResults_t *results;
-	aircraft_t *aircraft = ccs.geoscape.missionAircraft;
-	mission_t *mission = ccs.geoscape.selectedMission;
+	aircraft_t *aircraft = MAP_GetMissionAircraft();
+	mission_t *mission = MAP_GetSelectedMission();
 
 	CP_ParseCharacterData(msg);
 
@@ -375,7 +375,7 @@ void GAME_CP_Results (struct dbuffer *msg, int winner, int *numSpawned, int *num
 
 qboolean GAME_CP_Spawn (void)
 {
-	aircraft_t *aircraft = ccs.geoscape.missionAircraft;
+	aircraft_t *aircraft = MAP_GetMissionAircraft();
 	base_t *base;
 	employee_t *employee;
 	chrList_t *chrList;

src/client/campaign/cp_aircraft.c
@@ -1171,9 +1171,9 @@ static void AIR_Move (aircraft_t* aircraft, int deltaTime)
 			assert(aircraft->mission);
 			aircraft->mission->active = qtrue;
 			aircraft->status = AIR_DROP;
-			ccs.geoscape.missionAircraft = aircraft;
+			MAP_SetMissionAircraft(aircraft);
 			MAP_SelectMission(aircraft->mission);
-			ccs.geoscape.interceptAircraft = aircraft;
+			MAP_SetInterceptorAircraft(aircraft);
 			CL_GameTimeStop();
 			UI_PushWindow("popup_intercept_ready", NULL, NULL);
 			break;
@@ -1349,7 +1349,7 @@ qboolean AIR_SendAircraftToMission (aircraft_t *aircraft, mission_t *mission)
 	}
 
 	/* ensure interceptAircraft is set correctly */
-	ccs.geoscape.interceptAircraft = aircraft;
+	MAP_SetInterceptorAircraft(aircraft);
 
 	/* if mission is a base-attack and aircraft already in base, launch
 	 * mission immediately */

src/client/campaign/cp_campaign.c
@@ -852,7 +852,7 @@ qboolean CP_SaveXML (mxml_node_t *parent)
 void CP_StartSelectedMission (void)
 {
 	mission_t *mis;
-	aircraft_t *aircraft = ccs.geoscape.missionAircraft;
+	aircraft_t *aircraft = MAP_GetMissionAircraft();
 	base_t *base;
 	battleParam_t *battleParam = &ccs.battleParameters;
 
@@ -863,10 +863,10 @@ void CP_StartSelectedMission (void)
 
 	base = aircraft->homebase;
 
-	if (!ccs.geoscape.selectedMission)
-		ccs.geoscape.selectedMission = aircraft->mission;
+	if (MAP_GetSelectedMission() == NULL)
+		MAP_SetSelectedMission(aircraft->mission);
 
-	mis = ccs.geoscape.selectedMission;
+	mis = MAP_GetSelectedMission();
 	if (!mis) {
 		Com_Printf("CP_StartSelectedMission: No mission selected\n");
 		return;

src/client/campaign/cp_map.c
@@ -211,7 +211,7 @@ static void MAP_MultiSelectExecuteAction_f (void)
 		INS_SelectInstallation(INS_GetFoundedInstallationByIDX(id));
 		break;
 	case MULTISELECT_TYPE_MISSION: {
-		mission_t *mission = ccs.geoscape.selectedMission;
+		mission_t *mission = MAP_GetSelectedMission();
 		/* Select a mission */
 		if (ccs.mapAction == MA_INTERCEPT && mission && mission == MAP_GetMissionByIDX(id)) {
 			CL_DisplayPopupInterceptMission(mission);
@@ -257,7 +257,7 @@ static void MAP_MultiSelectExecuteAction_f (void)
 			MAP_SelectUFO(aircraft);
 			if (multiSelection)
 				/* if we come from a multiSelection menu, no need to open twice this popup to choose an action */
-				CL_DisplayPopupInterceptUFO(ccs.geoscape.selectedUFO);
+				CL_DisplayPopupInterceptUFO(MAP_GetSelectedUFO());
 		}
 		break;
 	case MULTISELECT_TYPE_NONE :	/* Selection of an element that has been removed */
@@ -371,7 +371,7 @@ void MAP_MapClick (uiNode_t* node, int x, int y)
 		CL_GameTimeStop();
 		UI_PushWindow("popup_multi_selection", NULL, NULL);
 	} else {
-		aircraft_t *aircraft = ccs.geoscape.selectedAircraft;
+		aircraft_t *aircraft = MAP_GetSelectedAircraft();
 		/* Nothing selected */
 		if (!aircraft)
 			MAP_ResetAction();
@@ -1968,7 +1968,7 @@ void MAP_DrawMap (const uiNode_t* node, campaign_t *campaign)
 		MAP_DrawMapMarkers(node);
 	}
 
-	mission = ccs.geoscape.selectedMission;
+	mission = MAP_GetSelectedMission();
 	/* display text */
 	UI_ResetData(TEXT_STANDARD);
 	switch (ccs.mapAction) {
@@ -2000,16 +2000,16 @@ void MAP_DrawMap (const uiNode_t* node, campaign_t *campaign)
 	/* Nothing is displayed yet */
 	if (mission) {
 		UI_RegisterText(TEXT_STANDARD, MAP_GetMissionText(textStandard, sizeof(textStandard), mission));
-	} else if (ccs.geoscape.selectedAircraft) {
-		const aircraft_t *aircraft = ccs.geoscape.selectedAircraft;
+	} else if (MAP_GetSelectedAircraft() != NULL) {
+		const aircraft_t *aircraft = MAP_GetSelectedAircraft();
 		if (AIR_IsAircraftInBase(aircraft)) {
 			UI_RegisterText(TEXT_STANDARD, NULL);
 			MAP_ResetAction();
 			return;
 		}
 		UI_RegisterText(TEXT_STANDARD, MAP_GetAircraftText(textStandard, sizeof(textStandard), aircraft));
-	} else if (ccs.geoscape.selectedUFO) {
-		UI_RegisterText(TEXT_STANDARD, MAP_GetUFOText(textStandard, sizeof(textStandard), ccs.geoscape.selectedUFO));
+	} else if (MAP_GetSelectedUFO() != NULL) {
+		UI_RegisterText(TEXT_STANDARD, MAP_GetUFOText(textStandard, sizeof(textStandard), MAP_GetSelectedUFO()));
 	} else {
 #ifdef DEBUG
 		if (debug_showInterest->integer) {
@@ -2031,10 +2031,10 @@ void MAP_ResetAction (void)
 	if (B_AtLeastOneExists())
 		ccs.mapAction = MA_NONE;
 
-	ccs.geoscape.interceptAircraft = NULL;
-	ccs.geoscape.selectedMission = NULL;
-	ccs.geoscape.selectedAircraft = NULL;
-	ccs.geoscape.selectedUFO = NULL;
+	MAP_SetInterceptorAircraft(NULL);
+	MAP_SetSelectedMission(NULL);
+	MAP_SetSelectedAircraft(NULL);
+	MAP_SetSelectedUFO(NULL);
 
 	if (!radarOverlayWasSet)
 		MAP_DeactivateOverlay("radar");
@@ -2046,7 +2046,7 @@ void MAP_ResetAction (void)
 void MAP_SelectUFO (aircraft_t* ufo)
 {
 	MAP_ResetAction();
-	ccs.geoscape.selectedUFO = ufo;
+	MAP_SetSelectedUFO(ufo);
 }
 
 /**
@@ -2055,7 +2055,7 @@ void MAP_SelectUFO (aircraft_t* ufo)
 void MAP_SelectAircraft (aircraft_t* aircraft)
 {
 	MAP_ResetAction();
-	ccs.geoscape.selectedAircraft = aircraft;
+	MAP_SetSelectedAircraft(aircraft);
 }
 
 /**
@@ -2067,7 +2067,7 @@ void MAP_SelectMission (mission_t* mission)
 		return;
 	MAP_ResetAction();
 	ccs.mapAction = MA_INTERCEPT;
-	ccs.geoscape.selectedMission = mission;
+	MAP_SetSelectedMission(mission);
 }
 
 /**
@@ -2091,14 +2091,14 @@ void MAP_NotifyUFORemoved (const aircraft_t* ufo, qboolean destroyed)
 {
 	MAP_UpdateGeoscapeDock();
 
-	if (!ccs.geoscape.selectedUFO)
+	if (MAP_GetSelectedUFO() == NULL)
 		return;
 
 	/* Unselect the current selected ufo if its the same */
 	if (MAP_IsUFOSelected(ufo))
 		MAP_ResetAction();
 	else if (destroyed && ccs.geoscape.selectedUFO > ufo)
-		/** @todo still needed? */
+		/** @todo convert to linked list */
 		ccs.geoscape.selectedUFO--;
 }
 

src/client/campaign/cp_map.h
@@ -43,6 +43,18 @@ struct uiNode_s;
 #define MAP_IsUFOSelected(ufo) ((ufo) == ccs.geoscape.selectedUFO)
 #define MAP_IsMissionSelected(mission) ((mission) == ccs.geoscape.selectedMission)
 
+#define MAP_GetSelectedAircraft() (ccs.geoscape.selectedAircraft)
+#define MAP_GetInterceptorAircraft() (ccs.geoscape.interceptAircraft)
+#define MAP_GetSelectedUFO() (ccs.geoscape.selectedUFO)
+#define MAP_GetSelectedMission() (ccs.geoscape.selectedMission)
+#define MAP_GetMissionAircraft() (ccs.geoscape.missionAircraft)
+
+#define MAP_SetSelectedAircraft(aircraft) (ccs.geoscape.selectedAircraft = (aircraft))
+#define MAP_SetInterceptorAircraft(interceptor) (ccs.geoscape.interceptAircraft = (interceptor))
+#define MAP_SetSelectedUFO(ufo) (ccs.geoscape.selectedUFO = (ufo))
+#define MAP_SetSelectedMission(mission) (ccs.geoscape.selectedMission = (mission))
+#define MAP_SetMissionAircraft(aircraft) (ccs.geoscape.missionAircraft = (aircraft))
+
 nation_t* MAP_GetNation(const vec2_t pos);
 qboolean MAP_AllMapToScreen(const struct uiNode_s* node, const vec2_t pos, int *x, int *y, int *z);
 qboolean MAP_MapToScreen(const struct uiNode_s* node, const vec2_t pos, int *x, int *y);

src/client/campaign/cp_mission_triggers.c
@@ -94,7 +94,7 @@ static void CP_ChangeNationHappiness_f (void)
 	float change;
 	nation_t *nation;
 	const nationInfo_t *stats;
-	const mission_t *mission = ccs.geoscape.selectedMission;
+	const mission_t *mission = MAP_GetSelectedMission();
 
 	if (Cmd_Argc() < 2) {
 		Com_Printf("Usage: %s <absolute change value>\n", Cmd_Argv(0));

src/client/campaign/cp_missions.c
@@ -1120,7 +1120,7 @@ void CP_MissionEnd (const campaign_t *campaign, mission_t* mission, const battle
 		/* HACK */
 		aircraft = base->aircraftCurrent;
 	} else {
-		aircraft = ccs.geoscape.missionAircraft;
+		aircraft = MAP_GetMissionAircraft();
 		base = aircraft->homebase;
 	}
 
@@ -1288,7 +1288,7 @@ void CP_SpawnRescueMission (aircraft_t *aircraft, aircraft_t *ufo)
 	}
 
 	if (MAP_IsAircraftSelected(aircraft))
-		ccs.geoscape.selectedAircraft = NULL;
+		MAP_SetSelectedAircraft(NULL);
 
 	Vector2Copy(aircraft->pos, mission->pos);
 	mission->posAssigned = qtrue;

src/client/campaign/cp_popup.c
@@ -166,7 +166,7 @@ static void CL_PopupChangeHomebase_f (void)
 	int i;
 	base_t *base;
 	int baseIdx;
-	aircraft_t *aircraft = ccs.geoscape.selectedAircraft;
+	aircraft_t *aircraft = MAP_GetSelectedAircraft();
 
 	/* If popup is opened, that means an aircraft is selected */
 	if (!aircraft) {
@@ -528,7 +528,7 @@ static void CL_PopupInterceptBaseClick_f (void)
 	}
 
 	/* If popup is opened, that means that ufo is selected on geoscape */
-	if (!ccs.geoscape.selectedUFO)
+	if (MAP_GetSelectedUFO() == NULL)
 		return;
 
 	num = atoi(Cmd_Argv(1));
@@ -576,12 +576,12 @@ static void CL_PopupInterceptBaseClick_f (void)
 	assert(base || installation);
 	if (installation) {
 		for (i = 0; i < installation->installationTemplate->maxBatteries; i++)
-			installation->batteries[i].target = ccs.geoscape.selectedUFO;
+			installation->batteries[i].target = MAP_GetSelectedUFO();
 	} else {
 		for (i = 0; i < base->numBatteries; i++)
-			base->batteries[i].target = ccs.geoscape.selectedUFO;
+			base->batteries[i].target = MAP_GetSelectedUFO();
 		for (i = 0; i < base->numLasers; i++)
-			base->lasers[i].target = ccs.geoscape.selectedUFO;
+			base->lasers[i].target = MAP_GetSelectedUFO();
 	}
 
 	UI_PopWindow(qfalse);

commit d77855be4e050eb23cbc730769d585b486126b9b

* fixed inverse logic bug introduced in 4eb47e08f84d3bf9d2cc9790d5859c77466435a4

src/client/cl_game_campaign.c
@@ -52,12 +52,12 @@ static void GAME_CP_MissionAutoGo_f (void)
 		return;
 	}
 
-	if (MAP_GetMissionAircraft() != NULL) {
+	if (MAP_GetMissionAircraft() == NULL) {
 		Com_Printf("GAME_CP_MissionAutoGo_f: No aircraft at target pos\n");
 		return;
 	}
 
-	if (MAP_GetInterceptorAircraft() != NULL) {
+	if (MAP_GetInterceptorAircraft() == NULL) {
 		Com_Printf("GAME_CP_MissionAutoGo_f: No intercept aircraft given\n");
 		return;
 	}