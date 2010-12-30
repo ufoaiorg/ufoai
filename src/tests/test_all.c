36d6920573d199af4bcbb9a3aaa71e716fafd4dd
bayo <bayo.fr@gmail.com>
2010-12-30 02:10:50 +0100

    * allow to disable by default some test suites
    * manage a list of disabled test suite
    * add command --status to display disabled/enabled suites
    * add command --enable-SUITE --enable-all --disable-all

--- src/tests/test_all.c
+++ src/tests/test_all.c
@@ -83,7 +83,11 @@ static config_t config;
 
 static FILE *logFile;
 
-static void Test_List (void)
+static CU_pSuite disabledSuites;
+
+static qboolean displayStatus;
+
+static void Test_List (qboolean displayStatus)
 {
 	CU_pTestRegistry registry;
 	CU_pSuite suite;
@@ -92,19 +96,54 @@ static void Test_List (void)
 	registry = CU_get_registry();
 	suite = registry->pSuite;
 	while (suite) {
-		printf("* %s\n", suite->pName);
+		if (displayStatus)
+			printf("* %s [enabled]\n", suite->pName);
+		else
+			printf("* %s\n", suite->pName);
+		suite = suite->pNext;
+	}
+	suite = disabledSuites;
+	while (suite) {
+		if (displayStatus)
+			printf("* %s [disabled]\n", suite->pName);
+		else
+			printf("* %s\n", suite->pName);
 		suite = suite->pNext;
 	}
 }
 
-static void Test_RemovePSuite (CU_pSuite suite)
+static void Test_AddSuiteTo (CU_pSuite *list, CU_pSuite suite)
 {
+	assert(suite->pNext == NULL);
+	assert(suite->pPrev == NULL);
+	suite->pNext = *list;
+	if (*list == NULL)
+		*list = suite;
+	else
+		(*list)->pPrev = suite;
+}
+
+static void Test_RemoveSuiteFrom (CU_pSuite *list, CU_pSuite suite)
+{
+	if (*list == suite)
+		*list = suite->pNext;
 	if (suite->pNext)
 		suite->pNext->pPrev = suite->pPrev;
 	if (suite->pPrev)
 		suite->pPrev->pNext = suite->pNext;
-	else
-		CU_get_registry()->pSuite = suite->pNext;
+	suite->pPrev = NULL;
+	suite->pNext = NULL;
+}
+
+static CU_pSuite Test_GetSuiteByName (CU_pSuite first, const char *name)
+{
+	CU_pSuite current = first;
+	while (current) {
+		if (Q_streq(current->pName, name))
+			return current;
+		current = current->pNext;
+	}
+	return NULL;
 }
 
 static int Test_RemoveSuite (const char *name)
@@ -113,38 +152,54 @@ static int Test_RemoveSuite (const char *name)
 	CU_pSuite suite;
 
 	registry = CU_get_registry();
-	suite = registry->pSuite;
-	while (suite) {
-		if (Q_streq(suite->pName, name)) {
-			Test_RemovePSuite(suite);
-			return 0;
-		}
-		suite = suite->pNext;
+	suite = Test_GetSuiteByName(registry->pSuite, name);
+	if (suite) {
+		Test_RemoveSuiteFrom(&registry->pSuite, suite);
+		Test_AddSuiteTo(&disabledSuites, suite);
+		return 0;
 	}
 	return 1;
 }
 
-static int Test_RemoveAllSuitesExcept (const char *name)
+static int Test_AddSuite (const char *name)
 {
-	int found = 0;
+	CU_pTestRegistry registry;
+	CU_pSuite suite;
+
+	registry = CU_get_registry();
+	suite = Test_GetSuiteByName(disabledSuites, name);
+	if (suite) {
+		Test_RemoveSuiteFrom(&disabledSuites, suite);
+		Test_AddSuiteTo(&registry->pSuite, suite);
+		return 0;
+	}
+	return 1;
+}
+
+static void Test_RemoveAllSuites (void)
+{
 	CU_pTestRegistry registry;
 	CU_pSuite suite;
 
 	registry = CU_get_registry();
 	suite = registry->pSuite;
 	while (suite) {
-		if (Q_streq(suite->pName, name)) {
-			suite = suite->pNext;
-			found = 1;
-		} else {
-			CU_pSuite remove = suite;
-			suite = suite->pNext;
-			Test_RemovePSuite(remove);
-		}
+		CU_pSuite tmp = suite->pNext;
+		Test_RemoveSuite(suite->pName);
+		suite = tmp;
 	}
-	if (found)
-		return 0;
-	return 1;
+}
+
+static void Test_AddAllSuites (void)
+{
+	CU_pSuite suite;
+
+	suite = disabledSuites;
+	while (suite) {
+		CU_pSuite tmp = suite->pNext;
+		Test_AddSuite(suite->pName);
+		suite = tmp;
+	}
 }
 
 static void Test_Parameters (const int argc, const char **argv)
@@ -156,6 +211,8 @@ static void Test_Parameters (const int argc, const char **argv)
 			config.console = qtrue;
 		} else if (Q_streq(argv[i], "-a") || Q_streq(argv[i], "--automated")) {
 			config.automated = qtrue;
+		} else if (Q_streq(argv[i], "--disable-all")) {
+			Test_RemoveAllSuites();
 		} else if (Q_strstart(argv[i], "--disable-")) {
 			const char *name = argv[i] + 10;
 			if (Test_RemoveSuite(name) != 0) {
@@ -163,9 +220,19 @@ static void Test_Parameters (const int argc, const char **argv)
 				printf("Use \"%s -l\" to show the list of suites\n", argv[0]);
 				exit(2);
 			}
+		} else if (Q_streq(argv[i], "--enable-all")) {
+			Test_AddAllSuites();
+		} else if (Q_strstart(argv[i], "--enable-")) {
+			const char *name = argv[i] + 9;
+			if (Test_AddSuite(name) != 0) {
+				printf("Suite \"%s\" unknown\n", name);
+				printf("Use \"%s -l\" to show the list of suites\n", argv[0]);
+				exit(2);
+			}
 		} else if (Q_strstart(argv[i], "--only-")) {
 			const char *name = argv[i] + 7;
-			if (Test_RemoveAllSuitesExcept(name) != 0) {
+			Test_RemoveAllSuites();
+			if (Test_AddSuite(name) != 0) {
 				printf("Suite \"%s\" unknown\n", name);
 				printf("Use \"%s -l\" to show the list of suites\n", argv[0]);
 				exit(2);
@@ -173,21 +240,28 @@ static void Test_Parameters (const int argc, const char **argv)
 		} else if (Q_strstart(argv[i], "--output-prefix=")) {
 			resultPrefix = argv[i] + 16;
 		} else if (Q_streq(argv[i], "-l") || Q_streq(argv[i], "--list")) {
-			Test_List();
+			Test_List(qfalse);
 			exit(0);
 		} else if (Q_streq(argv[i], "--log")) {
 			config.log = qtrue;
+		} else if (Q_streq(argv[i], "-s") || Q_streq(argv[i], "--status")) {
+			displayStatus = qtrue;
 		} else if (Q_streq(argv[i], "-h") || Q_streq(argv[i], "--help")) {
 			printf("Usage:\n");
 			printf("-h  --help                 | show this help screen\n");
 			printf("-c  --console              | run tests in console mode\n");
 			printf("-a  --automated            | run tests in automated mode (create xml file)\n");
-			printf("-l  --list                 | list suite name available\n");
+			printf("-l  --list                 | list suite name available, and exit.\n");
+			printf("-s  --status               | list suite name with there status, and exit.\n");
 			printf("    --log                  | log ufo output to file (testall.log in working dir)\n");
 			printf("    --output-prefix=PREFIX | set a prefix for the xml result\n");
 			printf("                           | default value is \"ufoai\"\n");
+			printf("    --enable-SUITE         | Enable a suite by name\n");
+			printf("                           | SUITE=suite name\n");
+			printf("    --enable-all           | Enable all suites\n");
 			printf("    --disable-SUITE        | Disable a suite by name\n");
 			printf("                           | SUITE=suite name\n");
+			printf("    --disable-all          | Disable all suites\n");
 			printf("    --only-SUITE           | Enable only the suite given by name\n");
 			printf("                           | SUITE=suite name\n");
 			exit(0);
@@ -233,6 +307,11 @@ int main (int argc, const char **argv)
 	}
 
 	Test_Parameters(argc, argv);
+
+	if (displayStatus) {
+		Test_List(qtrue);
+		exit(0);
+	}
 
 	if (config.log) {
 		const char *path = "testall.log";
