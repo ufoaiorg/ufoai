5462ebd8af9ffa15c97510ec31e600f2473c62f4
mutonufoai <mutonufoai@users.sourceforge.net>
2011-01-05 22:45:52 +0100

    * change revision level, to force a map recompile * commit 5b5ff808d45e4af2abda0a9bce1b335954b2e106 * commit 862e1e3bbca5026e4bf4d7a8ea35b425c82912ac

--- src/tools/ufo2map/ufo2map.c
+++ src/tools/ufo2map/ufo2map.c
@@ -24,7 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #define VERSION "1.2.5"
-#define REVISION "1"
+#define REVISION "2"
 
 /* valid -nolighting parameters */
 #define	LIGHTING_NONE			1
