d9106da16f2009253be9998eb46ac7a98c8688f7
bayo <bayo.fr@gmail.com>
2011-01-14 23:40:58 +0100

    * fix warning

--- src/client/battlescape/cl_hud.c
+++ src/client/battlescape/cl_hud.c
@@ -35,6 +35,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../ui/ui_nodes.h"
 #include "../ui/ui_draw.h"
 #include "../ui/ui_render.h"
+#include "../ui/ui_tooltip.h"
 #include "../renderer/r_mesh_anim.h"
 #include "../renderer/r_draw.h"
 #include "../../common/grid.h"
