b05e85f06508f9c0a2033b50e01d09852cefc671
destructavator <destructavator@users.sourceforge.net>
2011-04-25 21:08:35 -0400

    * Auto Mission code: Lots of work, yet quite a bit more to come. * I tried to keep it expandable so that it will fit into future additions to the game and mods without too much headache. * CL_AutoMission_Go will still exist, but it will create an auto mission battle data object that it will fill and init from values passed to it, and then call functions to run, then get results from the object. * The auto mission battle struct will be easily adaptable to work with base defense missions, alien base assault missions, rescue missions (downed pilots) and others, even forces that don't involve the player at all (if desired). * NOTE:  With this in in partially completed form, I don't expect the branch to compile as it is -- not until I do some more work on it.

--- src/client/cgame/campaign/cp_auto_mission.c
+++ src/client/cgame/campaign/cp_auto_mission.c
@@ -25,4 +25,40 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "cp_auto_mission.h"
 
-/* new code goes here */
+/** @brief Clears, initializes, or resets a single auto mission, sets default values.
+ * @param[in,out] battle The battle that should be init to defaults */
+void CP_AutoBattle_Clear_Battle (autoMission_battle_t *battle) {
+	int x = 0;
+	int y = 0;
+	for(x = 0; x < 8; x++) {
+		battle->teamID[x] = -1;
+		battle->teamActive[x] = false;
+		battle->nUnits[x] = 0;
+		battle->amScore_team_difficulty[x] = 0.5f;
+		battle->amScore_team_equipment[x] = 0.5f;
+		battle->amScore_team_skill[x] = 0.5f;
+		battle->amScore_team_special_attribute_A[x] = 0.5f;
+		battle->amScore_team_special_attribute_B[x] = 0.5f;
+		battle->amScore_team_special_attribute_C[x] = 0.5f;
+		battle->amScore_team_special_attribute_D[x] = 0.5f;
+
+		for(y = 0; y < 8; y++) {
+			battle->isHostile[x][y] = true; /* If you forget to set this and run a battle, then f*** it, everyone will just kill each other by default! */
+		}
+
+		for(y = 0; y < 64; y++) {
+			battle->unit_health[x][y] = 0;
+			battle->unit_health_max[x][y] = 0;
+		}
+	}
+	battle->am_use_special_attribute_factor[0] = false;
+	battle->am_use_special_attribute_factor[1] = false;
+	battle->am_use_special_attribute_factor[2] = false;
+	battle->am_use_special_attribute_factor[3] = false;
+
+	battle->winning_team = -1;
+	battle->is_rescue_mission = false;
+	battle->team_to_rescue = -1;
+	battle->team_needing_rescue = -1;
+	battle->has_been_fought = false;
+}
--- src/client/cgame/campaign/cp_auto_mission.h
+++ src/client/cgame/campaign/cp_auto_mission.h
@@ -30,4 +30,65 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "cp_campaign.h"
 #include "../../src/shared/mathlib_extra.h"
 
+/** @brief Possilbe results from an auto mission to display on-screen after a simulated battle.
+ * @note This list of potential results is NOT meant to replace missionResults_t, but is mainly used for displaying the proper message on the screen afterwards. */
+typedef enum autoMission_results_s {
+	AUTOMISSION_RESULT_SUCCESS,					/**< Victory! Aliens dead or otherwise neutralized */
+	AUTOMISSION_RESULT_COSTLY_SUCCESS,			/**< Aliens dead, but so are most or all civilians. */
+	AUTOMISSION_RESULT_FAILED_SOME_SURVIVORS,	/**< Defeat, but some soldiers managed to retreat and escape */
+	AUTOMISSION_RESULT_FAILED_NO_SURVIVORS,		/**< Defeat, all soldiers/units dead, total loss */
+	AUTOMISSION_RESULT_RESCUE_SUCCESSFUL,		/**< Special case for downed pilot rescue missions */
+	AUTOMISSION_RESULT_RESCUE_FAILED,			/**< Soldiers survived and killed enemies, but pilot to be rescued did not survive */
+	AUTOMISSION_RESULT_PLAYER_BASE_LOST,		/**< Base defence mission where the player's forces lost the battle */
+	AUTOMISSION_RESULT_ALIEN_BASE_CAPTURED,		/**< An assault on an alien base was successful */
+
+	AUTOMISSION_RESULT_MAX
+} autoMission_results_t;
+
+/* FIX ME: The hard-coded max teams of 8 and units on a team (64) should be changed to #define(ed) values set elsewhere! */
+/** @brief Data structure for a simulated or auto mission.
+ * @note Supports a hardcoded max of 8 teams that can be simulated in a battle, to
+ * include any aliens, player soldiers, downed pilots, civilians, and any other forces.
+ * The player's forces don't have to be any of the teams.  This is useful if a special
+ * battle should be simulated for the plot, or if more than one alien threat is in the
+ * game (not in the primary campaign plot though, but good for a MOD or whatever).
+ * "Special Attribute" modifiers can be used for psi-abilities, cyber-implant stuff,
+ * magic (only in a mod, thank you), or any other un-implemented future abilities of
+ * a team.  If a team doesn't have such abilites, set it to 0.5f.
+ * ALSO: A team does not have to attack (isHostile toward) another team that attacks it.
+ * Teams that are isHostile toward no one will wander around like sheep, doing nothing else. */
+typedef struct autoMission_battle_s {
+	bool teamActive[8];						/**< Which teams exist in a battle, supports hardcoded MAX of 8 teams */
+	int teamID[8];								/**< An ID for each team, to keep track, useful if needed.  Note: The same ID may be repeated, but two teams of same ID can't be hostile */
+	bool isHostile[8][8];						/**< Friendly or hostile?  Params are [Each team] [Each other team] */
+	short nUnits[8];							/**< Number of units (soldiers, aliens, UGVs, whatever) on each team, hardcoded MAX of 64 per team */
+
+	float amScore_team_equipment[8];			/**< Number from 0.f to 1.f, represents how good a team's equipment is (higher is better) */
+	float amScore_team_skill[8];				/**< Number from 0.f to 1.f, represents how good a team's abilities are (higher is better) */
+	float amScore_team_difficulty[8];			/**< Number from 0.f to 1.f, represents a team's global fighting ability, difficulty, or misc. adjustments (higher is better) */
+	float amScore_team_special_attribute_A[8];	/**< UNUSED Special combat modifier, RESERVED FOR FUTURE USE */
+	float amScore_team_special_attribute_B[8];	/**< UNUSED Special combat modifier, RESERVED FOR FUTURE USE */
+	float amScore_team_special_attribute_C[8];	/**< UNUSED Special combat modifier, RESERVED FOR FUTURE USE */
+	float amScore_team_special_attribute_D[8];	/**< UNUSED Special combat modifier, RESERVED FOR FUTURE USE */
+
+	bool am_use_special_attribute_factor[4];	/**< These decide if special team attributes should be used in calculations (TURE) or ignored and not even factored (FALSE) */
+
+	int unit_health[8][64];						/**< Health score of each unit for each team */
+	int unit_health_max[8][64];					/**< Max health of each unit on each team */
+
+	short winning_team;						/**< Which team is victorious */
+	bool has_been_fought;						/**< Did this battle run already?  Auto Battles can be fought only once, please. */
+	bool is_rescue_mission;						/**< Is this a rescue or special mission? (Such as recovering a downed aircraft pilot) */
+	short team_to_rescue;						/**< If a rescue mission, which team needs rescue? */
+	short team_needing_rescue;					/**< If a rescue mission, which team is attempting the rescue? */
+} autoMission_battle_t;
+
+/* Functions for auto missions */
+void CP_AutoBattle_Clear_Battle(autoMission_battle_t *battle);
+void CP_AutoBattle_Run_Battle(autoMission_battle_t *battle);
+void CP_AutoBattle_Fill_Team_From_Aircraft(autoMission_battle_t *battle, const short teamNum, const aircraft_t *aircraft);
+void CP_AutoBattle_Fill_Team_From_AlienGroup(autoMission_battle_t *battle, const short teamNum, const alienTeamGroup_t *alienGroup);
+void CP_AutoBattle_Create_Team_From_Scratch(autoMission_battle_t *battle, const short teamNum);
+short CP_AutoBattle_Get_Winning_Team(const autoMission_battle_t *battle);
+
 #endif
