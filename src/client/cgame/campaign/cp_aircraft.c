ab5825d06dbfce852da37eeacd09943fbc1775e9
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-05-07 18:08:02 +0200

    * don't store aircraft template data in cl_genericPool, but only in cp_campaignPool
    * this is campaign only data and should not be in the generic client pool
    * the aircraft templates are no longer available once we shut the campaign down, so the memory stays allocated
      until we shut the game down

--- src/client/cgame/campaign/cp_aircraft.c
+++ src/client/cgame/campaign/cp_aircraft.c
@@ -1451,9 +1451,6 @@ static const value_t aircraft_vals[] = {
  * @sa CL_ParseClientData
  * @sa CL_ParseScriptSecond
  * @note parses the aircraft into our aircraft_sample array to use as reference
- * @note This parsing function writes into two different memory pools
- * one is the cp_campaignPool which is cleared on every new game, the other is
- * cl_genericPool which is existent until you close the game
  */
 void AIR_ParseAircraft (const char *name, const char **text, qboolean assignAircraftItems)
 {
@@ -1491,7 +1488,7 @@ void AIR_ParseAircraft (const char *name, const char **text, qboolean assignAirc
 
 		Com_DPrintf(DEBUG_CLIENT, "...found aircraft %s\n", name);
 		aircraftTemplate->tpl = aircraftTemplate;
-		aircraftTemplate->id = Mem_PoolStrDup(name, cl_genericPool, 0);
+		aircraftTemplate->id = Mem_PoolStrDup(name, cp_campaignPool, 0);
 		aircraftTemplate->status = AIR_HOME;
 		/* default is no ufo */
 		aircraftTemplate->ufotype = UFO_MAX;
@@ -1674,7 +1671,7 @@ void AIR_ParseAircraft (const char *name, const char **text, qboolean assignAirc
 					case V_TRANSLATION_STRING:
 						token++;
 					case V_CLIENT_HUNK_STRING:
-						Mem_PoolStrDupTo(token, (char**) ((char*)aircraftTemplate + (int)vp->ofs), cl_genericPool, 0);
+						Mem_PoolStrDupTo(token, (char**) ((char*)aircraftTemplate + (int)vp->ofs), cp_campaignPool, 0);
 						break;
 					default:
 						Com_EParseValue(aircraftTemplate, token, vp->type, vp->ofs, vp->size);
@@ -1736,7 +1733,7 @@ void AIR_ParseAircraft (const char *name, const char **text, qboolean assignAirc
 								case V_TRANSLATION_STRING:
 									token++;
 								case V_CLIENT_HUNK_STRING:
-									Mem_PoolStrDupTo(token, (char**) ((char*)aircraftTemplate + (int)vp->ofs), cl_genericPool, 0);
+									Mem_PoolStrDupTo(token, (char**) ((char*)aircraftTemplate + (int)vp->ofs), cp_campaignPool, 0);
 									break;
 								default:
 									Com_EParseValue(aircraftTemplate, token, vp->type, vp->ofs, vp->size);
