78c4a11f0afd80de448b36da75889d5f1a927687
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-05-13 15:41:48 +0200

    * revert 'Also save the cvar settings for cl_campitchmin, cl_campitchmax, cl_camzoommin & cl_camzoommax (Battlescape camera settings) in the CVAR_ARCHIVE'
    * these cvars are no CVAR_ARCHIVE for a reason - use a autoexec config file if you want them to get restored
    
    This reverts commit 2babe40cf81c546584549378c1c0b136c256bd07.

--- src/client/battlescape/cl_camera.c
+++ src/client/battlescape/cl_camera.c
@@ -358,11 +358,11 @@ void CL_CameraInit (void)
 	cl_camrotspeed = Cvar_Get("cl_camrotspeed", "250", CVAR_ARCHIVE, NULL);
 	cl_cammovespeed = Cvar_Get("cl_cammovespeed", "750", CVAR_ARCHIVE, NULL);
 	cl_cammoveaccel = Cvar_Get("cl_cammoveaccel", "1250", CVAR_ARCHIVE, NULL);
-	cl_campitchmax = Cvar_Get("cl_campitchmax", "89", CVAR_ARCHIVE, "Max camera pitch - over 90 presents apparent mouse inversion");
-	cl_campitchmin = Cvar_Get("cl_campitchmin", "10", CVAR_ARCHIVE, "Min camera pitch - under 35 presents difficulty positioning cursor");
+	cl_campitchmax = Cvar_Get("cl_campitchmax", "89", 0, "Max camera pitch - over 90 presents apparent mouse inversion");
+	cl_campitchmin = Cvar_Get("cl_campitchmin", "10", 0, "Min camera pitch - under 35 presents difficulty positioning cursor");
 	cl_camzoomquant = Cvar_Get("cl_camzoomquant", "0.16", CVAR_ARCHIVE, NULL);
-	cl_camzoommin = Cvar_Get("cl_camzoommin", "0.3", CVAR_ARCHIVE, "Minimum zoom value for tactical missions");
-	cl_camzoommax = Cvar_Get("cl_camzoommax", "3.4", CVAR_ARCHIVE, "Maximum zoom value for tactical missions");
+	cl_camzoommin = Cvar_Get("cl_camzoommin", "0.3", 0, "Minimum zoom value for tactical missions");
+	cl_camzoommax = Cvar_Get("cl_camzoommax", "3.4", 0, "Maximum zoom value for tactical missions");
 	cl_centerview = Cvar_Get("cl_centerview", "1", CVAR_ARCHIVE, "Center the view when selecting a new soldier");
 
 #ifdef DEBUG
