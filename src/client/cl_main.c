fdb140930a42d6a8389927cafbec2b8da612c035
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-05-28 09:46:53 +0200

    * fixed bug #3307646 (Wrong Language Settings in Multiplayer Screen)

--- src/client/cl_main.c
+++ src/client/cl_main.c
@@ -662,7 +662,7 @@ static void CL_TeamDefInitMenu (void)
 		for (i = 0; i < csi.numTeamDefs; i++) {
 			const teamDef_t *td = &csi.teamDef[i];
 			if (td->race != RACE_CIVILIAN)
-				UI_AddOption(&option, td->id, _(td->name), td->id);
+				UI_AddOption(&option, td->id, va("_%s", td->name), td->id);
 		}
 		UI_RegisterOption(OPTION_TEAMDEFS, option);
 	}
