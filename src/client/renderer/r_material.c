0c8346bf5ee72c1594ed8d9e32e8db6fd3671b0f
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-06-05 17:04:42 +0200

    * converted comment into doxygen

--- src/client/renderer/r_material.c
+++ src/client/renderer/r_material.c
@@ -1095,7 +1095,9 @@ void R_LoadMaterials (const char *map)
 	R_CreateMaterialData();
 }
 
-/* assumes that change listener is called after cvar gets a new vale */
+/**
+ * @brief Change listener callback for material value cvars
+ */
 void R_UpdateDefaultMaterial (const char *cvarName, const char *oldValue, const char *newValue)
 {
 	defaultMaterial.specular = r_default_specular->value;
