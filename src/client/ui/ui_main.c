a75587d73df9b16f02dabfa7635eb0233ea84292
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-06-14 21:15:43 +0200

    * removed client.h inclusion from ui_main.c

--- src/client/ui/ui_main.c
+++ src/client/ui/ui_main.c
@@ -30,8 +30,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "ui_sound.h"
 #include "node/ui_node_abstractnode.h"
 
-#include "../client.h"
-
 uiGlobal_t ui_global;
 
 struct memPool_s *ui_dynStringPool;
