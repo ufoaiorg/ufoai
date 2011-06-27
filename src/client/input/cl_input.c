0348d6717491d066865966d96392c2e5f79c254c
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-06-27 20:56:00 +0200

    * added fall through comment

--- src/client/input/cl_input.c
+++ src/client/input/cl_input.c
@@ -808,6 +808,7 @@ void IN_Frame (void)
 				mouse_buttonstate = K_MOUSE3;
 				break;
 			}
+			/* fall through */
 		case SDL_MOUSEBUTTONUP:
 			switch (event.button.button) {
 			case SDL_BUTTON_LEFT:
