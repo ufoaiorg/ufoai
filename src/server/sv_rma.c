ufoaiorg/ufoai

commit 64aa4c3a28edfbf1578f2c1a5e156a7405befd59

* simplied code in SV_GapCheckNeighbour

src/server/sv_rma.c
@@ -1342,10 +1342,7 @@ static qboolean SV_GapCheckNeighbour (mapInfo_t *map, int tc1, int mapW, int nx,
 			break;
 		}
 	}
-	if (!found) {
-		return qtrue; /* eliminate tile */
-	}
-	return qfalse;
+	return found;
 }
 
 /**

commit dcffa4c1a59a5a68d7cb464cc9c4efa4016522e3

* fixed bug in SV_GapCheckNeighbour introduced in 64aa4c3a28edfbf1578f2c1a5e156a7405befd59

src/server/sv_rma.c
@@ -1317,7 +1317,6 @@ static qboolean SV_GapListBuild (mapInfo_t *map, int tilePosListCnt)
  */
 static qboolean SV_GapCheckNeighbour (mapInfo_t *map, int tc1, int mapW, int nx, int ny)
 {
-	qboolean found = qfalse;
 	if (nx < 1)
 		return qfalse;				/* map border */
 	if (ny < 1)
@@ -1338,11 +1337,10 @@ static qboolean SV_GapCheckNeighbour (mapInfo_t *map, int tc1, int mapW, int nx,
 		const unsigned long flags2 = SV_GapGetFlagsAtAbsPos(map, tc2, mapW, nx, ny);
 
 		if (flags1 & flags2) {
-			found = qtrue;			/* found at least one tile that would work */
-			break;
+			return qfalse;			/* found at least one tile that would work */
 		}
 	}
-	return found;
+	return qfalse;
 }
 
 /**