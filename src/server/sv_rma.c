ufoaiorg/ufoai

commit 0d4947ffa6e37756b4d00be4cff01cd2dd508df6

* make the new rma algorithm available via cvar

src/server/sv_rma.c
@@ -929,7 +929,7 @@ static qboolean SV_PickRandomTile (mapInfo_t *map, int* idx, int* pos)
  * @sa SV_FitTile
  * @sa SV_AddTile
  */
-static qboolean SV_AddMissingTiles (mapInfo_t *map)
+static qboolean SV_AddMissingTilesOld (mapInfo_t *map)
 {
 	int i;
 	int idx[CHECK_ALTERNATIVES_COUNT];
@@ -1021,9 +1021,6 @@ static void SV_PrintMapStrings (mapInfo_t *map, char *asmMap, char *asmPos)
 }
 
 #define PRINT_RMA_PROGRESS 0	/* print some debugging info */
-/* RMA2 code. Still disabled to avoid warnings */
-#define RMA2 0
-#if RMA2
 #define RMA2_MAX_REC 50		/* max # of recursions */
 #define TCM 50				/* tile code multiplier */
 #define GAPS 25				/* the # of different tiles we can store for a gap */
@@ -1518,7 +1515,12 @@ static qboolean SV_AddMissingTiles3 (mapInfo_t *map)
 	return SV_AddMissingTiles3_r(map, 0, n, posTileList[0], NULL, 0, 0);
 }
 
-#endif
+static qboolean SV_AddMissingTiles (mapInfo_t *map)
+{
+	if (sv_rma->integer == 2)
+		return !SV_AddMissingTiles3(map);
+	return !SV_AddMissingTilesOld(map);
+}
 
 /**
  * @brief Tries to build the map
@@ -1626,11 +1628,7 @@ static qboolean SV_AddMapTiles (mapInfo_t *map)
 			pos++;
 		}
 
-#if RMA2
-		if (idx == numToPlace && !SV_AddMissingTiles3(map)) {
-#else
 		if (idx == numToPlace && !SV_AddMissingTiles(map)) {
-#endif
 			SV_RemoveTile(map, &idx, &pos);
 			pos++;
 		}

src/server/server.h
@@ -168,6 +168,7 @@ extern serverInstanceStatic_t svs;		/**< persistant server instance info */
 extern serverInstanceGame_t * sv;			/**< server data per game/map */
 
 extern cvar_t *sv_mapname;
+extern cvar_t *sv_rma;
 extern cvar_t *sv_public;			/**< should heartbeats be sent? (only for public servers) */
 extern cvar_t *sv_dumpmapassembly;
 extern cvar_t *sv_threads;	/**< run the game lib threaded */

src/server/sv_main.c
@@ -43,6 +43,7 @@ static cvar_t *sv_reconnect_limit;
 cvar_t *sv_maxclients = NULL;
 cvar_t *sv_dumpmapassembly;
 cvar_t *sv_threads;
+cvar_t *sv_rma;
 /** should heartbeats be sent */
 cvar_t *sv_public;
 cvar_t *sv_mapname;
@@ -759,6 +760,7 @@ void SV_Init (void)
 	sv_dumpmapassembly = Cvar_Get("sv_dumpmapassembly", "0", CVAR_ARCHIVE, "Dump map assembly information to game console");
 
 	sv_threads = Cvar_Get("sv_threads", "1", CVAR_LATCH | CVAR_ARCHIVE, "Run the server threaded");
+	sv_rma = Cvar_Get("sv_rma", "1", CVAR_LATCH | CVAR_ARCHIVE, "1 = old algorithm, 2 = new algorithm");
 	sv_public = Cvar_Get("sv_public", "1", 0, "Should heartbeats be send to the masterserver");
 	sv_reconnect_limit = Cvar_Get("sv_reconnect_limit", "3", CVAR_ARCHIVE, "Minimum seconds between connect messages");
 

src/tests/test_rma.c
@@ -29,6 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 cvar_t *sv_dumpmapassembly;
 cvar_t *sv_threads;
+cvar_t *sv_rma;
 
 /**
  * The suite initialization function.
@@ -40,6 +41,7 @@ static int UFO_InitSuiteRandomMapAssembly (void)
 	Com_ParseScripts(qtrue);
 
 	sv_dumpmapassembly = Cvar_Get("sv_dumpassembly", "0", 0, NULL);
+	sv_rma = Cvar_Get("sv_rma_tmp", "1", 0, NULL);
 
 	return 0;
 }

commit 930dd444d96322be8abba938292a2c800d07c573

* fixed bug in SV_AddMissingTiles introduced in 0d4947ffa6e37756b4d00be4cff01cd2dd508df6

src/server/sv_rma.c
@@ -1515,11 +1515,16 @@ static qboolean SV_AddMissingTiles3 (mapInfo_t *map)
 	return SV_AddMissingTiles3_r(map, 0, n, posTileList[0], NULL, 0, 0);
 }
 
+/**
+ * @brief Tries to fill the missing tiles of the current map.
+ * @param map All we know about the map to assemble
+ * @return @c false if the tiles does not fit, @c true if the map could be filled.
+ */
 static qboolean SV_AddMissingTiles (mapInfo_t *map)
 {
 	if (sv_rma->integer == 2)
-		return !SV_AddMissingTiles3(map);
-	return !SV_AddMissingTilesOld(map);
+		return SV_AddMissingTiles3(map);
+	return SV_AddMissingTilesOld(map);
 }
 
 /**