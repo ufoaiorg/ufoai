f5706e77af8ae7a5f6164809329f465d8d100759
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-08-17 07:33:24 +0200

    * removed alienb2 mapdef

--- src/tests/test_rma.c
+++ src/tests/test_rma.c
@@ -202,7 +202,7 @@ static void testNewSeedlists (void)
 		srand(i);
 		time = Sys_Milliseconds();
 		Com_Printf("Seed: %i\n", i);
-		randomMap = SV_AssembleMap("alienbase", "alienbase1", mapStr, posStr, i);
+		randomMap = SV_AssembleMap("alienbase", "small1", mapStr, posStr, i);
 #if 0
 		randomMap = SV_AssembleMap("japan", "large", mapStr, posStr, i);
 		randomMap = SV_AssembleMap("hills", "desert_harvester", mapStr, posStr, i);
