4407d7eb2c1c2f1d5c86921e480f36ce29803ae6
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-09-03 16:16:33 +0200

    * coding guidelines
    * extract to local variable to shorten the line
    * whitespaces
    * r_debug_lights->modified = qfalse is not needed, as this is no context cvar

--- src/client/renderer/r_main.c
+++ src/client/renderer/r_main.c
@@ -347,8 +347,10 @@ void R_RenderFrame (void)
 		R_EnableFog(qfalse);
 
 		if (r_debug_lights->integer)
-			for (i = 0; i< r_state.numStaticLights; i++)
-				R_AddCorona(r_state.staticLights[i].loc, 1.0/sqrt(r_state.staticLights[i].quadraticAttenuation/255), r_state.staticLights[i].diffuseColor);
+			for (i = 0; i < r_state.numStaticLights; i++) {
+				const float radius = 1.0 / sqrt(r_state.staticLights[i].quadraticAttenuation / 255);
+				R_AddCorona(r_state.staticLights[i].loc, radius, r_state.staticLights[i].diffuseColor);
+			}
 
 		R_DrawCoronas();
 		R_EnableBlend(qfalse);
@@ -553,7 +555,6 @@ static void R_RegisterSystemVars (void)
 	r_debug_tangents = Cvar_Get("r_debug_tangents", "0", CVAR_R_PROGRAMS, "Draw tangent, bitangent, and normal dotted with light dir as RGB espectively");
 	r_debug_tangents->modified = qfalse;
 	r_debug_lights = Cvar_Get("r_debug_lights", "0", CVAR_ARCHIVE, "Draw active light sources");
-	r_debug_lights->modified = qfalse;
 	r_ext_texture_compression = Cvar_Get("r_ext_texture_compression", "0", CVAR_ARCHIVE | CVAR_R_CONTEXT, NULL);
 	r_ext_nonpoweroftwo = Cvar_Get("r_ext_nonpoweroftwo", "1", CVAR_ARCHIVE, "Enable or disable the non power of two extension");
 	r_ext_s3tc_compression = Cvar_Get("r_ext_s3tc_compression", "1", CVAR_ARCHIVE, "Also see r_ext_texture_compression");
