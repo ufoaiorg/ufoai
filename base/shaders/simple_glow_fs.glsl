3e9a91c7e9cd9520f1fe59bc5b5a6fb101e42b5f
Alexander Tishin <alextishin@users.sourceforge.net>
2011-10-01 16:18:23 +0400

    * Do not override diffuse alpha
    * Fixes bug 3416218 (Animated textures with glowmaplink appear black)

--- base/shaders/simple_glow_fs.glsl
+++ base/shaders/simple_glow_fs.glsl
@@ -21,12 +21,11 @@ uniform sampler2D SAMPLER1;
 uniform float GLOWSCALE;
 
 void main(void) {
-	vec4 color;
-	color.rgba = texture2D(SAMPLER0, gl_TexCoord[0].st);
-	gl_FragData[0].rgb = color.rgb * color.a;
-	gl_FragData[0].a = 1.0;
+	vec4 glow;
 
-	color.rgba = texture2D(SAMPLER1, gl_TexCoord[0].st);
-	gl_FragData[1].rgb = color.rgb * color.a * GLOWSCALE;
+	gl_FragData[0] = texture2D(SAMPLER0, gl_TexCoord[0].st);
+
+	glow = texture2D(SAMPLER1, gl_TexCoord[0].st);
+	gl_FragData[1].rgb = glow.rgb * glow.a * GLOWSCALE;
 	gl_FragData[1].a = 1.0;
 }
