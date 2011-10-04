a1408df01b7a3b5f0f20233f1388fd4078e4ebf8
Martin Gerhardy <tlh2000@users.sourceforge.net>
2011-10-04 14:37:33 +0200

    * added inventory shared function INVSH_SearchInInventoryByItem

--- src/game/inv_shared.c
+++ src/game/inv_shared.c
@@ -451,6 +451,28 @@ invList_t *INVSH_SearchInInventory (const inventory_t* const i, const invDef_t *
 }
 
 /**
+ * @brief Searches if there is an item of the given type in a container.
+ * @param[in] i Pointer to the inventory where we will search.
+ * @param[in] container Container in the inventory.
+ * @return invList_t Pointer to the invList_t/item.
+ */
+invList_t *INVSH_SearchInInventoryByItem (const inventory_t* const i, const invDef_t * container, const objDef_t *item)
+{
+	invList_t *ic;
+
+	if (item == NULL)
+		return NULL;
+
+	for (ic = i->c[container->id]; ic; ic = ic->next) {
+		if (ic && item == ic->item.t)
+			return ic;
+	}
+
+	/* No matching item found. */
+	return NULL;
+}
+
+/**
  * @brief Finds space for item in inv at container
  * @param[in] inv The inventory to search space in
  * @param[in] item The item to check the space for
