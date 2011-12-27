fe0b1b3a982c8c9986abf4a26294bd7f3edb1d08
Christoph Mallon <dertron@users.sourceforge.net>
2011-12-27 10:02:16 +0100

    Turn a long list of ifs into a table.

--- src/tests/test_mapdef.c
+++ src/tests/test_mapdef.c
@@ -128,42 +128,50 @@ static void testMapDefsMassRMA (void)
 						time = Sys_Milliseconds();
 						Com_Printf("Seed: %i\n", i);
 
-						/* we have a known problem with these combinations, so skip it */
-						/* seed 20 is slow on linux (10 mins) */
-						if (i == 20 && Q_streq(p, "forest") && Q_streq(md->param, "large") && Q_streq(craftIter->data, "craft_drop_raptor"))
-							continue;
-						if (i == 27 && Q_streq(p, "forest") && Q_streq(md->param, "large") && Q_streq(craftIter->data, "craft_drop_raptor"))
-							continue;
-						if (i == 3 && Q_streq(p, "forest") && Q_streq(md->param, "large") && Q_streq(craftIter->data, "craft_drop_raptor") && Q_streq(ufoIter->data, "craft_ufo_harvester"))
-							continue;
-						/* combinations slow on linux */
-						if (i == 12 && Q_streq(p, "forest") && Q_streq(md->param, "large") && Q_streq(craftIter->data, "craft_drop_firebird") && Q_streq(ufoIter->data, "craft_ufo_harvester"))
-							continue;
-						if (i == 16 && Q_streq(p, "forest") && Q_streq(md->param, "large") && Q_streq(craftIter->data, "craft_drop_raptor") && Q_streq(ufoIter->data, "craft_ufo_harvester"))
-							continue;
-						if (i == 12 && Q_streq(p, "forest") && Q_streq(md->param, "large") && Q_streq(craftIter->data, "craft_drop_herakles") && Q_streq(ufoIter->data, "craft_ufo_harvester"))
-							continue;
-					/*	if (i == 34 && Q_streq(p, "village") && Q_streq(md->param, "commercial") && Q_streq(craftIter->data, "craft_drop_raptor"))
-							continue; */
-						if (Q_streq(p, "village") && Q_streq(md->param, "commercial") && Q_streq(craftIter->data, "craft_drop_firebird") && Q_streq(ufoIter->data, "craft_ufo_scout"))
-							continue;
-						if (Q_streq(p, "village") && Q_streq(md->param, "commercial") && Q_streq(craftIter->data, "craft_drop_firebird") && Q_streq(ufoIter->data, "craft_ufo_fighter"))
-							continue;
-						/* at least 18, 21 and 26 don't work on windows */
-						if (Q_streq(p, "village") && Q_streq(md->param, "commercial") && Q_streq(craftIter->data, "craft_drop_raptor") && Q_streq(ufoIter->data, "craft_ufo_scout"))
-							continue;
-						/* at least 8, 10 and 18 don't work on windows */
-						if (Q_streq(p, "village") && Q_streq(md->param, "commercial") && Q_streq(craftIter->data, "craft_drop_raptor") && Q_streq(ufoIter->data, "craft_ufo_fighter"))
-							continue;
-						/* at least 10, 21 and 18 don't work on windows */
-						if (Q_streq(p, "village") && Q_streq(md->param, "commercial") && Q_streq(craftIter->data, "craft_drop_herakles") && Q_streq(ufoIter->data, "craft_ufo_scout"))
-							continue;
-						/* at least 13, 18 and 21 don't work on windows */
-						if (Q_streq(p, "village") && Q_streq(md->param, "commercial") && Q_streq(craftIter->data, "craft_drop_herakles") && Q_streq(ufoIter->data, "craft_ufo_fighter"))
-							continue;
-						if (i == 18 && Q_streq(p, "village") && Q_streq(md->param, "commercial") && Q_streq(craftIter->data, "craft_drop_herakles") && Q_streq(ufoIter->data, "craft_ufo_fighter"))
-							continue;
-						if (Q_streq(p, "ufocrash"))
+						typedef struct skip_info {
+							int         seed;
+							char const* map;
+							char const* param;
+							char const* craft;
+							char const* ufo;
+						} skip_info;
+
+						/* we have a known problems with these combinations, so skip them */
+						skip_info const skip_list[] = {
+							/* seed 20 is slow on linux (10 mins) */
+							{ 20, "forest",   "large",      "craft_drop_raptor",   0                     },
+							{ 27, "forest",   "large",      "craft_drop_raptor",   0                     },
+							{  3, "forest",   "large",      "craft_drop_raptor",   "craft_ufo_harvester" },
+							/* combinations slow on linux */
+							{ 12, "forest",   "large",      "craft_drop_firebird", "craft_ufo_harvester" },
+							{ 16, "forest",   "large",      "craft_drop_raptor",   "craft_ufo_harvester" },
+							{ 12, "forest",   "large",      "craft_drop_herakles", "craft_ufo_harvester" },
+							//{ 34, "village",  "commercial", "craft_drop_raptor",   0                     },
+							{ -1, "village",  "commercial", "craft_drop_firebird", "craft_ufo_scout"     },
+							{ -1, "village",  "commercial", "craft_drop_firebird", "craft_ufo_fighter"   },
+							/* at least 18, 21 and 26 don't work on windows */
+							{ -1, "village",  "commercial", "craft_drop_raptor",   "craft_ufo_scout"     },
+							/* at least 8, 10 and 18 don't work on windows */
+							{ -1, "village",  "commercial", "craft_drop_raptor",   "craft_ufo_fighter"   },
+							/* at least 10, 21 and 18 don't work on windows */
+							{ -1, "village",  "commercial", "craft_drop_herakles", "craft_ufo_scout"     },
+							/* at least 13, 18 and 21 don't work on windows */
+							{ -1, "village",  "commercial", "craft_drop_herakles", "craft_ufo_fighter"   },
+							{ 18, "village",  "commercial", "craft_drop_herakles", "craft_ufo_fighter"   },
+							{ -1, "ufocrash", 0,            0,                     0                     },
+						};
+
+						qboolean skip = qfalse;
+						for (skip_info const* e = skip_list; e != endof(skip_list); ++e) {
+							if (e->seed >= 0 && i != e->seed)                        continue;
+							if (e->map       && !Q_streq(p,               e->map))   continue;
+							if (e->param     && !Q_streq(md->param,       e->param)) continue;
+							if (e->craft     && !Q_streq(craftIter->data, e->craft)) continue;
+							if (e->ufo       && !Q_streq(ufoIter->data,   e->ufo))   continue;
+							skip = qtrue;
+							break;
+						}
+						if (skip)
 							continue;
 
 						randomMap = SV_AssembleMap(p, md->param, mapStr, posStr, i);
