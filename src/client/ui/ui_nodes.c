f8d771ff2e4883d6d8d3ced56d5cb820eacc94a9
bayo <bayo.fr@gmail.com>
2012-01-02 19:06:20 +0100

    * clean up initial behaviour memory

--- src/client/ui/ui_nodes.c
+++ src/client/ui/ui_nodes.c
@@ -633,6 +633,7 @@ void UI_InitNodes (void)
 
 	/* compute list of node behaviours */
 	for (i = 0; i < NUMBER_OF_BEHAVIOURS; i++) {
+		memset(current, 0, sizeof(*current));
 		current->registration = qtrue;
 		registerFunctions[i](current);
 		current->registration = qfalse;
