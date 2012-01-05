622a07315590d5c10f1f9863ca1020e7a35cfb70
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-01-05 15:36:48 +0100

    * cl_map_draw_rescue_zone must no longer be an archive cvar
    * it's modified by the huds anyway

--- src/client/battlescape/cl_view.c
+++ src/client/battlescape/cl_view.c
@@ -330,5 +330,5 @@ void CL_ViewInit (void)
 {
 	cl_precache = Cvar_Get("cl_precache", "1", CVAR_ARCHIVE, "Precache character models at startup - more memory usage but smaller loading times in the game");
 	cl_map_displayavailablecells = Cvar_Get("cl_map_displayavailablecells", "0", 0, "Display cells where a solider can move");
-	cl_map_draw_rescue_zone = Cvar_Get("cl_map_draw_rescue_zone", "2", CVAR_ARCHIVE, "Draw rescue zone: 1 - draw perimeter, 2 - draw circles on the ground, 3 - draw both");
+	cl_map_draw_rescue_zone = Cvar_Get("cl_map_draw_rescue_zone", "2", 0, "Draw rescue zone: 1 - draw perimeter, 2 - draw circles on the ground, 3 - draw both");
 }
