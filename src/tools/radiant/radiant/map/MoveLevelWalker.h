8b150a7bd62d79946a3669ff7f0e6fec1d32527c
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-01-18 09:02:31 +0100

    * fixed bug #3475262 (UFORadiant level up/down feature not working correctly)

--- src/tools/radiant/radiant/map/MoveLevelWalker.h
+++ src/tools/radiant/radiant/map/MoveLevelWalker.h
@@ -48,33 +48,34 @@ class MoveLevelWalker: public scene::Graph::Walker
 
 		bool pre (const scene::Path& path, scene::Instance& instance) const
 		{
-			if (path.top().get().visible()) {
-				if (Node_isBrush(path.top())) {
-					Brush* brush = Node_getBrush(path.top());
-					if (brush != 0) {
-						Brush_forEachFace(*brush, BrushMoveLevel(_up));
-					}
-				} else if (Node_isEntity(path.top())) {
-					Entity* entity = Node_getEntity(path.top());
-					if (entity != 0) {
-						std::string name = entity->getKeyValue("classname");
-						// TODO: get this info from entities.ufo
-						if (name == "func_rotating" || name == "func_door" || name == "func_door_sliding" || name
-								== "func_breakable" || name == "misc_item" || name == "misc_mission" || name
-								== "misc_mission_alien" || name == "misc_model" || name == "misc_sound" || name
-								== "misc_particle") {
-							const std::string spawnflags = entity->getKeyValue("spawnflags");
-							if (!spawnflags.empty()) {
-								int levels = string::toInt(spawnflags) & 255;
-								if (_up) {
-									levels <<= 1;
-								} else {
-									levels >>= 1;
-								}
-								if (levels == 0)
-									levels = 1;
-								entity->setKeyValue("spawnflags", string::toString(levels & 255));
+			if (!instance.isSelected())
+				return true;
+
+			if (Node_isBrush(path.top())) {
+				Brush* brush = Node_getBrush(path.top());
+				if (brush != 0) {
+					Brush_forEachFace(*brush, BrushMoveLevel(_up));
+				}
+			} else if (Node_isEntity(path.top())) {
+				Entity* entity = Node_getEntity(path.top());
+				if (entity != 0) {
+					std::string name = entity->getKeyValue("classname");
+					// TODO: get this info from entities.ufo
+					if (name == "func_rotating" || name == "func_door" || name == "func_door_sliding" || name
+							== "func_breakable" || name == "misc_item" || name == "misc_mission" || name
+							== "misc_mission_alien" || name == "misc_model" || name == "misc_sound" || name
+							== "misc_particle") {
+						const std::string spawnflags = entity->getKeyValue("spawnflags");
+						if (!spawnflags.empty()) {
+							int levels = string::toInt(spawnflags) & 255;
+							if (_up) {
+								levels <<= 1;
+							} else {
+								levels >>= 1;
 							}
+							if (levels == 0)
+								levels = 1;
+							entity->setKeyValue("spawnflags", string::toString(levels & 255));
 						}
 					}
 				}
