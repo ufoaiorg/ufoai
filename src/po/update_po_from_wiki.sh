a5a7da3cb72eec9b9ac0e237968c924d0bff1be5
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-02-19 19:12:01 +0100

    * applied patch from bug #3489345 (update_po script issue)

--- src/po/update_po_from_wiki.sh
+++ src/po/update_po_from_wiki.sh
@@ -279,8 +279,8 @@ update_one_sentence()
 	$awk_soft 'BEGIN {FS="]";RS="<p>"}
 		$0 ~ /^[ \t]*$/ {next}
 		test && ($0 ~ /^\[/ || $0 ~ /<.*>/) {exit}
-		$0 ~ /^\['$english'\]/ || test == 1 {
-			gsub(/^\['$english'\][ \t]*/,"")
+		$0 ~ /\['$english'\]/ || test == 1 {
+			gsub(/.*\['$english'\][ \t]*<\/a>/,"")
 			gsub(/[ \t]*$/,"")
 			if (test && test2) {
 				printf "\\n"
@@ -632,7 +632,7 @@ if [[ $? -eq 0 ]]
 then
 	clean_html
 
-	for english in "mail_alien_ufo_crashed" "mail_alien_ufo_recovered" "mail_aircraft_landed" "mail_aircraft_bingo_fuel" "mail_aircraft_ready" "mail_aircraft_lost_target" "mail_aircraft_new_at_base" "mail_aircraft_lost_target" "mail_alien_activity_reported" "mail_alien_ufo_downed" "mail_alien_response_too_late" "mail_alien_new_radar_contact" "mail_alien_lost_radar_contact" "mail_alien_base_discovered" "mail_general_mission_summary" "mail_general_base_attack_report" "mail_general_new_base" "mail_general_construction_finished" "mail_general_equipment_received" "mail_general_transfer_received" "mail_general_ufo_in_hangar" "mail_general_monthly_report" "mail_production_finished" "mail_production_not_enough_resources" "mail_production_not_enough_money" "mail_prolog" "mail_stunned_alien_died"
+	for english in "mail_stunned_alien_died" "mail_prolog" "mail_alien_ufo_crashed" "mail_alien_ufo_recovered" "mail_aircraft_landed" "mail_aircraft_bingo_fuel" "mail_aircraft_ready" "mail_aircraft_lost_target" "mail_aircraft_new_at_base" "mail_aircraft_lost_target" "mail_alien_activity_reported" "mail_alien_ufo_downed" "mail_alien_response_too_late" "mail_alien_new_radar_contact" "mail_alien_lost_radar_contact" "mail_alien_base_discovered" "mail_general_mission_summary" "mail_general_base_attack_report" "mail_general_new_base" "mail_general_construction_finished" "mail_general_equipment_received" "mail_general_transfer_received" "mail_general_ufo_in_hangar" "mail_general_monthly_report" "mail_production_finished" "mail_production_not_enough_resources" "mail_production_not_enough_money" "mail_prolog" "mail_stunned_alien_died"
 	do
 		update_one_sentence "1"
 	done
