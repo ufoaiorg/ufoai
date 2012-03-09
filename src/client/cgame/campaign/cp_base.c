ufoaiorg/ufoai

commit 81871aa2d83af076de5248f3a111641b09104c6b

* fixed bug that does not remove scientists from a selled object
* see http://ufoai.org/forum/index.php/topic,6488.0.html

src/client/cgame/campaign/cp_base.c
@@ -2883,6 +2883,11 @@ qboolean B_UpdateStorageAndCapacity (base_t* base, const objDef_t *obj, int amou
 			Com_Printf("B_UpdateStorageAndCapacity: current number of item '%s' is negative: reset to 0\n", obj->id);
 			base->storage.numItems[obj->idx] = 0;
 		}
+
+		if (base->storage.numItems[obj->idx] == 0) {
+			technology_t *tech = RS_GetTechForItem(obj);
+			RS_StopResearch(tech);
+		}
 	}
 
 	return qtrue;

commit d57a3737d9acfc849f1a2ced7d342408e712b512

* fixed assert in RS_StopResearch introduced in 81871aa2

src/client/cgame/campaign/cp_base.c
@@ -2879,7 +2879,8 @@ qboolean B_UpdateStorageAndCapacity (base_t* base, const objDef_t *obj, int amou
 
 	if (base->storage.numItems[obj->idx] == 0) {
 		technology_t *tech = RS_GetTechForItem(obj);
-		RS_StopResearch(tech);
+		if (tech->statusResearch == RS_RUNNING)
+			RS_StopResearch(tech);
 	}
 
 	return qtrue;