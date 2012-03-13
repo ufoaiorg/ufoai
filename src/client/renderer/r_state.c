48f0b6133eb516399662ea1f1a6972c1b3515ad1
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-03-13 20:56:16 +0100

    * reduced scope of some variables + const

--- src/client/renderer/r_state.c
+++ src/client/renderer/r_state.c
@@ -105,7 +105,7 @@ void R_BindNormalmapTexture (GLuint texnum)
 void R_UseMaterial (const material_t *material)
 {
 	static float last_b, last_p, last_s, last_h;
-	float b, p, s, h;
+	float b;
 
 	if (r_state.active_material == material)
 		return;
@@ -124,17 +124,17 @@ void R_UseMaterial (const material_t *material)
 	last_b = b;
 
 	if (r_state.active_program != r_state.world_program || r_programs->integer > 1) {
-		p = r_state.active_material->parallax * r_parallax->value;
+		const float p = r_state.active_material->parallax * r_parallax->value;
 		if (p != last_p)
 			R_ProgramParameter1f("PARALLAX", p);
 		last_p = p;
 
-		h = r_state.active_material->hardness * r_hardness->value;
+		const float h = r_state.active_material->hardness * r_hardness->value;
 		if (h != last_h)
 			R_ProgramParameter1f("HARDNESS", h);
 		last_h = h;
 
-		s = r_state.active_material->specular * r_specular->value;
+		const float s = r_state.active_material->specular * r_specular->value;
 		if (s != last_s)
 			R_ProgramParameter1f("SPECULAR", s);
 		last_s = s;
