13911362c69231dc02b032720f36519013b99f2a
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-03-18 15:27:09 +0100

    * fixed rendering of trigger_rescue area

--- src/client/battlescape/cl_localentity.c
+++ src/client/battlescape/cl_localentity.c
@@ -943,6 +943,9 @@ qboolean LE_BrushModelAction (le_t * le, entity_t * ent)
 		float x, y, z, xmax;
 		const int drawFlags = cl_map_draw_rescue_zone->integer;
 
+		if (!((1 << cl_worldlevel->integer) & le->levelflags))
+			break;
+
 		ent->flags = 0; /* Do not draw anything at all, if drawFlags set to 0 */
 		enum { DRAW_TEXTURE = 0x1, DRAW_CIRCLES = 0x2 };
 		ent->model = NULL;
