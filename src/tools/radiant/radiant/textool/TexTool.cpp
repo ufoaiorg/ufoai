278bc27edb2c49b1f8ebd51bb15b68a85ebae63a
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-04-16 09:05:02 +0200

    * fixed initial size of the texture tool window

--- src/tools/radiant/radiant/textool/TexTool.cpp
+++ src/tools/radiant/radiant/textool/TexTool.cpp
@@ -58,6 +58,7 @@ TexTool::TexTool() :
 	_grid(GRID_DEFAULT),
 	_gridActive(GlobalRegistry().get(RKEY_GRID_STATE) == "1")
 {
+	gtk_window_set_default_size(GTK_WINDOW(getWindow()), 400, 300);
 	gtk_window_set_type_hint(GTK_WINDOW(getWindow()), GDK_WINDOW_TYPE_HINT_DIALOG);
 
 	g_signal_connect(G_OBJECT(getWindow()), "focus-in-event", G_CALLBACK(triggerRedraw), this);
