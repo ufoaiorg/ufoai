b9c7eb0f4176a3aedb99e856090ace716a392bd5
bayo <bayo.fr@gmail.com>
2012-05-23 01:07:50 +0200

    * Eclipse: add TranslatableString with validator (and use it for some elements)

--- eclipse/net.sourceforge.ufoai.dsl/src/net/sourceforge/ufoai/validation/UFOScriptJavaValidator.java
+++ eclipse/net.sourceforge.ufoai.dsl/src/net/sourceforge/ufoai/validation/UFOScriptJavaValidator.java
@@ -1,7 +1,9 @@
 package net.sourceforge.ufoai.validation;
 
+import net.sourceforge.ufoai.ufoScript.TranslatableString;
 import net.sourceforge.ufoai.ufoScript.UINodeImage;
 import net.sourceforge.ufoai.ufoScript.UINodeWindow;
+import net.sourceforge.ufoai.ufoScript.UfoScriptPackage;
 
 import org.eclipse.xtext.validation.Check;
 
@@ -16,6 +18,15 @@ public class UFOScriptJavaValidator extends AbstractUFOScriptJavaValidator {
 		// else if (!name.equals(name.toLowerCase()))
 		// warning("Window name should be lowercase",
 		// UfoScriptPackage.UI_NODE_WINDOW__NAME, INVALID_NAME, name);
+	}
+
+	@Check
+	public void checkTranslatable(TranslatableString translatable) {
+		final String value = translatable.getValue();
+		if (!value.startsWith("_")) {
+			warning("String should be prefixed by'_' to became translatable.",
+				UfoScriptPackage.Literals.TRANSLATABLE_STRING__VALUE);
+		}
 	}
 
 	@Check
