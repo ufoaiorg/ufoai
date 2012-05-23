7959d5f8a77b478caad70ab350d29596c0962247
bayo <bayo.fr@gmail.com>
2012-05-23 21:44:53 +0200

    * Eclipse: (missing file) util to load game resources

---
+++ eclipse/net.sourceforge.ufoai.dsl/src/net/sourceforge/ufoai/util/UfoResources.java
@@ -0,0 +1,68 @@
+package net.sourceforge.ufoai.util;
+
+import java.io.File;
+
+/**
+ * UFOAI resource util to access to the content of the project.
+ * QUESTION maybe we should use IResource (ecplise resource) instead of File
+ */
+public class UfoResources {
+
+	private static String[] supportedImageExtensions = {".png", ".jpg"};
+
+	/**
+	 * True if "base" dir already checked.
+	 * This value can be true, and base null, if base not found
+	 */
+	private static boolean isBaseChecked;
+
+	private static File base;
+
+	/**
+	 * Search for a parent file which contains "base" dir
+	 * @param absolutePath
+	 */
+	public static void checkBase(String absolutePath) {
+		File file = new File(absolutePath);
+		for (int i = 0; i < 5; i++) {
+			File dir = new File(file, "base");
+			if (dir.exists() && dir.isDirectory()) {
+				base = dir;
+				isBaseChecked = true;
+				return;
+			}
+			file = file.getParentFile();
+			if (file == null) {
+				break;
+			}
+		}
+		System.out.println("UfoResources: Base dir was not found.");
+		isBaseChecked = true;
+	}
+
+	public static boolean isBaseChecked() {
+		return isBaseChecked;
+	}
+
+	private static File getFileFromBase(String id, String[] expectedExtensions) {
+		if (base == null)
+			return null;
+		for (String extension: expectedExtensions) {
+			File file = new File(base, id + extension);
+			if (file.exists() || file.isFile()) {
+				return file;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Get a file from base/pics
+	 * @param name Name of the file resource
+	 * @return A file, else null if it do not exists
+	 */
+	public static File getFileFromPics(String id) {
+		return getFileFromBase("pics/" + id, supportedImageExtensions);
+	}
+
+}
