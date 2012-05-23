c317e51a59200f57ed2c762e3a86d7635f33a874
bayo <bayo.fr@gmail.com>
2012-05-23 20:55:11 +0200

    * Ecplise: fix batiment parsing
    * fix syntax for batiments
    * rework ufo script (it is more easy to parse quoted image)
    * still missing reference with research object
    * add validator for image existance

--- eclipse/net.sourceforge.ufoai.dsl/src/net/sourceforge/ufoai/validation/UFOScriptJavaValidator.java
+++ eclipse/net.sourceforge.ufoai.dsl/src/net/sourceforge/ufoai/validation/UFOScriptJavaValidator.java
@@ -1,14 +1,37 @@
 package net.sourceforge.ufoai.validation;
 
+import java.io.File;
+
+import net.sourceforge.ufoai.ufoScript.PicsImage;
 import net.sourceforge.ufoai.ufoScript.TranslatableString;
-import net.sourceforge.ufoai.ufoScript.UINodeImage;
 import net.sourceforge.ufoai.ufoScript.UINodeWindow;
 import net.sourceforge.ufoai.ufoScript.UfoScriptPackage;
+import net.sourceforge.ufoai.util.UfoResources;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.validation.Check;
 
 public class UFOScriptJavaValidator extends AbstractUFOScriptJavaValidator {
 	public static final String INVALID_NAME = "INVALID_NAME";
+
+
+	/**
+	 * Check for "base" dir.
+	 * Maybe something better exists, but i dont find it.
+	 * @param object An object witch contains information to source file
+	 */
+	private void checkUfoBase(EObject object) {
+		if (UfoResources.isBaseChecked())
+			return;
+		Resource resource = object.eResource();
+		String platformString = resource.getURI().toPlatformString(true);
+		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(platformString));
+		UfoResources.checkBase(file.getLocation().toOSString());
+	}
 
 	@Check
 	public void checkWindowName(UINodeWindow window) {
@@ -30,6 +53,15 @@ public class UFOScriptJavaValidator extends AbstractUFOScriptJavaValidator {
 	}
 
 	@Check
-	public void checkImage(UINodeImage image) {
+	public void checkImage(PicsImage image) {
+		checkUfoBase(image);
+
+		final String id = image.getValue();
+		final File file = UfoResources.getFileFromPics(id);
+		if (file == null) {
+			warning("Image not found.",
+				UfoScriptPackage.Literals.PICS_IMAGE__VALUE);
+		}
 	}
+
 }
