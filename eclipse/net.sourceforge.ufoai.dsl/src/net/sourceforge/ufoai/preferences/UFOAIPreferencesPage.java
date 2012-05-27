ca01c0113e6d7018e7f74172b15c0c62656f8b4a
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-05-27 19:40:57 +0200

    * eclipse tools: removed invalid preference initializer

--- eclipse/net.sourceforge.ufoai.dsl/src/net/sourceforge/ufoai/preferences/UFOAIPreferencesPage.java
+++ eclipse/net.sourceforge.ufoai.dsl/src/net/sourceforge/ufoai/preferences/UFOAIPreferencesPage.java
@@ -1,11 +1,14 @@
 package net.sourceforge.ufoai.preferences;
 
+import java.io.File;
+
 import net.sourceforge.ufoai.UFOAIPlugin;
 
 import org.eclipse.jface.preference.DirectoryFieldEditor;
-import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.FileFieldEditor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -21,10 +24,17 @@ public class UFOAIPreferencesPage extends FieldEditorPreferencePage implements I
 
 	@Override
 	protected void createFieldEditors() {
-		FieldEditor pathEditor = getDirectoryEditor(PreferenceConstants.UFORADIANT_EXEC_PATH, "UFORadiant path");
-		FieldEditor binaryEditor = getFileEditor(PreferenceConstants.UFORADIANT_BINARY, "UFORadiant binary");
+		DirectoryFieldEditor pathEditor = getDirectoryEditor(PreferenceConstants.UFORADIANT_EXEC_PATH, "UFORadiant path");
+		final FileFieldEditor binaryEditor = getFileEditor(PreferenceConstants.UFORADIANT_BINARY, "UFORadiant binary");
 		addField(pathEditor);
 		addField(binaryEditor);
+
+		pathEditor.setPropertyChangeListener(new IPropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent event) {
+				binaryEditor.setFilterPath(new File(event.getNewValue().toString()));
+			}
+		});
 	}
 
 	private FileFieldEditor getFileEditor(String preferenceConstant, String title, boolean emptyAllowed) {
