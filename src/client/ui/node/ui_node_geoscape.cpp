57348a42f5657b9f940c85b8cae664636bc3067f
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-08-11 09:55:54 +0200

    * reduced header inclusion

--- src/client/ui/node/ui_node_geoscape.cpp
+++ src/client/ui/node/ui_node_geoscape.cpp
@@ -30,7 +30,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../ui_render.h"
 #include "ui_node_geoscape.h"
 
-#include "../../client.h"
+#include "../../cl_shared.h"
+#include "../../input/cl_input.h"
+#include "../../input/cl_keys.h"
 #include "../../cgame/campaign/cp_campaign.h"
 #include "../../cgame/campaign/cp_map.h"
 
