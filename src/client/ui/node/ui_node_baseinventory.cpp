65bb6164e9ea49bab9663a482bd031a9ee790d7f
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-08-11 08:20:52 +0200

    * reduced header inclusion

--- src/client/ui/node/ui_node_baseinventory.cpp
+++ src/client/ui/node/ui_node_baseinventory.cpp
@@ -40,14 +40,17 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "ui_node_abstractnode.h"
 #include "ui_node_abstractscrollable.h"
 
-#include "../../client.h"
+#include "../../cl_shared.h"
 #include "../../cgame/cl_game.h"
-#include "../../battlescape/cl_actor.h"
+#include "../../input/cl_keys.h"
 #include "../../cl_inventory.h"
 
 #define EXTRADATA_TYPE baseInventoryExtraData_t
 #define EXTRADATA(node) UI_EXTRADATA(node, EXTRADATA_TYPE)
 #define EXTRADATACONST(node) UI_EXTRADATACONST(node, EXTRADATA_TYPE)
+
+extern bool CL_BattlescapeRunning(void);
+extern cvar_t *cl_selected;
 
 /**
  * self cache for drag item
