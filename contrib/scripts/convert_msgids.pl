fd0571987cf5fe2c773b792f8ef99000fb0c09d6
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-08-12 15:15:10 +0200

    * added perl script to convert the ufoai-en.po into a ufo msgid script

---
+++ contrib/scripts/convert_msgids.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $file = "../../src/po/ufoai-en.po";
+
+open(FILE, $file) || die "Could not read $file";
+my $started = 0;
+foreach (<FILE>) {
+	$_ =~ s/\r?\n//g;
+	if ($_ =~ /^msgid\s\"(.*_txt)\"$/) {
+		if ($started) {
+			print "\"\n}\n";
+		}
+		print "msgid $1 {\ntext \"_";
+		$started = 1;
+		next;
+	} elsif ($_ =~ /^msgid.*/) {
+		if ($started) {
+			print "\"\n}\n";
+			$started = 0;
+		}
+		next;
+	}
+
+	if ($_ eq "") {
+		if ($started) {
+			print "\"\n}\n\n";
+			$started = 0;
+		}
+	} else {
+		if ($started) {
+			$_ =~ s/msgstr\s+//g;
+			$_ =~ s/^\"//g;
+			$_ =~ s/\"$//g;
+			$_ =~ s/\\n/\n/g;
+			print $_;
+		}
+	}
+}
+close(FILE);
