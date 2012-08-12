3fcef78cd551e4de00bfa5909bce7e0b815c5ee4
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-08-12 22:11:54 +0200

    * add msgid support to mail headers

--- src/client/cgame/campaign/cp_ufopedia.cpp
+++ src/client/cgame/campaign/cp_ufopedia.cpp
@@ -534,12 +534,14 @@ static void UP_SetMailHeader (technology_t* tech, techMailType_t type, eventMail
 			return;
 		}
 	}
-	Com_sprintf(mailHeader, sizeof(mailHeader), _("FROM: %s\nTO: %s\nDATE: %s"),
-		_(from), _(to), dateBuf);
+	from = cgi->CL_Translate(from);
+	to = cgi->CL_Translate(to);
+	subject = cgi->CL_Translate(subject);
+	Com_sprintf(mailHeader, sizeof(mailHeader), _("FROM: %s\nTO: %s\nDATE: %s"), from, to, dateBuf);
 	Cvar_Set("mn_mail_sender_head", model ? model : "");
-	Cvar_Set("mn_mail_from", _(from));
-	Cvar_Set("mn_mail_subject", va("%s%s", subjectType, _(subject)));
-	Cvar_Set("mn_mail_to", _(to));
+	Cvar_Set("mn_mail_from", from);
+	Cvar_Set("mn_mail_subject", va("%s%s", subjectType, subject));
+	Cvar_Set("mn_mail_to", to);
 	Cvar_Set("mn_mail_date", dateBuf);
 	cgi->UI_RegisterText(TEXT_UFOPEDIA_MAILHEADER, mailHeader);
 }
