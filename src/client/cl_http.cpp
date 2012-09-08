d8adc4bc952c10ea6815327a0a3c0ef9a7f046e6
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-09-08 16:35:48 +0200

    * readded jpg to the valid filetypes for the http downloads

--- src/client/cl_http.cpp
+++ src/client/cl_http.cpp
@@ -425,6 +425,7 @@ static void CL_CheckAndQueueDownload (char *path)
 			!Q_streq(ext, "ogg") &&
 			!Q_streq(ext, "md3") &&
 			!Q_streq(ext, "png") &&
+			!Q_streq(ext, "jpg") &&
 			!Q_streq(ext, "obj") &&
 			!Q_streq(ext, "mat") &&
 			!Q_streq(ext, "ump")) {
