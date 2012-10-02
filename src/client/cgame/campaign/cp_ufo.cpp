a33f120728dc6dd0a8746fc502f48684b316ff29
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-10-02 19:10:26 +0200

    * fixed missing debug_ prefix for removeufo console command

--- src/client/cgame/campaign/cp_ufo.cpp
+++ src/client/cgame/campaign/cp_ufo.cpp
@@ -921,7 +921,7 @@ void UFO_InitStartup (void)
 #ifdef DEBUG
 	cgi->Cmd_AddCommand("debug_destroyufos", UFO_DestroyUFOs_f, "Destroys all UFOs on the geoscape");
 	cgi->Cmd_AddCommand("debug_listufo", UFO_ListOnGeoscape_f, "Print UFO information to game console");
-	cgi->Cmd_AddCommand("removeufo", UFO_RemoveFromGeoscape_f, "Remove a UFO from geoscape");
+	cgi->Cmd_AddCommand("debug_removeufo", UFO_RemoveFromGeoscape_f, "Remove a UFO from geoscape");
 	cgi->Cvar_Get("debug_showufos", "0", CVAR_DEVELOPER, "Show all UFOs on geoscape");
 #endif
 }
