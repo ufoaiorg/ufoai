a70acba592d5642a8fa2e2afa3dc9b19b13accea
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-10-09 22:38:13 +0200

    * removed client.h include

--- src/client/ui/node/ui_node_item.cpp
+++ src/client/ui/node/ui_node_item.cpp
@@ -32,7 +32,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "ui_node_container.h"
 #include "ui_node_abstractnode.h"
 
-#include "../../client.h"
 #include "../../cgame/cl_game.h"
 #include "../../renderer/r_draw.h"
 
