c943f45f7e09ee216fd54165e8ed9ff58aeb521f
Duke <aduke1@gmx.de>
2012-10-11 23:55:56 +0200

    * fixed mathlib includes

--- src/shared/mathlib.h
+++ src/shared/mathlib.h
@@ -28,6 +28,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "ufotypes.h"
 #include "defines.h"
 #include <algorithm>
+#include <cmath>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846  /* matches value in gcc v2 math.h */
