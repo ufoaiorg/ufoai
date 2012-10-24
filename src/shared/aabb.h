182ce25f77d0625f85944d9770b6a0952532e3d8
Duke <aduke1@gmx.de>
2012-10-24 21:59:44 +0200

    * copyright notice for vector.h

--- src/shared/aabb.h
+++ src/shared/aabb.h
@@ -3,7 +3,7 @@
  */
 
 /*
- Copyright (C) 1997-2001 Id Software, Inc.
+Copyright (C) 2002-2012 UFO: Alien Invasion.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
