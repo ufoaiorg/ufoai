7682b5f8c6cdbda7de02fe30d8a4ed0f9f76164b
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-10-25 23:11:08 +0200

    * temp mission briefing for skirmish

--- src/client/cgame/skirmish/cl_game_skirmish.cpp
+++ src/client/cgame/skirmish/cl_game_skirmish.cpp
@@ -37,6 +37,7 @@ CGAME_HARD_LINKED_FUNCTIONS
 
 static void GAME_SK_StartBattlescape (bool isTeamPlay)
 {
+	cgi->UI_RegisterText(TEXT_MISSIONBRIEFING, "*msgid:defaultmissionbriefing");
 	cgi->UI_InitStack("skirmish_wait", NULL, true, true);
 }
 
