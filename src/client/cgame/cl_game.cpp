ufoaiorg/ufoai

commit 55d5d443adbfc75155321e2038502a5fac25afb6

* added skirmish wait dialog for testing the map/mission descriptions

src/client/cgame/skirmish/cl_game_skirmish.cpp
@@ -35,6 +35,17 @@ static const cgame_import_t *cgi;
 
 CGAME_HARD_LINKED_FUNCTIONS
 
+static void GAME_SK_StartBattlescape (bool isTeamPlay)
+{
+	cgi->UI_InitStack("skirmish_wait", NULL, true, true);
+}
+
+static void GAME_SK_NotifyEvent (event_t eventType)
+{
+	if (eventType == EV_RESET)
+		cgi->HUD_InitUI(NULL, true);
+}
+
 static inline const char* GAME_SK_GetRandomMapAssemblyNameForCraft (const char *name)
 {
 	return cgi->Com_GetRandomMapAssemblyNameForCraft(name);
@@ -113,9 +124,6 @@ static void GAME_SK_Start_f (void)
 
 	assert(md->map);
 	Com_sprintf(map, sizeof(map), "map %s %s %s;", cgi->Cvar_GetInteger("mn_serverday") ? "day" : "night", md->map, md->param ? md->param : "");
-
-	/* prepare */
-	cgi->UI_InitStack(NULL, "singleplayermission", true, false);
 
 	cgi->Cbuf_AddText(map);
 }
@@ -357,6 +365,8 @@ const cgame_export_t *GetCGameSkirmishAPI (const cgame_import_t *import)
 	e.Shutdown = GAME_SK_Shutdown;
 	e.MapInfo = GAME_SK_MapInfo;
 	e.Results = GAME_SK_Results;
+	e.StartBattlescape = GAME_SK_StartBattlescape;
+	e.NotifyEvent = GAME_SK_NotifyEvent;
 
 	cgi = import;
 

LICENSES
@@ -3638,6 +3638,7 @@ base/pics/techs/universal_serum.jpg | Creative Commons Attribution-Share Alike 3
 base/pics/techs/xvi.jpg | Creative Commons Attribution-Share Alike 2.0 | Joshua "Josh" Combs
 base/pics/tiled/panel_green.png | GNU General Public License 2.0 or later | bayo
 base/pics/tiled/panel_green_slim.png | GNU General Public License 2.0 or later | bayo
+base/pics/tiled/panel_green_slim_trans.png | GNU General Public License 2.0 or later | bayo, Martin Gerhardy
 base/pics/tiled/panel_green_tab.png | GNU General Public License 2.0 or later | bayo
 base/pics/tiled/panel_grey.png | GNU General Public License 2.0 or later |  | Based on original menu artwork by SparX and Rasta
 base/pics/tiled/panel_grey_black.png | GNU General Public License 2.0 or later |  | Based on original menu artwork by SparX and Rasta

base/ufos/sprites_tiled.ufo
@@ -86,6 +86,13 @@ sprite ui/panel_green_slim {
 	texl "0 0"
 }
 
+sprite ui/panel_green_slim_trans {
+	image "tiled/panel_green_slim_trans"
+	tiled_25_1_3	true
+	size "64 64"
+	texl "0 0"
+}
+
 sprite ui/panel_green_tab {
 	image "tiled/panel_green_tab"
 	tiled_25_1_3	true

base/ufos/ui/skirmish.ufo
@@ -499,3 +499,83 @@ window skirmish
 		cmd "define g_endlessaliens 0;"
 	}
 }
+
+// ===========================================
+// SKIRMISH WAIT DIALOG
+// ===========================================
+window skirmish_wait
+{
+	{
+		fill		true
+		starlayout	true
+	}
+
+	battlescape render
+	{
+		pos			"0 0"
+		size		"1024 768"
+		align		LAYOUTALIGN_FILL
+	}
+
+	panel mission_description {
+		{
+			pos			"212 134"
+			size		"600 500"
+			background	"ui/panel_green_slim_trans"
+		}
+
+		string name
+		{
+			pos 	"10 10"
+			font	"f_menu"
+			string	"*cvar:sv_mapname"
+		}
+
+		text briefing
+		{
+			dataid		TEXT_MISSIONBRIEFING
+			pos			"10 40"
+			size		"580 240"
+			lineheight	20
+		}
+
+		string victory_conditions
+		{
+			pos		"10 300"
+			font	"f_menu"
+			string "_Victory conditions:"
+		}
+
+		text briefing_conditions
+		{
+			dataid		TEXT_MISSIONBRIEFING
+			pos			"10 330"
+			size		"360 160"
+			lineheight	20
+		}
+
+		button button_ready
+		{
+			string		"_Start"
+			tooltip		"_Start the match"
+			background	"ui/button_green_small"
+			pos			"380 450"
+			width		"200"
+			font		"f_menu"
+			color		"0 0.5 0 1"
+			selectcolor	"1 1 1 1"
+			onClick		{
+				cmd "cl_ready 1;"
+			}
+		}
+	}
+
+	// ===================
+	// EVENTS
+	// ===================
+
+	func onWindowOpened
+	{
+		*cvar:cl_ready = 0
+	}
+}

src/client/cgame/campaign/cl_game_campaign.cpp
@@ -57,6 +57,7 @@ const cgame_export_t *GetCGameCampaignAPI (const cgame_import_t *import)
 	e.IsTeamKnown = GAME_CP_TeamIsKnown;
 	e.Drop = GAME_CP_Drop;
 	e.InitializeBattlescape = GAME_CP_InitializeBattlescape;
+	e.StartBattlescape = GAME_CP_StartBattlescape;
 	e.RunFrame = GAME_CP_Frame;
 	e.HandleBaseClick = GAME_CP_HandleBaseClick;
 	e.DrawBase = GAME_CP_DrawBase;

src/client/cgame/campaign/cp_cgame_callbacks.cpp
@@ -564,6 +564,12 @@ const char* GAME_CP_GetTeamDef (void)
 	return cgi->Com_ValueToStr(&team, V_TEAM, 0);
 }
 
+void GAME_CP_StartBattlescape (bool isTeamPlay)
+{
+	/* auto start the game until we have a proper missing briefing for the campaign */
+	Cvar_Set("cl_ready", "1");
+}
+
 /**
  * @brief Changes some actor states for a campaign game
  * @param team The team to change the states for

src/client/cgame/campaign/cp_cgame_callbacks.h
@@ -39,6 +39,7 @@ void GAME_CP_CharacterCvars(const character_t *chr);
 bool GAME_CP_TeamIsKnown(const teamDef_t *teamDef);
 void GAME_CP_Drop(void);
 dbuffer *GAME_CP_InitializeBattlescape(const chrList_t *team);
+void GAME_CP_StartBattlescape(bool isTeamPlay);
 void GAME_CP_Frame(float secondsSinceLastFrame);
 const char* GAME_CP_GetTeamDef(void);
 void GAME_CP_HandleBaseClick(int baseIdx, int key, int col, int row);

commit 71f5f75111227b9af98b5abf0fe3a5da1dbf79e5

* fixed option menu not working in skirmish
* regression was introduced in 55d5d443adbfc75155321e2038502a5fac25afb6
* this also fixes the option menu not working when starting a map without a cgame type given (via commandline)

base/ufos/ui/missionbriefing.ufo
@@ -62,6 +62,7 @@ window mission_briefing
 
 		button button_ready
 		{
+			visiblewhen	"*cvar:cl_onbattlescape == 1"
 			string		"_Start"
 			tooltip		"_Start the match"
 			background	"ui/button_green_small"
@@ -71,7 +72,7 @@ window mission_briefing
 			color		"0 0.5 0 1"
 			selectcolor	"1 1 1 1"
 			onClick		{
-				cmd "cl_ready 1;"
+				cmd "cl_ready 1;ui_pop;"
 			}
 		}
 	}

src/client/cgame/cl_game.cpp
@@ -1345,6 +1345,7 @@ void GAME_StartBattlescape (bool isTeamPlay)
 	if (list != NULL && list->StartBattlescape) {
 		list->StartBattlescape(isTeamPlay);
 	} else {
+		UI_InitStack(NULL, "singleplayermission", true, false);
 		HUD_InitUI(NULL, true);
 	}
 

src/client/cgame/skirmish/cl_game_skirmish.cpp
@@ -131,6 +131,9 @@ static void GAME_SK_Start_f (void)
 	assert(md->map);
 	Com_sprintf(map, sizeof(map), "map %s %s %s;", cgi->Cvar_GetInteger("mn_serverday") ? "day" : "night", md->map, md->param ? md->param : "");
 
+	/* prepare */
+	cgi->UI_InitStack(NULL, "singleplayermission", true, false);
+
 	cgi->Cbuf_AddText(map);
 }
 