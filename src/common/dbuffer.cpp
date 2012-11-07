cbfeec16aa21fc09e69abbed66043de9523407ea
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-11-07 20:10:30 +0100

    * moved INITIAL_CAPACITY into dbuffer.cpp and replaced outdated copyright header

--- src/common/dbuffer.cpp
+++ src/common/dbuffer.cpp
@@ -1,11 +1,30 @@
 /**
  * @file
- * @brief A dbuffer is a dynamically sized buffer that stores arbitrary bytes
- * in a queue. It does not provide random access; characters may be
- * inserted only at the end and removed only from the beginning.
  */
 
+/*
+Copyright (C) 2002-2011 UFO: Alien Invasion.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
 #include "common.h"
+
+#define INITIAL_CAPACITY 4096
 
 dbuffer::dbuffer () : _length(0)
 {
