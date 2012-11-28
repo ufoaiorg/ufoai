52b5b1f14caa16258e365cd4e0ee75e144cdce16
Duke <aduke1@gmx.de>
2012-11-28 18:15:17 +0100

    * added g_vis.h to game.cbp

--- src/game/g_vis.h
+++ src/game/g_vis.h
@@ -21,7 +21,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-
+#pragma once
 #include "g_local.h"
 
 bool G_FrustumVis(const edict_t *from, const vec3_t point);
