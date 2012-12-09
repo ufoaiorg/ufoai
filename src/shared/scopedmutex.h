2c7c11e09548c149e594181761cd798486583004
Martin Gerhardy <tlh2000@users.sourceforge.net>
2012-12-09 10:29:33 +0100

    * added scoped mutex locking/unlocking

---
+++ src/shared/scopedmutex.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <SDL_mutex.h>
+
+/**
+ * Ensures that a mutex is released once the scope is left
+ */
+class ScopedMutex {
+private:
+	SDL_mutex *_mutex;
+public:
+	ScopedMutex (SDL_mutex *mutex) :
+			_mutex(mutex)
+	{
+		SDL_LockMutex(_mutex);
+	}
+
+	~ScopedMutex ()
+	{
+		SDL_UnlockMutex(_mutex);
+	}
+};
