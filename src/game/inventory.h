1cd950ef4fbf0ba3028b19a2b0149b7bed1ebeb8
Duke <aduke1@gmx.de>
2012-12-13 21:23:10 +0100

    * copyright

--- src/game/inventory.h
+++ src/game/inventory.h
@@ -1,5 +1,31 @@
-#ifndef INVENTORY_H_
-#define INVENTORY_H_
+/**
+ * @file
+ */
+
+/*
+All original material Copyright (C) 2002-2012 UFO: Alien Invasion.
+
+Original file from Quake 2 v3.21: quake2-2.31/game/g_utils.c
+Copyright (C) 1997-2001 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#pragma once
 
 #include "q_shared.h"
 
@@ -50,5 +76,3 @@ typedef struct inventoryInterface_s
 
 void INV_InitInventory(const char *name, inventoryInterface_t *ii, const csi_t* csi, const inventoryImport_t *iimport);
 void INV_DestroyInventory(inventoryInterface_t *ii);
-
-#endif /* INVENTORY_H_ */
