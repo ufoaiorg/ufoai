459015abcc7da1e9d694204559a0fdfdee4bdd3d
Duke <aduke1@gmx.de>
2012-12-15 01:05:24 +0100

    * use forward decl

--- src/client/battlescape/events/e_main.h
+++ src/client/battlescape/events/e_main.h
@@ -25,6 +25,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef E_MAIN_H_
 #define E_MAIN_H_
 
+class dbuffer;
+
 /** @brief CL_ParseEvent timers and vars */
 typedef struct eventTiming_s {
 	int nextTime;	/**< time when the next event should be executed */
