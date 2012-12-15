68853bdef302db6569a5909022784753bf165dee
Duke <aduke1@gmx.de>
2012-12-15 01:07:11 +0100

    * include yourself sooner

--- src/client/battlescape/events/event/actor/e_event_actorappear.cpp
+++ src/client/battlescape/events/event/actor/e_event_actorappear.cpp
@@ -23,13 +23,13 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "../../../../client.h"
+#include "e_event_actorappear.h"
 #include "../../../cl_localentity.h"
 #include "../../../cl_ugv.h"
 #include "../../../cl_actor.h"
 #include "../../../cl_hud.h"
 #include "../../../../cgame/cl_game.h"
 #include "../../../cl_particle.h"
-#include "e_event_actorappear.h"
 #include "../../../../../common/grid.h"
 
 /**
