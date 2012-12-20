97bfbf13464361cb229bc4093ec30845655e0630
Duke <aduke1@gmx.de>
2012-12-21 00:10:11 +0100

    * typedefs.h no longer needed here

--- src/common/common.h
+++ src/common/common.h
@@ -30,7 +30,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../shared/shared.h"
 #include "../shared/mathlib.h"
 #include "../shared/defines.h"
-#include "../shared/typedefs.h"
 #include "cvar.h"
 #include "mem.h"
 
