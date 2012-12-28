f65d5f4eb53d5ab16ec7969369ad89a42e859f9e
Duke <aduke1@gmx.de>
2012-12-28 14:00:05 +0100

    * missing copyright

--- src/common/list.h
+++ src/common/list.h
@@ -1,5 +1,29 @@
-#ifndef LIST_H_
-#define LIST_H_
+/**
+ * @file
+ * @brief LINKED LIST interface
+ */
+
+/*
+Copyright (C) 2002-2012 UFO: Alien Invasion.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#pragma once
 
 #include "../shared/ufotypes.h"
 
@@ -51,5 +75,3 @@ template<typename T> inline T& LIST_Add(linkedList_t** const list, T const& data
 {
 	return *static_cast<T*>(LIST_Add(list, &data, sizeof(data))->data);
 }
-
-#endif /* LIST_H_ */
