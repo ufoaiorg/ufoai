6332299f4990473e4ab981e52a1e0da5adfc178f
Tamás Fehérvári <geever@users.sourceforge.net>
2013-02-10 11:28:19 +0100

    * Update UFOAIBot with new Issue tracker urls

--- contrib/scripts/UFOAIBot.pl
+++ contrib/scripts/UFOAIBot.pl
@@ -19,19 +19,40 @@ my %options = (
 sub said {
 	my ($self, $message) = @_;
 
+	# New Bug/Feature/Patch Tracker links
 	if ($message->{'body'} =~ /^\!bug\s*#?(\d{1,5})/i) {
-		return "http://sourceforge.net/p/ufoai/bugs/$1/";
+		return "http://ufoai.org/bugs/ufoalieninvasion/issues/$1/";
 	} elsif ($message->{'body'} =~ /^\!fr\s*#?(\d{1,5})/i) {
-		return "http://sourceforge.net/p/ufoai/feature-requests/$1/";
+		return "http://ufoai.org/bugs/ufoalieninvasion/issues/$1/";
 	} elsif ($message->{'body'} =~ /^\!patch\s*#?(\d+)/i) {
+		return "http://ufoai.org/bugs/ufoalieninvasion/issues/$1/";
+	} elsif ($message->{'body'} =~ /^\!issue\s*#?(\d+)/i) {
+		return "http://ufoai.org/bugs/ufoalieninvasion/issues/$1/";
+	}
+
+	# SourceForge Bug/Feature/Patch Tracker links
+	elsif ($message->{'body'} =~ /^\!sfbug\s*#?(\d{1,5})/i) {
+		return "http://sourceforge.net/p/ufoai/bugs/$1/";
+	} elsif ($message->{'body'} =~ /^\!sffr\s*#?(\d{1,5})/i) {
+		return "http://sourceforge.net/p/ufoai/feature-requests/$1/";
+	} elsif ($message->{'body'} =~ /^\!sfpatch\s*#?(\d+)/i) {
 		return "http://sourceforge.net/p/ufoai/patches/$1/";
-	} elsif ($message->{'body'} =~ /^\!r(?:ev)?\s*#?([0-9a-f]{6,40})/i) {
+	}
+
+	# GitHub Commit Links
+	elsif ($message->{'body'} =~ /^\!r(?:ev)?\s*#?([0-9a-f]{6,40})/i) {
 		return "https://github.com/ufoai/ufoai/commit/$1/";
-	} elsif ($message->{'body'} =~ /^\!faq\s*#?(\w*)?/i) {
+	}
+
+	# Wiki Links
+	elsif ($message->{'body'} =~ /^\!faq\s*#?(\w*)?/i) {
 		return "http://ufoai.org/wiki/index.php/Manual:FAQ#$1";
 	} elsif ($message->{'body'} =~ /^\!todo/i) {
 		return "http://ufoai.org/wiki/index.php/TODO";
-	} elsif ($message->{'body'} =~ /^\!topic\s*#?(\d+)(?:\s*[\.#\s](\d+))?/i) {
+	}
+
+	# Forum Links
+	elsif ($message->{'body'} =~ /^\!topic\s*#?(\d+)(?:\s*[\.#\s](\d+))?/i) {
 		return "http://ufoai.org/forum/index.php?topic=$1".( (defined($2)) ? ".msg$2#msg$2" : "" ) ;
 	}
 }
@@ -39,13 +60,18 @@ sub said {
 sub help {
 	return
 		"UFOAI Channel Bot\n".
-		"!bug #tracker-id\n".
-		"!patch #tracker-id\n".
-		"!fr #tracker-id\n".
-		"!rev #git-hash\n".
-		"!faq [#section_name]\n".
-		"!todo\n".
-		"!topic #forum-topic-id [.message-id]\n";
+		"!issue #tracker-id :: Link to the new issue tracker\n".
+		"!bug #tracker-id :: Alias to the !issue command\n".
+		"!patch #tracker-id :: Alias to the !issue command\n".
+		"!fr #tracker-id :: Alias to the !issue command\n".
+		"!sfbug #tracker-id :: Link to the SourceForge bug tracker\n".
+		"!sfpatch #tracker-id :: Link to the SourceForge patch tracker\n".
+		"!sffr #tracker-id :: Link to the SourceForge feature request tracker\n".
+		"!rev #git-hash :: Link to the commit on GitHub\n".
+		"!faq [#section_name] :: Link to FAQ on the Wiki\n".
+		"!todo :: Link to the TODO Lists on the Wiki\n".
+		"!topic #forum-topic-id [.message-id] :: Link to a topic on the Forum\n".
+		"";
 }
 
 my $bot = UFOAIBot->new(%options)->run();
