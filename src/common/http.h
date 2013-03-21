2f0068e2377abf8093bb60d4d4a37f4baac1271a
Martin Gerhardy <tlh2000@users.sourceforge.net>
2013-03-21 10:27:16 +0100

    * guard the define of CURL_STATICLIB

--- src/common/http.h
+++ src/common/http.h
@@ -26,7 +26,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define HTTP_H
 
 #include "common.h"
+#ifndef CURL_STATICLIB
 #define CURL_STATICLIB
+#endif
 #include <curl/curl.h>
 
 typedef enum {
