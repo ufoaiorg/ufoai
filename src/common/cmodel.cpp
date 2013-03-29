ufoaiorg/ufoai

commit ec797bb57134f6a3e8fcb0c98056029f35622dad

* reduced trace_t object creation + const

src/common/cmodel.cpp
@@ -95,7 +95,6 @@ static bool CM_LineMissesModel (const vec3_t start, const vec3_t stop, const cBs
  */
 trace_t CM_HintedTransformedBoxTrace (MapTile &tile, const vec3_t start, const vec3_t end, const AABB &traceBox, const int headnode, const int contentmask, const int brushrejects, const vec3_t origin, const vec3_t angles, const vec3_t rmaShift, const float fraction)
 {
-	trace_t trace;
 	vec3_t start_l, end_l;
 	vec3_t forward, right, up;
 	vec3_t temp;
@@ -134,7 +133,7 @@ trace_t CM_HintedTransformedBoxTrace (MapTile &tile, const vec3_t start, const v
 	}
 
 	/* sweep the box through the model */
-	trace = TR_BoxTrace(&tile, start_l, end_l, traceBox, headnode, contentmask, brushrejects, fraction);
+	trace_t trace = TR_BoxTrace(&tile, start_l, end_l, traceBox, headnode, contentmask, brushrejects, fraction);
 	trace.mapTile = tile.idx;
 
 	if (rotated && trace.fraction != 1.0) {
@@ -194,7 +193,6 @@ int32_t CM_HeadnodeForBox (MapTile &tile, const vec3_t mins, const vec3_t maxs)
  */
 bool CM_EntTestLine (mapTiles_t *mapTiles, const vec3_t start, const vec3_t stop, const int levelmask, const char **entlist)
 {
-	trace_t trace;
 	const char **name;
 
 	/* trace against world first */
@@ -220,7 +218,7 @@ bool CM_EntTestLine (mapTiles_t *mapTiles, const vec3_t start, const vec3_t stop
 		if (CM_LineMissesModel(start, stop, model))
 			continue;
 
-		trace = CM_HintedTransformedBoxTrace(mapTiles->mapTiles[model->tile], start, stop, AABB(),
+		const trace_t trace = CM_HintedTransformedBoxTrace(mapTiles->mapTiles[model->tile], start, stop, AABB(),
 				model->headnode, MASK_VISIBILILITY, 0, model->origin, model->angles, model->shift, 1.0);
 		/* if we started the trace in a wall */
 		/* or the trace is not finished */
@@ -245,7 +243,6 @@ bool CM_EntTestLine (mapTiles_t *mapTiles, const vec3_t start, const vec3_t stop
  */
 bool CM_EntTestLineDM (mapTiles_t *mapTiles, const vec3_t start, const vec3_t stop, vec3_t end, const int levelmask, const char **entlist)
 {
-	trace_t trace;
 	const char **name;
 	bool blocked;
 	float fraction = 2.0f;
@@ -272,7 +269,7 @@ bool CM_EntTestLineDM (mapTiles_t *mapTiles, const vec3_t start, const vec3_t st
 		if (CM_LineMissesModel(start, stop, model))
 			continue;
 
-		trace = CM_HintedTransformedBoxTrace(mapTiles->mapTiles[model->tile], start, end, AABB(),
+		const trace_t trace = CM_HintedTransformedBoxTrace(mapTiles->mapTiles[model->tile], start, end, AABB(),
 				model->headnode, MASK_ALL, 0, model->origin, model->angles, vec3_origin, fraction);
 		/* if we started the trace in a wall */
 		if (trace.startsolid) {
@@ -303,11 +300,11 @@ bool CM_EntTestLineDM (mapTiles_t *mapTiles, const vec3_t start, const vec3_t st
  */
 trace_t CM_CompleteBoxTrace (mapTiles_t *mapTiles, const vec3_t start, const vec3_t end, const AABB &box, int levelmask, int brushmask, int brushreject)
 {
-	trace_t newtr, tr;
 	int tile, i;
 	vec3_t smin, smax, emin, emax, wpmins, wpmaxs;
 	const vec3_t offset = {UNIT_SIZE / 2, UNIT_SIZE / 2, UNIT_HEIGHT / 2};
 
+	trace_t tr;
 	OBJZERO(tr);
 	tr.fraction = 2.0f;
 
@@ -339,7 +336,7 @@ trace_t CM_CompleteBoxTrace (mapTiles_t *mapTiles, const vec3_t start, const vec
 			continue;
 		if (smin[2] > wpmaxs[2] && emin[2] > wpmaxs[2])
 			continue;
-		newtr = TR_TileBoxTrace(&myTile, start, end, box, levelmask, brushmask, brushreject);
+		trace_t newtr = TR_TileBoxTrace(&myTile, start, end, box, levelmask, brushmask, brushreject);
 		newtr.mapTile = tile;
 
 		/* memorize the trace with the minimal fraction */
@@ -367,30 +364,26 @@ trace_t CM_CompleteBoxTrace (mapTiles_t *mapTiles, const vec3_t start, const vec
  */
 trace_t CM_EntCompleteBoxTrace (mapTiles_t *mapTiles, const Line &traceLine, const AABB *traceBox, int levelmask, int brushmask, int brushreject, const char **list)
 {
-	trace_t trace, newtr;
-	const char **name;
-
 	/* trace against world first */
-	trace = CM_CompleteBoxTrace(mapTiles, traceLine.start, traceLine.stop, *traceBox, levelmask, brushmask, brushreject);
-	if (!list || trace.fraction == 0.0)
-		return trace;
+	const trace_t tr = CM_CompleteBoxTrace(mapTiles, traceLine.start, traceLine.stop, *traceBox, levelmask, brushmask, brushreject);
+	if (!list || tr.fraction == 0.0)
+		return tr;
 
 	AABB lineBox(traceLine);	/* Find the original bounding box for the tracing line. */
 	lineBox.add(*traceBox);		/* Now increase the bounding box by traceBox in both directions. */
 	/* Now lineBox specifies the whole volume to be traced through. */
 
-	for (name = list; *name; name++) {
-		vec3_t amins, amaxs;
-		const cBspModel_t *model;
-
+	trace_t trace;
+	for (const char **name = list; *name; name++) {
 		/* check whether this is really an inline model */
 		if (*name[0] != '*')
 			Com_Error(ERR_DROP, "name in the inlineList is no inline model: '%s'", *name);
-		model = CM_InlineModel(mapTiles, *name);
+		const cBspModel_t *model = CM_InlineModel(mapTiles, *name);
 		assert(model);
 		if (model->headnode >= mapTiles->mapTiles[model->tile].numnodes + 6)
 			continue;
 
+		vec3_t amins, amaxs;
 		/* Quickly calculate the bounds of this model to see if they can overlap. */
 		CM_CalculateBoundingBox(model, amins, amaxs);
 
@@ -399,7 +392,7 @@ trace_t CM_EntCompleteBoxTrace (mapTiles_t *mapTiles, const Line &traceLine, con
 		if (!lineBox.doesIntersect(modelBox))
 			continue;
 
-		newtr = CM_HintedTransformedBoxTrace(mapTiles->mapTiles[model->tile], traceLine.start, traceLine.stop, *traceBox,
+		trace_t newtr = CM_HintedTransformedBoxTrace(mapTiles->mapTiles[model->tile], traceLine.start, traceLine.stop, *traceBox,
 				model->headnode, brushmask, brushreject, model->origin, model->angles, model->shift, trace.fraction);
 
 		/* memorize the trace with the minimal fraction */

src/client/battlescape/cl_actor.cpp
@@ -1684,7 +1684,6 @@ static void CL_TargetingStraight (const pos3_t fromPos, actorSizeEnum_t fromActo
 {
 	vec3_t start, end;
 	vec3_t dir, mid, temp;
-	trace_t tr;
 	bool crossNo;
 	le_t *target = NULL;
 	actorSizeEnum_t toActorSize;
@@ -1722,7 +1721,7 @@ static void CL_TargetingStraight (const pos3_t fromPos, actorSizeEnum_t fromActo
 	/* switch up to top level, this is needed to make sure our trace doesn't go through ceilings ... */
 	/** @todo is this really needed for straight targetting? - for grenades, yes, but not for straight no?
 	 * cl_worldlevel->integer should be enough here */
-	tr = CL_Trace(start, temp, AABB(), selActor, NULL, MASK_SHOT, cl.mapMaxLevel - 1);
+	trace_t tr = CL_Trace(start, temp, AABB(), selActor, NULL, MASK_SHOT, cl.mapMaxLevel - 1);
 	if (tr.le && (tr.le->team == cls.team || LE_IsCivilian(tr.le)) && LE_IsCrouched(tr.le))
 		VectorMA(start, UNIT_SIZE * 1.4, dir, temp);
 	else
@@ -1767,7 +1766,6 @@ static void CL_TargetingGrenade (const pos3_t fromPos, actorSizeEnum_t fromActor
 	vec3_t from, at, cross;
 	float vz, dt;
 	vec3_t v0, ds, next;
-	trace_t tr;
 	bool obstructed = false;
 	int i;
 	le_t *target = NULL;
@@ -1818,7 +1816,7 @@ static void CL_TargetingGrenade (const pos3_t fromPos, actorSizeEnum_t fromActor
 
 		/* trace for obstacles. Switch up to top level, to make sure our trace
 		 * doesn't go through ceilings ... */
-		tr = CL_Trace(from, next, AABB(), selActor, target, MASK_SHOT, cl.mapMaxLevel - 1);
+		const trace_t tr = CL_Trace(from, next, AABB(), selActor, target, MASK_SHOT, cl.mapMaxLevel - 1);
 
 		/* something was hit */
 		if (tr.fraction < 1.0 && (!tr.le || (!LE_IsInvisible(tr.le) && !VectorCompare(tr.le->pos, toPos)))) {

src/client/battlescape/cl_localentity.cpp
@@ -550,7 +550,6 @@ static void LE_PlayFootStepSound (le_t *le)
 	}
 	/* walking in water will not play the normal footstep sounds */
 	if (!le->pathContents[le->pathPos]) {
-		trace_t trace;
 		vec3_t from, to;
 
 		/* prepare trace vectors */
@@ -559,7 +558,7 @@ static void LE_PlayFootStepSound (le_t *le)
 		/* we should really hit the ground with this */
 		to[2] -= UNIT_HEIGHT;
 
-		trace = CL_Trace(from, to, AABB(), NULL, NULL, MASK_SOLID, cl_worldlevel->integer);
+		const trace_t trace = CL_Trace(from, to, AABB(), NULL, NULL, MASK_SOLID, cl_worldlevel->integer);
 		if (trace.surface)
 			LE_PlaySoundFileAndParticleForSurface(le, trace.surface->name);
 	} else
@@ -1672,7 +1671,6 @@ static void CL_ClipMoveToLEs (moveclip_t *clip)
 
 	while ((le = LE_GetNextInUse(le))) {
 		int tile = 0;
-		trace_t trace;
 		int32_t headnode;
 		vec3_t angles;
 		vec3_t origin, shift;
@@ -1687,7 +1685,7 @@ static void CL_ClipMoveToLEs (moveclip_t *clip)
 
 		VectorCopy(le->origin, origin);
 
-		trace = CM_HintedTransformedBoxTrace(cl.mapTiles->mapTiles[tile], clip->start, clip->end, AABB(clip->mins, clip->maxs),
+		trace_t trace = CM_HintedTransformedBoxTrace(cl.mapTiles->mapTiles[tile], clip->start, clip->end, AABB(clip->mins, clip->maxs),
 				headnode, clip->contentmask, 0, origin, angles, shift, 1.0);
 
 		if (trace.fraction < clip->trace.fraction) {

src/client/battlescape/cl_particle.cpp
@@ -931,7 +931,6 @@ static void CL_ParticleRun2 (ptl_t *p)
 
 	/* basic 'physics' for particles */
 	if (p->physics) {
-		trace_t tr;
 		vec3_t mins, maxs;
 		const float size = std::max(p->size[0], p->size[1]);
 
@@ -948,7 +947,7 @@ static void CL_ParticleRun2 (ptl_t *p)
 
 		VectorSet(mins, -size, -size, -size);
 		VectorSet(maxs, size, size, size);
-		tr = PTL_Trace(p, mins, maxs);
+		const trace_t tr = PTL_Trace(p, mins, maxs);
 
 		/* hit something solid */
 		if (tr.fraction < 1.0 || tr.startsolid) {

src/client/renderer/r_lightmap.cpp
@@ -320,7 +320,6 @@ void R_Trace (const vec3_t start, const vec3_t end, float size, int contentmask)
 {
 	vec3_t mins, maxs;
 	float frac;
-	trace_t tr;
 	int i;
 
 	r_locals.tracenum++;
@@ -345,7 +344,7 @@ void R_Trace (const vec3_t start, const vec3_t end, float size, int contentmask)
 		if (!m || m->type != mod_bsp_submodel)
 			continue;
 
-		tr = CM_TransformedBoxTrace(refdef.mapTiles->mapTiles[m->bsp.maptile], start, end, box, m->bsp.firstnode,
+		trace_t tr = CM_TransformedBoxTrace(refdef.mapTiles->mapTiles[m->bsp.maptile], start, end, box, m->bsp.firstnode,
 				contentmask, 0, ent->origin, ent->angles);
 
 		if (tr.fraction < frac) {

src/common/routing.cpp
@@ -288,7 +288,6 @@ void RT_GetMapSize (mapTiles_t *mapTiles, vec3_t map_min, vec3_t map_max)
 	pos3_t start, end, test;
 	vec3_t origin;
 	int i;
-	trace_t trace;
 
 	/* Initialize start, end, and normal */
 	VectorSet(start, 0, 0, 0);
@@ -307,7 +306,7 @@ void RT_GetMapSize (mapTiles_t *mapTiles, vec3_t map_min, vec3_t map_max)
 			PosToVec(end, box.maxs);
 			VectorAdd(box.maxs, normal, box.maxs);
 			/* Test for stuff in a small box, if there is something then exit while */
-			trace = RT_COMPLETEBOXTRACE_SIZE(mapTiles, &box, NULL);
+			const trace_t trace = RT_COMPLETEBOXTRACE_SIZE(mapTiles, &box, NULL);
 			if (trace.fraction < 1.0)
 				break;
 			/* There is nothing, lower the boundary. */
@@ -325,7 +324,7 @@ void RT_GetMapSize (mapTiles_t *mapTiles, vec3_t map_min, vec3_t map_max)
 			PosToVec(test, box.maxs);
 			VectorAdd(box.maxs, normal, box.maxs);
 			/* Test for stuff in a small box, if there is something then exit while */
-			trace = RT_COMPLETEBOXTRACE_SIZE(mapTiles, &box, NULL);
+			const trace_t trace = RT_COMPLETEBOXTRACE_SIZE(mapTiles, &box, NULL);
 			if (trace.fraction < 1.0)
 				break;
 			/* There is nothing, raise the boundary. */
@@ -417,7 +416,6 @@ int RT_CheckCell (mapTiles_t *mapTiles, Routing &routing, const int actorSize, c
 	int bottomQ, topQ; /* The floor and ceiling in QUANTs */
 	int i;
 	int fz, cz; /* Floor and ceiling Z cell coordinates */
-	trace_t tr;
 
 	assert(actorSize > ACTOR_SIZE_INVALID && actorSize <= ACTOR_MAX_SIZE);
 	/* x and y cannot exceed PATHFINDING_WIDTH - actorSize */
@@ -452,7 +450,7 @@ int RT_CheckCell (mapTiles_t *mapTiles, Routing &routing, const int actorSize, c
 	 *      restart below the current floor.
 	 */
 	for (;;) { /* Loop forever, we will exit if we hit the model bottom or find a valid floor. */
-		tr = RT_COMPLETEBOXTRACE_PASSAGE(mapTiles, Line(start, end), &footBox, list);
+		trace_t tr = RT_COMPLETEBOXTRACE_PASSAGE(mapTiles, Line(start, end), &footBox, list);
 		if (tr.fraction >= 1.0) {						/* If there is no brush underneath this starting point, */
 			routing.setFilled(actorSize, x, y, 0, z);	/* mark all cells to the model base as filled. */
 			return 0;									/* return (a z-value of)0 to indicate we just scanned the model bottom. */
@@ -685,7 +683,6 @@ static trace_t RT_ObstructedTrace (const RoutingData *rtd, const Line &traceLine
 static int RT_FindOpeningFloorFrac (const RoutingData *rtd, const vec3_t start, const vec3_t end, const float frac, const int startingHeight)
 {
 	vec3_t mstart, mend;	/**< Midpoint line to trace across */	/**< Tracing box extents */
-	trace_t tr;
 	const AABB* box = (rtd->actorSize == ACTOR_SIZE_NORMAL ? &actor1x1Box : &actor2x2Box);
 
 	/* Position mstart and mend at the fraction point */
@@ -694,7 +691,7 @@ static int RT_FindOpeningFloorFrac (const RoutingData *rtd, const vec3_t start, 
 	mstart[2] = QuantToModel(startingHeight) + (QUANT / 2); /* Set at the starting height, plus a little more to keep us off a potential surface. */
 	mend[2] = -QUANT;  /* Set below the model. */
 
-	tr = RT_COMPLETEBOXTRACE_PASSAGE(rtd->mapTiles, Line(mstart, mend), box, rtd->list);
+	const trace_t tr = RT_COMPLETEBOXTRACE_PASSAGE(rtd->mapTiles, Line(mstart, mend), box, rtd->list);
 
 	if (debugTrace)
 		Com_Printf("Brush found at %f.\n", tr.endpos[2]);
@@ -718,7 +715,6 @@ static int RT_FindOpeningFloorFrac (const RoutingData *rtd, const vec3_t start, 
 static int RT_FindOpeningCeilingFrac (const RoutingData *rtd, const vec3_t start, const vec3_t end, const float frac, const int startingHeight)
 {
 	vec3_t mstart, mend;	/**< Midpoint line to trace across */
-	trace_t tr;
 	const AABB* box = (rtd->actorSize == ACTOR_SIZE_NORMAL ? &actor1x1Box : &actor2x2Box);	/**< Tracing box extents */
 
 	/* Position mstart and mend at the midpoint */
@@ -727,7 +723,7 @@ static int RT_FindOpeningCeilingFrac (const RoutingData *rtd, const vec3_t start
 	mstart[2] = QuantToModel(startingHeight) - (QUANT / 2); /* Set at the starting height, minus a little more to keep us off a potential surface. */
 	mend[2] = UNIT_HEIGHT * PATHFINDING_HEIGHT + QUANT;  /* Set above the model. */
 
-	tr = RT_COMPLETEBOXTRACE_PASSAGE(rtd->mapTiles, Line(mstart, mend), box, rtd->list);
+	const trace_t tr = RT_COMPLETEBOXTRACE_PASSAGE(rtd->mapTiles, Line(mstart, mend), box, rtd->list);
 
 	if (debugTrace)
 		Com_Printf("Brush found at %f.\n", tr.endpos[2]);
@@ -850,7 +846,7 @@ static int RT_CalcNewZ (const RoutingData *rtd, const int ax, const int ay, cons
  */
 static int RT_TraceOpening (const RoutingData *rtd, const vec3_t start, const vec3_t end, const int ax, const int ay, const int bottom, const int top, int lo, int hi, int *foundLow, int *foundHigh)
 {
-	trace_t tr = RT_ObstructedTrace(rtd, Line(start, end), hi, lo);
+	const trace_t tr = RT_ObstructedTrace(rtd, Line(start, end), hi, lo);
 	if (tr.fraction >= 1.0) {
 		lo = RT_FindOpeningFloor(rtd, start, end, lo, bottom);
 		hi = RT_FindOpeningCeiling(rtd, start, end, hi, top);
@@ -916,7 +912,6 @@ static int RT_FindOpening (RoutingData *rtd, const place_t *from, const int ax, 
 	 && from->cell[2] * CELL_HEIGHT + rtd->routing.getCeiling(rtd->actorSize, ax, ay, from->cell[2]) >= PATHFINDING_HEIGHT * CELL_HEIGHT) {
 		vec3_t sky, earth;
 		const AABB* box = (rtd->actorSize == ACTOR_SIZE_NORMAL ? &actor1x1Box : &actor2x2Box);
-		trace_t tr;
 		int tempBottom;
 
 		VectorInterpolation(start, end, 0.5, sky);	/* center it halfway between the cells */
@@ -924,7 +919,7 @@ static int RT_FindOpening (RoutingData *rtd, const place_t *from, const int ax, 
 		sky[2] = UNIT_HEIGHT * PATHFINDING_HEIGHT;  /* Set to top of model. */
 		earth[2] = QuantToModel(bottom);
 
-		tr = RT_COMPLETEBOXTRACE_PASSAGE(rtd->mapTiles, Line(sky, earth), box, rtd->list);
+		const trace_t tr = RT_COMPLETEBOXTRACE_PASSAGE(rtd->mapTiles, Line(sky, earth), box, rtd->list);
 		tempBottom = ModelFloorToQuant(tr.endpos[2]);
 		if (tempBottom <= bottom + PATHFINDING_MIN_STEPUP) {
 			const int hi = bottom + PATHFINDING_MIN_OPENING;
@@ -985,7 +980,6 @@ static int RT_MicroTrace (RoutingData *rtd, const place_t *from, const int ax, c
 	float sx, sy, ex, ey;
 	/* Shortcut the value of UNIT_SIZE / PATHFINDING_MICROSTEP_SIZE. */
 	const int steps = UNIT_SIZE / PATHFINDING_MICROSTEP_SIZE;
-	trace_t tr;
 	int i, current_h, highest_h, highest_i = 0, skipped, newBottom;
 	vec3_t start, end;
 	pos3_t pos;
@@ -1020,7 +1014,7 @@ static int RT_MicroTrace (RoutingData *rtd, const place_t *from, const int ax, c
 		start[1] = end[1] = sy + (ey - sy) * (i / (float)steps);
 
 		/* perform the trace, then return true if the trace was obstructed. */
-		tr = RT_COMPLETEBOXTRACE_PASSAGE(rtd->mapTiles, Line(start, end), &footBox, rtd->list);
+		const trace_t tr = RT_COMPLETEBOXTRACE_PASSAGE(rtd->mapTiles, Line(start, end), &footBox, rtd->list);
 		if (tr.fraction >= 1.0) {
 			bases[i] = -1;
 		} else {

src/common/tracing.cpp
@@ -1088,9 +1088,9 @@ trace_t TR_BoxTrace (TR_TILE_TYPE *tile, const vec3_t start, const vec3_t end, c
  */
 trace_t TR_TileBoxTrace (TR_TILE_TYPE *myTile, const vec3_t start, const vec3_t end, const AABB &aabb, const int levelmask, const int brushmask, const int brushreject)
 {
-	trace_t newtr, tr;
 	int i;
 	cBspHead_t *h;
+	trace_t tr;
 
 	OBJZERO(tr);
 	/* ensure that the first trace is set in every case */
@@ -1106,7 +1106,7 @@ trace_t TR_TileBoxTrace (TR_TILE_TYPE *myTile, const vec3_t start, const vec3_t 
 			continue;
 
 		assert(h->cnode < myTile->numnodes + 6); /* +6 => bbox */
-		newtr = TR_BoxTrace(myTile, start, end, aabb, h->cnode, brushmask, brushreject, tr.fraction);
+		const trace_t newtr = TR_BoxTrace(myTile, start, end, aabb, h->cnode, brushmask, brushreject, tr.fraction);
 
 		/* memorize the trace with the minimal fraction */
 		if (newtr.fraction == 0.0)

src/game/g_combat.cpp
@@ -831,7 +831,6 @@ static void G_ShootSingle (Edict *ent, const fireDef_t *fd, const vec3_t from, c
 	vec3_t cur_loc;		/* The current location of the projectile. */
 	vec3_t temp;
 	vec3_t tracefrom;	/* sum */
-	trace_t tr;			/* the traceing */
 	float acc;			/* Accuracy modifier for the angle of the shot. */
 	float range;
 	float gauss1;
@@ -902,7 +901,7 @@ static void G_ShootSingle (Edict *ent, const fireDef_t *fd, const vec3_t from, c
 		damage = std::max(0.0f, fd->damage[0] + (fd->damage[1] * crand()));
 
 	VectorMA(cur_loc, UNIT_SIZE, dir, impact);
-	tr = G_Trace(cur_loc, impact, ent, MASK_SHOT);
+	trace_t tr = G_Trace(cur_loc, impact, ent, MASK_SHOT);
 	Edict *trEnt = G_EdictsGetByNum(tr.entNum);	/* the ent possibly hit by the trace */
 	if (trEnt && (trEnt->team == ent->team || G_IsCivilian(trEnt)) && G_IsCrouched(trEnt) && !FIRESH_IsMedikit(fd))
 		VectorMA(cur_loc, UNIT_SIZE * 1.4, dir, cur_loc);

commit 7ef5b6ebec0b9e2928ad9683e96d008b9ae2370c

* fixed missing trace_t initialization
* regression introduced in ec797bb57134f6a3e8fcb0c98056029f35622dad

src/common/cmodel.cpp
@@ -373,7 +373,7 @@ trace_t CM_EntCompleteBoxTrace (mapTiles_t *mapTiles, const Line &traceLine, con
 	lineBox.add(*traceBox);		/* Now increase the bounding box by traceBox in both directions. */
 	/* Now lineBox specifies the whole volume to be traced through. */
 
-	trace_t trace;
+	trace_t trace = tr;
 	for (const char **name = list; *name; name++) {
 		/* check whether this is really an inline model */
 		if (*name[0] != '*')