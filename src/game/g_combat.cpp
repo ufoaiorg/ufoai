63f9fa97867b7413c9d4383aede0a211c8cb21d0
Martin Gerhardy <martin.gerhardy@gmail.com>
2013-06-11 12:19:48 +0200

    * implemented fr #5024 (allow to fire GL behind crouched teammate)

--- src/game/g_combat.cpp
+++ src/game/g_combat.cpp
@@ -692,6 +692,13 @@ static void G_ShootGrenade (const Player &player, Edict *ent, const fireDef_t *f
 		trace_t tr = G_Trace(oldPos, newPos, ent, MASK_SHOT);
 		if (tr.fraction < 1.0 || time + dt > 4.0) {
 			Edict *trEnt = G_EdictsGetByNum(tr.entNum);	/* the ent possibly hit by the trace */
+			if (trEnt && (trEnt->team == ent->team || G_IsCivilian(trEnt)) && G_IsCrouched(trEnt)) {
+				dt += GRENADE_DT;
+				VectorCopy(newPos, oldPos);
+				VectorCopy(newPos, impact);
+				continue;
+			}
+
 			const float bounceFraction = tr.surface ? gi.GetBounceFraction(tr.surface->name) : 1.0f;
 			int i;
 
