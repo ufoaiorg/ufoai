576af34c107c926ca4f1242a30f9e125e59bc75e
Martin Gerhardy <martin.gerhardy@gmail.com>
2013-08-06 14:20:02 +0200

    * fixed windows build with sdl2

--- build/platforms/mingw32.mk
+++ build/platforms/mingw32.mk
@@ -17,8 +17,13 @@ endif
 PKG_CONFIG               ?= $(CROSS)pkg-config
 OPENGL_LIBS              ?= -lopengl32
 OPENAL_LIBS              ?= $(call PKG_LIBS,openal,OpenAL32)
+ifdef HAVE_SDL2_TTF_SDL_TTF_H
+SDL_TTF_LIBS             ?= $(call PKG_LIBS,SDL2_ttf) $(call PKG_LIBS,freetype2)
+SDL_TTF_CFLAGS           ?= $(call PKG_CFLAGS,SDL2_ttf) $(call PKG_CFLAGS,freetype2)
+else
 SDL_TTF_LIBS             ?= $(call PKG_LIBS,SDL_ttf) $(call PKG_LIBS,freetype2)
 SDL_TTF_CFLAGS           ?= $(call PKG_CFLAGS,SDL_ttf) $(call PKG_CFLAGS,freetype2)
+endif
 INTL_LIBS                ?= -lintl
 
 ufo_LDFLAGS              += -lws2_32 -lwinmm -lgdi32 -lfreetype -mwindows
