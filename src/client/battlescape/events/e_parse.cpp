ufoaiorg/ufoai

commit ef9c4a929599d2e83064021a6f4fe20e76a5fde8

* activate the event delaying for testing

src/common/common.cpp
@@ -1389,7 +1389,7 @@ ScheduleEventPtr Dequeue_Event (int now)
 			return event;
 		}
 
-#if 0
+#if 1
 		/* delay all other events if this one is blocked */
 		if (event->delayFollowing > 0) {
 			EventPriorityQueue reOrder;

commit a471620f56d712267a9b2b1d068c11eda1e5d2cd

* fixed bug #5130 (Battlescape issues)
* This bug was introduced in ef9c4a92 (due to activating the event delays)
* New battlescape events can get scheduled before the old (delayed) events, because the eventTiming_t data wasn't advanced while event execution was blocked.

src/client/battlescape/events/e_parse.cpp
@@ -120,6 +120,24 @@ static bool CL_CheckBattlescapeEvent (int now, void* data)
 }
 
 /**
+ * @brief If we delayed the battlescape events due to event locking (e.g. le is locked or camera is locked),
+ * we have to advance the time for new events, too. Otherwise they might be scheduled before the just delayed
+ * events.
+ * @param[in] now
+ * @param[in] data The userdata that is passed to the function callback. In our case this is the
+ * @c eventTiming_t data
+ * @param[in] delay The milliseconds that the battlescape events were delayed and that we have to use to
+ * advance the @c eventTiming_t values.
+ */
+static void CL_NotifyBattlescapeEventDelay (int now, void* data, int delay)
+{
+	eventTiming_t *eventTiming = (eventTiming_t *)data;
+	eventTiming->impactTime += delay;
+	eventTiming->nextTime += delay;
+	eventTiming->shootTime += delay;
+}
+
+/**
  * @brief Checks if a given battlescape event should get delayed.
  * @param now The current time.
  */
@@ -269,6 +287,8 @@ event_t CL_ParseEvent (dbuffer *msg)
 		const int when = CL_GetEventTime(cur->eType, msg, &eventTiming);
 		ScheduleEventPtr e = Schedule_Event(when, &CL_ExecuteBattlescapeEvent, &CL_CheckBattlescapeEvent, &CL_FreeBattlescapeEvent, cur);
 		e->delayFollowing = 50;
+		e->notifyDelay = &CL_NotifyBattlescapeEventDelay;
+		e->notifyDelayUserData = (void*)&eventTiming;
 		e->delay = &CL_DelayBattlescapeEvent;
 
 		Com_DPrintf(DEBUG_EVENTSYS, "event(at %d): %s %p\n", when, eventData->name, (void*)cur);

src/common/common.cpp
@@ -1456,6 +1456,8 @@ ScheduleEventPtr Dequeue_Event (int now)
 		/* delay all other events if this one is blocked */
 		if (event->delayFollowing > 0) {
 			Delay_Events(now, i);
+			if (event->notifyDelay != nullptr)
+				event->notifyDelay(now, event->notifyDelayUserData, event->delayFollowing);
 			break;
 		}
 	}

src/common/common.h
@@ -312,6 +312,7 @@ void Con_Print(const char* txt);
 typedef void event_func(int now, void* data);
 typedef bool event_check_func(int now, void* data);
 typedef bool event_delay_func(int now, void* data);
+typedef void event_notify_delay_func(int now, void* data, int delay);
 /**
  * @return @c true to keep the event, @c false to remove it from the queue
  */
@@ -331,6 +332,8 @@ struct scheduleEvent_t {
 	 * @param[in] data The event userdata
 	 */
 	event_delay_func *delay;
+	event_notify_delay_func *notifyDelay;
+	void* notifyDelayUserData;
 	event_clean_func *clean;
 	void* data;
 };