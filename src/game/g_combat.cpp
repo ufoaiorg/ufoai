1ddfbb5e3f56bcf7574440a58f670025cb76b0b8
Martin Gerhardy <martin.gerhardy@gmail.com>
2013-09-21 12:16:43 +0200

    * fixed typo

--- src/game/g_combat.cpp
+++ src/game/g_combat.cpp
@@ -783,7 +783,7 @@ static void G_ShootGrenade (const Player &player, Edict* ent, const fireDef_t* f
 #ifdef DEBUG
 /**
  * @brief Displays the results of a trace. Used to see if a bullet hit something.
- * @param[in] start The starting loaction of the trace.
+ * @param[in] start The starting location of the trace.
  * @param[in] tr The trace data.
  */
 static void DumpTrace (vec3_t start, const trace_t &tr)
