4335dde8a9b8c51dafedeea762669566dcd3a421
Duke <aduke1@gmx.de>
2013-11-28 15:40:40 +0100

    * tabs

--- src/client/cgame/campaign/cp_research.h
+++ src/client/cgame/campaign/cp_research.h
@@ -118,16 +118,16 @@ typedef struct techMail_s {
 } techMail_t;
 
 typedef enum {
-	MAILSENT_NONE,		/**< No mail sent yet. */
-	MAILSENT_PROPOSAL,	/**< Mail for research proposal already send. */
-	MAILSENT_FINISHED,	/**< Mail for finished research sent (and maybe the previous as well). */
+	MAILSENT_NONE,			/**< No mail sent yet. */
+	MAILSENT_PROPOSAL,		/**< Mail for research proposal already send. */
+	MAILSENT_FINISHED,		/**< Mail for finished research sent (and maybe the previous as well). */
 
 	MAILSENT_MAX
 } mailSentType_t;
 
 typedef struct technologyDescriptions_s {
-	int numDescriptions;	/**< The number of descriptions. */
-	int usedDescription;	/**< The index of the first used description, so we do not get a different text each time it should be displayed. undef=-1
+	int numDescriptions;			/**< The number of descriptions. */
+	int usedDescription;			/**< The index of the first used description, so we do not get a different text each time it should be displayed. undef=-1
 				 * @todo Check if we should set this in the function that updates the research_proposals? Currently it's only in RS_GetDescription. */
 	char* text[MAX_DESCRIPTIONS];	/**< A list of descriptions (Short text-id to get the full text via gettext). */
 	char* tech[MAX_DESCRIPTIONS];	/**< The technology to check (i.e. are its requirements met?) if this description should be displayed. */
@@ -135,9 +135,9 @@ typedef struct technologyDescriptions_s {
 
 /** @brief This is the technology parsed from research.ufo */
 typedef struct technology_s {
-	char* id;		/**< Short (unique) id/name. */
-	int idx;		/**< Self-link in the global list */
-	char* name;		/**< Full name of this technology. */
+	char* id;						/**< Short (unique) id/name. */
+	int idx;						/**< Self-link in the global list */
+	char* name;						/**< Full name of this technology. */
 	technologyDescriptions_t description;		/**< Descriptions of researched item.  */
 	technologyDescriptions_t preDescription;	/**< Descriptions of item before it's researched. */
 	researchType_t type;			/**< Defines what type this tech-entry is an where to search for other information "tech", "weapon" etc... see research.ufo for more */
@@ -145,44 +145,44 @@ typedef struct technology_s {
 	struct technology_s* redirect;	/**< Set this to the entry that is supposed to get displayed instead of this one.
 									 * Mostly used for e.g ammo that doesn't need its own description but rather the one for the weapon. */
 
-	requirements_t requireAND;	/**< A list of requirements that ALL need to be met (= AND-related) See struct above. */
-	requirements_t requireOR;	/**< A list of requirements where ANY need to be met (= OR-related) See struct above. */
-	bool statusCollected;		/**< Did we loot any items of this tech?
-								* This is updated from the info stored in the requireOR and requireAND lists.
-								* @see RS_CheckCollected. */
+	requirements_t requireAND;		/**< A list of requirements that ALL need to be met (= AND-related) See struct above. */
+		requirements_t requireOR;	/**< A list of requirements where ANY need to be met (= OR-related) See struct above. */
+		bool statusCollected;		/**< Did we loot any items of this tech?
+									* This is updated from the info stored in the requireOR and requireAND lists.
+									* @see RS_CheckCollected. */
 
-	char* provides;				/**< The item that this technology enables. */
+	char* provides;					/**< The item that this technology enables. */
 
-	float overallTime, time;	/**< The time that is needed to research this tech. (in days).
-					 * "overallTime" stays always the same,
-					 * "time" will be modified when it is under research.*/
-	int delay;			/**< @todo Number in days the system should wait until the tech is available for research.
-					 * Starting from the first time all other dependencies have been fulfilled and
-					 * counting only if a day has passed and they still are met. */
+	float overallTime, time;		/**< The time that is needed to research this tech. (in days).
+									* "overallTime" stays always the same,
+									* "time" will be modified when it is under research.*/
+	int delay;						/**< @todo Number in days the system should wait until the tech is available for research.
+									* Starting from the first time all other dependencies have been fulfilled and
+									* counting only if a day has passed and they still are met. */
 
 	researchStatus_t statusResearch;	/**< Current status of the research. */
-	char* finishedResearchEvent;		/**< nullptr or string with scriptable commands that are executed after the tech was successfully researched */
-	bool announce;			/**< if this is true the ufopedia will be opened on finishing the research */
+	char* finishedResearchEvent;	/**< nullptr or string with scriptable commands that are executed after the tech was successfully researched */
+	bool announce;					/**< if this is true the ufopedia will be opened on finishing the research */
 
-	struct base_s	*base;	/**< The base this tech is researched in. */
-	int scientists;			/**< How many scientists (from "base") are researching this tech. */
+	struct base_s	*base;			/**< The base this tech is researched in. */
+	int scientists;					/**< How many scientists (from "base") are researching this tech. */
 
-	char* image;			/**< Image to display in the Ufopedia and other menus for this tech.
-							 * If not set in the .ufo file this is auto-set in RS_InitTree.
-							 * @sa cl_research.c: RS_InitTree */
-	char* mdl;				/**< Same as "image" but it's a 3d model.. */
+	char* image;					/**< Image to display in the Ufopedia and other menus for this tech.
+									* If not set in the .ufo file this is auto-set in RS_InitTree.
+									* @sa cl_research.c: RS_InitTree */
+	char* mdl;						/**< Same as "image" but it's a 3d model.. */
 
 	bool statusResearchable;		/**< Is this item researchable? */
 
-	int produceTime;			/**< How many hours the production of this items runs. */
-	mailSentType_t mailSent;	/**< Store if a Mail has been sent to the commander (player). */
+	int produceTime;				/**< How many hours the production of this items runs. */
+	mailSentType_t mailSent;		/**< Store if a Mail has been sent to the commander (player). */
 	requirements_t requireForProduction;	/**< A list of items that are needed (and used up) on production of _this_ item.
-						 * Only "item"-type is allowed.
-						 * All requirements need to be fulfilled in order for _one_ item to be produced.
-						 * This check is done for each item.*/
+									* Only "item"-type is allowed.
+									* All requirements need to be fulfilled in order for _one_ item to be produced.
+									* This check is done for each item.*/
 
-	date_t preResearchedDate;	/**< Date for UFOpaedia. Research proposal. */
-	date_t researchedDate;		/**< Date for UFOpaedia. Finished research. */
+	date_t preResearchedDate;		/**< Date for UFOpaedia. Research proposal. */
+	date_t researchedDate;			/**< Date for UFOpaedia. Finished research. */
 
 	markResearched_t markResearched;	/**< Mark as researched at parsing state - but i only know the date if we already started a campaign. */
 
