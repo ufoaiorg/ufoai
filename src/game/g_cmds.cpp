32aa1222f39becb3741f32f5c44dfc61e9fd86cb
DarkRain <darkrainx@users.sourceforge.net>
2014-02-19 04:43:42 +0000

    * Implement FR #5277 (Extend debug info about alien´s inventory/encumbrance)

--- src/game/g_cmds.cpp
+++ src/game/g_cmds.cpp
@@ -314,6 +314,14 @@ void G_InvList_f (const Player& player)
 					Com_Printf(".... ammo:   %s (%i)\n", item->ammoDef()->id, item->getAmmoLeft());
 			}
 		}
+		const float invWeight = ent->chr.inv.getWeight();
+		const int maxWeight = ent->chr.score.skills[ABILITY_POWER];
+		const float penalty = GET_ENCUMBRANCE_PENALTY(invWeight, maxWeight);
+		const int normalTU = GET_TU(ent->chr.score.skills[ABILITY_SPEED], 1.0f - WEIGHT_NORMAL_PENALTY);
+		const int tus = GET_TU(ent->chr.score.skills[ABILITY_SPEED], penalty);
+		const int tuPenalty = tus - normalTU;
+		const char* penaltyStr = 1.0f - penalty < WEIGHT_NORMAL_PENALTY ? "'Light weight'" : (1.0f - penalty < WEIGHT_HEAVY_PENALTY ? "'Normal weight'" : "'Encumbered'");
+		Com_Printf("Weight: %g/%i, Encumbrance: %s (%g%%), TU's: %i (normal: %i, penalty/bonus: %+i)\n", invWeight, maxWeight, penaltyStr, invWeight / maxWeight * 100.0f, tus, normalTU, tuPenalty);
 	}
 }
 
