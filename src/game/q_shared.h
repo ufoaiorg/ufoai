0027d2e97ccca4e7138c021d48d8db9a282a0f32
Duke <aduke1@gmx.de>
2014-04-11 20:53:47 +0200

    * corrected comment

--- src/game/q_shared.h
+++ src/game/q_shared.h
@@ -301,7 +301,7 @@ typedef int32_t shoot_types_t;
 /**
  * config strings are a general means of communication from
  * the server to all connected clients.
- * Each config string can be at most MAX_QPATH characters. */
+ * Each config string can be at most MAX_TOKEN_CHARS characters. */
 #define CS_NAME				0
 #define CS_MAPTITLE			1		/**< display map title string - translated client side */
 #define CS_MAXCLIENTS		2
