a8fc5503f9ea719fe4feee193ef64eaeceef3daf
Duke <aduke1@gmx.de>
2014-04-18 16:45:15 +0200

    * another ctor for terrainDef_s

--- src/game/q_shared.h
+++ src/game/q_shared.h
@@ -363,6 +363,15 @@ struct terrainDef_s		/* a single entry in the table */
 	float snowChance;
 
 	terrainDef_s() : rgbRed(128), rgbGreen(255), rgbBlue(0), terrainName("grass"), survivalChance(2.5), rainChance(0.2), snowChance(0.1) {}
+	terrainDef_s(byte _rgbRed, byte _rgbGreen, byte _rgbBlue, const char* _terrainName, float _survivalChance, float _rainChance, float _snowChance) {
+		rgbRed = _rgbRed;
+		rgbGreen = _rgbGreen;
+		rgbBlue = _rgbBlue;
+		terrainName = _terrainName;
+		survivalChance = _survivalChance;
+		rainChance = _rainChance;
+		snowChance = _snowChance;
+	}
 };
 
 typedef struct mapDef_s {
