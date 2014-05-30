ea27726de782543eab402bfeeee8ea8b30436b6c
Duke <aduke1@gmx.de>
2014-05-30 11:47:17 +0200

    * removed UFO_CanDoSupplyMission

--- src/client/cgame/campaign/cp_ufo.cpp
+++ src/client/cgame/campaign/cp_ufo.cpp
@@ -159,18 +159,6 @@ static bool UFO_CanDoBaseAttackMission (const ufoType_t type)
 }
 
 /**
- * @brief Check if the UFO type is available for recon missions
- * @param type The UFO type to check
- */
-static bool UFO_CanDoSupplyMission (const ufoType_t type)
-{
-	if (type == UFO_SUPPLY)
-		return true;
-
-	return false;
-}
-
-/**
  * @brief Fill an array with available UFOs for the mission type.
  * @param[in] missionType The kind ofmission we are currently creating.
  * @param[out] ufoTypes Array of ufoType_t that may be used for this mission.
@@ -190,7 +178,7 @@ int UFO_GetAvailableUFOsForMission (const interestCategory_t missionType, ufoTyp
 				if (UFO_CanDoBaseAttackMission(uType))
 					break;
 			case INTERESTCATEGORY_SUPPLY:
-				if (UFO_CanDoSupplyMission(uType))
+				if (UFO_CanDoMission(uType, "supply"))
 					break;
 			default:
 				continue;
