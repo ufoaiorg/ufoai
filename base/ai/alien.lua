bed99175cd97d7a674c067f8805c4676d196f9ca
DarkRain <darkrainx@users.sourceforge.net>
2014-06-08 22:42:02 -0500

    * Let the LUA AÏ reload and grab weapons.

--- base/ai/alien.lua
+++ base/ai/alien.lua
@@ -3,24 +3,66 @@
 --[[
 	AI entry point.  Run at the start of the AI's turn.
 --]]
-function think()
+function think ()
 
-	-- Look around for potential targets.  We prioritize phalanx.
-	phalanx  = ai.see("all","phalanx")
-
-	-- Choose proper action
-	if #phalanx < 1 then
-
-		civilian = ai.see("all","civilian")
-		if #civilian < 1 then
-			search()
+	-- Try to make sure we have a weapon
+	if not readyweapon() then
+		-- No weapon, retreat
+		teammates = ai.see("all", "alien")
+		if #teammates > 0 then
+			herd( teammates[1] )
 		else
-			engage( civilian[1] )
+			hide()
 		end
 	else
-		engage( phalanx[1] )
+		-- Look around for potential targets.  We prioritize phalanx.
+		phalanx  = ai.see("all","phalanx")
+		-- Choose proper action
+		if #phalanx < 1 then
+			civilian = ai.see("all","civilian")
+			if #civilian < 1 then
+				search()
+			else
+				engage( civilian[1] )
+			end
+		else
+			engage( phalanx[1] )
+		end
+	end
+end
+
+
+--[[
+	Search for a new weapon to grab
+--]]
+function searchweapon ()
+	-- TODO: actually implement the search
+	ai.print("No weapon available")
+
+	return false
+end
+
+
+--[[
+	Try to make sure to have a working weapon
+--]]
+function readyweapon ()
+	has_right, has_left = ai.isarmed()
+	right_ammo, left_ammo = ai.roundsleft()
+	if not right_ammo and not left_ammo then
+		if has_right then
+			ai.reload("right")
+		elseif has_left then
+			ai.reload("left")
+		end
 	end
 
+	has_right, has_left = ai.isarmed()
+	if not has_right and not has_left and not ai.getweapon() then
+		return searchweapon()
+	end
+
+	return true
 end
 
 
@@ -67,11 +109,31 @@ function engage( target )
 
 
 	-- Hide
+	hide()
+	ai.crouch(true)
+	target:face()
+end
+
+
+--[[
+	Tries to move to hide position
+--]]
+function hide ()
 	hide_pos = ai.positionhide()
 	if not hide_pos then -- No position available
 	else
 		hide_pos:goto()
 	end
-	ai.crouch(true)
-	target:face()
+end
+
+
+--[[
+	Tries to move to herd position
+--]]
+function herd (target)
+	herd_pos = ai.positionherd(target)
+	if not herd_pos then -- No position available
+	else
+		herd_pos:goto()
+	end
 end
