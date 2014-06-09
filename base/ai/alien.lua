9ab8031e2cb39925400b7b3497a15c9e4419a72e
DarkRain <darkrainx@users.sourceforge.net>
2014-06-09 13:26:14 -0500

    * Reduced scope.

--- base/ai/alien.lua
+++ base/ai/alien.lua
@@ -8,7 +8,7 @@ function think ()
 	-- Try to make sure we have a weapon
 	if not readyweapon() then
 		-- No weapon, retreat
-		teammates = ai.see("all", "alien")
+		local teammates = ai.see("all", "alien")
 		if #teammates > 0 then
 			herd( teammates[1] )
 		else
@@ -16,10 +16,10 @@ function think ()
 		end
 	else
 		-- Look around for potential targets.  We prioritize phalanx.
-		phalanx  = ai.see("all","phalanx")
+		local phalanx  = ai.see("all","phalanx")
 		-- Choose proper action
 		if #phalanx < 1 then
-			civilian = ai.see("all","civilian")
+			local civilian = ai.see("all","civilian")
 			if #civilian < 1 then
 				search()
 			else
@@ -47,8 +47,8 @@ end
 	Try to make sure to have a working weapon
 --]]
 function readyweapon ()
-	has_right, has_left = ai.isarmed()
-	right_ammo, left_ammo = ai.roundsleft()
+	local has_right, has_left = ai.isarmed()
+	local right_ammo, left_ammo = ai.roundsleft()
 	if not right_ammo and not left_ammo then
 		if has_right then
 			ai.reload("right")
@@ -78,11 +78,11 @@ end
 	Attempts to approach the target.
 --]]
 function approach( target )
-	near_position = ai.positionapproach(target)
-	if not near_position then
+	local near_pos = ai.positionapproach(target)
+	if not near_pos then
 		ai.print("Can't approach target")
 	else
-		near_position:goto()
+		near_pos:goto()
 	end
 end
 
@@ -93,10 +93,10 @@ end
 	Currently attempts to see target, shoot then hide.
 --]]
 function engage( target )
-	hide_tu = 4 -- Crouch + face
+	local hide_tu = 4 -- Crouch + face
 
 	-- Move until target in sight
-	shoot_pos = ai.positionshoot(target, "fastest", ai.TU() - hide_tu) -- Get a shoot position
+	local shoot_pos = ai.positionshoot(target, "fastest", ai.TU() - hide_tu) -- Get a shoot position
 	if not shoot_pos then -- No position available
 		approach(target)
 	else
@@ -119,7 +119,7 @@ end
 	Tries to move to hide position
 --]]
 function hide ()
-	hide_pos = ai.positionhide()
+	local hide_pos = ai.positionhide()
 	if not hide_pos then -- No position available
 	else
 		hide_pos:goto()
@@ -131,7 +131,7 @@ end
 	Tries to move to herd position
 --]]
 function herd (target)
-	herd_pos = ai.positionherd(target)
+	local herd_pos = ai.positionherd(target)
 	if not herd_pos then -- No position available
 	else
 		herd_pos:goto()
