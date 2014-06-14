9d0313bf1e5ab1e802b36e94e0c4ae5e72813af1
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-06-14 13:16:41 +0200

    Revert "* compile in c++11 mode"
    This reverts commit 4da4c1891b06ab73a29486e52b967a9c4413d6cd.

--- build/flags.mk
+++ build/flags.mk
@@ -37,6 +37,7 @@ CFLAGS += -Wno-format-zero-length
 # clang stuff
 ifneq (,$(findstring clang,$(CXX)))
   CFLAGS += -Wno-extended-offsetof
+  CFLAGS += -Wno-c++11-extensions
   CFLAGS += -Wno-cast-align
 endif
 
@@ -60,5 +61,4 @@ CCFLAGS += -std=c99
 #CCFLAGS += -Wdeclaration-after-statement
 #CCFLAGS += -Wc++-compat
 
-CXXFLAGS += -std=c++11
 CXXFLAGS += -Wnon-virtual-dtor
