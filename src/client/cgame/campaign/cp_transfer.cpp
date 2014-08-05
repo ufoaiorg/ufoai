6166565b939887a28ced1468e6f9fdf0f3c86172
Tamás Fehérvári <geever@users.sourceforge.net>
2014-08-05 21:02:22 +0200

    * making employee transfers more flexible about bases

--- src/client/cgame/campaign/cp_transfer.cpp
+++ src/client/cgame/campaign/cp_transfer.cpp
@@ -155,8 +155,12 @@ transfer_t* TR_TransferStart (base_t* srcBase, transfer_t& transData)
 
 	/* Initialize transfer. */
 	OBJZERO(transfer);
-	/* calculate time to go from 1 base to another : 1 day for one quarter of the globe*/
-	time = GetDistanceOnGlobe(transData.destBase->pos, srcBase->pos) / 90.0f;
+	if (srcBase != nullptr && transData.destBase != nullptr) {
+		/* calculate time to go from 1 base to another : 1 day for one quarter of the globe*/
+		time = GetDistanceOnGlobe(transData.destBase->pos, srcBase->pos) / 90.0f;
+	} else {
+		time = DEFAULT_TRANSFER_TIME;
+	}
 	transfer.event.day = ccs.date.day + floor(time);	/* add day */
 	time = (time - floor(time)) * SECONDS_PER_DAY;	/* convert remaining time in second */
 	transfer.event.sec = ccs.date.sec + round(time);
@@ -187,17 +191,13 @@ transfer_t* TR_TransferStart (base_t* srcBase, transfer_t& transData)
 	 * it takes Living Quarters capacity, etc, but it cannot be used anywhere. */
 	for (i = 0; i < MAX_EMPL; i++) {		/* Employees. */
 		LIST_Foreach(transData.employees[i], Employee, employee) {
-			assert(employee->isHiredInBase(srcBase));
-
-			transfer.hasEmployees = true;
 			employee->unassign();
-
 			/** @TODO We unarm soldiers so we don't need to manage storage. This need to be changed later */
 			employee->unequip();
-
+			transfer.hasEmployees = true;
 			E_MoveIntoNewBase(employee, transfer.destBase);
-			cgi->LIST_AddPointer(&transfer.employees[i], (void*) employee);
 			employee->transfer = true;
+			cgi->LIST_AddPointer(&transfer.employees[i], (void*) employee);
 			count++;
 		}
 	}
