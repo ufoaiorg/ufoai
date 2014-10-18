f55d765e897b348a25b78cece95d782192ebd58f
DarkRain <darkrainx@users.sourceforge.net>
2014-10-18 08:49:57 -0500

    * Set g_endlessaliens to 0 on skirmish shutdown.
    * Turns out that it can't be deleted after a battle due to it acquiring flags.

--- src/client/cgame/skirmish/cl_game_skirmish.cpp
+++ src/client/cgame/skirmish/cl_game_skirmish.cpp
@@ -340,6 +340,8 @@ static void GAME_SK_InitStartup (void)
 static void GAME_SK_Shutdown (void)
 {
 	cgi->Cmd_TableRemoveList(skirmishCmds);
+	/* You really don't want this with campaign */
+	cgi->Cvar_ForceSet("g_endlessaliens", "0");
 
 	cgi->UI_ResetData(OPTION_DROPSHIPS);
 	cgi->UI_ResetData(OPTION_UFOS);
