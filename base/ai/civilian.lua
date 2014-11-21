4ae7281a05d584e3add6d71218bb29f254e2919e
DarkRain <darkrainx@users.sourceforge.net>
2014-11-21 12:33:44 -0600

    * Tweaked civilian ai lua script.
    * Renamed things, fixed errors...

--- base/ai/civilian.lua
+++ base/ai/civilian.lua
@@ -17,32 +17,32 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 --]]
 
-local civai = ai
+local ailc = { }
 
-function civai.hidetus ()
-	return civai.actor():TU() - 4
+function ailc.tustouse ()
+	return ai.actor():TU() - 4
 end
 
-function civai.flee()
-	local flee_pos = civai.positionflee()
+function ailc.flee()
+	local flee_pos = ai.positionflee()
 	if flee_pos then
 		return flee_pos:goto()
 	end
 	return false
 end
 
-function civai.hide (team)
-	local hide_pos = civai.positionhide(team)
+function ailc.hide (team)
+	local hide_pos = ai.positionhide(team)
 	if hide_pos then
 		return hide_pos:goto()
 	end
 	return false
 end
 
-function civai.herd (targets)
+function ailc.herd (targets)
 	if #targets > 0 then
 		for i = 1, #targets do
-			local herd_pos = civai.positionherd(targets[i])
+			local herd_pos = ai.positionherd(targets[i])
 			if herd_pos then
 				return herd_pos:goto()
 			end
@@ -51,10 +51,10 @@ function civai.herd (targets)
 	return false
 end
 
-function civai.approach (targets)
+function ailc.approach (targets)
 	for i = 1, #targets do
 		for j = 1, 2 do
-			local near_pos = civai.positionapproach(targets[i], civai.hidetus(), j == 1)
+			local near_pos = ai.positionapproach(targets[i], ailc.tustouse(), j == 1)
 			if near_pos then
 				return near_pos:goto()
 			end
@@ -63,72 +63,75 @@ function civai.approach (targets)
 	return false
 end
 
-function civai.route (waypoints)
+function ailc.route (waypoints)
 	if #waypoints > 0 then
 		for i = 1, #waypoints do
-			local target_pos = civai.positionmission(waypoints[i])
+			local target_pos = ai.positionmission(waypoints[i])
 			if target_pos then
 				if target_pos:goto() then
-					civai.setwaypoint(waypoints[i])
+					ai.setwaypoint(waypoints[i])
+					return true
 				end
 			end
 		end
 		-- Can't get to any waypoints, try to approach the nearest one
-		return civai.approach(waypoints)
+		return ailc.approach(waypoints)
 	end
 end
 
-function civai.wander ()
-	local next_pos = civai.positionwander("rand", (civai.actor():TU() + 1) / 6)
+function ailc.wander ()
+	local next_pos = ai.positionwander("rand", (ai.actor():TU() + 1) / 6)
 	if next_pos then
 		next_pos:goto()
 	end
 end
 
-function civai.think_nf ()
-	local aliens = civai.see("sight", "alien")
+function ailc.think_nf ()
+	local aliens = ai.see("sight", "alien")
 	if #aliens > 0 then
-		if not civai.hide(aliens[1]:team()) then
-			civai.flee()
+		if not ailc.hide(aliens[1]:team()) then
+			ailc.flee()
 		end
 	else
-		local waypoints = civai.waypoints(25, "path")
+		local waypoints = ai.waypoints(25, "path")
 		if #waypoints > 0 then
-			civai.route(waypoints)
+			ailc.route(waypoints)
 		else
-			civai.setwaypoint(nil)
-			local phalanx = civai.see("sight", "phalanx")
+			ai.setwaypoint(nil)
+			local phalanx = ai.see("sight", "phalanx")
 			if #phalanx > 0 then
-				civai.herd(phalanx)
+				ailc.herd(phalanx)
 			else
-				local civs = civai.see("sight", "civilian")
+				local civs = ai.see("sight", "civilian")
 				if #civs > 0 then
-					civai.herd(civs)
+					ailc.herd(civs)
 				else
-					civai.wander()
+					ailc.wander()
 				end
 			end
 		end
 	end
-	aliens = civai.see("sight", "alien")
+	aliens = ai.see("sight", "alien")
 	if #aliens > 0 then
 		-- Some civ models don't have crouching animations
-		-- civai.crouch(true)
+		-- ailc.crouch(true)
 	end
 end
 
-function civai.think ()
-	if civai.actor().morale() ~= "normal" then
-		civai.flee()
+function ailc.think ()
+	if ai.actor():morale() ~= "normal" then
+		if not ailc.hide() then
+			ailc.flee()
+		end
 	else
-		if not civai.isfighter() then
-			civai.think_nf()
+		if not ai.isfighter() then
+			ailc.think_nf()
 		else
 			-- We don't currently have fight capable civ teams, so no point in implementing this yet
-			civai.print("Warning: no fight capable lua ai available for civilian ", civai.actor())
-			civai.think_nf()
+			ai.print("Warning: no fight capable lua ai available for civilian ", ailc.actor())
+			ailc.think_nf()
 		end
 	end
 end
 
-return ai
+return ailc
