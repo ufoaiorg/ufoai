08b2fb0c22ce0da447f3eabb57e9305e980597c7
DarkRain <darkrainx@users.sourceforge.net>
2015-01-08 21:24:45 -0600

    * Welcome to 2015 second part.

--- src/client/cl_http.h
+++ src/client/cl_http.h
@@ -4,7 +4,7 @@
  */
 
 /*
-All original material Copyright (C) 2002-2014 UFO: Alien Invasion.
+All original material Copyright (C) 2002-2015 UFO: Alien Invasion.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
