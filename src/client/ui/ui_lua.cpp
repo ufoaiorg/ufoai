1c4e3c713ab4460fbdd8b6eadfef1c5f3207c52a
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-11-19 13:06:28 +0100

    * todo comment about weird else case in the ui lua code

--- src/client/ui/ui_lua.cpp
+++ src/client/ui/ui_lua.cpp
@@ -314,9 +314,10 @@ bool UI_ExecuteLuaConFunc (uiNode_t* node, LUA_FUNCTION fcn) {
 			if (!errno && *p == '\0') {
 				// push argument as float
 				lua_pushnumber(CL_GetLuaState(), val_f);
+			} /*else TODO: shouldn't this be a real else? */ {
+				// else consider it a string
+				lua_pushstring(CL_GetLuaState(), s);
 			}
-			// else consider it a string
-			lua_pushstring(CL_GetLuaState(), Cmd_Argv(i));
 		}
 	}
 	/* execute the confunc */
