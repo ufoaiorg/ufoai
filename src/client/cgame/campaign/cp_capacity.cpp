a1928aa3eb018fbee0dbf3699f358f679a64b589
Tamás Fehérvári <geever@users.sourceforge.net>
2017-07-26 23:35:42 +0200

    * Convert Capacity overload popup to LUA

--- src/client/cgame/campaign/cp_capacity.cpp
+++ src/client/cgame/campaign/cp_capacity.cpp
@@ -157,7 +157,7 @@ void CAP_CheckOverflow (void)
 					const buildingType_t bldgType = B_GetBuildingTypeByCapacity((baseCapacities_t)i);
 					const building_t* bldg = B_GetBuildingTemplateByType(bldgType);
 					CP_GameTimeStop();
-					cgi->Cmd_ExecuteString("ui_push popup_cap_overload base %d \"%s\" \"%s\" %d %d",
+					cgi->Cmd_ExecuteString("ui_push popup_cap_overload %d \"%s\" \"%s\" %d %d",
 						base->idx, base->name, _(bldg->name), cap->max - cap->cur, cap->max);
 				}
 				break;
