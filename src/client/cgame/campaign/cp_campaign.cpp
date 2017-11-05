1b0d7f3f8d60e8a4d22220598b4b4dd191929c27
DarkRain <darkrainx@users.sourceforge.net>
2017-11-04 23:12:55 -0600

    * Don't unload the campaign library from within the campaign library.

--- src/client/cgame/campaign/cp_campaign.cpp
+++ src/client/cgame/campaign/cp_campaign.cpp
@@ -88,9 +88,8 @@ bool CP_IsRunning (void)
 void CP_EndCampaign (bool won)
 {
 	cgi->Cmd_ExecuteString("game_save slotend \"End of game\"");
-	cgi->Cmd_ExecuteString("game_exit");
-	cgi->UI_ExecuteConfunc(va("ui_set_endgame %s", won ? "won" : "lose"));
-	cgi->Cmd_ExecuteString("ui_initstack endgame");
+	cgi->Cbuf_AddText(va("ui_set_endgame %s;", won ? "won" : "lose"));
+	cgi->Cbuf_AddText("ui_initstack endgame;");
 
 	cgi->Com_Drop();
 }
