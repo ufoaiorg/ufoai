a880fcf4bc530c49c82ad33ff75df1dffabc31db
Tamás Fehérvári <geever@users.sourceforge.net>
2018-06-03 22:48:09 +0200

    * Remove duplicated FORTIFY_SOURCE option from release builds

--- build/modes/release.mk
+++ build/modes/release.mk
@@ -1,4 +1,4 @@
-CFLAGS += -ffast-math -funroll-loops -D_FORTIFY_SOURCE=2 -DNDEBUG
+CFLAGS += -ffast-math -funroll-loops -DNDEBUG
 
 ifeq ($(filter -O0 -O1 -O2 -O3 -O4 -Os -Ofast,$(CFLAGS)),) # If you use multiple -O options, with or without level numbers, the last such option is the one that is effective
   CFLAGS += -O2
