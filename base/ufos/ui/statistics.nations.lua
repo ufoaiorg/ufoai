c4df0c5b920656fbcf52a1cf35c312081fbfa75d
Tamás Fehérvári <geever@users.sourceforge.net>
2019-06-23 21:24:35 +0200

    * New, generic datatable builder ufox extension, migrate nations stat to it

--- base/ufos/ui/statistics.nations.lua
+++ base/ufos/ui/statistics.nations.lua
@@ -25,177 +25,48 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 --]]
 
 require("ufox.lua")
+require("ufox.datatable.lua")
 
 function build_nationstats (rootNode)
 	if (rootNode == nil) then
 		return
 	end
 
-	if (rootNode:child("nation_stats") ~= nil) then
-		rootNode:child("nation_stats"):delete_node()
+	if (rootNode:child("nationslist") ~= nil) then
+		rootNode:child("nationslist"):delete_node()
 	end
-	local nationStat = ufox.build({
-		name = "nation_stats",
-		class = "panel",
-		pos = {0, 0},
+
+	local nationStat = ufox.datatable.build(rootNode, {
+		name = "nationslist",
+		pos = {465, 75},
 		size = {545, 235},
-		backgroundcolor = {0.5, 0.5, 0.5, 0.2},
+	})
 
-		{
-			name = "header",
-			class = "panel",
-			pos = {0, 0},
-			size = {545, 20},
-			layout = ufo.LAYOUT_LEFT_RIGHT_FLOW,
-			backgroundcolor = {0.527, 0.6, 0.21, 0.2},
+	nationStat:add_headerFields({
+		{ name = "flag",        width =  20, },
+		{ name = "nation_name", width = 270, text = "_Nation", },
+		{ name = "funding",     width = 100, text = "_Funding",   contentalign = ufo.ALIGN_CR, },
+		{ name = "happiness",   width = 135, text = "_Happiness", contentalign = ufo.ALIGN_CR, },
+	})
 
-			{
-				name = "placeholder",
-				class = "panel",
-				size = {20, 20},
-			},
+	local nations_add = ufox.build({
+		name = "ui_nations_fill",
+		class = "confunc",
 
-			{
-				name = "nation_name",
-				class = "string",
-				text = "_Nation",
-				size = {270, 20},
-				contentalign = ufo.ALIGN_CC,
-			},
+		on_click = function (sender, nationID, nationName, monthIDX, nationHappiness, nationHappinessString, nationFunding, nationColor)
+			if (monthIDX ~= "0") then
+				return
+			end
 
-			{
-				name = "nation_funding_label",
-				class = "string",
-				text = "_Funding",
-				size = {100, 20},
-				contentalign = ufo.ALIGN_CR,
-			},
-
-			{
-				name = "nation_happiness_label",
-				class = "string",
-				text = "_Happiness",
-				size = {135, 20},
-				contentalign = ufo.ALIGN_CR,
-			},
-		},
-
-		{
-			name = "nation_list",
-			class = "panel",
-			pos = {0, 20},
-			size = {525, 215},
-			layout = ufo.LAYOUT_TOP_DOWN_FLOW,
-			wheelscrollable = true,
-
-			on_viewchange = function (sender)
-				local scrollbar = sender:parent():child("nation_list_scrollbar")
-				scrollbar:set_fullsize(sender:fullsize())
-				scrollbar:set_current(sender:viewpos())
-				scrollbar:set_viewsize(sender:viewsize())
-			end,
-
-			on_wheel = function (sender)
-				local scrollbar = sender:parent():child("nation_list_scrollbar")
-				scrollbar:set_current(sender:viewpos())
-			end,
-
-			clear = function (sender)
-				sender:remove_children()
-			end,
-
-			add = function (sender, nationID, nationName, nationFlag, nationFunding, nationHappinessString)
-				local row = ufox.build({
-					name = nationID,
-					class = "panel",
-					size = {520, 20},
-					layout = ufo.LAYOUT_LEFT_RIGHT_FLOW,
-
-					{
-						name = "nation_enabled",
-						class = "image",
-						size = {20, 20},
-						--backgroundcolor = {0.5, 1, 1, 1},
-						source = nationFlag,
-						keepratio = true;
-						ghost = true,
-					},
-
-					{
-						name = "nation_name",
-						class = "string",
-						text = nationName,
-						size = {270, 20},
-						ghost = true,
-					},
-
-					{
-						name = "nation_funding",
-						class = "string",
-						text = string.format("%s c", nationFunding),
-						contentalign = ufo.ALIGN_CR,
-						size = {100, 20},
-						ghost = true,
-					},
-
-					{
-						name = "nation_happiness",
-						class = "string",
-						text = nationHappinessString,
-						contentalign = ufo.ALIGN_CR,
-						size = {135, 20},
-						ghost = true,
-					},
-				}, sender)
-			end,
-		},
-
-		{
-			name = "nation_list_scrollbar",
-			class = "vscrollbar",
-			image = "ui/scrollbar_v",
-			pos = {525, 20},
-			height = 215,
-			current = 0,
-			viewsize = 10,
-			fullsize = 0,
-			autoshowscroll = true,
-
-			on_change = function (sender)
-				local panel = sender:parent():child("nation_list")
-				panel:set_viewpos(sender:current())
-			end,
-		},
-
-		{
-			name = "ui_nations_clear",
-			class = "confunc",
-
-			on_click = function (sender)
-				sender:parent():child("nation_list"):clear()
-			end,
-		},
-
-		{
-			name = "ui_nations_fill",
-			class = "confunc",
-
-			on_click = function (sender, nationID, nationName, monthIDX, nationHappiness, nationHappinessString, nationFunding, nationColor)
-				if (monthIDX ~= "0") then
-					return
-				end
-
-				-- ufo.print(string.format("Got data for nation %s month %s\n", nationID, monthIDX))
-				sender:parent():child("nation_list"):add(
-					nationID,
-					nationName,
-					"nations/" .. nationID,
-					nationFunding,
-					nationHappinessString
-				)
-			end,
-		},
-	}, rootNode)
+			-- ufo.print(string.format("Got data for nation %s month %s\n", nationID, monthIDX))
+			local row = sender:parent():child("nationslist"):add_dataRow({name = nationID}, {
+				{ name = "flag",        source = "nations/" .. nationID, class = "image", keepratio = true, ghost = true, },
+				{ name = "nation_name", text = nationName, },
+				{ name = "funding",     text = nationFunding,            contentalign = ufo.ALIGN_CR, },
+				{ name = "happiness",   text = nationHappinessString,    contentalign = ufo.ALIGN_CR, },
+			})
+		end,
+	}, rootNode);
 
 	return nationStat
 end
---
+++ base/ufos/ui/ufox.datatable.lua
@@ -0,0 +1,286 @@
+--!usr/bin/lua
+
+--[[
+-- @file
+-- @brief DataTable builder extension
+--]]
+
+--[[
+Copyright (C) 2002-2019 UFO: Alien Invasion.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+--]]
+
+-- ufox.build_datatable Header Guard
+if (ufox.datatable == nil) then
+
+require("ufox.lua")
+
+ufox.datatable = {
+	--[[
+	-- @brief Add header fields to the table
+	-- @param sender Reference to the dataTable node
+	-- @param fields List of (partial) header node definitions:
+	--	name	(Required) Name of the table column
+	--	width	(Required) Width of the column.
+	-- @note These field names and widths are going to be used for data cells too
+	--]]
+	add_headerFields = function (sender, fields)
+		local header = sender:child("header")
+		if (header == nil) then
+			ufo.print(string.format("ufox.datatable %s has no header\n", sender:name()))
+			return
+		end
+		if (type(fields) ~= "table") then
+			ufo.print("ufox.datatable:add_headerFields() was called with incompatible fields parameter\n")
+			return
+		end
+
+		local left = 0;
+		local node = header:first()
+		while (node ~= nil) do
+			left = left + node:width()
+			node = node:next()
+		end
+
+		local headerFieldDefaults = {
+			class = "string",
+			height = 20,
+			contentalign = ufo.ALIGN_CC,
+		}
+		for i, field in pairs(fields) do
+			-- fill missing data
+			for key, value in pairs(headerFieldDefaults) do
+				if (field[key] == nil) then
+					field[key] = value
+				end
+			end
+			local field = ufox.build(field, header)
+			if (field == nil) then
+				ufo.print(string.format("ufox.datatable.build encountered error on creating field no. %d\n", i))
+				return
+			end
+			field:set_left(left)
+			left = left + field:width()
+		end
+
+		return header
+	end,
+
+	--[[
+	-- @brief Add data cells to a data row
+	-- @param sender data row node
+	-- @param cellData Table with the data cell
+	--]]
+	addCell = function (sender, cellData)
+		local header = sender:parent():parent():child("header")
+		if (header == nil) then
+			ufo.print("ufox.datatable has no header\n")
+			return
+		end
+
+		if (type(cellData) ~= "table") then
+			ufo.print(string.format("ufox.datatable %s has no data space\n", sender:name()))
+			return
+		end
+
+		if (cellData.name == nil or header:child(cellData.name) == nil) then
+			ufo.print(string.format("ufox.datatable tried to add data to a non-existent field\n", sender:name()))
+			return
+		end
+
+		local column = header:child(cellData.name)
+		local cellDefaults = {
+			class = "string",
+			left = column:left(),
+			top = 0,
+			width = column:width(),
+			height = sender:height(),
+		}
+		-- fill missing data
+		for key, value in pairs(cellDefaults) do
+			if (cellData[key] == nil) then
+				cellData[key] = value
+			end
+		end
+		local cell = ufox.build(cellData, sender)
+		return cell
+	end,
+
+	--[[
+	-- @brief Adds a datarow to the table
+	-- @param sender Reference to the data table node
+	-- @param rowData Table with row properties:
+	--	name	(Required) name (id) of the data row
+	-- @param fields Array of tables with dataCell properties
+	-- @sa addCell
+	--]]
+	add_dataRow = function (sender, rowData, fields)
+		local header = sender:child("header")
+		if (header == nil) then
+			ufo.print(string.format("ufox.datatable %s has no header\n", sender:name()))
+			return
+		end
+
+		local dataSpace = sender:child("data")
+		if (dataSpace == nil) then
+			ufo.print(string.format("ufox.datatable %s has no data space\n", sender:name()))
+			return
+		end
+
+		local dataRowDefaults = {
+			class = "panel",
+			width = sender:width(),
+			height = 20,
+			addCell = ufox.datatable.addCell,
+		}
+		for key, value in pairs(dataRowDefaults) do
+			if (rowData[key] == nil) then
+				rowData[key] = value
+			end
+		end
+		local row = ufox.build(rowData, dataSpace)
+		if (row == nil) then
+			ufo.print("ufox.datatable:add_dataRow() was called with incompatible parameter\n")
+			return
+		end
+
+		if (type(fields) == "table") then
+			for i, field in ipairs(fields) do
+				row:addCell(field)
+			end
+		end
+
+		return row
+	end,
+
+	--[[
+	-- @brief Clears the entire datatable (both header and data)
+	-- @param sender UI node reference to the data table
+	--]]
+	clear = function (sender)
+		sender:clear_data()
+		sender:child("header"):remove_children()
+	end,
+
+	--[[
+	-- @brief Clears all data from the table
+	-- @param sender UI node reference to the data table
+	--]]
+	clear_data = function (sender)
+		sender:child("data"):remove_children()
+	end,
+
+	--[[
+	-- @brief Function builds a generic datatable
+	-- @param rootNode UI node in that the table should be created in
+	-- @param tableData Table describes the base node:
+	--	name	(Required) Name of the dataTable
+	--	pos	(Recommended) Position of the dataTable. Defaults to (0, 0)
+	--	size	(Recommended) Size of the dataTable. Fills the entire rootNode by default
+	--]]
+	build = function (rootNode, tableData, headerProperties)
+		if (rootNode == nil or tableData == nil or tableData.name == nil) then
+			ufo.print("ufox.datatable.build was called with missing parameters\n")
+			return
+		end
+
+		local tableDefaults = {
+			class = "panel",
+			pos = {0, 0},
+			size = {rootNode:width(), rootNode:height()},
+			backgroundcolor = {0.5, 0.5, 0.5, 0.2},
+		}
+		for key, value in pairs(tableDefaults) do
+			if (tableData[key] == nil) then
+				tableData[key] = value
+			end
+		end
+
+		local dataTable = ufox.build(tableData, rootNode)
+		if (dataTable == nil) then
+			ufo.print("ufox.datatable.build failed due to incompatible parameters\n")
+			return
+		end
+
+		local header = ufox.build({
+			name = "header",
+			class = "panel",
+			pos = {0, 0},
+			size = {dataTable:width(), 20},
+			backgroundcolor = {0.527, 0.6, 0.21, 0.2},
+		}, dataTable)
+		-- Allow override properties
+		if (headerProperties ~= nil) and (type(headerProperties) == "table") then
+			ufox.build_properties(headerProperties, dataTable, header)
+		end
+
+		-- add data space
+		ufox.build_properties({
+			{
+				name = "data",
+				class = "panel",
+				pos = {0, header:height()},
+				size = {dataTable:width() - 20, dataTable:height() - header:height()},
+				layout = ufo.LAYOUT_TOP_DOWN_FLOW,
+				wheelscrollable = true,
+
+				on_viewchange = function (sender)
+					local scrollbar = sender:parent():child("data_scrollbar")
+					scrollbar:set_fullsize(sender:fullsize())
+					scrollbar:set_current(sender:viewpos())
+					scrollbar:set_viewsize(sender:viewsize())
+				end,
+
+				on_wheel = function (sender)
+					local scrollbar = sender:parent():child("data_scrollbar")
+					scrollbar:set_current(sender:viewpos())
+				end,
+			},
+
+			{
+				name = "data_scrollbar",
+				class = "vscrollbar",
+				image = "ui/scrollbar_v",
+				pos = {dataTable:width() - 20, 20},
+				height = (dataTable:height() - 20),
+				current = 0,
+				viewsize = 10,
+				fullsize = 0,
+				autoshowscroll = true,
+
+				on_change = function (sender)
+					local panel = sender:parent():child("data")
+					panel:set_viewpos(sender:current())
+				end,
+			},
+
+			-- Add commands
+			add_headerFields = ufox.datatable.add_headerFields,
+			clear = ufox.datatable.clear,
+			clear_data = ufox.datatable.clear_data,
+			add_dataRow = ufox.datatable.add_dataRow,
+		}, rootnode, dataTable)
+
+		ufox.build_properties({
+		}, rootnode, dataTable)
+
+		return dataTable
+	end,
+}
+
+-- ufox.datatable Header Guard
+end
