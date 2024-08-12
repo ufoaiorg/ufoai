d94068f8bff1c79f98f525420f0e8b48d5796680
Tamás Fehérvári <geever@users.sourceforge.net>
2024-08-12 14:00:36 +0200

    * cross-compile: MinGW64_64 LDFlags fixes

--- build/platforms/mingw64_64.mk
+++ build/platforms/mingw64_64.mk
@@ -5,13 +5,14 @@ SO_LIBS                  :=
 EXE_EXT                  := .exe
 
 CFLAGS                   += -DWINVER=0x501
-LDFLAGS                  +=
+LDFLAGS                  += -Wl,--stack,8388608
 
 INTL_LIBS                ?= -lintl
 OPENGL_LIBS              ?= -lopengl32
 
+game_LDFLAGS             += $(SRCDIR)/game/game.def
 ufo_LDFLAGS              += -lws2_32 -lwinmm
-ufo2map_LDFLAGS          += -lwinmm
+ufo2map_LDFLAGS          += -lwinmm -mconsole
 ufoded_LDFLAGS           += -lws2_32 -lwinmm
-ufomodel_LDFLAGS         += -lwinmm
-ufoslicer_LDFLAGS        += -lwinmm
+ufomodel_LDFLAGS         += -lwinmm -mconsole
+ufoslicer_LDFLAGS        += -lwinmm -mconsole
